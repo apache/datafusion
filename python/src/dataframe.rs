1d89dfd0186b7dc4b151b900c03b8f03d7f6178f
Heres, Daniel <danielheres@gmail.com>
2021-05-08 11:52:17 +0200

    Revert python changes

--- python/src/dataframe.rs
+++ python/src/dataframe.rs
@@ -135,7 +135,6 @@ impl DataFrame {
             "inner" => JoinType::Inner,
             "left" => JoinType::Left,
             "right" => JoinType::Right,
-            "full" => JoinType::Full,
             how => {
                 return Err(DataFusionError::Common(format!(
                     "The join type {} does not exist or is not implemented",
