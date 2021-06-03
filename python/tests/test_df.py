d9d855bef35d047123169a5f7a34052f21739765
Krisztián Szűcs <szucs.krisztian@gmail.com>
2021-06-03 15:58:57 +0200

    Use pytest

--- python/tests/test_df.py
+++ python/tests/test_df.py
@@ -15,100 +15,104 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import unittest
+import pytest
 
 import pyarrow as pa
-import datafusion
-
-f = datafusion.functions
+from datafusion import ExecutionContext, functions as f
 
 
-class TestCase(unittest.TestCase):
-    def _prepare(self):
-        ctx = datafusion.ExecutionContext()
+@pytest.fixture
+def df():
+    ctx = ExecutionContext()
 
-        # create a RecordBatch and a new DataFrame from it
-        batch = pa.RecordBatch.from_arrays(
-            [pa.array([1, 2, 3]), pa.array([4, 5, 6])],
-            names=["a", "b"],
-        )
-        return ctx.create_dataframe([[batch]])
+    # create a RecordBatch and a new DataFrame from it
+    batch = pa.RecordBatch.from_arrays(
+        [pa.array([1, 2, 3]), pa.array([4, 5, 6])],
+        names=["a", "b"],
+    )
 
-    def test_select(self):
-        df = self._prepare()
+    return ctx.create_dataframe([[batch]])
 
-        df = df.select(
+
+def test_select(df):
+    df = df.select(
+        f.col("a") + f.col("b"),
+        f.col("a") - f.col("b"),
+    )
+
+    # execute and collect the first (and only) batch
+    result = df.collect()[0]
+
+    assert result.column(0) == pa.array([5, 7, 9])
+    assert result.column(1) == pa.array([-3, -3, -3])
+
+
+def test_filter(df):
+    df = df \
+        .select(
             f.col("a") + f.col("b"),
             f.col("a") - f.col("b"),
-        )
+        ) \
+        .filter(f.col("a") > f.lit(2))
 
-        # execute and collect the first (and only) batch
-        result = df.collect()[0]
+    # execute and collect the first (and only) batch
+    result = df.collect()[0]
 
-        self.assertEqual(result.column(0), pa.array([5, 7, 9]))
-        self.assertEqual(result.column(1), pa.array([-3, -3, -3]))
+    assert result.column(0) == pa.array([9])
+    assert result.column(1) == pa.array([-3])
 
-    def test_filter(self):
-        df = self._prepare()
 
-        df = df.select(
-            f.col("a") + f.col("b"),
-            f.col("a") - f.col("b"),
-        ).filter(f.col("a") > f.lit(2))
+def test_sort(df):
+    df = df.sort([
+        f.col("b").sort(ascending=False)
+    ])
 
-        # execute and collect the first (and only) batch
-        result = df.collect()[0]
+    table = pa.Table.from_batches(df.collect())
+    expected = {'a': [3, 2, 1], 'b': [6, 5, 4]}
 
-        self.assertEqual(result.column(0), pa.array([9]))
-        self.assertEqual(result.column(1), pa.array([-3]))
+    assert table.to_pydict() == expected
 
-    def test_sort(self):
-        df = self._prepare()
-        df = df.sort([f.col("b").sort(ascending=False)])
 
-        table = pa.Table.from_batches(df.collect())
-        expected = {"a": [3, 2, 1], "b": [6, 5, 4]}
-        self.assertEqual(table.to_pydict(), expected)
+def test_limit(df):
+    df = df.limit(1)
 
-    def test_limit(self):
-        df = self._prepare()
+    # execute and collect the first (and only) batch
+    result = df.collect()[0]
 
-        df = df.limit(1)
+    assert len(result.column(0)) == 1
+    assert len(result.column(1)) == 1
 
-        # execute and collect the first (and only) batch
-        result = df.collect()[0]
 
-        self.assertEqual(len(result.column(0)), 1)
-        self.assertEqual(len(result.column(1)), 1)
+def test_udf(df):
+    # is_null is a pa function over arrays
+    udf = f.udf(lambda x: x.is_null(), [pa.int64()], pa.bool_())
 
-    def test_udf(self):
-        df = self._prepare()
+    df = df.select(udf(f.col("a")))
+    result = df.collect()[0].column(0)
 
-        # is_null is a pa function over arrays
-        udf = f.udf(lambda x: x.is_null(), [pa.int64()], pa.bool_())
+    assert result == pa.array([False, False, False])
 
-        df = df.select(udf(f.col("a")))
 
-        self.assertEqual(df.collect()[0].column(0), pa.array([False, False, False]))
+def test_join():
+    ctx = ExecutionContext()
 
-    def test_join(self):
-        ctx = datafusion.ExecutionContext()
+    batch = pa.RecordBatch.from_arrays(
+        [pa.array([1, 2, 3]), pa.array([4, 5, 6])],
+        names=["a", "b"],
+    )
+    df = ctx.create_dataframe([[batch]])
 
-        batch = pa.RecordBatch.from_arrays(
-            [pa.array([1, 2, 3]), pa.array([4, 5, 6])],
-            names=["a", "b"],
-        )
-        df = ctx.create_dataframe([[batch]])
+    batch = pa.RecordBatch.from_arrays(
+        [pa.array([1, 2]), pa.array([8, 10])],
+        names=["a", "c"],
+    )
+    df1 = ctx.create_dataframe([[batch]])
 
-        batch = pa.RecordBatch.from_arrays(
-            [pa.array([1, 2]), pa.array([8, 10])],
-            names=["a", "c"],
-        )
-        df1 = ctx.create_dataframe([[batch]])
+    df = df.join(df1, on="a", how="inner")
+    df = df.sort([
+        f.col("a").sort(ascending=True)
+    ])
+    table = pa.Table.from_batches(df.collect())
 
-        df = df.join(df1, on="a", how="inner")
-        df = df.sort([f.col("a").sort(ascending=True)])
-        table = pa.Table.from_batches(df.collect())
-
-        expected = {"a": [1, 2], "c": [8, 10], "b": [4, 5]}
-        self.assertEqual(table.to_pydict(), expected)
+    expected = {'a': [1, 2], 'c': [8, 10], 'b': [4, 5]}
+    assert table.to_pydict() == expected
--- python/tests/test_udaf.py
+++ python/tests/test_udaf.py
@@ -15,12 +15,12 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import unittest
-import pyarrow
-import pyarrow.compute
-import datafusion
+import pytest
 
-f = datafusion.functions
+import pyarrow as pa
+import pyarrow.compute as pc
+
+from datafusion import ExecutionContext, functions as f
 
 
 class Accumulator:
@@ -29,63 +29,62 @@ class Accumulator:
     """
 
     def __init__(self):
-        self._sum = pyarrow.scalar(0.0)
+        self._sum = pa.scalar(0.0)
 
-    def to_scalars(self) -> [pyarrow.Scalar]:
+    def to_scalars(self) -> [pa.Scalar]:
         return [self._sum]
 
-    def update(self, values: pyarrow.Array) -> None:
+    def update(self, values: pa.Array) -> None:
         # not nice since pyarrow scalars can't be summed yet. This breaks on `None`
-        self._sum = pyarrow.scalar(
-            self._sum.as_py() + pyarrow.compute.sum(values).as_py()
+        self._sum = pa.scalar(
+            self._sum.as_py() + pc.sum(values).as_py()
         )
 
-    def merge(self, states: pyarrow.Array) -> None:
+    def merge(self, states: pa.Array) -> None:
         # not nice since pyarrow scalars can't be summed yet. This breaks on `None`
-        self._sum = pyarrow.scalar(
-            self._sum.as_py() + pyarrow.compute.sum(states).as_py()
+        self._sum = pa.scalar(
+            self._sum.as_py() + pc.sum(states).as_py()
         )
 
-    def evaluate(self) -> pyarrow.Scalar:
+    def evaluate(self) -> pa.Scalar:
         return self._sum
 
 
-class TestCase(unittest.TestCase):
-    def _prepare(self):
-        ctx = datafusion.ExecutionContext()
 
-        # create a RecordBatch and a new DataFrame from it
-        batch = pyarrow.RecordBatch.from_arrays(
-            [pyarrow.array([1, 2, 3]), pyarrow.array([4, 4, 6])],
-            names=["a", "b"],
-        )
-        return ctx.create_dataframe([[batch]])
+@pytest.fixture
+def df():
+    ctx = ExecutionContext()
 
-    def test_aggregate(self):
-        df = self._prepare()
+    # create a RecordBatch and a new DataFrame from it
+    batch = pa.RecordBatch.from_arrays(
+        [pa.array([1, 2, 3]), pa.array([4, 4, 6])],
+        names=["a", "b"],
+    )
+    return ctx.create_dataframe([[batch]])
 
-        udaf = f.udaf(
-            Accumulator, pyarrow.float64(), pyarrow.float64(), [pyarrow.float64()]
-        )
 
-        df = df.aggregate([], [udaf(f.col("a"))])
+def test_aggregate(df):
+    udaf = f.udaf(
+        Accumulator, pa.float64(), pa.float64(), [pa.float64()]
+    )
 
-        # execute and collect the first (and only) batch
-        result = df.collect()[0]
+    df = df.aggregate([], [udaf(f.col("a"))])
 
-        self.assertEqual(result.column(0), pyarrow.array([1.0 + 2.0 + 3.0]))
+    # execute and collect the first (and only) batch
+    result = df.collect()[0]
 
-    def test_group_by(self):
-        df = self._prepare()
+    assert result.column(0) == pa.array([1.0 + 2.0 + 3.0])
 
-        udaf = f.udaf(
-            Accumulator, pyarrow.float64(), pyarrow.float64(), [pyarrow.float64()]
-        )
 
-        df = df.aggregate([f.col("b")], [udaf(f.col("a"))])
+def test_group_by(df):
+    udaf = f.udaf(
+        Accumulator, pa.float64(), pa.float64(), [pa.float64()]
+    )
 
-        # execute and collect the first (and only) batch
-        batches = df.collect()
-        arrays = [batch.column(1) for batch in batches]
-        joined = pyarrow.concat_arrays(arrays)
-        self.assertEqual(joined, pyarrow.array([1.0 + 2.0, 3.0]))
+    df = df.aggregate([f.col("b")], [udaf(f.col("a"))])
+
+    batches = df.collect()
+    arrays = [batch.column(1) for batch in batches]
+    joined = pa.concat_arrays(arrays)
+    assert joined == pa.array([1.0 + 2.0, 3.0]))
+
