eba0fcf1c4dcec199e6b7843c04e3be0e7a8261e
Ximo Guanter <ximo.guanter@gmail.com>
2021-06-28 16:21:40 +0200

    Add Keda autoscaling for ballista in k8s (#586)

--- ballista/rust/scheduler/build.rs
+++ ballista/rust/scheduler/build.rs
@@ -20,5 +20,10 @@ extern crate configure_me_codegen;
 fn main() -> Result<(), String> {
     println!("cargo:rerun-if-changed=scheduler_config_spec.toml");
     configure_me_codegen::build_script_auto()
-        .map_err(|e| format!("configure_me code generation failed: {}", e))
+        .map_err(|e| format!("configure_me code generation failed: {}", e))?;
+
+    println!("cargo:rerun-if-changed=proto/keda.proto");
+    tonic_build::configure()
+        .compile(&["proto/keda.proto"], &["proto"])
+        .map_err(|e| format!("protobuf compilation failed: {}", e))
 }
