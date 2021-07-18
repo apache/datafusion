69a3483fee70e9e76eeb05f200be0ea6099adb05
Jiayu Liu <jiayu@hey.com>
2021-07-18 23:56:09 +0800

    add python math functions

--- python/tests/test_math_functions.py
+++ python/tests/test_math_functions.py
@@ -15,11 +15,11 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import numpy as np
 import pyarrow as pa
 import pytest
 from datafusion import ExecutionContext
 from datafusion import functions as f
-import numpy as np
 
 
 @pytest.fixture
@@ -48,13 +48,13 @@ def test_math_functions(df):
     result = df.collect()
     assert len(result) == 1
     result = result[0]
-    assert result.column(0) == pa.array(np.abs(values))
-    assert result.column(1) == pa.array(np.sin(values))
-    assert result.column(2) == pa.array(np.cos(values))
-    assert result.column(3) == pa.array(np.tan(values))
-    assert result.column(4) == pa.array(np.arcsin(values))
-    assert result.column(5) == pa.array(np.arccos(values))
-    assert result.column(6) == pa.array(np.exp(values))
-    assert result.column(7) == pa.array(np.log(values + 1.0))
-    assert result.column(8) == pa.array(np.log2(values + 1.0))
-    assert result.column(9) == pa.array(np.log10(values + 1.0))
+    np.testing.assert_array_almost_equal(result.column(0), np.abs(values))
+    np.testing.assert_array_almost_equal(result.column(1), np.sin(values))
+    np.testing.assert_array_almost_equal(result.column(2), np.cos(values))
+    np.testing.assert_array_almost_equal(result.column(3), np.tan(values))
+    np.testing.assert_array_almost_equal(result.column(4), np.arcsin(values))
+    np.testing.assert_array_almost_equal(result.column(5), np.arccos(values))
+    np.testing.assert_array_almost_equal(result.column(6), np.exp(values))
+    np.testing.assert_array_almost_equal(result.column(7), np.log(values + 1.0))
+    np.testing.assert_array_almost_equal(result.column(8), np.log2(values + 1.0))
+    np.testing.assert_array_almost_equal(result.column(9), np.log10(values + 1.0))
