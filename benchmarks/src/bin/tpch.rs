970ce01ed6de929519d1baff88f317bbd1625bfb
Andy Grove <andygrove73@gmail.com>
2021-08-07 15:47:41 -0600

    Change datatype of tpch keys from Int32 to UInt64 to support sf=1000 (#836)
    * Change datatype of tpch keys from Int32 to UInt64 to support sf=1000
    
    * revert accidental change

--- benchmarks/src/bin/tpch.rs
+++ benchmarks/src/bin/tpch.rs
@@ -490,7 +490,7 @@ fn get_schema(table: &str) -> Schema {
 
     match table {
         "part" => Schema::new(vec![
-            Field::new("p_partkey", DataType::Int32, false),
+            Field::new("p_partkey", DataType::UInt64, false),
             Field::new("p_name", DataType::Utf8, false),
             Field::new("p_mfgr", DataType::Utf8, false),
             Field::new("p_brand", DataType::Utf8, false),
@@ -502,28 +502,28 @@ fn get_schema(table: &str) -> Schema {
         ]),
 
         "supplier" => Schema::new(vec![
-            Field::new("s_suppkey", DataType::Int32, false),
+            Field::new("s_suppkey", DataType::UInt64, false),
             Field::new("s_name", DataType::Utf8, false),
             Field::new("s_address", DataType::Utf8, false),
-            Field::new("s_nationkey", DataType::Int32, false),
+            Field::new("s_nationkey", DataType::UInt64, false),
             Field::new("s_phone", DataType::Utf8, false),
             Field::new("s_acctbal", DataType::Float64, false),
             Field::new("s_comment", DataType::Utf8, false),
         ]),
 
         "partsupp" => Schema::new(vec![
-            Field::new("ps_partkey", DataType::Int32, false),
-            Field::new("ps_suppkey", DataType::Int32, false),
+            Field::new("ps_partkey", DataType::UInt64, false),
+            Field::new("ps_suppkey", DataType::UInt64, false),
             Field::new("ps_availqty", DataType::Int32, false),
             Field::new("ps_supplycost", DataType::Float64, false),
             Field::new("ps_comment", DataType::Utf8, false),
         ]),
 
         "customer" => Schema::new(vec![
-            Field::new("c_custkey", DataType::Int32, false),
+            Field::new("c_custkey", DataType::UInt64, false),
             Field::new("c_name", DataType::Utf8, false),
             Field::new("c_address", DataType::Utf8, false),
-            Field::new("c_nationkey", DataType::Int32, false),
+            Field::new("c_nationkey", DataType::UInt64, false),
             Field::new("c_phone", DataType::Utf8, false),
             Field::new("c_acctbal", DataType::Float64, false),
             Field::new("c_mktsegment", DataType::Utf8, false),
@@ -531,8 +531,8 @@ fn get_schema(table: &str) -> Schema {
         ]),
 
         "orders" => Schema::new(vec![
-            Field::new("o_orderkey", DataType::Int32, false),
-            Field::new("o_custkey", DataType::Int32, false),
+            Field::new("o_orderkey", DataType::UInt64, false),
+            Field::new("o_custkey", DataType::UInt64, false),
             Field::new("o_orderstatus", DataType::Utf8, false),
             Field::new("o_totalprice", DataType::Float64, false),
             Field::new("o_orderdate", DataType::Date32, false),
@@ -543,9 +543,9 @@ fn get_schema(table: &str) -> Schema {
         ]),
 
         "lineitem" => Schema::new(vec![
-            Field::new("l_orderkey", DataType::Int32, false),
-            Field::new("l_partkey", DataType::Int32, false),
-            Field::new("l_suppkey", DataType::Int32, false),
+            Field::new("l_orderkey", DataType::UInt64, false),
+            Field::new("l_partkey", DataType::UInt64, false),
+            Field::new("l_suppkey", DataType::UInt64, false),
             Field::new("l_linenumber", DataType::Int32, false),
             Field::new("l_quantity", DataType::Float64, false),
             Field::new("l_extendedprice", DataType::Float64, false),
@@ -562,14 +562,14 @@ fn get_schema(table: &str) -> Schema {
         ]),
 
         "nation" => Schema::new(vec![
-            Field::new("n_nationkey", DataType::Int32, false),
+            Field::new("n_nationkey", DataType::UInt64, false),
             Field::new("n_name", DataType::Utf8, false),
-            Field::new("n_regionkey", DataType::Int32, false),
+            Field::new("n_regionkey", DataType::UInt64, false),
             Field::new("n_comment", DataType::Utf8, false),
         ]),
 
         "region" => Schema::new(vec![
-            Field::new("r_regionkey", DataType::Int32, false),
+            Field::new("r_regionkey", DataType::UInt64, false),
             Field::new("r_name", DataType::Utf8, false),
             Field::new("r_comment", DataType::Utf8, false),
         ]),
