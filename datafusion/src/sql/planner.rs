2500bff4d6774c008d92ac3504c722098a3db309
Andrew Lamb <andrew@nerdnetworks.org>
2021-09-26 06:48:15 -0400

    Update sqlparser-rs to 0.11 (#1052)
    * Update sqlparser-rs to 0.11
    
    * Update code to handle optional display

--- datafusion/src/sql/planner.rs
+++ datafusion/src/sql/planner.rs
@@ -102,6 +102,7 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
                 verbose,
                 statement,
                 analyze,
+                describe_alias: _,
             } => self.explain_statement_to_plan(*verbose, *analyze, statement),
             Statement::Query(query) => self.query_to_plan(query),
             Statement::ShowVariable { variable } => self.show_variable_to_plan(variable),
@@ -275,9 +276,9 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
     /// Maps the SQL type to the corresponding Arrow `DataType`
     fn make_data_type(&self, sql_type: &SQLDataType) -> Result<DataType> {
         match sql_type {
-            SQLDataType::BigInt => Ok(DataType::Int64),
-            SQLDataType::Int => Ok(DataType::Int32),
-            SQLDataType::SmallInt => Ok(DataType::Int16),
+            SQLDataType::BigInt(_display) => Ok(DataType::Int64),
+            SQLDataType::Int(_display) => Ok(DataType::Int32),
+            SQLDataType::SmallInt(_display) => Ok(DataType::Int16),
             SQLDataType::Char(_) | SQLDataType::Varchar(_) | SQLDataType::Text => {
                 Ok(DataType::Utf8)
             }
@@ -1829,9 +1830,9 @@ fn extract_possible_join_keys(
 pub fn convert_data_type(sql: &SQLDataType) -> Result<DataType> {
     match sql {
         SQLDataType::Boolean => Ok(DataType::Boolean),
-        SQLDataType::SmallInt => Ok(DataType::Int16),
-        SQLDataType::Int => Ok(DataType::Int32),
-        SQLDataType::BigInt => Ok(DataType::Int64),
+        SQLDataType::SmallInt(_display) => Ok(DataType::Int16),
+        SQLDataType::Int(_display) => Ok(DataType::Int32),
+        SQLDataType::BigInt(_display) => Ok(DataType::Int64),
         SQLDataType::Float(_) | SQLDataType::Real => Ok(DataType::Float64),
         SQLDataType::Double => Ok(DataType::Float64),
         SQLDataType::Char(_) | SQLDataType::Varchar(_) => Ok(DataType::Utf8),
