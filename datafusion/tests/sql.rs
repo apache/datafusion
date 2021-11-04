d79544a3f87ebf8d2f3a0dfa37ca87bb4c5a2608
Jason Tianyi Wang <capkurmagati@gmail.com>
2021-11-05 00:12:14 +0900

    Fix `between` in select query (#1202)
    * Fix `between` in select query
    resolves #1196
    
    * add tests
    
    * Make test not depend on number of cores
    
    * remove the default match arm in `create_physical_name`
    
    * make destructuring in match block simpler
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/tests/sql.rs
+++ datafusion/tests/sql.rs
@@ -5410,6 +5410,54 @@ async fn case_with_bool_type_result() -> Result<()> {
 }
 
 #[tokio::test]
+async fn use_between_expression_in_select_query() -> Result<()> {
+    let mut ctx = ExecutionContext::new();
+
+    let sql = "SELECT 1 NOT BETWEEN 3 AND 5";
+    let actual = execute_to_batches(&mut ctx, sql).await;
+    let expected = vec![
+        "+---------------+",
+        "| Boolean(true) |",
+        "+---------------+",
+        "| true          |",
+        "+---------------+",
+    ];
+    assert_batches_eq!(expected, &actual);
+
+    let input = Int64Array::from(vec![1, 2, 3, 4]);
+    let batch = RecordBatch::try_from_iter(vec![("c1", Arc::new(input) as _)]).unwrap();
+    let table = MemTable::try_new(batch.schema(), vec![vec![batch]])?;
+    ctx.register_table("test", Arc::new(table))?;
+
+    let sql = "SELECT abs(c1) BETWEEN 0 AND LoG(c1 * 100 ) FROM test";
+    let actual = execute_to_batches(&mut ctx, sql).await;
+    // Expect field name to be correctly converted for expr, low and high.
+    let expected = vec![
+        "+--------------------------------------------------------------------+",
+        "| abs(test.c1) BETWEEN Int64(0) AND log(test.c1 Multiply Int64(100)) |",
+        "+--------------------------------------------------------------------+",
+        "| true                                                               |",
+        "| true                                                               |",
+        "| false                                                              |",
+        "| false                                                              |",
+        "+--------------------------------------------------------------------+",
+    ];
+    assert_batches_eq!(expected, &actual);
+
+    let sql = "EXPLAIN SELECT c1 BETWEEN 2 AND 3 FROM test";
+    let actual = execute_to_batches(&mut ctx, sql).await;
+    let formatted = arrow::util::pretty::pretty_format_batches(&actual).unwrap();
+
+    // Only test that the projection exprs arecorrect, rather than entire output
+    let needle = "ProjectionExec: expr=[c1@0 >= 2 AND c1@0 <= 3 as test.c1 BETWEEN Int64(2) AND Int64(3)]";
+    assert_contains!(&formatted, needle);
+    let needle = "Projection: #test.c1 BETWEEN Int64(2) AND Int64(3)";
+    assert_contains!(&formatted, needle);
+
+    Ok(())
+}
+
+#[tokio::test]
 async fn query_get_indexed_field() -> Result<()> {
     let mut ctx = ExecutionContext::new();
     let schema = Arc::new(Schema::new(vec![Field::new(
