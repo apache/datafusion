30d152e0bd1311f0ad7e2a8500c8b0b5f49c0b65
Yang <37145547+Ted-Jiang@users.noreply.github.com>
2021-12-18 22:41:36 +0800

    fix calculate in many_to_many_hash_partition test. (#1463)
    * fix calculate in many_to_many_hash_partition test.
    
    * fix Clippy Lints

--- datafusion/src/physical_plan/repartition.rs
+++ datafusion/src/physical_plan/repartition.rs
@@ -576,7 +576,10 @@ mod tests {
         )
         .await?;
 
-        let total_rows: usize = output_partitions.iter().map(|x| x.len()).sum();
+        let total_rows: usize = output_partitions
+            .iter()
+            .map(|x| x.iter().map(|x| x.num_rows()).sum::<usize>())
+            .sum();
 
         assert_eq!(8, output_partitions.len());
         assert_eq!(total_rows, 8 * 50 * 3);
