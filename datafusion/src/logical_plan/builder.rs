f939040c0dac0cb4d40e30bdc8d45e7fbd6cbf42
Liang-Chi Hsieh <viirya@gmail.com>
2022-01-01 04:04:58 -0800

    Fix ORDER BY on aggregate (#1506)
    * Fix sort on aggregate
    
    * Use ExprRewriter.
    
    * For review comment
    
    * Update datafusion/src/logical_plan/expr.rs
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>
    
    * Update datafusion/src/logical_plan/expr.rs
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>
    
    * Update datafusion/src/logical_plan/expr.rs
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>
    
    * Fix format.
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/src/logical_plan/builder.rs
+++ datafusion/src/logical_plan/builder.rs
@@ -46,8 +46,8 @@ use std::{
 use super::dfschema::ToDFSchema;
 use super::{exprlist_to_fields, Expr, JoinConstraint, JoinType, LogicalPlan, PlanType};
 use crate::logical_plan::{
-    columnize_expr, normalize_col, normalize_cols, Column, CrossJoin, DFField, DFSchema,
-    DFSchemaRef, Limit, Partitioning, Repartition, Values,
+    columnize_expr, normalize_col, normalize_cols, rewrite_sort_cols_by_aggs, Column,
+    CrossJoin, DFField, DFSchema, DFSchemaRef, Limit, Partitioning, Repartition, Values,
 };
 use crate::sql::utils::group_window_expr_by_sort_keys;
 
@@ -521,6 +521,8 @@ impl LogicalPlanBuilder {
         &self,
         exprs: impl IntoIterator<Item = impl Into<Expr>> + Clone,
     ) -> Result<Self> {
+        let exprs = rewrite_sort_cols_by_aggs(exprs, &self.plan)?;
+
         let schema = self.plan.schema();
 
         // Collect sort columns that are missing in the input plan's schema
@@ -530,7 +532,7 @@ impl LogicalPlanBuilder {
             .into_iter()
             .try_for_each::<_, Result<()>>(|expr| {
                 let mut columns: HashSet<Column> = HashSet::new();
-                utils::expr_to_columns(&expr.into(), &mut columns)?;
+                utils::expr_to_columns(&expr, &mut columns)?;
 
                 columns.into_iter().for_each(|c| {
                     if schema.field_from_column(&c).is_err() {
