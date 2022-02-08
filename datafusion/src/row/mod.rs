240e9876b33a8bf37b4ee8769f8c4211c75fb3cf
Yijie Shen <henry.yijieshen@gmail.com>
2022-02-08 18:54:31 +0800

    Test with parquet data

--- datafusion/src/row/mod.rs
+++ datafusion/src/row/mod.rs
@@ -119,7 +119,16 @@ fn supported(schema: &Arc<Schema>) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::datasource::file_format::parquet::ParquetFormat;
+    use crate::datasource::file_format::FileFormat;
+    use crate::datasource::object_store::local::{
+        local_object_reader, local_object_reader_stream, local_unpartitioned_file,
+        LocalFileSystem,
+    };
     use crate::error::Result;
+    use crate::execution::runtime_env::RuntimeEnv;
+    use crate::physical_plan::file_format::FileScanConfig;
+    use crate::physical_plan::{collect, ExecutionPlan};
     use crate::row::reader::read_as_batch;
     use crate::row::writer::write_batch_unchecked;
     use arrow::record_batch::RecordBatch;
@@ -229,4 +238,96 @@ mod tests {
         Utf8,
         vec![Some("hello"), Some("world"), None, Some(""), Some("")]
     );
+
+    #[test]
+    fn test_single_binary() -> Result<()> {
+        let schema = Arc::new(Schema::new(vec![Field::new("a", Binary, false)]));
+        let values: Vec<Option<&[u8]>> =
+            vec![Some(b"one"), Some(b"two"), None, Some(b""), Some(b"three")];
+        let a = BinaryArray::from_opt_vec(values);
+        let batch = RecordBatch::try_new(schema.clone(), vec![Arc::new(a)])?;
+        let mut vector = vec![0; 8192];
+        let row_offsets =
+            { write_batch_unchecked(&mut vector, 0, &batch, 0, schema.clone()) };
+        let output_batch = { read_as_batch(&mut vector, schema, row_offsets)? };
+        assert_eq!(batch, output_batch);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_with_parquet() -> Result<()> {
+        let runtime = Arc::new(RuntimeEnv::default());
+        let projection = Some(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        let exec = get_exec("alltypes_plain.parquet", &projection, None).await?;
+        let schema = exec.schema().clone();
+
+        let batches = collect(exec, runtime).await?;
+        assert_eq!(1, batches.len());
+        let batch = &batches[0];
+
+        let mut vector = vec![0; 20480];
+        let row_offsets =
+            { write_batch_unchecked(&mut vector, 0, batch, 0, schema.clone()) };
+        let output_batch = { read_as_batch(&mut vector, schema, row_offsets)? };
+        assert_eq!(*batch, output_batch);
+
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic(expected = "supported(schema)")]
+    fn test_unsupported_type_write() {
+        let a: ArrayRef = Arc::new(TimestampNanosecondArray::from(vec![8, 7, 6, 5, 8]));
+        let batch = RecordBatch::try_from_iter(vec![("a", a)]).unwrap();
+        let schema = batch.schema();
+        let mut vector = vec![0; 1024];
+        write_batch_unchecked(&mut vector, 0, &batch, 0, schema);
+    }
+
+    #[test]
+    #[should_panic(expected = "supported(schema)")]
+    fn test_unsupported_type_read() {
+        let schema = Arc::new(Schema::new(vec![Field::new(
+            "a",
+            DataType::Decimal(5, 2),
+            false,
+        )]));
+        let mut vector = vec![0; 1024];
+        let row_offsets = vec![0];
+        read_as_batch(&mut vector, schema, row_offsets).unwrap();
+    }
+
+    async fn get_exec(
+        file_name: &str,
+        projection: &Option<Vec<usize>>,
+        limit: Option<usize>,
+    ) -> Result<Arc<dyn ExecutionPlan>> {
+        let testdata = crate::test_util::parquet_test_data();
+        let filename = format!("{}/{}", testdata, file_name);
+        let format = ParquetFormat::default();
+        let file_schema = format
+            .infer_schema(local_object_reader_stream(vec![filename.clone()]))
+            .await
+            .expect("Schema inference");
+        let statistics = format
+            .infer_stats(local_object_reader(filename.clone()))
+            .await
+            .expect("Stats inference");
+        let file_groups = vec![vec![local_unpartitioned_file(filename.clone())]];
+        let exec = format
+            .create_physical_plan(
+                FileScanConfig {
+                    object_store: Arc::new(LocalFileSystem {}),
+                    file_schema,
+                    file_groups,
+                    statistics,
+                    projection: projection.clone(),
+                    limit,
+                    table_partition_cols: vec![],
+                },
+                &[],
+            )
+            .await?;
+        Ok(exec)
+    }
 }
