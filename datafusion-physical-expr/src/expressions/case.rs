bc78f970a13bb70de2011d84ceb414b8ecec8fb0
Yijie Shen <henry.yijieshen@gmail.com>
2022-03-24 11:15:43 +0800

    short-circuit case_when

--- datafusion-physical-expr/src/expressions/case.rs
+++ datafusion-physical-expr/src/expressions/case.rs
@@ -20,7 +20,7 @@ use std::{any::Any, sync::Arc};
 use crate::expressions::try_cast;
 use crate::PhysicalExpr;
 use arrow::array::{self, *};
-use arrow::compute::{eq, eq_utf8};
+use arrow::compute::{and, eq, eq_utf8, is_null, not, or, or_kleene};
 use arrow::datatypes::{DataType, Schema};
 use arrow::record_batch::RecordBatch;
 use datafusion_common::{DataFusionError, Result};
@@ -323,24 +323,19 @@ impl CaseExpr {
         let base_value = expr.evaluate(batch)?;
         let base_type = expr.data_type(&batch.schema())?;
         let base_value = base_value.into_array(batch.num_rows());
+        let base_nulls = is_null(base_value.as_ref())?;
 
-        // start with the else condition, or nulls
-        let mut current_value: Option<ArrayRef> = if let Some(e) = &self.else_expr {
-            // keep `else_expr`'s data type and return type consistent
-            let expr = try_cast(e.clone(), &*batch.schema(), return_type.clone())
-                .unwrap_or_else(|_| e.clone());
-            Some(expr.evaluate(batch)?.into_array(batch.num_rows()))
-        } else {
-            Some(new_null_array(&return_type, batch.num_rows()))
-        };
-
-        // walk backwards through the when/then expressions
-        for i in (0..self.when_then_expr.len()).rev() {
+        // start with nulls as default output
+        let mut current_value = new_null_array(&return_type, batch.num_rows());
+        // We only consider non-null values while comparing with whens
+        let mut remainder = not(&base_nulls)?;
+        for i in 0..self.when_then_expr.len() {
             let i = i as usize;
 
-            let when_value = self.when_then_expr[i].0.evaluate(batch)?;
+            let when_value = self.when_then_expr[i]
+                .0
+                .evaluate_selection(batch, &remainder)?;
             let when_value = when_value.into_array(batch.num_rows());
-
             // build boolean array representing which rows match the "when" value
             let when_match = array_equals(&base_type, when_value, base_value.clone())?;
 
@@ -349,15 +344,24 @@ impl CaseExpr {
                 .evaluate_selection(batch, &when_match)?;
             let then_value = then_value.into_array(batch.num_rows());
 
-            current_value = Some(if_then_else(
-                &when_match,
-                then_value,
-                current_value.unwrap(),
-                &return_type,
-            )?);
+            current_value =
+                if_then_else(&when_match, then_value, current_value, &return_type)?;
+
+            remainder = and(&remainder, &or_kleene(&not(&when_match)?, &base_nulls)?)?;
         }
 
-        Ok(ColumnarValue::Array(current_value.unwrap()))
+        if let Some(e) = &self.else_expr {
+            // keep `else_expr`'s data type and return type consistent
+            let expr = try_cast(e.clone(), &*batch.schema(), return_type.clone())
+                .unwrap_or_else(|_| e.clone());
+            let else_ = expr
+                .evaluate_selection(batch, &remainder)?
+                .into_array(batch.num_rows());
+            remainder = or(&base_nulls, &remainder)?;
+            current_value = if_then_else(&remainder, else_, current_value, &return_type)?;
+        }
+
+        Ok(ColumnarValue::Array(current_value))
     }
 
     /// This function evaluates the form of CASE where each WHEN expression is a boolean
@@ -370,20 +374,15 @@ impl CaseExpr {
     fn case_when_no_expr(&self, batch: &RecordBatch) -> Result<ColumnarValue> {
         let return_type = self.when_then_expr[0].1.data_type(&batch.schema())?;
 
-        // start with the else condition, or nulls
-        let mut current_value: Option<ArrayRef> = if let Some(e) = &self.else_expr {
-            let expr = try_cast(e.clone(), &*batch.schema(), return_type.clone())
-                .unwrap_or_else(|_| e.clone());
-            Some(expr.evaluate(batch)?.into_array(batch.num_rows()))
-        } else {
-            Some(new_null_array(&return_type, batch.num_rows()))
-        };
-
-        // walk backwards through the when/then expressions
-        for i in (0..self.when_then_expr.len()).rev() {
+        // start with nulls as default output
+        let mut current_value = new_null_array(&return_type, batch.num_rows());
+        let mut remainder = BooleanArray::from(vec![true; batch.num_rows()]);
+        for i in 0..self.when_then_expr.len() {
             let i = i as usize;
 
-            let when_value = self.when_then_expr[i].0.evaluate(batch)?;
+            let when_value = self.when_then_expr[i]
+                .0
+                .evaluate_selection(batch, &remainder)?;
             let when_value = when_value.into_array(batch.num_rows());
             let when_value = when_value
                 .as_ref()
@@ -396,20 +395,34 @@ impl CaseExpr {
                 .evaluate_selection(batch, when_value)?;
             let then_value = then_value.into_array(batch.num_rows());
 
-            current_value = Some(if_then_else(
-                when_value,
-                then_value,
-                current_value.unwrap(),
-                &return_type,
-            )?);
+            current_value =
+                if_then_else(when_value, then_value, current_value, &return_type)?;
+
+            // Succeed tuples should be filtered out for short-circuit evaluation,
+            // null values for the current when expr should be kept
+            remainder = and(
+                &remainder,
+                &or_kleene(&not(when_value)?, &is_null(when_value)?)?,
+            )?;
         }
 
-        Ok(ColumnarValue::Array(current_value.unwrap()))
+        if let Some(e) = &self.else_expr {
+            // keep `else_expr`'s data type and return type consistent
+            let expr = try_cast(e.clone(), &*batch.schema(), return_type.clone())
+                .unwrap_or_else(|_| e.clone());
+            let else_ = expr
+                .evaluate_selection(batch, &remainder)?
+                .into_array(batch.num_rows());
+            remainder = or(&is_null(&remainder)?, &remainder)?;
+            current_value = if_then_else(&remainder, else_, current_value, &return_type)?;
+        }
+
+        Ok(ColumnarValue::Array(current_value))
     }
 }
 
 impl PhysicalExpr for CaseExpr {
-    /// Return a reference to Any that can be used for downcasting
+    /// Return a reference to Any that can be used for down-casting
     fn as_any(&self) -> &dyn Any {
         self
     }
