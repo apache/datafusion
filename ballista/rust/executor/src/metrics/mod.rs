3a69c9689bcb792ac9b9bcc39a0d4e14f9c096db
Dan Harris <dan@coralogix.com>
2022-04-14 17:12:49 -0400

    Comments and license header

--- ballista/rust/executor/src/metrics/mod.rs
+++ ballista/rust/executor/src/metrics/mod.rs
@@ -1,7 +1,30 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use ballista_core::execution_plans::ShuffleWriterExec;
 use datafusion::physical_plan::display::DisplayableExecutionPlan;
 
+/// `ExecutorMetricsCollector` records metrics for `ShuffleWriteExec`
+/// after they are executed.
+///
+/// After each stage completes, `ShuffleWriteExec::record_stage` will be
+/// called.
 pub trait ExecutorMetricsCollector: Send + Sync {
+    /// Record metrics for stage after it is executed
     fn record_stage(
         &self,
         job_id: &str,
@@ -11,6 +34,8 @@ pub trait ExecutorMetricsCollector: Send + Sync {
     );
 }
 
+/// Implementation of `ExecutorMetricsCollector` which logs the completed
+/// plan to stdout.
 #[derive(Default)]
 pub struct LoggingMetricsCollector {}
 
