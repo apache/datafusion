c3f2c0e85a4747cb23ff565be4f4519eec4d9135
Andrew Lamb <andrew@nerdnetworks.org>
2022-04-26 15:57:37 -0400

    RAT, add Bytes API

--- datafusion/proto/src/serde.rs
+++ datafusion/proto/src/serde.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use std::{collections::HashSet, sync::Arc};
 
 use crate::{from_proto::parse_expr, protobuf};
@@ -7,6 +24,9 @@ use datafusion::{
     logical_plan::{Expr, FunctionRegistry},
 };
 use prost::{bytes::BytesMut, Message};
+
+// Reexport Bytes which appears in the API
+pub use prost::bytes::Bytes;
 
 /// Encodes an [`Expr`] into a stream of bytes. See
 /// [`deserialize_expr`] to convert a stream of bytes back to an Expr
@@ -19,6 +39,7 @@ use prost::{bytes::BytesMut, Message};
 ///
 /// ```
 /// use datafusion::prelude::*;
+/// use datafusion::logical_plan::Expr;
 /// use datafusion_proto::serde::Serializeable;
 ///
 /// // Create a new `Expr` a < 32
@@ -32,25 +53,26 @@ use prost::{bytes::BytesMut, Message};
 /// assert_eq!(expr, decoded_expr);
 /// ```
 pub trait Serializeable: Sized {
-    /// Convert this to some serialized form (the internal format is not guaranteed)
-    fn serialize(&self) -> Result<Vec<u8>>;
+    /// Convert `self` to a serialized form (the internal format is not guaranteed)
+    fn to_bytes(&self) -> Result<Bytes>;
 
-    /// convert the output of serialize back into an Expr, assuming
-    /// Expr has no user defined functions
-    fn deserialize(bytes: &[u8]) -> Result<Self> {
-        Self::deserialize_with_registry(bytes, &NoRegistry {})
+    /// Convenience wy to convert the `bytes` (output of [`to_bytes`]
+    /// back into an [`Expr']. This will error if there are any user
+    /// defined functions, in which case use [`from_bytes_with_registry`]
+    fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        Self::from_bytes_with_registry(bytes, &NoRegistry {})
     }
 
     /// convert the output of serialize back into an Expr, given the
     /// specfified function registry for resolving UDFs
-    fn deserialize_with_registry(
+    fn from_bytes_with_registry(
         bytes: &[u8],
         registry: &dyn FunctionRegistry,
     ) -> Result<Self>;
 }
 
 impl Serializeable for Expr {
-    fn serialize(&self) -> Result<Vec<u8>> {
+    fn to_bytes(&self) -> Result<Bytes> {
         let mut buffer = BytesMut::new();
         let protobuf: protobuf::LogicalExprNode = self.try_into().map_err(|e| {
             DataFusionError::Plan(format!("Error encoding expr as protobuf: {}", e))
@@ -60,11 +82,10 @@ impl Serializeable for Expr {
             DataFusionError::Plan(format!("Error encoding protobuf as bytes: {}", e))
         })?;
 
-        // this copies the expr -- maybe it would be nice to avoid doing so
-        Ok(buffer.to_vec())
+        Ok(buffer.into())
     }
 
-    fn deserialize_with_registry(
+    fn from_bytes_with_registry(
         bytes: &[u8],
         registry: &dyn FunctionRegistry,
     ) -> Result<Self> {
