9fb347290922f54cb35fe3a271d417670c13ac5f
duripeng <453243496@qq.com>
2022-05-03 18:21:43 +0800

    revise annotation

--- datafusion/physical-expr/src/aggregate/sum_distinct.rs
+++ datafusion/physical-expr/src/aggregate/sum_distinct.rs
@@ -130,7 +130,7 @@ impl DistinctSumAccumulator {
 impl Accumulator for DistinctSumAccumulator {
     fn state(&self) -> Result<Vec<ScalarValue>> {
         // 1. Stores aggregate state in `ScalarValue::List`
-        // 2. Construct this `ScalarValue::List` state from distinct numeric in hash set
+        // 2. Constructs `ScalarValue::List` state from distinct numeric stored in hash set
         let state_out = {
             let mut distinct_values = Box::new(Vec::new());
             let data_type = Box::new(self.data_type.clone());
