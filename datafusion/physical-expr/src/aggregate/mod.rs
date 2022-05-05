bcf216df3a8b56d81f22fc2fdbb84b53b01b4977
duripeng <453243496@qq.com>
2022-05-06 03:57:11 +0800

    fix clippy

--- datafusion/physical-expr/src/aggregate/mod.rs
+++ datafusion/physical-expr/src/aggregate/mod.rs
@@ -17,7 +17,7 @@
 
 use crate::PhysicalExpr;
 use arrow::datatypes::Field;
-use datafusion_common::{Result, ScalarValue};
+use datafusion_common::Result;
 use datafusion_expr::Accumulator;
 use std::any::Any;
 use std::fmt::Debug;
