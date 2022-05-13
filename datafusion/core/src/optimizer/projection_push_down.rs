fd65625cf4a6091098a9c8d2e4ad0b95ca515320
Jon Mease <jon.mease.dev@gmail.com>
2022-05-13 14:28:51 -0400

    Fix projection pushdown produces incorrect results when column names are reused (#2463)
    * Add failing select_with_alias_overwrite test
    
    * candidate fix
    
    * reinstate optimization, but check that expressions are plain columns

--- datafusion/core/src/optimizer/projection_push_down.rs
+++ datafusion/core/src/optimizer/projection_push_down.rs
@@ -32,6 +32,7 @@ use crate::optimizer::utils;
 use crate::sql::utils::find_sort_exprs;
 use arrow::datatypes::{Field, Schema};
 use arrow::error::Result as ArrowResult;
+use datafusion_expr::Expr;
 use std::{
     collections::{BTreeSet, HashSet},
     sync::Arc,
@@ -179,8 +180,12 @@ fn optimize_plan(
                 .map(|f| f.qualified_column())
                 .collect::<HashSet<Column>>();
 
+            let all_column_exprs = new_expr.iter().all(|e| matches!(e, Expr::Column(_)));
+
             if new_fields.is_empty()
-                || (has_projection && &new_required_columns_optimized == required_columns)
+                || (has_projection
+                    && all_column_exprs
+                    && &new_required_columns_optimized == required_columns)
             {
                 // no need for an expression at all
                 Ok(new_input)
