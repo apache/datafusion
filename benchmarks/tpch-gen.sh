0f808938bfae0426aef270e67f35f9cb645c941c
xudong.w <wxd963996380@gmail.com>
2022-06-18 21:11:23 +0800

    improve: supports user-defined scale_factor for dbgen (#2741)

--- benchmarks/tpch-gen.sh
+++ benchmarks/tpch-gen.sh
@@ -21,7 +21,8 @@
 pushd ..
 . ./dev/build-set-env.sh
 popd
-docker build -t datafusion-tpchgen:$DATAFUSION_VERSION -f tpchgen.dockerfile .
+
+docker build  -f tpchgen.dockerfile -t datafusion-tpchgen:$DATAFUSION_VERSION .
 
 # Generate data into the ./data directory if it does not already exist
 FILE=./data/supplier.tbl
@@ -29,6 +30,6 @@ if test -f "$FILE"; then
     echo "$FILE exists."
 else
   mkdir data 2>/dev/null
-  docker run -v `pwd`/data:/data -it --rm datafusion-tpchgen:$DATAFUSION_VERSION
+  docker run datafusion-tpchgen:$DATAFUSION_VERSION $1 -v `pwd`/data:/data -it --rm datafusion-tpchgen:$DATAFUSION_VERSION
   ls -l data
 fi
