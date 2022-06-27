d86861c61bce230a9ac881761867e4005d76a630
Brent Gardner <bgardner@squarelabs.net>
2022-06-27 12:03:05 -0600

    Fix query

--- datafusion/core/tests/sql/subqueries.rs
+++ datafusion/core/tests/sql/subqueries.rs
@@ -21,7 +21,7 @@ async fn tpch_q20() -> Result<()> {
             where ps_partkey in ( select p_partkey from part where p_name like 'forest%' )
               and ps_availqty > ( select 0.5 * sum(l_quantity) from lineitem
                 where l_partkey = ps_partkey and l_suppkey = ps_suppkey and l_shipdate >= date '1994-01-01'
-                and l_shipdate < 'date 1994-01-01' + interval '1' year
+                and l_shipdate < date '1994-01-01' + interval '1' year
             )
         )
         and s_nationkey = n_nationkey and n_name = 'CANADA'
