eac67cdd072d41a23bb1cd0248c7b34183eca101
Brent Gardner <bgardner@squarelabs.net>
2022-06-29 16:33:26 -0600

    Tests still pass because we are losing remaining predicate

--- datafusion/optimizer/src/subquery_decorrelate.rs
+++ datafusion/optimizer/src/subquery_decorrelate.rs
@@ -1,5 +1,5 @@
 use crate::{utils, OptimizerConfig, OptimizerRule};
-use datafusion_common::Column;
+use datafusion_common::{Column, DFSchemaRef};
 use datafusion_expr::logical_plan::{Filter, JoinType, Subquery};
 use datafusion_expr::{Expr, LogicalPlan, LogicalPlanBuilder, Operator};
 use hashbrown::HashSet;
@@ -78,65 +78,86 @@ fn optimize_exists(
         return Ok(plan.clone());
     };
 
-    // Only operate on a single binary equality expression (for now)
-    let (left, op, right) =
-        if let Expr::BinaryExpr { left, op, right } = &filter.predicate {
-            (left, op, right)
-        } else {
-            return Ok(plan.clone());
-        };
-    match op {
-        Operator::Eq => {}
-        _ => return Ok(plan.clone()),
-    }
+    // split into filters
+    let mut filters = vec![];
+    utils::split_conjunction(&filter.predicate, &mut filters);
 
-    // collect list of columns
-    let lcol = match &**left {
-        Expr::Column(col) => col,
-        _ => return Ok(plan.clone()),
-    };
-    let rcol = match &**right {
-        Expr::Column(col) => col,
-        _ => return Ok(plan.clone()),
-    };
-    let cols = vec![lcol, rcol];
-    let cols: HashSet<_> = cols.iter().map(|c| &c.name).collect();
-    let fields: HashSet<_> = sub_input
-        .schema()
-        .fields()
-        .iter()
-        .map(|f| f.name())
-        .collect();
+    // Grab column names to join on
+    let cols = find_join_exprs(filters, sub_input.schema());
+    if cols.is_empty() {
+        return Ok(plan.clone());
+    }
 
     // Only operate if one column is present and the other closed upon from outside scope
-    let found: Vec<_> = cols.intersection(&fields).map(|it| (*it).clone()).collect();
-    let closed_upon: Vec<_> = cols.difference(&fields).map(|it| (*it).clone()).collect();
-    if found.len() != 1 || closed_upon.len() != 1 {
-        return Ok(plan.clone());
-    }
-    let found = if let Some(it) = found.get(0) {
-        it
-    } else {
-        return Ok(plan.clone());
-    };
-    let closed_upon = if let Some(it) = closed_upon.get(0) {
-        it
-    } else {
-        return Ok(plan.clone());
-    };
-
-    let c_col = vec![Column::from_qualified_name(closed_upon)];
-    let f_col = vec![Column::from_qualified_name(found)];
-    let expr = vec![Expr::Column(found.as_str().into())];
-    let group_expr = vec![Expr::Column(found.as_str().into())];
+    let l_col: Vec<_> = cols.iter()
+        .map(|it| &it.0)
+        .map(|it| Column::from_qualified_name(it.as_str()))
+        .collect();
+    let r_col: Vec<_> = cols.iter()
+        .map(|it| &it.1)
+        .map(|it| Column::from_qualified_name(it.as_str()))
+        .collect();
+    let expr: Vec<_> = r_col.iter().map(|it| Expr::Column(it.clone())).collect();
     let aggr_expr: Vec<Expr> = vec![];
-    let join_keys = (c_col.clone(), f_col.clone());
+    let join_keys = (l_col.clone(), r_col.clone());
     let right = LogicalPlanBuilder::from((*filter.input).clone())
-        .aggregate(group_expr, aggr_expr)?
+        .aggregate(expr.clone(), aggr_expr)?
         .project(expr)?
         .build()?;
     let new_plan = LogicalPlanBuilder::from((**input).clone())
         .join(&right, JoinType::Inner, join_keys, None)?
         .build()?;
     Ok(new_plan)
+}
+
+fn find_join_exprs(filters: Vec<&Expr>, schema: &DFSchemaRef) -> Vec<(String, String)> {
+    // only process equals expressions for joins
+    let equals: Vec<_> = filters.iter().map(|it| {
+        match it {
+            Expr::BinaryExpr { left, op, right } => {
+                match op {
+                    Operator::Eq => Some((*left.clone(), *right.clone())),
+                    _ => None,
+                }
+            }
+            _ => None
+        }
+    }).flatten().collect();
+
+    // only process column expressions for joins
+    let cols: Vec<_> = equals.iter().map(|it| {
+        let l = match &it.0 {
+            Expr::Column(col) => col,
+            _ => return None,
+        };
+        let r = match &it.1 {
+            Expr::Column(col) => col,
+            _ => return None,
+        };
+        Some((l.name.clone(), r.name.clone()))
+    }).flatten().collect();
+
+    // get names of fields TODO: Must fully qualify these!
+    let fields: HashSet<_> = schema
+        .fields()
+        .iter()
+        .map(|f| f.name())
+        .collect();
+
+    // Ensure closed-upon fields are always on left, and in-scope on the right
+    let sorted: Vec<_> = cols.iter().map(|it| {
+        if fields.contains(&it.0) && fields.contains(&it.1) {
+            return None; // Need one of each
+        }
+        if !fields.contains(&it.0) && !fields.contains(&it.1) {
+            return None; // Need one of each
+        }
+        if fields.contains(&it.0) {
+            Some((it.1.clone(), it.0.clone()))
+        } else {
+            Some((it.0.clone(), it.1.clone()))
+        }
+    }).flatten().collect();
+
+    sorted
 }
