9377cdf30db3f665d2e642113daa85607a800626
Brent Gardner <bgardner@squarelabs.net>
2022-06-29 16:33:26 -0600

    Tests still pass because we are losing remaining predicate

--- datafusion/core/tests/sql/subqueries.rs
+++ datafusion/core/tests/sql/subqueries.rs
@@ -23,7 +23,8 @@ async fn tpch_q4_correlated() -> Result<()> {
     let sql = r#"
         select o_orderpriority, count(*) as order_count
         from orders
-        where exists ( select * from lineitem where l_orderkey = o_orderkey )
+        where exists (
+            select * from lineitem where l_orderkey = o_orderkey and l_commitdate < l_receiptdate)
         group by o_orderpriority
         order by o_orderpriority;
         "#;
