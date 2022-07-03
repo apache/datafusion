aaa952edc36bb933da6e0dd7cb81e4d7fb3bd6f2
Brent Gardner <bgardner@squarelabs.net>
2022-07-03 12:27:58 -0600

    Query 21 test

--- datafusion/core/tests/sql/subqueries.rs
+++ datafusion/core/tests/sql/subqueries.rs
@@ -186,17 +186,76 @@ order by s_name;
         .unwrap();
     let actual = format!("{}", plan.display_indent());
     println!("after:\n{}", actual);
-    let expected = r#"Projection: #SUM(lineitem.l_extendedprice) / Float64(7) AS avg_yearly
-  Aggregate: groupBy=[[]], aggr=[[SUM(#lineitem.l_extendedprice)]]
-    Filter: #lineitem.l_quantity < #__sq_1.__value
-      Filter: #part.p_brand = Utf8("Brand#23") AND #part.p_container = Utf8("MED BOX")
-        Inner Join: #part.p_partkey = #__sq_1.l_partkey
-          Inner Join: #lineitem.l_partkey = #part.p_partkey
-            TableScan: lineitem
-            TableScan: part
-          Projection: #lineitem.l_partkey, Float64(0.2) * #AVG(lineitem.l_quantity) AS __value, alias=__sq_1
-            Aggregate: groupBy=[[#lineitem.l_partkey]], aggr=[[AVG(#lineitem.l_quantity)]]
-              TableScan: lineitem"#
+    let expected = r#"Sort: #supplier.s_name ASC NULLS LAST
+  Projection: #supplier.s_name, #supplier.s_address
+    Filter: #nation.n_name = Utf8("CANADA")
+      Inner Join: #supplier.s_suppkey = #__sq_2.ps_suppkey
+        Inner Join: #supplier.s_nationkey = #nation.n_nationkey
+          TableScan: supplier
+          TableScan: nation
+        Projection: #partsupp.ps_suppkey, alias=__sq_2
+          Aggregate: groupBy=[[#partsupp.ps_suppkey]], aggr=[[]]
+            Filter: #partsupp.ps_availqty > #__sq_3.__value
+              Inner Join: #partsupp.ps_partkey = #__sq_3.l_partkey, #partsupp.ps_suppkey = #__sq_3.l_suppkey
+                Inner Join: #partsupp.ps_partkey = #__sq_1.p_partkey
+                  TableScan: partsupp
+                  Projection: #part.p_partkey, alias=__sq_1
+                    Aggregate: groupBy=[[#part.p_partkey]], aggr=[[]]
+                      Filter: #part.p_name LIKE Utf8("forest%")
+                        TableScan: part
+                Projection: #lineitem.l_partkey, #lineitem.l_suppkey, Float64(0.5) * #SUM(lineitem.l_quantity) AS __value, alias=__sq_3
+                  Aggregate: groupBy=[[#lineitem.l_partkey, #lineitem.l_suppkey]], aggr=[[SUM(#lineitem.l_quantity)]]
+                    Filter: #lineitem.l_shipdate >= CAST(Utf8("1994-01-01") AS Date32)
+                      TableScan: lineitem"#
+        .to_string();
+    assert_eq!(actual, expected);
+
+    // assert data
+    let results = execute_to_batches(&ctx, sql).await;
+    let expected = vec![
+        "++",
+        "++",
+    ];
+    assert_batches_eq!(expected, &results);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn tpch_q21_correlated() -> Result<()> {
+    reset_id();
+    let ctx = SessionContext::new();
+    register_tpch_csv(&ctx, "orders").await?;
+    register_tpch_csv(&ctx, "supplier").await?;
+    register_tpch_csv(&ctx, "lineitem").await?;
+    register_tpch_csv(&ctx, "nation").await?;
+
+    /*
+     */
+    let sql = r#"select s_name, count(*) as numwait
+from supplier, lineitem l1, orders, nation
+where s_suppkey = l1.l_suppkey and o_orderkey = l1.l_orderkey and o_orderstatus = 'F' and l1.l_receiptdate > l1.l_commitdate
+  and exists (
+        select * from lineitem l2 where l2.l_orderkey = l1.l_orderkey and l2.l_suppkey <> l1.l_suppkey
+    ) and not exists (
+        select * from lineitem l3 where l3.l_orderkey = l1.l_orderkey and l3.l_suppkey <> l1.l_suppkey and l3.l_receiptdate > l3.l_commitdate
+    )
+  and s_nationkey = n_nationkey and n_name = 'SAUDI ARABIA'
+group by s_name order by numwait desc, s_name;"#;
+
+    // assert plan
+    let plan = ctx
+        .create_logical_plan(sql)
+        .map_err(|e| format!("{:?} at {}", e, "error"))
+        .unwrap();
+    println!("before:\n{}", plan.display_indent());
+    let plan = ctx
+        .optimize(&plan)
+        .map_err(|e| format!("{:?} at {}", e, "error"))
+        .unwrap();
+    let actual = format!("{}", plan.display_indent());
+    println!("after:\n{}", actual);
+    let expected = r#""#
         .to_string();
     // assert_eq!(actual, expected);
 
@@ -210,4 +269,3 @@ order by s_name;
 
     Ok(())
 }
-
