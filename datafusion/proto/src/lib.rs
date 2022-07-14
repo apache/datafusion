179bdbba3137a047c2fa050f15ce5ed020c99325
comphead <comphead@ukr.net>
2022-07-14 12:12:37 -0700

    Formatting

--- datafusion/proto/src/lib.rs
+++ datafusion/proto/src/lib.rs
@@ -824,7 +824,10 @@ mod roundtrip_tests {
             ScalarValue::Date32(None),
             ScalarValue::TimestampMicrosecond(None, None),
             ScalarValue::TimestampNanosecond(None, None),
-            ScalarValue::List(None, Box::new(Field::new("item", DataType::Boolean, false)))
+            ScalarValue::List(
+                None,
+                Box::new(Field::new("item", DataType::Boolean, false)),
+            ),
         ];
 
         for test_case in test_types.into_iter() {
