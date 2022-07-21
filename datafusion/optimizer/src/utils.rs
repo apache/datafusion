50a952f234cbe18389e4be76652ed3b6cbff8521
Brent Gardner <bgardner@squarelabs.net>
2022-07-21 11:53:05 -0600

    PR feedback

--- datafusion/optimizer/src/utils.rs
+++ datafusion/optimizer/src/utils.rs
@@ -213,7 +213,7 @@ pub fn exprs_to_join_cols(
     for filter in exprs.iter() {
         let (left, op, right) = match filter {
             Expr::BinaryExpr { left, op, right } => (*left.clone(), *op, *right.clone()),
-            _ => plan_err!("Invalid expression!")?,
+            _ => plan_err!("Invalid correlation expression!")?,
         };
         match op {
             Operator::Eq => {}
@@ -223,7 +223,7 @@ pub fn exprs_to_join_cols(
                     continue;
                 }
             }
-            _ => plan_err!("Invalid expression!")?,
+            _ => plan_err!(format!("Correlation operator unsupported: {}", op))?,
         }
         let left = left.try_into_col()?;
         let right = right.try_into_col()?;
