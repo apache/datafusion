b0ba167a08ccfe8cd742440fd5f422b035bcfc1c
Stuart Carnie <stuart.carnie@gmail.com>
2022-08-04 09:41:36 +1000

    feat: Enable typed strings expressions for VALUES clause (#3018)
    * feat: Add support for typed strings to VALUES clause
    
    * chore: Tidy up 🧹

--- datafusion/sql/src/planner.rs
+++ datafusion/sql/src/planner.rs
@@ -1613,6 +1613,10 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
                                 &schema,
                                 &mut HashMap::new(),
                             ),
+                        SQLExpr::TypedString { data_type, value } => Ok(Expr::Cast {
+                            expr: Box::new(lit(value)),
+                            data_type: convert_data_type(&data_type)?,
+                        }),
                         other => Err(DataFusionError::NotImplemented(format!(
                             "Unsupported value {:?} in a values list expression",
                             other
@@ -1809,11 +1813,11 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
             }),
 
             SQLExpr::TypedString {
-                ref data_type,
-                ref value,
+                data_type,
+                value,
             } => Ok(Expr::Cast {
-                expr: Box::new(lit(&**value)),
-                data_type: convert_data_type(data_type)?,
+                expr: Box::new(lit(value)),
+                data_type: convert_data_type(&data_type)?,
             }),
 
             SQLExpr::IsNull(expr) => Ok(Expr::IsNull(Box::new(
@@ -3244,6 +3248,17 @@ mod tests {
             "Projection: #MIN(person.age) AS a, #MIN(person.age) AS b\
              \n  Aggregate: groupBy=[[]], aggr=[[MIN(#person.age)]]\
              \n    TableScan: person",
+        );
+    }
+
+    #[test]
+    fn select_from_typed_string_values() {
+        quick_test(
+            "SELECT col1, col2 FROM (VALUES (TIMESTAMP '2021-06-10 17:01:00Z', DATE '2004-04-09')) as t (col1, col2)",
+            "Projection: #t.col1, #t.col2\
+            \n  Projection: #t.column1 AS col1, #t.column2 AS col2, alias=t\
+            \n    Projection: #column1, #column2, alias=t\
+            \n      Values: (CAST(Utf8(\"2021-06-10 17:01:00Z\") AS Timestamp(Nanosecond, None)), CAST(Utf8(\"2004-04-09\") AS Date32))",
         );
     }
 
