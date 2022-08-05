d5217ed7ed722b8470ba6db8a5ce8f254d8e4e0e
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2022-08-06 05:20:44 +0800

    dummy commit to trigger git action

--- datafusion/core/tests/sql/timestamp.rs
+++ datafusion/core/tests/sql/timestamp.rs
@@ -1070,7 +1070,7 @@ async fn cast_to_timestamp_micros_twice() -> Result<()> {
 async fn to_timestamp_i32() -> Result<()> {
     let ctx = SessionContext::new();
 
-    let sql = "select to_timestamp(cast (1 as int));";
+    let sql = "select to_timestamp(cast (1 as int));"; 
     let results = execute_to_batches(&ctx, sql).await;
 
     let expected = vec![
