db6e2af73f5a59ef765c4cc983ae4f977a5e3cef
Heres, Daniel <danielheres@gmail.com>
2022-08-07 13:52:06 +0200

    Remove integration test

--- integration-tests/test_psql_parity.py
+++ integration-tests/test_psql_parity.py
@@ -82,7 +82,7 @@ test_files = set(root.glob("*.sql"))
 
 class TestPsqlParity:
     def test_tests_count(self):
-        assert len(test_files) == 23, "tests are missed"
+        assert len(test_files) == 22, "tests are missed"
 
     @pytest.mark.parametrize("fname", test_files)
     def test_sql_file(self, fname):
