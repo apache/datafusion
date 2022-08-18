167a654f0588dd9903582e241a6f28dff99772a1
Andy Grove <andygrove73@gmail.com>
2022-08-18 16:01:22 -0600

    rename array function to make_array

--- datafusion/expr/src/expr_fn.rs
+++ datafusion/expr/src/expr_fn.rs
@@ -384,7 +384,7 @@ unary_scalar_expr!(ArrowTypeof, arrow_typeof, "data type");
 /// Returns an array of fixed size with each argument on it.
 pub fn array(args: Vec<Expr>) -> Expr {
     Expr::ScalarFunction {
-        fun: built_in_function::BuiltinScalarFunction::Array,
+        fun: built_in_function::BuiltinScalarFunction::MakeArray,
         args,
     }
 }
