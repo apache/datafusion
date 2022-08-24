6719fc10a9d3bf74ca4f3ad5ba6cf41870219836
Andy Grove <andygrove73@gmail.com>
2022-08-24 10:17:08 -0600

    fmt

--- datafusion/core/src/datasource/view.rs
+++ datafusion/core/src/datasource/view.rs
@@ -91,14 +91,17 @@ impl TableProvider for ViewTable {
         state_cloned.execution_props.start_execution();
         if let Some(projection) = projection {
             // avoiding adding a redundant projection (e.g. SELECT * FROM view)
-            let current_projection = (0..self.logical_plan.schema().fields().len()).collect::<Vec<usize>>();
+            let current_projection =
+                (0..self.logical_plan.schema().fields().len()).collect::<Vec<usize>>();
             if projection == &current_projection {
                 state_cloned.create_physical_plan(&self.logical_plan).await
             } else {
                 let fields: Vec<Expr> = projection
                     .iter()
                     .map(|i| {
-                        Expr::Column(self.logical_plan.schema().field(*i).qualified_column())
+                        Expr::Column(
+                            self.logical_plan.schema().field(*i).qualified_column(),
+                        )
                     })
                     .collect();
                 let plan = LogicalPlanBuilder::from(self.logical_plan.clone())
