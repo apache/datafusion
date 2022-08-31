03963db80628711edac560aaeac3a474ca78076f
kmitchener <kmitchener@gmail.com>
2022-08-31 06:34:59 -0400

    cargo fmt

--- datafusion/optimizer/src/lib.rs
+++ datafusion/optimizer/src/lib.rs
@@ -16,7 +16,6 @@
 // under the License.
 
 pub mod common_subexpr_eliminate;
-pub mod scalar_subquery_to_join;
 pub mod decorrelate_where_exists;
 pub mod decorrelate_where_in;
 pub mod eliminate_filter;
@@ -28,6 +27,7 @@ pub mod limit_push_down;
 pub mod optimizer;
 pub mod projection_push_down;
 pub mod reduce_outer_join;
+pub mod scalar_subquery_to_join;
 pub mod simplify_expressions;
 pub mod single_distinct_to_groupby;
 pub mod subquery_filter_to_join;
