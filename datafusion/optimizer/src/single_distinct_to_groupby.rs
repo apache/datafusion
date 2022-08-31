3d37016de3647d90e6f78fd0e106142923799969
Andy Grove <andygrove73@gmail.com>
2022-08-31 08:58:32 -0600

    Add `Aggregate::try new` with validation checks (#3286)
    * Add Aggregate::try_new with validation checks
    
    * fix calculation of number of grouping expressions
    
    * use suggested error message

--- datafusion/optimizer/src/single_distinct_to_groupby.rs
+++ datafusion/optimizer/src/single_distinct_to_groupby.rs
@@ -100,12 +100,12 @@ fn optimize(plan: &LogicalPlan) -> Result<LogicalPlan> {
                     all_field,
                     input.schema().metadata().clone(),
                 )?;
-                let grouped_agg = LogicalPlan::Aggregate(Aggregate {
-                    input: input.clone(),
-                    group_expr: all_group_args,
-                    aggr_expr: Vec::new(),
-                    schema: Arc::new(grouped_schema.clone()),
-                });
+                let grouped_agg = LogicalPlan::Aggregate(Aggregate::try_new(
+                    input.clone(),
+                    all_group_args,
+                    Vec::new(),
+                    Arc::new(grouped_schema.clone()),
+                )?);
                 let grouped_agg = optimize_children(&grouped_agg);
                 let final_agg_schema = Arc::new(DFSchema::new_with_metadata(
                     base_group_expr
@@ -129,13 +129,12 @@ fn optimize(plan: &LogicalPlan) -> Result<LogicalPlan> {
                         ));
                     });
 
-                let final_agg = LogicalPlan::Aggregate(Aggregate {
-                    input: Arc::new(grouped_agg?),
-                    group_expr: group_expr.clone(),
-                    aggr_expr: new_aggr_expr,
-                    schema: final_agg_schema,
-                });
-
+                let final_agg = LogicalPlan::Aggregate(Aggregate::try_new(
+                    Arc::new(grouped_agg?),
+                    group_expr.clone(),
+                    new_aggr_expr,
+                    final_agg_schema,
+                )?);
                 Ok(LogicalPlan::Projection(Projection::try_new_with_schema(
                     alias_expr,
                     Arc::new(final_agg),
