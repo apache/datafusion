dc03f409906c3e478857d67d0fbb2324a6156991
Ian Joiner <iajoiner809@gmail.com>
2022-09-02 16:22:03 -0400

    change func name to to_unoptimized_plan

--- datafusion/core/src/dataframe.rs
+++ datafusion/core/src/dataframe.rs
@@ -521,7 +521,7 @@ impl DataFrame {
     }
 
     /// Return the unoptimized logical plan represented by this DataFrame.
-    pub fn to_unoptimized_logical_plan(&self) -> LogicalPlan {
+    pub fn to_unoptimized_plan(&self) -> LogicalPlan {
         // Optimize the plan first for better UX
         self.plan.clone()
     }
@@ -1272,7 +1272,7 @@ mod tests {
         \n      Inner Join: #t1.c1 = #t2.c1\
         \n        TableScan: t1\
         \n        TableScan: t2",
-            format!("{:?}", df_renamed.to_unoptimized_logical_plan())
+            format!("{:?}", df_renamed.to_unoptimized_plan())
         );
 
         assert_eq!("\
