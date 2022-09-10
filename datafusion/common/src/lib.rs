2f0fa25604152223c275eeaac6fdd13226c3032b
Andy Grove <andygrove73@gmail.com>
2022-09-10 09:02:34 -0600

    Update datafusion/common/src/lib.rs

--- datafusion/common/src/lib.rs
+++ datafusion/common/src/lib.rs
@@ -30,7 +30,7 @@ pub use scalar::{ScalarType, ScalarValue};
 
 /// Downcast an Arrow Array to a concrete type, return an `Err` if the cast is
 /// not possible.
-/// 
+///
 /// Example: `let array = downcast_value!(values, Int32Array)`
 #[macro_export]
 macro_rules! downcast_value {
