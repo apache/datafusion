ffc87ab9f955801216becc95a1189fd9ad4580a8
Andrew Lamb <andrew@nerdnetworks.org>
2022-09-14 14:46:12 -0400

    Change `downcast_any!` macro so it does not need to use `use std::any::type_name;`

--- datafusion/physical-expr/src/aggregate/min_max.rs
+++ datafusion/physical-expr/src/aggregate/min_max.rs
@@ -17,7 +17,7 @@
 
 //! Defines physical expressions that can evaluated at runtime during query execution
 
-use std::any::{type_name, Any};
+use std::any::Any;
 use std::convert::TryFrom;
 use std::sync::Arc;
 
