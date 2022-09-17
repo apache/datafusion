71ffc37de0ac327498ec55a3e609dfc3e671c70b
Andy Grove <andygrove73@gmail.com>
2022-09-16 18:31:32 -0600

    Update datafusion/core/src/lib.rs
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/core/src/lib.rs
+++ datafusion/core/src/lib.rs
@@ -171,7 +171,7 @@
 //! * Scan CSV: [`CsvExec`](physical_plan::file_format::CsvExec)
 //! * Scan Parquet: [`ParquetExec`](physical_plan::file_format::ParquetExec)
 //! * Scan Avro: [`AvroExec`](physical_plan::file_format::AvroExec)
-//! * Scan newline-delimited JSON: [`ParquetExec`](physical_plan::file_format::NdJsonExec)
+//! * Scan newline-delimited JSON: [`NdJsonExec`](physical_plan::file_format::NdJsonExec)
 //! * Scan from memory: [`MemoryExec`](physical_plan::memory::MemoryExec)
 //! * Explain the plan: [`ExplainExec`](physical_plan::explain::ExplainExec)
 //!
