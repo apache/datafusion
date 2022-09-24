095c6249d9d02cee646ee6f2c6501cfb811e71be
Andrew Lamb <andrew@nerdnetworks.org>
2022-09-24 08:01:16 -0400

    MINOR: improve docstrings on SessionContext

--- datafusion/core/src/physical_plan/aggregates/mod.rs
+++ datafusion/core/src/physical_plan/aggregates/mod.rs
@@ -90,8 +90,8 @@ pub struct PhysicalGroupBy {
     null_expr: Vec<(Arc<dyn PhysicalExpr>, String)>,
     /// Null mask for each group in this grouping set. Each group is
     /// composed of either one of the group expressions in expr or a null
-    /// expression in null_expr. If groups[i][j] is true, then the the
-    /// j-th expression in the i-th group is NULL, otherwise it is expr[j].
+    /// expression in null_expr. If `groups[i][j]` is true, then the the
+    /// j-th expression in the i-th group is NULL, otherwise it is `expr[j]`.
     groups: Vec<Vec<bool>>,
 }
 
