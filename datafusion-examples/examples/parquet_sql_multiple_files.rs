7179c54a11ad42d975c1a870ec2a5688fab98f13
Andrew Lamb <andrew@nerdnetworks.org>
2022-10-12 16:12:35 -0400

    Run all `datafusion-examples` in CI tests (#3761)
    * Run more examples in CI
    
    * fix compilation
    
    * Do not run tests twice
    
    * Add expr_api to example

--- datafusion-examples/examples/parquet_sql_multiple_files.rs
+++ datafusion-examples/examples/parquet_sql_multiple_files.rs
@@ -56,9 +56,9 @@ async fn main() -> Result<()> {
     // execute the query
     let df = ctx
         .sql(
-            "SELECT int_col, double_col, CAST(date_string_col as VARCHAR) \
-        FROM alltypes_plain \
-        WHERE id > 1 AND tinyint_col < double_col",
+            "SELECT * \
+        FROM my_table \
+        LIMIT 1",
         )
         .await?;
 
