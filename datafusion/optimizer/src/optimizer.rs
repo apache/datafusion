01706541fdb56318a4d4ff7685e017a665a72d23
Heres, Daniel <danielheres@gmail.com>
2022-10-15 17:23:48 +0200

    Just move projection push down

--- datafusion/optimizer/src/optimizer.rs
+++ datafusion/optimizer/src/optimizer.rs
@@ -162,13 +162,13 @@ impl Optimizer {
         rules.push(Arc::new(FilterPushDown::new()));
         rules.push(Arc::new(LimitPushDown::new()));
         rules.push(Arc::new(SingleDistinctToGroupBy::new()));
-        rules.push(Arc::new(ProjectionPushDown::new()));
 
         // The previous optimizations added expressions and projections,
         // that might benefit from the following rules
         rules.push(Arc::new(SimplifyExpressions::new()));
         rules.push(Arc::new(UnwrapCastInComparison::new()));
         rules.push(Arc::new(CommonSubexprEliminate::new()));
+        rules.push(Arc::new(ProjectionPushDown::new()));
 
         Self::with_rules(rules)
     }
