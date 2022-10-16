f8605236727890d8dd42ff4c46cbec789bd5dd39
retikulum <burakcarikci1@gmail.com>
2022-10-16 09:43:05 +0300

    Check for nullability and fix linting

--- datafusion/optimizer/src/simplify_expressions.rs
+++ datafusion/optimizer/src/simplify_expressions.rs
@@ -799,15 +799,18 @@ impl<'a, S: SimplifyInfo> ExprRewriter for Simplifier<'a, S> {
                 left,
                 op: Divide,
                 right,
-            } if is_zero(&left) && is_zero(&right) => Expr::Literal(ScalarValue::Int32(None)),
-            // A / 0 --> DivideByZeroError 
+            } if is_zero(&left) && is_zero(&right) => {
+                Expr::Literal(ScalarValue::Int32(None))
+            }
+            // A / 0 -> DivideByZero Error
             BinaryExpr {
-                left: _,
+                left,
                 op: Divide,
                 right,
-            } if is_zero(&right) => {
+            } if !info.nullable(&left)? && is_zero(&right) => {
                 return Err(DataFusionError::ArrowError(ArrowError::DivideByZero))
             }
+
             //
             // Rules for Modulo
             //
@@ -1192,7 +1195,7 @@ mod tests {
 
         assert_eq!(simplify(expr), expected);
     }
-   
+
     #[test]
     fn test_simplify_divide_zero_by_zero() {
         // 0 / 0 -> null
