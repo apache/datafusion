761e1671bd3c4988d21a38bb19e50bdac6cfaa61
Burak <burakcarikci1@gmail.com>
2022-11-03 16:14:20 +0300

    Improve Error Handling and Readibility for downcasting `StructArray` (#4061)
    * improve error messages for StructArray
    
    * refactor newly added Date32Array downcasting and correct error string
    
    * beautify code
    
    * changes after code review
    
    * fix formatting

--- benchmarks/src/tpch.rs
+++ benchmarks/src/tpch.rs
@@ -16,8 +16,7 @@
 // under the License.
 
 use arrow::array::{
-    Array, ArrayRef, Date32Array, Decimal128Array, Float64Array, Int32Array, Int64Array,
-    StringArray,
+    Array, ArrayRef, Decimal128Array, Float64Array, Int32Array, Int64Array, StringArray,
 };
 use arrow::datatypes::SchemaRef;
 use arrow::record_batch::RecordBatch;
@@ -27,6 +26,7 @@ use std::path::Path;
 use std::sync::Arc;
 use std::time::Instant;
 
+use datafusion::common::cast::as_date32_array;
 use datafusion::common::ScalarValue;
 use datafusion::logical_expr::Cast;
 use datafusion::prelude::*;
@@ -440,7 +440,7 @@ fn col_to_scalar(column: &ArrayRef, row_index: usize) -> ScalarValue {
             ScalarValue::Decimal128(Some(array.value(row_index)), *p, *s)
         }
         DataType::Date32 => {
-            let array = column.as_any().downcast_ref::<Date32Array>().unwrap();
+            let array = as_date32_array(column).unwrap();
             ScalarValue::Date32(Some(array.value(row_index)))
         }
         DataType::Utf8 => {
