a5721c841522947782bb7d791f8088af1ec7c397
jackwener <jakevingoo@gmail.com>
2022-11-15 11:43:23 +0800

    Remove alias in Union

--- datafusion/expr/src/logical_plan/plan.rs
+++ datafusion/expr/src/logical_plan/plan.rs
@@ -1285,8 +1285,6 @@ pub struct Union {
     pub inputs: Vec<Arc<LogicalPlan>>,
     /// Union schema. Should be the same for all inputs.
     pub schema: DFSchemaRef,
-    /// Union output relation alias
-    pub alias: Option<String>,
 }
 
 /// Creates an in memory table.
