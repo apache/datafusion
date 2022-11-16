f605adfce41e314ae97a11701dc2fc7fe1ad2b56
ygf11 <yanggf23@gmail.com>
2022-11-16 05:55:52 -0500

    fix typo

--- datafusion/sql/src/planner.rs
+++ datafusion/sql/src/planner.rs
@@ -5788,7 +5788,7 @@ mod tests {
 
     #[test]
     fn test_one_side_constant_full_join() {
-        // TODO: this sql should transfer to join after
+        // TODO: this sql should be parsed as join after
         // https://github.com/apache/arrow-datafusion/issues/2877 is resolved.
         let sql = "SELECT id, order_id \
             FROM person \
