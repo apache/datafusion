5e36d5b6f8beeb440eccf11b9cdb019aa611882a
zhangli20 <zhangli20@kuaishou.com>
2022-11-20 19:49:58 +0800

    Use tournament loser tree for k-way sort-merging

--- datafusion/core/src/physical_plan/sorts/cursor.rs
+++ datafusion/core/src/physical_plan/sorts/cursor.rs
@@ -109,8 +109,14 @@ impl PartialOrd for SortKeyCursor {
 
 impl Ord for SortKeyCursor {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.current()
-            .cmp(&other.current())
-            .then_with(|| self.stream_idx.cmp(&other.stream_idx))
+        match (self.is_finished(), other.is_finished()) {
+            (true, true) => Ordering::Equal,
+            (_, true) => Ordering::Less,
+            (true, _) => Ordering::Greater,
+            _ => self
+                .current()
+                .cmp(&other.current())
+                .then_with(|| self.stream_idx.cmp(&other.stream_idx)),
+        }
     }
 }
