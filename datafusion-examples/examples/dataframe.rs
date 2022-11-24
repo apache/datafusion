af44be895e3b0006365905efebc080a93b45991b
ALAMSHC <ALAMSHC@c02dq0dsml7h.eu.novartis.net>
2022-11-24 17:17:36 +0100

    Adding more dataframe example to read csv files

--- datafusion-examples/examples/dataframe.rs
+++ datafusion-examples/examples/dataframe.rs
@@ -17,6 +17,9 @@
 
 use datafusion::error::Result;
 use datafusion::prelude::*;
+use datafusion::arrow::datatypes::{DataType, Field, Schema};
+use std::sync::Arc;
+use std::fs;
 
 /// This example demonstrates executing a simple query against an Arrow data source (Parquet) and
 /// fetching results, using the DataFrame trait
@@ -40,4 +43,38 @@ async fn main() -> Result<()> {
     df.show().await?;
 
     Ok(())
+}
+
+// Example to read data from a csv file with inferred schema
+async fn example_read_csv_file_with_inferred_schema() -> Arc<DataFrame> {
+    let path = "example.csv";
+    // Create the data to put into the csv file with headers
+    let content = "id,time,vote,unixtime,rating\na1,\"10 6, 2013\",3,1381017600,5.0\na2,\"08 9, 2013\",2,1376006400,4.5";
+    // write the data
+    fs::write(path, content).expect("Problem with writting file!");
+    // Create a session context and create a lazy
+    let ctx = SessionContext::new();
+    let df = ctx.read_csv(path, CsvReadOptions::default()).await.unwrap();
+    df
+}
+
+// Example to read csv file with a given csv file
+async fn example_read_csv_file_with_schema() -> Arc<DataFrame>{
+    let path = "example.csv";
+    // Create the data to put into the csv file with headers
+    let content = "id,time,vote,unixtime,rating\na1,\"10 6, 2013\",3,1381017600,5.0\na2,\"08 9, 2013\",2,1376006400,4.5";
+    // write the data
+    fs::write(path, content).expect("Problem with writting file!");
+    let ctx = SessionContext::new();
+    let schema = Schema::new(vec![
+        Field::new("id", DataType::Utf8, false),
+        Field::new("time", DataType::Utf8, false),
+        Field::new("vote", DataType::Int32, true),
+        Field::new("unixtime", DataType::Int64, false),
+        Field::new("rating", DataType::Float32, true),
+    ]);
+    let mut csv_read_option = CsvReadOptions::default();
+    csv_read_option.schema = Some(&schema);
+    let df = ctx.read_csv(path, csv_read_option).await.unwrap();
+    df
 }
