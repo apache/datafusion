19fb3baffddd1b74fc3dbb7fbbaafb7c78b98619
Andy Grove <andygrove73@gmail.com>
2022-12-01 15:06:24 -0700

    update versions for test-utils and parquet-test-utils

--- dev/update_datafusion_versions.py
+++ dev/update_datafusion_versions.py
@@ -40,6 +40,8 @@ crates = {
     'datafusion-sql': 'datafusion/sql/Cargo.toml',
     'datafusion-benchmarks': 'benchmarks/Cargo.toml',
     'datafusion-examples': 'datafusion-examples/Cargo.toml',
+    'parquet-test-utils': 'parquet-test-utils/Cargo.toml',
+    'test-utils': 'test-utils/Cargo.toml',
 }
 
 def update_datafusion_version(cargo_toml: str, new_version: str):
