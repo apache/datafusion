e1f8ae2469156d11e7dc20b3fa3fa85822c608ed
Andrew Lamb <andrew@nerdnetworks.org>
2022-12-04 09:00:51 -0500

    RAT

--- datafusion/core/tests/sqllogictests/src/normalize.rs
+++ datafusion/core/tests/sqllogictests/src/normalize.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use std::sync::Arc;
 
 use arrow::{
@@ -7,17 +24,6 @@ use arrow::{
     datatypes::DataType,
     record_batch::RecordBatch,
 };
-
-fn normalize_string(v: Option<&str>) -> Option<&str> {
-    v.map(|v| {
-        // All empty strings are replaced with this value
-        if v.is_empty() {
-            "(empty)"
-        } else {
-            v
-        }
-    })
-}
 
 /// Normalizes the content of a RecordBatch prior to printing.
 ///
@@ -57,4 +63,15 @@ pub fn normalize_batch(batch: RecordBatch) -> RecordBatch {
         .collect();
 
     RecordBatch::try_new(batch.schema(), new_columns).expect("creating normalized batch")
+}
+
+fn normalize_string(v: Option<&str>) -> Option<&str> {
+    v.map(|v| {
+        // All empty strings are replaced with this value
+        if v.is_empty() {
+            "(empty)"
+        } else {
+            v
+        }
+    })
 }
