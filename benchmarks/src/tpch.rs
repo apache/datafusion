684ed57e8500ca5f6a5c235b8f0e52ea83645adb
comphead <comphead@ukr.net>
2022-12-06 08:10:28 -0800

    fix bench

--- benchmarks/src/tpch.rs
+++ benchmarks/src/tpch.rs
@@ -197,13 +197,13 @@ pub fn get_answer_schema(n: usize) -> Schema {
         ]),
 
         8 => Schema::new(vec![
-            Field::new("o_year", DataType::Int32, true),
+            Field::new("o_year", DataType::Float64, true),
             Field::new("mkt_share", DataType::Decimal128(15, 2), true),
         ]),
 
         9 => Schema::new(vec![
             Field::new("nation", DataType::Utf8, true),
-            Field::new("o_year", DataType::Int32, true),
+            Field::new("o_year", DataType::Float64, true),
             Field::new("sum_profit", DataType::Decimal128(15, 2), true),
         ]),
 
