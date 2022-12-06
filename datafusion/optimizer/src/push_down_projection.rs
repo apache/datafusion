de6da48479abd460abf83460db372244640012b8
jackwener <jakevingoo@gmail.com>
2022-12-06 09:03:03 +0800

    resolve problem `multiple_or_predicates`

--- datafusion/optimizer/src/push_down_projection.rs
+++ datafusion/optimizer/src/push_down_projection.rs
@@ -43,9 +43,9 @@ use std::{
 /// Optimizer that removes unused projections and aggregations from plans
 /// This reduces both scans and
 #[derive(Default)]
-pub struct ProjectionPushDown {}
+pub struct PushDownProjection {}
 
-impl OptimizerRule for ProjectionPushDown {
+impl OptimizerRule for PushDownProjection {
     fn optimize(
         &self,
         plan: &LogicalPlan,
@@ -62,11 +62,11 @@ impl OptimizerRule for ProjectionPushDown {
     }
 
     fn name(&self) -> &str {
-        "projection_push_down"
+        "push_down_projection"
     }
 }
 
-impl ProjectionPushDown {
+impl PushDownProjection {
     #[allow(missing_docs)]
     pub fn new() -> Self {
         Self {}
@@ -75,7 +75,7 @@ impl ProjectionPushDown {
 
 /// Recursively transverses the logical plan removing expressions and that are not needed.
 fn optimize_plan(
-    _optimizer: &ProjectionPushDown,
+    _optimizer: &PushDownProjection,
     plan: &LogicalPlan,
     required_columns: &HashSet<Column>, // set of columns required up to this step
     has_projection: bool,
@@ -94,6 +94,9 @@ fn optimize_plan(
 
             let mut new_expr = Vec::new();
             let mut new_fields = Vec::new();
+            // When meet projection, its expr must contain all columns that its child need.
+            // So we need create a empty required_columns instead use original new_required_columns.
+            // Otherwise it cause redundant columns.
             let mut new_required_columns = HashSet::new();
 
             // Gather all columns needed for expressions in this Projection
@@ -107,23 +110,6 @@ fn optimize_plan(
             for e in new_expr.iter() {
                 expr_to_columns(e, &mut new_required_columns)?
             }
-
-            // Gather all columns needed for expressions in this Projection
-            // schema
-            //     .fields()
-            //     .iter()
-            //     .enumerate()
-            //     .try_for_each(|(i, field)| {
-            //         if required_columns.contains(&field.qualified_column()) {
-            //             new_expr.push(expr[i].clone());
-            //             new_fields.push(field.clone());
-            //
-            //             // gather the new set of required columns
-            //             expr_to_columns(&expr[i], &mut new_required_columns)
-            //         } else {
-            //             Ok(())
-            //         }
-            //     })?;
 
             let new_input = optimize_plan(
                 _optimizer,
@@ -1025,7 +1011,7 @@ mod tests {
     }
 
     fn optimize(plan: &LogicalPlan) -> Result<LogicalPlan> {
-        let rule = ProjectionPushDown::new();
+        let rule = PushDownProjection::new();
         rule.optimize(plan, &mut OptimizerConfig::new())
     }
 }
