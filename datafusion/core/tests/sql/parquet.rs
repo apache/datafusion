63a0d75aacf274666e5d3516cd30397d930d7564
yangzhong <yangzhong@ebay.com>
2022-12-14 17:02:25 +0800

    Revert "Temporary remove unsupported ut caused by arrow-rs"
    This reverts commit fc136f618c625d0762c21a58b13840d295a991ba.

--- datafusion/core/tests/sql/parquet.rs
+++ datafusion/core/tests/sql/parquet.rs
@@ -146,10 +146,7 @@ async fn run_query_with_options(options: ListingOptions, num_files: usize) -> St
         .to_string()
 }
 
-#[allow(dead_code)]
-// There's some bug fixed by https://github.com/apache/arrow-rs/pull/3182.
-// Later we'll add this unit test back
-// #[tokio::test]
+#[tokio::test]
 async fn fixed_size_binary_columns() {
     let ctx = SessionContext::new();
     ctx.register_parquet(
