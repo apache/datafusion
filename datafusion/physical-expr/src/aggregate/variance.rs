ea06312b9356c049094aa16aa0e4cac368512158
Jon Mease <jonmmease@gmail.com>
2023-01-07 15:54:41 -0500

    Update variance/stddev to work with single values
    Following Postgres:
     - var/stddev of single element is NULL
     - var_pop/stddev_pop of single element is 0

--- datafusion/physical-expr/src/aggregate/variance.rs
+++ datafusion/physical-expr/src/aggregate/variance.rs
@@ -286,17 +286,17 @@ impl Accumulator for VarianceAccumulator {
             }
         };
 
-        if count <= 1 {
-            return Err(DataFusionError::Internal(
-                "At least two values are needed to calculate variance".to_string(),
-            ));
-        }
-
-        if self.count == 0 {
-            Ok(ScalarValue::Float64(None))
-        } else {
-            Ok(ScalarValue::Float64(Some(self.m2 / count as f64)))
-        }
+        Ok(ScalarValue::Float64(match self.count {
+            0 => None,
+            1 => {
+                if matches!(self.stats_type, StatsType::Population) {
+                    Some(0.0)
+                } else {
+                    None
+                }
+            }
+            _ => Some(self.m2 / count as f64),
+        }))
     }
 
     fn size(&self) -> usize {
