db64842637f341834685f29c12b2893cd50e49d2
Andy Grove <andygrove73@gmail.com>
2023-01-12 15:18:23 -0700

    Add datafusion-substrait crate (#4543)
    * Initial commit
    
    * initial commit
    
    * failing test
    
    * table scan projection
    
    * closer
    
    * test passes, with some hacks
    
    * use DataFrame (#2)
    
    * update README
    
    * update dependency
    
    * code cleanup (#3)
    
    * Add support for Filter operator and BinaryOp expressions (#4)
    
    * GitHub action (#5)
    
    * Split code into producer and consumer modules (#6)
    
    * Support more functions and scalar types (#7)
    
    * Use substrait 0.1 and datafusion 8.0 (#8)
    
    * use substrait 0.1
    
    * use datafusion 8.0
    
    * update datafusion to 10.0 and substrait to 0.2 (#11)
    
    * Add basic join support (#12)
    
    * Added fetch support (#23)
    
    Added fetch to consumer
    
    Added limit to producer
    
    Added unit tests for limit
    
    Added roundtrip_fill_none() for testing when None input can be converted to 0
    
    Update src/consumer.rs
    
    Co-authored-by: Andy Grove <andygrove73@gmail.com>
    
    Co-authored-by: Andy Grove <andygrove73@gmail.com>
    
    * Upgrade to DataFusion 13.0.0 (#25)
    
    * Add sort consumer and producer (#24)
    
    Add consumer
    
    Add producer and test
    
    Modified error string
    
    * Add serializer/deserializer (#26)
    
    * Add plan and function extension support (#27)
    
    * Add plan and function extension support
    
    * Removed unwraps
    
    * Implement GROUP BY (#28)
    
    * Add consumer, producer and tests for aggregate relation
    
    Change function extension registration from absolute to relative anchor
    (reference)
    
    Remove operator to/from reference
    
    * Fixed function registration bug
    
    * Add test
    
    * Addressed PR comments
    
    * Changed field reference from mask to direct reference (#29)
    
    * Changed field reference from masked reference to direct reference
    
    * Handle unsupported case (struct with child)
    
    * Handle SubqueryAlias (#30)
    
    Fixed aggregate function register bug
    
    * Add support for SELECT DISTINCT (#31)
    
    Add test case
    
    * Implement BETWEEN (#32)
    
    * Add case (#33)
    
    * Implement CASE WHEN
    
    * Add more case to test
    
    * Addressed comments
    
    * feat: support explicit catalog/schema names in ReadRel (#34)
    
    * feat: support explicit catalog/schema names in ReadRel
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * fix: use re-exported expr crate
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * move files to subfolder
    
    * RAT
    
    * remove rust.yaml
    
    * revert .gitignore changes
    
    * tomlfmt
    
    * tomlfmt
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    Co-authored-by: Daniël Heres <danielheres@gmail.com>
    Co-authored-by: JanKaul <jankaul@mailbox.org>
    Co-authored-by: nseekhao <37189615+nseekhao@users.noreply.github.com>
    Co-authored-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ datafusion/substrait/src/serializer.rs
@@ -0,0 +1,57 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use crate::producer;
+
+use datafusion::error::Result;
+use datafusion::prelude::*;
+
+use prost::Message;
+use substrait::protobuf::Plan;
+
+use std::fs::OpenOptions;
+use std::io::{Write, Read};
+
+pub async fn serialize(sql: &str, ctx: &SessionContext, path: &str) -> Result<()> {
+    let df = ctx.sql(sql).await?;
+    let plan = df.to_logical_plan()?;
+    let proto = producer::to_substrait_plan(&plan)?;
+
+    let mut protobuf_out = Vec::<u8>::new();
+    proto.encode(&mut protobuf_out).unwrap();
+    let mut file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open(path)?;
+    file.write_all(&protobuf_out)?;
+    Ok(())
+}
+
+pub async fn deserialize(path: &str) -> Result<Box<Plan>> {
+    let mut protobuf_in = Vec::<u8>::new();
+
+    let mut file = OpenOptions::new()
+                .read(true)
+                .open(path)?;
+
+    file.read_to_end(&mut protobuf_in)?;
+    let proto = Message::decode(&*protobuf_in).unwrap();
+
+    Ok(Box::new(proto))
+}
+
+
