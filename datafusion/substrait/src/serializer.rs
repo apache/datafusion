08eca9e0c0159cb4b9f38b47a4f15fca5ff10328
Andy Grove <andygrove73@gmail.com>
2023-01-13 02:09:50 -0700

    Update substrait create to depend on version of DataFusion in the repo (#4879)

--- datafusion/substrait/src/serializer.rs
+++ datafusion/substrait/src/serializer.rs
@@ -24,19 +24,16 @@ use prost::Message;
 use substrait::protobuf::Plan;
 
 use std::fs::OpenOptions;
-use std::io::{Write, Read};
+use std::io::{Read, Write};
 
 pub async fn serialize(sql: &str, ctx: &SessionContext, path: &str) -> Result<()> {
     let df = ctx.sql(sql).await?;
-    let plan = df.to_logical_plan()?;
+    let plan = df.into_optimized_plan()?;
     let proto = producer::to_substrait_plan(&plan)?;
 
     let mut protobuf_out = Vec::<u8>::new();
     proto.encode(&mut protobuf_out).unwrap();
-    let mut file = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .open(path)?;
+    let mut file = OpenOptions::new().create(true).write(true).open(path)?;
     file.write_all(&protobuf_out)?;
     Ok(())
 }
@@ -44,14 +41,10 @@ pub async fn serialize(sql: &str, ctx: &SessionContext, path: &str) -> Result<()
 pub async fn deserialize(path: &str) -> Result<Box<Plan>> {
     let mut protobuf_in = Vec::<u8>::new();
 
-    let mut file = OpenOptions::new()
-                .read(true)
-                .open(path)?;
+    let mut file = OpenOptions::new().read(true).open(path)?;
 
     file.read_to_end(&mut protobuf_in)?;
     let proto = Message::decode(&*protobuf_in).unwrap();
 
     Ok(Box::new(proto))
 }
-
-
