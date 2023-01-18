7fd74217ced936e57d19915f0336ed83434af469
Jeremy Dyer <jdye64@gmail.com>
2023-01-18 13:26:11 -0500

    Add DataFusionError::Substrait variant to DataFusionError enum (#4971)

--- datafusion/common/src/error.rs
+++ datafusion/common/src/error.rs
@@ -86,6 +86,9 @@ pub enum DataFusionError {
     JITError(ModuleError),
     /// Error with additional context
     Context(String, Box<DataFusionError>),
+    /// Errors originating from either mapping LogicalPlans to/from Substrait plans
+    /// or serializing/deserializing protobytes to Substrait plans
+    Substrait(String),
 }
 
 #[macro_export]
@@ -317,6 +320,9 @@ impl Display for DataFusionError {
             }
             DataFusionError::Context(ref desc, ref err) => {
                 write!(f, "{}\ncaused by\n{}", desc, *err)
+            }
+            DataFusionError::Substrait(ref desc) => {
+                write!(f, "Substrait error: {desc}")
             }
         }
     }
