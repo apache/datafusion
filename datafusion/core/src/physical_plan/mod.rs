19f6f19c1d4783f9bcfde83744ee436f8e984154
Marco Neumann <marco@crepererum.net>
2023-01-19 14:32:52 +0000

    refactor: display input partitions for `RepartitionExec` (#4969)
    Closes #4964.

--- datafusion/core/src/physical_plan/mod.rs
+++ datafusion/core/src/physical_plan/mod.rs
@@ -322,7 +322,7 @@ pub fn with_new_children_if_necessary(
 ///   assert_eq!("ProjectionExec: expr=[a@0 as a]\
 ///              \n  CoalesceBatchesExec: target_batch_size=8192\
 ///              \n    FilterExec: a@0 < 5\
-///              \n      RepartitionExec: partitioning=RoundRobinBatch(3)\
+///              \n      RepartitionExec: partitioning=RoundRobinBatch(3), input_partitions=1\
 ///              \n        CsvExec: files={1 group: [[WORKING_DIR/tests/data/example.csv]]}, has_header=true, limit=None, projection=[a]",
 ///               plan_string.trim());
 ///
