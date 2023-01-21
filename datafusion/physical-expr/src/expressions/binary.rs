4e1910854ed83ce0ea42d0d89807a0abe7c576ec
Andrew Lamb <andrew@nerdnetworks.org>
2023-01-21 09:56:08 +0100

    Minor: improve internal error message (#5006)

--- datafusion/physical-expr/src/expressions/binary.rs
+++ datafusion/physical-expr/src/expressions/binary.rs
@@ -1106,7 +1106,7 @@ pub fn binary(
     let rhs_type = &rhs.data_type(input_schema)?;
     if !lhs_type.eq(rhs_type) {
         return Err(DataFusionError::Internal(format!(
-            "The type of {lhs_type} {op} {rhs_type} of binary physical should be same"
+            "The type of {lhs_type} {op:?} {rhs_type} of binary physical should be same"
         )));
     }
     Ok(Arc::new(BinaryExpr::new(lhs, op, rhs)))
