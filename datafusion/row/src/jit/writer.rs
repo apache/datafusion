ca9bdaf3c3e6210561b3a1c895ca51338c44dbff
Andy Grove <andygrove73@gmail.com>
2023-01-26 11:44:17 -0700

    cargo clippy --fix --features pyarrow,jit

--- datafusion/row/src/jit/writer.rs
+++ datafusion/row/src/jit/writer.rs
@@ -139,7 +139,7 @@ fn gen_write_row(schema: &Schema, assembler: &Assembler) -> Result<GeneratedFunc
     let mut b = builder.enter_block();
     for (i, f) in schema.fields().iter().enumerate() {
         let dt = f.data_type();
-        let arr = format!("a{}", i);
+        let arr = format!("a{i}");
         b.declare_as(
             &arr,
             b.call("column", vec![b.id("batch")?, b.lit_i(i as i64)])?,
