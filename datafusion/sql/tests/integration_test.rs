644e9cf4d7cc126beafd7e21ca8dec6748b1c486
Jefffrey <22608443+Jefffrey@users.noreply.github.com>
2023-02-02 08:10:20 +1100

    Ambiguity check for where selection

--- datafusion/sql/tests/integration_test.rs
+++ datafusion/sql/tests/integration_test.rs
@@ -528,6 +528,16 @@ fn join_with_ambiguous_column() {
 }
 
 #[test]
+fn where_selection_with_ambiguous_column() {
+    let sql = "SELECT * FROM person a, person b WHERE id = id + 1";
+    let err = logical_plan(sql).expect_err("query should have failed");
+    assert_eq!(
+        "Internal(\"column reference id is ambiguous\")",
+        format!("{err:?}")
+    );
+}
+
+#[test]
 fn natural_join() {
     let sql = "SELECT * FROM lineitem a NATURAL JOIN lineitem b";
     let expected = "Projection: a.l_item_id, a.l_description, a.price\
