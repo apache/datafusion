8f1b8a2b592c4e5629eeede405ecff77597d2c4e
Vince <vince.vasta@gmail.com>
2023-02-02 14:24:13 +0100

    Add test for unnest logical plan builder.

--- datafusion/expr/src/logical_plan/builder.rs
+++ datafusion/expr/src/logical_plan/builder.rs
@@ -894,12 +894,7 @@ impl LogicalPlanBuilder {
 
     /// Unnest the given column.
     pub fn unnest_column(self, column: impl Into<Column>) -> Result<Self> {
-        let unnest_field = self
-            .plan
-            .schema()
-            .field_from_column(&column.into())?
-            .clone();
-        Ok(Self::from(unnest(self.plan, unnest_field)?))
+        Ok(Self::from(unnest(self.plan, column.into())?))
     }
 }
 
@@ -1185,30 +1180,32 @@ impl TableSource for LogicalTableSource {
 }
 
 /// Create an unnest plan.
-pub fn unnest(input_plan: LogicalPlan, input_field: DFField) -> Result<LogicalPlan> {
+pub fn unnest(input: LogicalPlan, column: Column) -> Result<LogicalPlan> {
+    let unnest_field = input.schema().field_from_column(&column)?;
+
     // Extract the type of the nested field in the list.
-    let unnested_field = match input_field.data_type() {
+    let unnested_field = match unnest_field.data_type() {
         DataType::List(field)
         | DataType::FixedSizeList(field, _)
         | DataType::LargeList(field) => DFField::new(
-            input_field.qualifier().map(String::as_str),
-            input_field.name(),
+            unnest_field.qualifier().map(String::as_str),
+            unnest_field.name(),
             field.data_type().clone(),
-            input_field.is_nullable(),
+            unnest_field.is_nullable(),
         ),
         _ => {
             // If the unnest field is not a list type return the input plan.
-            return Ok(input_plan);
+            return Ok(input);
         }
     };
 
     // Update the schema with the unnest column type changed to contain the nested type.
-    let input_schema = input_plan.schema();
+    let input_schema = input.schema();
     let fields = input_schema
         .fields()
         .iter()
         .map(|f| {
-            if f == &input_field {
+            if f == unnest_field {
                 unnested_field.clone()
             } else {
                 f.clone()
@@ -1222,7 +1219,7 @@ pub fn unnest(input_plan: LogicalPlan, input_field: DFField) -> Result<LogicalPl
     )?);
 
     Ok(LogicalPlan::Unnest(Unnest {
-        input: Arc::new(input_plan),
+        input: Arc::new(input),
         column: unnested_field.qualified_column(),
         schema,
     }))
@@ -1612,5 +1609,78 @@ mod tests {
         assert_eq!(err_msg1.to_string(), expected);
 
         Ok(())
+    }
+
+    #[test]
+    fn plan_builder_unnest() -> Result<()> {
+        // Unnesting a simple column should return the child plan.
+        let plan = nested_table_scan("test_table")?
+            .unnest_column("scalar")?
+            .build()?;
+
+        let expected = "TableScan: test_table";
+        assert_eq!(expected, format!("{plan:?}"));
+
+        // Unnesting the strings list.
+        let plan = nested_table_scan("test_table")?
+            .unnest_column("strings")?
+            .build()?;
+
+        let expected = "\
+        Unnest: test_table.strings\
+        \n  TableScan: test_table";
+        assert_eq!(expected, format!("{plan:?}"));
+
+        // Check unnested field is a scalar
+        let field = plan
+            .schema()
+            .field_with_name(Some("test_table"), "strings")
+            .unwrap();
+        assert_eq!(&DataType::Utf8, field.data_type());
+
+        // Unnesting multiple fields.
+        let plan = nested_table_scan("test_table")?
+            .unnest_column("strings")?
+            .unnest_column("structs")?
+            .build()?;
+
+        let expected = "\
+        Unnest: test_table.structs\
+        \n  Unnest: test_table.strings\
+        \n    TableScan: test_table";
+        assert_eq!(expected, format!("{plan:?}"));
+
+        // Check unnested struct list field should be a struct.
+        let field = plan
+            .schema()
+            .field_with_name(Some("test_table"), "structs")
+            .unwrap();
+        assert!(matches!(field.data_type(), DataType::Struct(_)));
+
+        // Unnesting missing column should fail.
+        let plan = nested_table_scan("test_table")?.unnest_column("missing");
+        assert!(plan.is_err());
+
+        Ok(())
+    }
+
+    fn nested_table_scan(table_name: &str) -> Result<LogicalPlanBuilder> {
+        // Create a schema with a scalar field, a list of strings, and a list of structs.
+        let struct_field = Box::new(Field::new(
+            "item",
+            DataType::Struct(vec![
+                Field::new("a", DataType::UInt32, false),
+                Field::new("b", DataType::UInt32, false),
+            ]),
+            false,
+        ));
+        let string_field = Box::new(Field::new("item", DataType::Utf8, false));
+        let schema = Schema::new(vec![
+            Field::new("scalar", DataType::UInt32, false),
+            Field::new("strings", DataType::List(string_field), false),
+            Field::new("structs", DataType::List(struct_field), false),
+        ]);
+
+        table_scan(Some(table_name), &schema, None)
     }
 }
