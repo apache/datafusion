d969234ddd4eb42e9512e926329b526f1f664fb5
Jeffrey <22608443+Jefffrey@users.noreply.github.com>
2023-02-07 08:14:34 +1100

    Ambiguity check for where selection (#5153)
    * Ambiguity check for where selection
    
    * Fix error types

--- datafusion/sql/tests/integration_test.rs
+++ datafusion/sql/tests/integration_test.rs
@@ -515,7 +515,7 @@ fn select_with_ambiguous_column() {
     let sql = "SELECT id FROM person a, person b";
     let err = logical_plan(sql).expect_err("query should have failed");
     assert_eq!(
-        "Internal(\"column reference id is ambiguous\")",
+        "Plan(\"column reference id is ambiguous\")",
         format!("{err:?}")
     );
 }
@@ -531,6 +531,16 @@ fn join_with_ambiguous_column() {
                         \n    SubqueryAlias: b\
                         \n      TableScan: person";
     quick_test(sql, expected);
+}
+
+#[test]
+fn where_selection_with_ambiguous_column() {
+    let sql = "SELECT * FROM person a, person b WHERE id = id + 1";
+    let err = logical_plan(sql).expect_err("query should have failed");
+    assert_eq!(
+        "Plan(\"column reference id is ambiguous\")",
+        format!("{err:?}")
+    );
 }
 
 #[test]
