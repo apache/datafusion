222205df2e26befa3f7f8d862fefea413fbafc4d
Eduard Karacharov <13005055+korowa@users.noreply.github.com>
2023-02-20 15:59:45 +0300

    datafusion.optimizer.repartition_file_scans enabled by default (#5295)

--- datafusion/common/src/config.rs
+++ datafusion/common/src/config.rs
@@ -284,7 +284,7 @@ config_namespace! {
         /// Currently supported only for Parquet format in which case
         /// multiple row groups from the same file may be read concurrently. If false then each
         /// row group is read serially, though different files may be read in parallel.
-        pub repartition_file_scans: bool, default = false
+        pub repartition_file_scans: bool, default = true
 
         /// Should DataFusion repartition data using the partitions keys to execute window
         /// functions in parallel using the provided `target_partitions` level
