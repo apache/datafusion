e66bcc3fe09ee136df0a6ce4cdc21409cb05a9cc
Denny Britz <dennybritz@gmail.com>
2023-02-27 13:12:59 +0100

    Support Zstd compressed files (#5397)
    * Zstd compression
    
    * datafusion-cli Cargo.lock
    
    * Update datafusion/core/src/datasource/file_format/file_type.rs
    
    Co-authored-by: Liang-Chi Hsieh <viirya@gmail.com>
    
    * Update datafusion/core/src/datasource/file_format/file_type.rs
    
    Co-authored-by: Liang-Chi Hsieh <viirya@gmail.com>
    
    * Update datafusion/core/src/datasource/file_format/file_type.rs
    
    Co-authored-by: Liang-Chi Hsieh <viirya@gmail.com>
    
    ---------
    
    Co-authored-by: Liang-Chi Hsieh <viirya@gmail.com>

--- datafusion/expr/src/logical_plan/plan.rs
+++ datafusion/expr/src/logical_plan/plan.rs
@@ -1591,7 +1591,7 @@ pub struct CreateExternalTable {
     pub if_not_exists: bool,
     /// SQL used to create the table, if available
     pub definition: Option<String>,
-    /// File compression type (GZIP, BZIP2, XZ)
+    /// File compression type (GZIP, BZIP2, XZ, ZSTD)
     pub file_compression_type: CompressionTypeVariant,
     /// Table(provider) specific options
     pub options: HashMap<String, String>,
