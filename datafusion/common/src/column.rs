aeb593f719508973fa4564a32bc2adc767c3713e
Jeffrey <22608443+Jefffrey@users.noreply.github.com>
2023-03-12 22:59:33 +1100

    Enforce ambiguity check whilst normalizing columns (#5509)
    * Enforce ambiguity check whilst normalizing columns
    
    * Update datafusion/expr/src/expr_rewriter.rs
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>
    
    * Fix clippy
    
    ---------
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/common/src/column.rs
+++ datafusion/common/src/column.rs
@@ -102,6 +102,10 @@ impl Column {
     /// In this case, both `t1.id` and `t2.id` will match unqualified column `id`. To express this possibility, use
     /// `using_columns`. Each entry in this array is a set of columns that are bound together via a `USING` clause. So
     /// in this example this would be `[{t1.id, t2.id}]`.
+    #[deprecated(
+        since = "20.0.0",
+        note = "use normalize_with_schemas_and_ambiguity_check instead"
+    )]
     pub fn normalize_with_schemas(
         self,
         schemas: &[&Arc<DFSchema>],
@@ -154,6 +158,105 @@ impl Column {
                 .collect(),
         }))
     }
+
+    /// Qualify column if not done yet.
+    ///
+    /// If this column already has a [relation](Self::relation), it will be returned as is and the given parameters are
+    /// ignored. Otherwise this will search through the given schemas to find the column.
+    ///
+    /// Will check for ambiguity at each level of `schemas`.
+    ///
+    /// A schema matches if there is a single column that -- when unqualified -- matches this column. There is an
+    /// exception for `USING` statements, see below.
+    ///
+    /// # Using columns
+    /// Take the following SQL statement:
+    ///
+    /// ```sql
+    /// SELECT id FROM t1 JOIN t2 USING(id)
+    /// ```
+    ///
+    /// In this case, both `t1.id` and `t2.id` will match unqualified column `id`. To express this possibility, use
+    /// `using_columns`. Each entry in this array is a set of columns that are bound together via a `USING` clause. So
+    /// in this example this would be `[{t1.id, t2.id}]`.
+    ///
+    /// Regarding ambiguity check, `schemas` is structured to allow levels of schemas to be passed in.
+    /// For example:
+    ///
+    /// ```text
+    /// schemas = &[
+    ///    &[schema1, schema2], // first level
+    ///    &[schema3, schema4], // second level
+    /// ]
+    /// ```
+    ///
+    /// Will search for a matching field in all schemas in the first level. If a matching field according to above
+    /// mentioned conditions is not found, then will check the next level. If found more than one matching column across
+    /// all schemas in a level, that isn't a USING column, will return an error due to ambiguous column.
+    ///
+    /// If checked all levels and couldn't find field, will return field not found error.
+    pub fn normalize_with_schemas_and_ambiguity_check(
+        self,
+        schemas: &[&[&DFSchema]],
+        using_columns: &[HashSet<Column>],
+    ) -> Result<Self> {
+        if self.relation.is_some() {
+            return Ok(self);
+        }
+
+        for schema_level in schemas {
+            let fields = schema_level
+                .iter()
+                .flat_map(|s| s.fields_with_unqualified_name(&self.name))
+                .collect::<Vec<_>>();
+            match fields.len() {
+                0 => continue,
+                1 => return Ok(fields[0].qualified_column()),
+                _ => {
+                    // More than 1 fields in this schema have their names set to self.name.
+                    //
+                    // This should only happen when a JOIN query with USING constraint references
+                    // join columns using unqualified column name. For example:
+                    //
+                    // ```sql
+                    // SELECT id FROM t1 JOIN t2 USING(id)
+                    // ```
+                    //
+                    // In this case, both `t1.id` and `t2.id` will match unqualified column `id`.
+                    // We will use the relation from the first matched field to normalize self.
+
+                    // Compare matched fields with one USING JOIN clause at a time
+                    for using_col in using_columns {
+                        let all_matched = fields
+                            .iter()
+                            .all(|f| using_col.contains(&f.qualified_column()));
+                        // All matched fields belong to the same using column set, in orther words
+                        // the same join clause. We simply pick the qualifer from the first match.
+                        if all_matched {
+                            return Ok(fields[0].qualified_column());
+                        }
+                    }
+
+                    // If not due to USING columns then due to ambiguous column name
+                    return Err(DataFusionError::SchemaError(
+                        SchemaError::AmbiguousReference {
+                            qualifier: None,
+                            name: self.name,
+                        },
+                    ));
+                }
+            }
+        }
+
+        Err(DataFusionError::SchemaError(SchemaError::FieldNotFound {
+            field: self,
+            valid_fields: schemas
+                .iter()
+                .flat_map(|s| s.iter())
+                .flat_map(|s| s.fields().iter().map(|f| f.qualified_column()))
+                .collect(),
+        }))
+    }
 }
 
 impl From<&str> for Column {
@@ -187,5 +290,94 @@ impl FromStr for Column {
 impl fmt::Display for Column {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.flat_name())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::DFField;
+    use arrow::datatypes::DataType;
+    use std::collections::HashMap;
+
+    fn create_schema(names: &[(Option<&str>, &str)]) -> Result<DFSchema> {
+        let fields = names
+            .iter()
+            .map(|(qualifier, name)| {
+                DFField::new(qualifier.to_owned(), name, DataType::Boolean, true)
+            })
+            .collect::<Vec<_>>();
+        DFSchema::new_with_metadata(fields, HashMap::new())
+    }
+
+    #[test]
+    fn test_normalize_with_schemas_and_ambiguity_check() -> Result<()> {
+        let schema1 = create_schema(&[(Some("t1"), "a"), (Some("t1"), "b")])?;
+        let schema2 = create_schema(&[(Some("t2"), "c"), (Some("t2"), "d")])?;
+        let schema3 = create_schema(&[
+            (Some("t3"), "a"),
+            (Some("t3"), "b"),
+            (Some("t3"), "c"),
+            (Some("t3"), "d"),
+            (Some("t3"), "e"),
+        ])?;
+
+        // already normalized
+        let col = Column::new(Some("t1"), "a");
+        let col = col.normalize_with_schemas_and_ambiguity_check(&[], &[])?;
+        assert_eq!(col, Column::new(Some("t1"), "a"));
+
+        // should find in first level (schema1)
+        let col = Column::from_name("a");
+        let col = col.normalize_with_schemas_and_ambiguity_check(
+            &[&[&schema1, &schema2], &[&schema3]],
+            &[],
+        )?;
+        assert_eq!(col, Column::new(Some("t1"), "a"));
+
+        // should find in second level (schema3)
+        let col = Column::from_name("e");
+        let col = col.normalize_with_schemas_and_ambiguity_check(
+            &[&[&schema1, &schema2], &[&schema3]],
+            &[],
+        )?;
+        assert_eq!(col, Column::new(Some("t3"), "e"));
+
+        // using column in first level (pick schema1)
+        let mut using_columns = HashSet::new();
+        using_columns.insert(Column::new(Some("t1"), "a"));
+        using_columns.insert(Column::new(Some("t3"), "a"));
+        let col = Column::from_name("a");
+        let col = col.normalize_with_schemas_and_ambiguity_check(
+            &[&[&schema1, &schema3], &[&schema2]],
+            &[using_columns],
+        )?;
+        assert_eq!(col, Column::new(Some("t1"), "a"));
+
+        // not found in any level
+        let col = Column::from_name("z");
+        let err = col
+            .normalize_with_schemas_and_ambiguity_check(
+                &[&[&schema1, &schema2], &[&schema3]],
+                &[],
+            )
+            .expect_err("should've failed to find field");
+        let expected = "Schema error: No field named 'z'. \
+        Valid fields are 't1'.'a', 't1'.'b', 't2'.'c', \
+        't2'.'d', 't3'.'a', 't3'.'b', 't3'.'c', 't3'.'d', 't3'.'e'.";
+        assert_eq!(err.to_string(), expected);
+
+        // ambiguous column reference
+        let col = Column::from_name("a");
+        let err = col
+            .normalize_with_schemas_and_ambiguity_check(
+                &[&[&schema1, &schema3], &[&schema2]],
+                &[],
+            )
+            .expect_err("should've found ambiguous field");
+        let expected = "Schema error: Ambiguous reference to unqualified field 'a'";
+        assert_eq!(err.to_string(), expected);
+
+        Ok(())
     }
 }
