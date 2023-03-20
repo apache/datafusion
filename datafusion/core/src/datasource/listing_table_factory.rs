01e5b80730c702d896471f99aad81b437016f7aa
Metehan Yıldırım <100111937+metesynnada@users.noreply.github.com>
2023-03-20 16:07:33 +0300

    WITH ORDER support on CREATE EXTERNAL TABLE (#5618)
    * Initial implementation
    
    * WITH ORDER impl.
    
    * Minor refactors and comment modifications
    
    * Update parser.rs
    
    * Minor comment update
    
    * Adding tests for cases.
    
    * Adding tests and enhance documentation.
    
    * Fixing merge issues
    
    * Prettier fix
    
    * Update ddl.md
    
    * A minor fix.
    
    ---------
    
    Co-authored-by: Mehmet Ozan Kabak <ozankabak@gmail.com>

--- datafusion/core/src/datasource/listing_table_factory.rs
+++ datafusion/core/src/datasource/listing_table_factory.rs
@@ -124,12 +124,18 @@ impl TableProviderFactory for ListingTableFactory {
             (Some(schema), table_partition_cols)
         };
 
+        let file_sort_order = if cmd.order_exprs.is_empty() {
+            None
+        } else {
+            Some(cmd.order_exprs.clone())
+        };
+
         let options = ListingOptions::new(file_format)
             .with_collect_stat(state.config().collect_statistics())
             .with_file_extension(file_extension)
             .with_target_partitions(state.config().target_partitions())
             .with_table_partition_cols(table_partition_cols)
-            .with_file_sort_order(None);
+            .with_file_sort_order(file_sort_order);
 
         let table_path = ListingTableUrl::parse(&cmd.location)?;
         let resolved_schema = match provided_schema {
