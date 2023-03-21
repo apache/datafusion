cb99a7e1c4b446635a1e4afaa78cf146045b34be
Andrew Lamb <andrew@nerdnetworks.org>
2023-03-21 17:26:42 -0400

    Minor: Add Extensions::new()

--- datafusion/common/src/config.rs
+++ datafusion/common/src/config.rs
@@ -557,10 +557,15 @@ pub trait ExtensionOptions: Send + Sync + std::fmt::Debug + 'static {
 }
 
 /// A type-safe container for [`ConfigExtension`]
-#[derive(Debug, Default, Clone)]
+#[derive(Default, Debug, Clone)]
 pub struct Extensions(BTreeMap<&'static str, ExtensionBox>);
 
 impl Extensions {
+    /// Create a new, empty [`Extensions`]
+    pub fn new() -> Self {
+        Self(BTreeMap::new())
+    }
+
     /// Registers a [`ConfigExtension`] with this [`ConfigOptions`]
     pub fn insert<T: ConfigExtension>(&mut self, extension: T) {
         assert_ne!(T::PREFIX, "datafusion");
