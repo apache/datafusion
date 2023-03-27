32874cd777ee10e1be19c470258b159f2c0b2eee
Łukasz <okragly25@gmail.com>
2023-03-27 22:53:08 +0200

    Move protoc generation to binary crate (#5718) (#5742)
    * Move protoc generation to binary crate (#5718)
    
    * Update license in Cargo.toml
    
    * Generate proto in CI
    
    * Make regen.sh executable by CI

--- datafusion/proto/gen/src/main.rs
+++ datafusion/proto/gen/src/main.rs
@@ -15,42 +15,31 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
 fn main() -> Result<(), String> {
-    // for use in docker build where file changes can be wonky
-    println!("cargo:rerun-if-env-changed=FORCE_REBUILD");
+    let proto_dir = Path::new("proto");
+    let proto_path = Path::new("proto/datafusion.proto");
 
-    // We don't include the proto files in releases so that downstreams
-    // do not need to have PROTOC included
-    if Path::new("proto/datafusion.proto").exists() {
-        println!("cargo:rerun-if-changed=proto/datafusion.proto");
-        build()?
-    }
-
-    Ok(())
-}
-
-fn build() -> Result<(), String> {
-    let out: PathBuf = std::env::var("OUT_DIR")
-        .expect("Cannot find OUT_DIR environment variable")
-        .into();
-    let descriptor_path = out.join("proto_descriptor.bin");
+    // proto definitions has to be there
+    let descriptor_path = proto_dir.join("proto_descriptor.bin");
 
     prost_build::Config::new()
         .file_descriptor_set_path(&descriptor_path)
+        .out_dir("src")
         .compile_well_known_types()
         .extern_path(".google.protobuf", "::pbjson_types")
-        .compile_protos(&["proto/datafusion.proto"], &["proto"])
+        .compile_protos(&[proto_path], &["proto"])
         .map_err(|e| format!("protobuf compilation failed: {e}"))?;
 
     let descriptor_set = std::fs::read(&descriptor_path)
         .unwrap_or_else(|e| panic!("Cannot read {:?}: {}", &descriptor_path, e));
 
     pbjson_build::Builder::new()
+        .out_dir("src")
         .register_descriptors(&descriptor_set)
         .unwrap_or_else(|e| {
             panic!("Cannot register descriptors {:?}: {}", &descriptor_set, e)
@@ -58,8 +47,8 @@ fn build() -> Result<(), String> {
         .build(&[".datafusion"])
         .map_err(|e| format!("pbjson compilation failed: {e}"))?;
 
-    let prost = out.join("datafusion.rs");
-    let pbjson = out.join("datafusion.serde.rs");
+    let prost = Path::new("src/datafusion.rs");
+    let pbjson = Path::new("src/datafusion.serde.rs");
 
     std::fs::copy(prost, "src/generated/prost.rs").unwrap();
     std::fs::copy(pbjson, "src/generated/pbjson.rs").unwrap();
