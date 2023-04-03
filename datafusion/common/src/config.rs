331851b5904652b5ffe7c101e8c101cbd03095d9
metesynnada <100111937+metesynnada@users.noreply.github.com>
2023-04-03 18:56:11 +0300

    Update config.rs

--- datafusion/common/src/config.rs
+++ datafusion/common/src/config.rs
@@ -282,6 +282,19 @@ config_namespace! {
 
         /// Should DataFusion allow symmetric hash joins for unbounded data sources even when
         /// its inputs do not have any ordering or filtering
+        ///
+        /// If the flag is not enabled, the SymmetricHashJoin operator will be unable to prune its internal buffers,
+        /// resulting in certain join types
+        ///     - [JoinType::Full],
+        ///     - [JoinType::Left]
+        ///     - [JoinType::LeftAnti]
+        ///     - [JoinType::LeftSemi]
+        ///     - [JoinType::Right]
+        ///     - [JoinType::RightAnti]
+        ///     - [JoinType::RightSemi]
+        /// being produced only at the end of the execution. This is not typical in stream processing.
+        /// Additionally, without proper design for long runner execution, all types of joins may
+        /// encounter out-of-memory errors.
         pub allow_symmetric_joins_without_pruning: bool, default = true
 
         /// When set to true, file groups will be repartitioned to achieve maximum parallelism.
