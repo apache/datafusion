e7b4eac693d84c536cf635361c674a44101cab2d
Mustafa Akur <106137913+mustafasrepo@users.noreply.github.com>
2023-04-27 17:13:01 +0300

    Implement Streaming Aggregation: Do not break pipeline in aggregation if group by columns are ordered (V2) (#6124)
    * add starting code for experimenting
    
    * stream group by linear implementation
    
    * sorted implementation
    
    * minor changes
    
    * simplifications
    
    * Simplifications
    
    * convert vec to Option
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * simplifications
    
    * minor changes
    
    * all tests pass
    
    * refactor
    
    * simplifications
    
    * remove unnecessary code
    
    * simplifications
    
    * minor changes
    
    * simplifications
    
    * minor changes
    
    * Simplify the GroupByOrderMode type
    
    * Address reviews
    
    * separate fully ordered case and remaining cases
    
    * change test data type
    
    * address reviews
    
    * Convert to option
    
    * retract back to old API.
    
    * Code quality: stylistic changes
    
    * Separate bounded stream and hash stream
    
    * Update comments
    
    ---------
    
    Co-authored-by: Mehmet Ozan Kabak <ozankabak@gmail.com>

--- datafusion/core/src/physical_plan/coalesce_batches.rs
+++ datafusion/core/src/physical_plan/coalesce_batches.rs
@@ -96,7 +96,7 @@ impl ExecutionPlan for CoalesceBatchesExec {
     }
 
     /// Specifies whether this plan generates an infinite stream of records.
-    /// If the plan does not support pipelining, but it its input(s) are
+    /// If the plan does not support pipelining, but its input(s) are
     /// infinite, returns an error to indicate this.
     fn unbounded_output(&self, children: &[bool]) -> Result<bool> {
         Ok(children[0])
