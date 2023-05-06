607702ebbda48f444a9cdebaad3634460d6c3a24
parkma99 <park-ma@hotmail.com>
2023-05-06 15:38:50 +0800

    Port tests in unicode.rs to sqllogictest

--- datafusion/core/tests/sql/mod.rs
+++ datafusion/core/tests/sql/mod.rs
@@ -106,8 +106,6 @@ pub mod parquet_schema;
 pub mod partitioned_csv;
 pub mod set_variable;
 pub mod subqueries;
-#[cfg(feature = "unicode_expressions")]
-pub mod unicode;
 
 fn assert_float_eq<T>(expected: &[Vec<T>], received: &[Vec<String>])
 where
