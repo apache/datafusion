ad475f920dd040da00b4beec321ee23721434848
Jon Mease <jonmmease@gmail.com>
2023-05-11 08:19:00 -0400

    Remove commented test

--- datafusion/core/src/datasource/file_format/arrow.rs
+++ datafusion/core/src/datasource/file_format/arrow.rs
@@ -73,38 +73,3 @@ fn read_arrow_schema_from_reader<R: Read + Seek>(reader: R) -> Result<SchemaRef>
     let reader = FileReader::try_new(reader, None)?;
     Ok(reader.schema())
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use arrow::datatypes::DataType;
-//     use crate::datasource::file_format::FileFormat;
-//
-//     use super::ArrowFormat;
-//
-//     #[tokio::test]
-//     async fn test_schema() {
-//         let filename = "tests/example.arrow";
-//         let format = ArrowFormat {};
-//         let file_schema = format
-//             .infer_schema(local_object_reader_stream(vec![filename.to_owned()]))
-//             .await
-//             .expect("Schema inference");
-//         assert_eq!(
-//             vec!["f0", "f1", "f2"],
-//             file_schema
-//                 .fields()
-//                 .iter()
-//                 .map(|x| x.name().clone())
-//                 .collect::<Vec<String>>()
-//         );
-//
-//         assert_eq!(
-//             vec![DataType::Int64, DataType::Utf8, DataType::Boolean],
-//             file_schema
-//                 .fields()
-//                 .iter()
-//                 .map(|x| x.data_type().clone())
-//                 .collect::<Vec<DataType>>()
-//         );
-//     }
-// }
