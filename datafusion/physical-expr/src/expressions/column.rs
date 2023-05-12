b258966075c029315aa9263355342780c93e0a09
berkaysynnada <berkay.sahin@synnada.ai>
2023-05-12 15:56:46 +0300

    Unused function is removed

--- datafusion/physical-expr/src/expressions/column.rs
+++ datafusion/physical-expr/src/expressions/column.rs
@@ -60,11 +60,6 @@ impl Column {
     pub fn index(&self) -> usize {
         self.index
     }
-
-    // Set the column index
-    pub fn set_index(&mut self, index: usize) {
-        self.index = index;
-    }
 }
 
 impl std::fmt::Display for Column {
