f742b87b6e027bbe58048e6cd2f58c3bf5bb3f2f
Louis Gariépy <louis.gariepy.42@gmail.com>
2023-05-29 17:42:36 -0400

    Fix typo.

--- datafusion/core/src/catalog/mod.rs
+++ datafusion/core/src/catalog/mod.rs
@@ -18,7 +18,7 @@
 //! This module contains interfaces and default implementations
 //! of table namespacing concepts, including catalogs and schemas.
 
-// TODO(clippy): Having a `datasource::datasource` module path is unclear and ambiguous.
+// TODO(clippy): Having a `catalog::catalog` module path is unclear and ambiguous.
 // The parent module should probably be renamed to something that more accurately
 // describes its content. Something along the lines of `database_meta`, `metadata`
 // or `meta`, perhaps?
