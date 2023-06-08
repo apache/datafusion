442b3bca6234976c96db6084f48c97c4c20e4f56
Andrew Lamb <andrew@nerdnetworks.org>
2023-06-08 09:56:01 -0400

    Minor: Remove some `use crate::` uses in physical_plan (#6573)
    * Update `use crate::error` to `use datafusion_common` in physical_plan
    
    * More updates

--- datafusion/core/src/physical_plan/sorts/sort.rs
+++ datafusion/core/src/physical_plan/sorts/sort.rs
@@ -19,7 +19,6 @@
 //! It will do in-memory sorting if it has enough memory budget
 //! but spills to disk if needed.
 
-use crate::error::{DataFusionError, Result};
 use crate::physical_plan::common::{batch_byte_size, spawn_buffered, IPCWriter};
 use crate::physical_plan::expressions::PhysicalSortExpr;
 use crate::physical_plan::metrics::{
@@ -36,6 +35,7 @@ use arrow::compute::{concat_batches, lexsort_to_indices, take};
 use arrow::datatypes::SchemaRef;
 use arrow::ipc::reader::FileReader;
 use arrow::record_batch::RecordBatch;
+use datafusion_common::{DataFusionError, Result};
 use datafusion_execution::memory_pool::{
     human_readable_size, MemoryConsumer, MemoryReservation,
 };
