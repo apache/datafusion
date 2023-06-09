cdf527fa3f0ef53fbdcc3963df6e66e4dd6799ec
jayzhan211 <jayzhan211@gmail.com>
2023-06-09 16:00:35 +0800

    Internal cast for array()
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- datafusion/expr/src/type_coercion/mod.rs
+++ datafusion/expr/src/type_coercion/mod.rs
@@ -36,6 +36,8 @@ pub mod binary;
 pub mod functions;
 pub mod other;
 
+pub use binary::comparison_coercion;
+
 use arrow::datatypes::DataType;
 /// Determine whether the given data type `dt` represents signed numeric values.
 pub fn is_signed_numeric(dt: &DataType) -> bool {
