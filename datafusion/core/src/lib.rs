e8a438df492638d4f950da1f7261a333f7aeba84
Andrew Lamb <andrew@nerdnetworks.org>
2023-06-12 06:28:47 -0400

    Fix doc link

--- datafusion/core/src/lib.rs
+++ datafusion/core/src/lib.rs
@@ -135,8 +135,8 @@
 //! DataFusion is designed to be a "disaggregated" query engine.  This
 //! means that developers can mix and extend the parts of DataFusion
 //! they need for their usecase. For example, just the
-//! [`ExecutionEngine`] runtime, or just the [`SqlToRel`] SQL planner
-//! and optimizer.
+//! [`ExecutionPlan`] operators, or the [`SqlToRel`] SQL planner and
+//! optimizer.
 //!
 //! In order to achieve this, DataFusion supports extension at many points:
 //!
