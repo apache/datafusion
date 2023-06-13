eb9abec6c933bc19b733cddc25982798d906dbb5
Andrew Lamb <andrew@nerdnetworks.org>
2023-06-13 08:37:47 -0400

    minor: use sql to setup test data for joins.slt rather than rust (#6656)
    * port register_right_semi_anti_join_table to sql
    
    * port register_left_semi_anti_join_table
    
    * port register_timestamps_table
    
    * Port register_hashjoin_datatype_table to sql
    
    * fix comment

--- datafusion/core/tests/sqllogictests/src/main.rs
+++ datafusion/core/tests/sqllogictests/src/main.rs
@@ -286,11 +286,7 @@ async fn context_for_test_file(relative_path: &Path) -> Option<TestContext> {
             }
         }
         "joins.slt" => {
-            info!("Registering timestamps tables");
-            setup::register_timestamps_table(test_ctx.session_ctx()).await;
-            setup::register_hashjoin_datatype_table(test_ctx.session_ctx()).await;
-            setup::register_left_semi_anti_join_table(test_ctx.session_ctx()).await;
-            setup::register_right_semi_anti_join_table(test_ctx.session_ctx()).await;
+            info!("Registering partition table tables");
 
             let mut test_ctx = test_ctx;
             setup::register_partition_table(&mut test_ctx).await;
