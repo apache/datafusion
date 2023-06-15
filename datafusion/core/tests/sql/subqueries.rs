5bd747cc816913f3d8049071ff69d01a0bc216b2
jiangzhx <jiangzhx@gmail.com>
2023-06-15 19:17:27 +0800

    port aggregated_correlated_scalar_subquery_with_extra_group_by_columns to sql

--- datafusion/core/tests/sql/subqueries.rs
+++ datafusion/core/tests/sql/subqueries.rs
@@ -22,24 +22,6 @@ use datafusion::prelude::SessionContext;
 use log::debug;
 
 #[tokio::test]
-async fn aggregated_correlated_scalar_subquery_with_extra_group_by_columns() -> Result<()>
-{
-    let ctx = create_join_context("t1_id", "t2_id", true)?;
-
-    let sql = "SELECT t1_id, (SELECT sum(t2_int) FROM t2 WHERE t2.t2_id = t1.t1_id group by t2_name) as t2_sum from t1";
-    let msg = format!("Creating logical plan for '{sql}'");
-    let dataframe = ctx.sql(sql).await.expect(&msg);
-    let err = dataframe.into_optimized_plan().err().unwrap();
-
-    assert_eq!(
-        r#"Context("check_analyzed_plan", Plan("A GROUP BY clause in a scalar correlated subquery cannot contain non-correlated columns"))"#,
-        &format!("{err:?}")
-    );
-
-    Ok(())
-}
-
-#[tokio::test]
 async fn support_agg_correlated_columns() -> Result<()> {
     let ctx = create_join_context("t1_id", "t2_id", true)?;
 
