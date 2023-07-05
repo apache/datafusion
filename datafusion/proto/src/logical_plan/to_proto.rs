e044b5ca9418d7a85226b7dae3a58ce0cdcd1e0d
Jay Zhan <jayzhan211@gmail.com>
2023-07-06 01:35:20 +0800

    Support fixed_size_list for make_array (#6759)
    * support make_array for fixed_size_list
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * add arrow-typeof in test
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix schema mismatch
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup code
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * create array data with correct len
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    ---------
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- datafusion/proto/src/logical_plan/to_proto.rs
+++ datafusion/proto/src/logical_plan/to_proto.rs
@@ -1068,6 +1068,10 @@ impl TryFrom<&ScalarValue> for protobuf::ScalarValue {
                     Value::LargeUtf8Value(s.to_owned())
                 })
             }
+            ScalarValue::Fixedsizelist(..) => Err(Error::General(
+                "Proto serialization error: ScalarValue::Fixedsizelist not supported"
+                    .to_string(),
+            )),
             ScalarValue::List(values, boxed_field) => {
                 let is_null = values.is_none();
 
