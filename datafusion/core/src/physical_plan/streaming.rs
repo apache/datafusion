edecdc1ea67b5199b33139be0ef05161cb53a47c
Andrew Lamb <andrew@nerdnetworks.org>
2023-07-07 11:04:16 -0400

    Fix (another) logical conflict

--- datafusion/core/src/physical_plan/streaming.rs
+++ datafusion/core/src/physical_plan/streaming.rs
@@ -28,11 +28,12 @@ use datafusion_common::{DataFusionError, Result, Statistics};
 use datafusion_physical_expr::{LexOrdering, PhysicalSortExpr};
 use log::debug;
 
+use crate::datasource::physical_plan::{OutputOrderingDisplay, ProjectSchemaDisplay};
 use crate::physical_plan::stream::RecordBatchStreamAdapter;
 use crate::physical_plan::{ExecutionPlan, Partitioning, SendableRecordBatchStream};
 use datafusion_execution::TaskContext;
 
-use super::DisplayAs;
+use super::{DisplayAs, DisplayFormatType};
 
 /// A partition that can be converted into a [`SendableRecordBatchStream`]
 pub trait PartitionStream: Send + Sync {
