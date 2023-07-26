574ee9e5757d73fb2bac934f8c44c57599f4f726
Andrew Lamb <andrew@nerdnetworks.org>
2023-07-26 16:03:10 -0400

    fix clippy

--- datafusion/core/tests/sqllogictests/src/main.rs
+++ datafusion/core/tests/sqllogictests/src/main.rs
@@ -273,11 +273,11 @@ async fn context_for_test_file(relative_path: &Path) -> Option<TestContext> {
         }
         "information_schema_table_types.slt" => {
             info!("Registering local temporary table");
-            setup::register_temp_table(&test_ctx.session_ctx()).await;
+            setup::register_temp_table(test_ctx.session_ctx()).await;
         }
         "information_schema_columns.slt" => {
             info!("Registering table with many types");
-            setup::register_table_with_many_types(&test_ctx.session_ctx()).await;
+            setup::register_table_with_many_types(test_ctx.session_ctx()).await;
         }
         "avro.slt" => {
             #[cfg(feature = "avro")]
