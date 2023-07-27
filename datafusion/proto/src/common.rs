d27847b7cb93379494e52e667e48dc6109c65315
jakevin <jakevingoo@gmail.com>
2023-07-27 13:57:49 +0800

    use pub(crate)

--- datafusion/proto/src/common.rs
+++ datafusion/proto/src/common.rs
@@ -26,7 +26,7 @@ pub(crate) fn str_to_byte(s: &String, description: &str) -> Result<u8> {
     Ok(s.as_bytes()[0])
 }
 
-pub fn byte_to_string(b: u8, description: &str) -> Result<String> {
+pub(crate) fn byte_to_string(b: u8, description: &str) -> Result<String> {
     let b = &[b];
     let b = std::str::from_utf8(b).map_err(|_| {
         DataFusionError::Internal(format!(
