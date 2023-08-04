de94ee4356be6ab7c16b4770211d0d58afa7c926
izveigor <izveigor@gmail.com>
2023-08-04 22:17:12 +0300

    fix: proto

--- datafusion/proto/src/logical_plan/from_proto.rs
+++ datafusion/proto/src/logical_plan/from_proto.rs
@@ -37,11 +37,11 @@ use datafusion_common::{
 use datafusion_expr::expr::{Alias, Placeholder};
 use datafusion_expr::{
     abs, acos, acosh, array, array_append, array_concat, array_dims, array_element,
-    array_fill, array_has, array_has_all, array_has_any, array_length, array_ndims,
-    array_position, array_positions, array_prepend, array_remove, array_remove_all,
-    array_remove_n, array_repeat, array_replace, array_replace_all, array_replace_n,
-    array_slice, array_to_string, ascii, asin, asinh, atan, atan2, atanh, bit_length,
-    btrim, cardinality, cbrt, ceil, character_length, chr, coalesce, concat_expr,
+    array_has, array_has_all, array_has_any, array_length, array_ndims, array_position,
+    array_positions, array_prepend, array_remove, array_remove_all, array_remove_n,
+    array_repeat, array_replace, array_replace_all, array_replace_n, array_slice,
+    array_to_string, ascii, asin, asinh, atan, atan2, atanh, bit_length, btrim,
+    cardinality, cbrt, ceil, character_length, chr, coalesce, concat_expr,
     concat_ws_expr, cos, cosh, cot, current_date, current_time, date_bin, date_part,
     date_trunc, degrees, digest, exp,
     expr::{self, InList, Sort, WindowFunction},
