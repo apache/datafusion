d1a0732f9e300add1d3408c76dbb7f46f15b4e7e
Andrew Lamb <andrew@nerdnetworks.org>
2023-08-15 06:28:16 -0400

    clippy

--- datafusion/sqllogictest/src/lib.rs
+++ datafusion/sqllogictest/src/lib.rs
@@ -15,7 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
-///! DataFusion sqllogictest driver
+//! DataFusion sqllogictest driver
+
 mod engines;
 
 pub use engines::DataFusion;
