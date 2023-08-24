8a714eef61fe1f9f0db2bea7c7940ba8c29d0680
zhongjingxiong <zhongjingxiong@bytedance.com>
2023-08-24 19:47:20 +0800

    Unnecessary to list all files during partition pruning

--- datafusion/core/src/datasource/listing/helpers.rs
+++ datafusion/core/src/datasource/listing/helpers.rs
@@ -321,11 +321,10 @@ pub async fn pruned_partition_list<'a>(
     file_extension: &'a str,
     partition_cols: &'a [(String, DataType)],
 ) -> Result<BoxStream<'a, Result<PartitionedFile>>> {
-    let list = table_path.list_all_files(store, file_extension);
 
     // if no partition col => simply list all the files
     if partition_cols.is_empty() {
-        return Ok(Box::pin(list.map_ok(|object_meta| object_meta.into())));
+        return Ok(Box::pin(table_path.list_all_files(store, file_extension).map_ok(|object_meta| object_meta.into())));
     }
 
     let partitions = list_partitions(store, table_path, partition_cols.len()).await?;
