ad512f07faab14bc758ec90abfaf5c22faa684c5
link-bands <ireallyagreewithyou@gmail.com>
2023-10-18 12:57:23 +0000

    lower&lpad

--- extension/functions/presto/src/lib.rs
+++ extension/functions/presto/src/lib.rs
@@ -1,8 +1,8 @@
-use arrow::array::{ArrayRef, Float64Array};
-use arrow::datatypes::DataType;
+use arrow::array::{ArrayRef, Float64Array, StringArray, Array};
+use arrow::datatypes::{DataType, Int32Type};
 use datafusion::error::Result;
 use datafusion::logical_expr::Volatility;
-use datafusion_common::cast::as_float64_array;
+use datafusion_common::cast::{as_float64_array, as_string_array, as_primitive_array};
 use datafusion_expr::{
     ReturnTypeFunction, ScalarFunctionDef, ScalarFunctionPackage, Signature,
 };
@@ -70,12 +70,97 @@ impl ScalarFunctionDef for MultiplyTwoFunction {
     }
 }
 
+#[derive(Debug)]
+pub struct LowerFunction;
+
+impl ScalarFunctionDef for LowerFunction{
+    fn name(&self) -> &str {
+        "lower"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::exact(vec![DataType::Utf8], Volatility::Immutable)
+    }
+
+    fn return_type(&self) -> ReturnTypeFunction {
+        let return_type = Arc::new(DataType::Utf8);
+        Arc::new(move |_| Ok(return_type.clone()))
+    }
+
+    fn execute(&self, args: &[ArrayRef]) -> Result<ArrayRef> {
+        assert_eq!(args.len(), 1);
+        let input = as_string_array(&args[0]).expect("cast failed");
+        let array = input
+            .iter()
+            .map(|value| match value {
+                Some(value) => Some(value.to_lowercase()),
+                _ => None,
+            })
+            .collect::<StringArray>();
+        Ok(Arc::new(array) as ArrayRef)
+    }
+}
+
+#[derive(Debug)]
+pub struct LpadFunction;
+
+impl ScalarFunctionDef for LpadFunction{
+    fn name(&self)->&str{
+        "lpad"
+    }
+
+    fn signature(&self)->Signature{
+        Signature::exact(vec![DataType::Utf8,DataType::Int64,DataType::Utf8], Volatility::Immutable)
+    }
+
+    fn return_type(&self) -> ReturnTypeFunction {
+        let return_type = Arc::new(DataType::Utf8);
+        Arc::new(move |_| Ok(return_type.clone()))
+    }
+
+    fn execute(&self, args: &[ArrayRef]) -> Result<ArrayRef> {
+        assert_eq!(args.len(), 3);
+
+        let string_array = as_string_array(&args[0]).expect("cast failed");
+        let size_array = as_primitive_array::<Int32Type>(&args[1]).expect("cast failed");
+        let padstring_array = as_string_array(&args[2]).expect("cast failed");
+
+        let string_values = string_array.values();
+        let size_values = size_array.values();
+        let padstring_values = padstring_array.values();
+
+        let array = (0..string_array.len()).map(|i| {
+            let string = string_values.get(i).map(|s| s.to_string());
+            let size = size_values.get(i).map(|&size| size as usize);
+            let padstring = padstring_values.get(i).map(|s| s.to_string());
+
+            match (string, size, padstring) {
+                (Some(string), Some(size), Some(padstring)) => {
+                    let padded_string = if string.len() < size {
+                        let pad_count = size - string.len();
+                        let pads = padstring.repeat(pad_count);
+                        let truncated_pads = &pads[..pad_count];
+                        format!("{}{}", truncated_pads, string)
+                    } else {
+                        string[..size].to_string()
+                    };
+                    Some(padded_string)
+                }
+                _ => None,
+            }
+        }).collect::<StringArray>();
+
+        Ok(Arc::new(array) as ArrayRef)
+    }
+}
+
+
 // Function package declaration
 pub struct TestFunctionPackage;
 
 impl ScalarFunctionPackage for TestFunctionPackage {
     fn functions(&self) -> Vec<Box<dyn ScalarFunctionDef>> {
-        vec![Box::new(AddOneFunction), Box::new(MultiplyTwoFunction)]
+        vec![Box::new(AddOneFunction), Box::new(MultiplyTwoFunction),Box::new(LowerFunction)]
     }
 }
 
@@ -149,11 +234,22 @@ mod test {
         test_expression!("add_one(-1)", "0.0");
         Ok(())
     }
-
-    #[tokio::test]
+     #[tokio::test]
     async fn test_multiply_two() -> Result<()> {
         test_expression!("multiply_two(1)", "2.0");
         test_expression!("multiply_two(-1)", "-2.0");
+        Ok(())
+    }
+    #[tokio::test]
+    async fn test_lower() -> Result<()> {
+        test_expression!("lower('ABc')", "abc");
+        test_expression!("lower('QWE')", "qwe");
+        Ok(())
+    }   
+    #[tokio::test]
+    async fn test_lpad() ->Result<()>{
+        test_expression!("lpad('hello',4,'rust')","hell");
+        test_expression!("lpad('bc',5,'a')","aaabc");
         Ok(())
     }
 }
