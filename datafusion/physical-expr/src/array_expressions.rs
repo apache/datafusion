2f7f5681780bc46f9a3e99d5c248cc36113432b5
veeupup <code@tanweime.com>
2023-11-11 00:36:03 +0800

    reimplemented array_append with MutableArrayData
    Signed-off-by: veeupup <code@tanweime.com>

--- datafusion/physical-expr/src/array_expressions.rs
+++ datafusion/physical-expr/src/array_expressions.rs
@@ -587,44 +587,48 @@ pub fn array_append(args: &[ArrayRef]) -> Result<ArrayRef> {
     let res = match arr.value_type() {
         DataType::List(_) => concat_internal(args)?,
         DataType::Null => return make_array(&[element.to_owned()]),
-        dt => {
-            let converter = RowConverter::new(vec![SortField::new(dt.clone())])?;
-            let r_rows = converter.convert_columns(&[element.clone()])?;
+        data_type => {
+            let mut new_values = vec![];
             let mut offsets = vec![0];
-            let mut new_arrays = vec![];
-            for (i, arr) in arr.iter().enumerate() {
-                let r_row = r_rows.row(i);
-                let rows = if let Some(arr) = arr {
-                    let mut l_rows = converter.convert_columns(&[arr])?;
-                    l_rows.push(r_row);
-                    l_rows
+
+            let elem_data = element.to_data();
+            for (row_index, arr) in arr.iter().enumerate() {
+                let new_array = if let Some(arr) = arr {
+                    let original_data = arr.to_data();
+                    let capacity = Capacities::Array(original_data.len() + 1);
+                    let mut mutable = MutableArrayData::with_capacities(
+                        vec![&original_data, &elem_data],
+                        false,
+                        capacity,
+                    );
+                    mutable.extend(0, 0, original_data.len());
+                    mutable.extend(1, row_index, row_index + 1);
+                    let data = mutable.freeze();
+                    arrow_array::make_array(data)
                 } else {
-                    let mut rows = converter.empty_rows(1, 1);
-                    rows.push(r_row);
-                    rows
+                    let capacity = Capacities::Array(1);
+                    let mut mutable = MutableArrayData::with_capacities(
+                        vec![&elem_data],
+                        false,
+                        capacity,
+                    );
+                    mutable.extend(0, row_index, row_index + 1);
+                    let data = mutable.freeze();
+                    arrow_array::make_array(data)
                 };
-                let last_offset: i32 = match offsets.last().copied() {
-                    Some(offset) => offset,
-                    None => return internal_err!("offsets should not be empty"),
-                };
-                offsets.push(last_offset + rows.num_rows() as i32);
-                let arrays = converter.convert_rows(rows.iter())?;
-                let array = match arrays.get(0) {
-                    Some(array) => array.clone(),
-                    None => {
-                        return internal_err!(
-                            "array_append: failed to get value from rows"
-                        )
-                    }
-                };
-                new_arrays.push(array);
+                offsets.push(offsets[row_index] + new_array.len() as i32);
+                new_values.push(new_array);
             }
-            let field = Arc::new(Field::new("item", dt, true));
-            let offsets = OffsetBuffer::new(offsets.into());
-            let new_arrays_ref =
-                new_arrays.iter().map(|v| v.as_ref()).collect::<Vec<_>>();
-            let values = compute::concat(&new_arrays_ref)?;
-            Arc::new(ListArray::try_new(field, offsets, values, None)?)
+
+            let new_values: Vec<_> = new_values.iter().map(|a| a.as_ref()).collect();
+            let values = arrow::compute::concat(&new_values)?;
+
+            Arc::new(ListArray::try_new(
+                Arc::new(Field::new("item", data_type.to_owned(), true)),
+                OffsetBuffer::new(offsets.into()),
+                values,
+                None,
+            )?)
         }
     };
 
