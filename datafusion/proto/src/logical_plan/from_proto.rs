d1ebff21440f5e1d04355f94381ecca39ca03e3a
cli2 <ss_leechan@pku.edu.cn>
2023-11-10 17:09:27 +0800

    code format

--- datafusion/proto/src/logical_plan/from_proto.rs
+++ datafusion/proto/src/logical_plan/from_proto.rs
@@ -55,7 +55,7 @@ use datafusion_expr::{
     round, rpad, rtrim, sha224, sha256, sha384, sha512, signum, sin, sinh, split_part,
     sqrt, starts_with, strpos, substr, substring, tan, tanh, to_hex, to_timestamp_micros,
     to_timestamp_millis, to_timestamp_nanos, to_timestamp_seconds, translate, trim,
-    trunc, upper, uuid
+    trunc, upper, uuid,
     window_frame::regularize,
     AggregateFunction, Between, BinaryExpr, BuiltInWindowFunction, BuiltinScalarFunction,
     Case, Cast, Expr, GetFieldAccess, GetIndexedField, GroupingSet,
