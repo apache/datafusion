6f04551546ebe1869c8595c0469d350f2d7d1b15
Andrew Lamb <andrew@nerdnetworks.org>
2023-11-14 16:23:20 -0500

    fix: use inexact

--- datafusion/common/src/stats.rs
+++ datafusion/common/src/stats.rs
@@ -152,11 +152,13 @@ impl Precision<usize> {
         }
     }
 
-    /// Return the estimate of applying a filter of selectivity `selectivity` to
-    /// this Precision. A selectivity of `1.0` means that all rows are selected.
-    /// A selectivity of `0.5` means half the rows are selected.
+    /// Return the estimate of applying a filter with estimated selectivity
+    /// `selectivity` to this Precision. A selectivity of `1.0` means that all
+    /// rows are selected. A selectivity of `0.5` means half the rows are
+    /// selected. Will always return inexact statistics.
     pub fn apply_filter(self, selectivity: f64) -> Self {
         self.map(|v| ((v as f64 * selectivity).ceil()) as usize)
+            .to_inexact()
     }
 }
 
