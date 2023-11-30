cb3341f8b9dfc737d985ac97b3230c73ddc72aff
Daniël Heres <danielheres@gmail.com>
2023-11-30 17:53:11 +0000

    Add pool_size method to MemoryPool (#218)
    * Add pool_size method to MemoryPool
    
    * Fix
    
    * Fmt

--- datafusion/execution/src/memory_pool/mod.rs
+++ datafusion/execution/src/memory_pool/mod.rs
@@ -75,6 +75,9 @@ pub trait MemoryPool: Send + Sync + std::fmt::Debug {
 
     /// Return the total amount of memory reserved
     fn reserved(&self) -> usize;
+
+    /// Return the configured pool size (if any)
+    fn pool_size(&self) -> Option<usize>;
 }
 
 /// A memory consumer that can be tracked by [`MemoryReservation`] in
@@ -286,7 +289,9 @@ mod tests {
 
     #[test]
     fn test_memory_pool_underflow() {
-        let pool = Arc::new(GreedyMemoryPool::new(50)) as _;
+        let pool: Arc<dyn MemoryPool> = Arc::new(GreedyMemoryPool::new(50)) as _;
+        assert_eq!(pool.pool_size(), Some(50));
+
         let mut a1 = MemoryConsumer::new("a1").register(&pool);
         assert_eq!(pool.reserved(), 0);
 
