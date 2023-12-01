44d723e04207acd723535eeaf286e947e03cbbe3
Daniël Heres <danielheres@gmail.com>
2023-12-01 13:08:27 +0100

    Improve roundrobin repartitioning

--- datafusion/physical-plan/src/repartition/mod.rs
+++ datafusion/physical-plan/src/repartition/mod.rs
@@ -100,12 +100,18 @@ impl BatchPartitioner {
     /// Create a new [`BatchPartitioner`] with the provided [`Partitioning`]
     ///
     /// The time spent repartitioning will be recorded to `timer`
-    pub fn try_new(partitioning: Partitioning, timer: metrics::Time) -> Result<Self> {
+    pub fn try_new(
+        partitioning: Partitioning,
+        partition: usize,
+        num_input_partitions: usize,
+        timer: metrics::Time,
+    ) -> Result<Self> {
         let state = match partitioning {
             Partitioning::RoundRobinBatch(num_partitions) => {
                 BatchPartitionerState::RoundRobin {
                     num_partitions,
-                    next_idx: 0,
+                    // start at input partition to spread batches more evenly
+                    next_idx: (num_partitions / num_input_partitions) * partition,
                 }
             }
             Partitioning::Hash(exprs, num_partitions) => BatchPartitionerState::Hash {
@@ -686,8 +692,12 @@ impl RepartitionExec {
         metrics: RepartitionMetrics,
         context: Arc<TaskContext>,
     ) -> Result<()> {
-        let mut partitioner =
-            BatchPartitioner::try_new(partitioning, metrics.repartition_time.clone())?;
+        let mut partitioner = BatchPartitioner::try_new(
+            partitioning,
+            partition,
+            input.output_partitioning().partition_count(),
+            metrics.repartition_time.clone(),
+        )?;
 
         // execute the child operator
         let timer = metrics.fetch_time.timer();
