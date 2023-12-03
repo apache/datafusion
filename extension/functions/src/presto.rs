8eb3a63a1b15c75e35636ac6224dd66b4f15a586
lbroot <l0broot@gmail.com>
2023-12-03 14:54:28 +0800

    human_readable_seconds

--- extension/functions/src/presto.rs
+++ extension/functions/src/presto.rs
@@ -17,8 +17,6 @@
 
 use arrow::{array::*, datatypes::DataType};
 use datafusion::error::Result;
-
-use datafusion_common::DataFusionError;
 use datafusion_expr::{
     ReturnTypeFunction, ScalarFunctionDef, ScalarFunctionPackage, Signature, Volatility,
 };
@@ -51,7 +49,7 @@ impl ScalarFunctionDef for HumanReadableSecondsFunction {
         let array = input_array
             .into_iter()
             .map(|sec| {
-                let seconds = sec.map(|value| value / 1_000_000_000.0).unwrap();
+                let seconds = sec.map(|value| value).unwrap();
                 let weeks = (seconds / 604800.0) as i64;
                 let days = ((seconds % 604800.0) / 86400.0) as i64;
                 let hours = ((seconds % 86400.0) / 3600.0) as i64;
@@ -61,30 +59,58 @@ impl ScalarFunctionDef for HumanReadableSecondsFunction {
                 let mut formatted = String::new();
 
                 if weeks > 0 {
-                    formatted +=
-                        &format!("{} week{}, ", weeks, if weeks > 1 { "s" } else { "" });
+                    formatted += &format!(
+                        "{} week{}{}",
+                        weeks,
+                        if weeks > 1 { "s" } else { "" },
+                        if days + hours + minutes + seconds_remainder > 0 {
+                            ", "
+                        } else {
+                            ""
+                        }
+                    ); //for splitting ,
                 }
                 if days > 0 {
-                    formatted +=
-                        &format!("{} day{}, ", days, if days > 1 { "s" } else { "" });
+                    formatted += &format!(
+                        "{} day{}{}",
+                        days,
+                        if days > 1 { "s" } else { "" },
+                        if hours + minutes + seconds_remainder > 0 {
+                            ", "
+                        } else {
+                            ""
+                        }
+                    ); //for splitting ,
                 }
                 if hours > 0 {
-                    formatted +=
-                        &format!("{} hour{}, ", hours, if hours > 1 { "s" } else { "" });
+                    formatted += &format!(
+                        "{} hour{}{}",
+                        hours,
+                        if hours > 1 { "s" } else { "" },
+                        if minutes + seconds_remainder > 0 {
+                            ", "
+                        } else {
+                            ""
+                        }
+                    ); //for splitting ,
                 }
                 if minutes > 0 {
                     formatted += &format!(
-                        "{} minute{}, ",
+                        "{} minute{}{}",
                         minutes,
-                        if minutes > 1 { "s" } else { "" }
+                        if minutes > 1 { "s" } else { "" },
+                        if seconds_remainder > 0 { ", " } else { "" }
                     );
                 }
                 if seconds_remainder > 0 {
                     formatted += &format!(
-                        "{} second{}, ",
+                        "{} second{}",
                         seconds_remainder,
                         if seconds_remainder > 1 { "s" } else { "" }
                     );
+                }
+                if weeks + days + hours + minutes + seconds_remainder == 0 {
+                    formatted = "0 second".to_string();
                 }
                 Some(formatted)
             })
@@ -115,12 +141,15 @@ mod test {
 
     #[tokio::test]
     async fn test_human_readable_seconds() -> Result<()> {
-        //test_expression!("human_readable_seconds(1.0)", "0 week, 0 day, 0 hour, 0 minute, 0 second");
         test_expression!("human_readable_seconds(604800.0)", "1 week");
         test_expression!("human_readable_seconds(86400.0)", "1 day");
         test_expression!("human_readable_seconds(3600.0)", "1 hour");
         test_expression!("human_readable_seconds(60.0)", "1 minute");
         test_expression!("human_readable_seconds(1.0)", "1 second");
+        test_expression!("human_readable_seconds(0.0)", "0 second");
+        test_expression!("human_readable_seconds(96)", "1 minute, 36 seconds");
+        test_expression!("human_readable_seconds(3762)", "1 hour, 2 minutes, 42 seconds");
+        test_expression!("human_readable_seconds(56363463)", "93 weeks, 1 day, 8 hours, 31 minutes, 3 seconds");
         Ok(())
     }
 }
