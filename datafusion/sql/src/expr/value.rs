385bb268889c5ace0b0921331bcb104553e7a0ca
Asura7969 <1402357969@qq.com>
2023-12-05 05:41:40 +0800

    Support named query parameters (#8384)
    * Minor: Improve the document format of JoinHashMap
    
    * support named query parameters
    
    * cargo fmt
    
    * add `ParamValues` conversion
    
    * improve doc

--- datafusion/sql/src/expr/value.rs
+++ datafusion/sql/src/expr/value.rs
@@ -108,7 +108,12 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
             }
             Ok(index) => index - 1,
             Err(_) => {
-                return plan_err!("Invalid placeholder, not a number: {param}");
+                return if param_data_types.is_empty() {
+                    Ok(Expr::Placeholder(Placeholder::new(param, None)))
+                } else {
+                    // when PREPARE Statement, param_data_types length is always 0
+                    plan_err!("Invalid placeholder, not a number: {param}")
+                };
             }
         };
         // Check if the placeholder is in the parameter list
