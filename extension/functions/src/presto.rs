28c0bbf1cf9844b2f44a80c060c5a9132ec32de5
link-bands <ireallyagreewithyou@gmail.com>
2023-12-11 14:22:50 +0800

    current_timestamp_p&current_timestamp

--- extension/functions/src/presto.rs
+++ extension/functions/src/presto.rs
@@ -161,7 +161,7 @@ mod test {
     };
 
     use arrow::array::{
-        ArrayRef, Int64Array, TimestampMillisecondArray, TimestampNanosecondArray,
+        Array, ArrayRef, Int64Array, TimestampMillisecondArray, TimestampNanosecondArray,
     };
     use chrono::Utc;
     use datafusion::error::Result;
@@ -184,39 +184,69 @@ mod test {
         Ok(())
     }
 
+    async fn test_timestamp_function<F, A>(
+        function: F,
+        args: &[A],
+        time_unit: TimeUnit,
+    ) -> Result<()>
+    where
+        F: Fn(&[A]) -> Result<ArrayRef>,
+        A: Array,
+    {
+        let result = function(args).unwrap();
+        let result_value = match time_unit {
+            TimeUnit::Millisecond => result
+                .as_any()
+                .downcast_ref::<TimestampMillisecondArray>()
+                .unwrap()
+                .value(0) as i64,
+            TimeUnit::Nanosecond => result
+                .as_any()
+                .downcast_ref::<TimestampNanosecondArray>()
+                .unwrap()
+                .value(0) as i64,
+        };
+
+        let now = match time_unit {
+            TimeUnit::Millisecond => SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_millis() as i64,
+            TimeUnit::Nanosecond => SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_nanos() as i64,
+        };
+
+        assert!((result_value - now).abs() <= 1_000_000_000);
+
+        Ok(())
+    }
+
+    enum TimeUnit {
+        Millisecond,
+        Nanosecond,
+    }
+
     #[tokio::test]
     async fn test_current_timestamp() -> Result<()> {
-        let current_timestamp_function = CurrentTimestampFunction {};
-        let result = current_timestamp_function.execute(&[]).unwrap();
-        let result_array = result
-            .as_any()
-            .downcast_ref::<TimestampMillisecondArray>()
-            .unwrap()
-            .value(0);
-        let now = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_millis() as i64;
-        assert!((result_array - now).abs() <= 1);
-        Ok(())
+        test_timestamp_function(
+            |args| CurrentTimestampFunction {}.execute(args),
+            &[],
+            TimeUnit::Millisecond,
+        )
+        .await
     }
 
     #[tokio::test]
     async fn test_current_timestamp_p() -> Result<()> {
-        let function = CurrentTimestampPFunction {};
         let precision_array = Int64Array::from(vec![9]);
         let args = vec![Arc::new(precision_array) as ArrayRef];
-        let result = function.execute(&args).unwrap();
-        let result_array = result
-            .as_any()
-            .downcast_ref::<TimestampNanosecondArray>()
-            .unwrap()
-            .value(0);
-        let now = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_nanos() as i64;
-        assert!((result_array - now).abs() <= 1_000_000_000);
-        Ok(())
+        test_timestamp_function(
+            |args| CurrentTimestampPFunction {}.execute(args),
+            &args,
+            TimeUnit::Nanosecond,
+        )
+        .await
     }
 }
