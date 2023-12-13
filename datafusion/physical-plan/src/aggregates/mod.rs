74825e99761ecc5b1cb262765928f117cb14dbee
Mustafa Akur <mustafa.akur@synnada.ai>
2023-12-13 13:09:39 +0300

    Simplifications

--- datafusion/physical-plan/src/aggregates/mod.rs
+++ datafusion/physical-plan/src/aggregates/mod.rs
@@ -18,7 +18,6 @@
 //! Aggregates functionalities
 
 use std::any::Any;
-use std::collections::HashMap;
 use std::sync::Arc;
 
 use super::DisplayAs;
@@ -39,7 +38,7 @@ use arrow::datatypes::{Field, Schema, SchemaRef};
 use arrow::record_batch::RecordBatch;
 use arrow_schema::DataType;
 use datafusion_common::stats::Precision;
-use datafusion_common::{exec_datafusion_err, plan_err, DataFusionError, Result};
+use datafusion_common::{plan_err, DataFusionError, Result};
 use datafusion_execution::TaskContext;
 use datafusion_expr::Accumulator;
 use datafusion_physical_expr::{
@@ -49,7 +48,6 @@ use datafusion_physical_expr::{
     physical_exprs_contains, AggregateExpr, EquivalenceProperties, LexOrdering,
     LexRequirement, PhysicalExpr, PhysicalSortExpr, PhysicalSortRequirement,
 };
-use hashbrown::HashSet;
 
 use itertools::Itertools;
 
@@ -928,89 +926,41 @@ fn get_aggregate_expr_groups(
     eq_properties: &EquivalenceProperties,
     agg_mode: &AggregateMode,
 ) -> Result<Vec<AggregateExprGroup>> {
-    // let mut used_indices: HashSet<usize> = HashSet::new();
-    // let mut groups = vec![];
-    let mut groups_v2 : HashMap<LexOrdering, Vec<usize>> = HashMap::new();
+    let mut groups: Vec<(LexOrdering, Vec<usize>)> = vec![];
     for (idx, aggr_expr) in aggr_exprs.iter_mut().enumerate() {
         let req = aggr_expr.order_bys().unwrap_or(&[]).to_vec();
-        let mut sa = groups_v2.remove(&req).unwrap_or(vec![]);
-        if let Some(finer_ordering) = finer_ordering(
-            &req,
-            aggr_expr,
-            group_by,
-            eq_properties,
-            agg_mode,
-        ) {
-            sa.push(idx);
-            groups_v2.insert(finer_ordering, sa);
-        } else if let Some(reversed_expr) = aggr_expr.reverse_expr() {
-            if let Some(finer_ordering) = finer_ordering(
-                &req,
-                &reversed_expr,
-                group_by,
-                eq_properties,
-                agg_mode,
-            ) {
-                *aggr_expr = reversed_expr;
-                sa.push(idx);
-                groups_v2.insert(finer_ordering, sa);
+        let mut group_match = false;
+        for (key, value) in groups.iter_mut() {
+            if let Some(finer_ordering) =
+                finer_ordering(key, aggr_expr, group_by, eq_properties, agg_mode)
+            {
+                value.push(idx);
+                *key = finer_ordering;
+                group_match = true;
+            } else if let Some(reversed_expr) = aggr_expr.reverse_expr() {
+                if let Some(finer_ordering) =
+                    finer_ordering(key, &reversed_expr, group_by, eq_properties, agg_mode)
+                {
+                    *aggr_expr = reversed_expr;
+                    value.push(idx);
+                    *key = finer_ordering;
+                    group_match = true;
+                }
             }
-        };
+        }
+        if !group_match {
+            // there is no existing group that matches. Insert new group
+            groups.push((req, vec![idx]));
+        }
     }
 
-
-
-    // while used_indices.len() != aggr_exprs.len() {
-    //     let mut current_group: Option<(Vec<usize>, LexOrdering)> = None;
-    //     for (idx, aggr_expr) in aggr_exprs.iter_mut().enumerate() {
-    //         // Group is empty and index is not already in another group.
-    //         if used_indices.contains(&idx) {
-    //             // Skip this group, it is already inserted.
-    //             continue;
-    //         }
-    //         // Initialize an empty group with empty requirement
-    //         let (mut group_indices, mut ordering_req) =
-    //             current_group.unwrap_or((vec![], vec![]));
-    //
-    //         if let Some(finer_ordering) = finer_ordering(
-    //             &ordering_req,
-    //             aggr_expr,
-    //             group_by,
-    //             eq_properties,
-    //             agg_mode,
-    //         ) {
-    //             ordering_req = finer_ordering;
-    //             group_indices.push(idx);
-    //         } else if let Some(reversed_expr) = aggr_expr.reverse_expr() {
-    //             if let Some(finer_ordering) = finer_ordering(
-    //                 &ordering_req,
-    //                 &reversed_expr,
-    //                 group_by,
-    //                 eq_properties,
-    //                 agg_mode,
-    //             ) {
-    //                 *aggr_expr = reversed_expr;
-    //                 ordering_req = finer_ordering;
-    //                 group_indices.push(idx);
-    //             }
-    //         }
-    //         // Update group with new entries
-    //         current_group = Some((group_indices, ordering_req));
-    //     }
-    //     // We cannot received None here.
-    //     let (indices, requirement) = current_group
-    //         .ok_or_else(|| exec_datafusion_err!("Cannot Receive empty group"))?;
-    //     used_indices.extend(indices.iter());
-    //     groups.push(AggregateExprGroup {
-    //         indices,
-    //         requirement,
-    //     });
-    // }
-
-    Ok(groups_v2.into_iter().map(|(requirement, indices)| AggregateExprGroup {
-        indices,
-        requirement,
-    }).collect())
+    Ok(groups
+        .into_iter()
+        .map(|(requirement, indices)| AggregateExprGroup {
+            indices,
+            requirement,
+        })
+        .collect())
 }
 
 /// returns physical expressions for arguments to evaluate against a batch
