c4d91096b9f248f16ba5e6a197ab31dc1dad21d5
tushushu <xmaseve1984@gmail.com>
2023-12-25 21:02:00 +0800

    fix ordering

--- datafusion/physical-expr/src/equivalence/ordering.rs
+++ datafusion/physical-expr/src/equivalence/ordering.rs
@@ -20,7 +20,7 @@ use std::hash::Hash;
 use std::sync::Arc;
 
 use crate::equivalence::add_offset_to_expr;
-use crate::{LexOrdering, LexOrderingRef, PhysicalExpr, PhysicalSortExpr};
+use crate::{LexOrdering, PhysicalExpr, PhysicalSortExpr};
 
 /// An `OrderingEquivalenceClass` object keeps track of different alternative
 /// orderings than can describe a schema. For example, consider the following table:
