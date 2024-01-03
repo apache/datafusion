a90539488e13879230d1ea9dcf393ae4c9e47522
jayzhan211 <jayzhan211@gmail.com>
2024-01-03 09:30:23 +0800

    add column cases
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- datafusion/optimizer/src/analyzer/rewrite_expr.rs
+++ datafusion/optimizer/src/analyzer/rewrite_expr.rs
@@ -17,14 +17,21 @@
 
 //! Optimizer rule for expression rewrite
 
+use std::sync::Arc;
+
+use arrow::datatypes::DataType;
 use datafusion_common::config::ConfigOptions;
 use datafusion_common::tree_node::TreeNode;
 use datafusion_common::tree_node::TreeNodeRewriter;
+use datafusion_common::utils::list_ndims;
+use datafusion_common::Column;
+use datafusion_common::DFSchema;
+use datafusion_common::DFSchemaRef;
 use datafusion_common::Result;
 use datafusion_expr::expr::ScalarFunction;
+use datafusion_expr::utils::merge_schema;
 use datafusion_expr::BuiltinScalarFunction;
 use datafusion_expr::Operator;
-use datafusion_expr::Projection;
 use datafusion_expr::ScalarFunctionDefinition;
 use datafusion_expr::{BinaryExpr, Expr, LogicalPlan};
 
@@ -45,20 +52,31 @@ impl AnalyzerRule for OperatorToFunction {
     }
 
     fn analyze(&self, plan: LogicalPlan, _: &ConfigOptions) -> Result<LogicalPlan> {
-        analyze_internal(plan)
+        analyze_internal(&plan)
     }
 }
 
-fn analyze_internal(plan: LogicalPlan) -> Result<LogicalPlan> {
-    // OperatorToFunction is only applied to Projection
-    match plan {
-        LogicalPlan::Projection(_) => {}
-        _ => {
-            return Ok(plan);
-        }
+fn analyze_internal(plan: &LogicalPlan) -> Result<LogicalPlan> {
+    // optimize child plans first
+    let new_inputs = plan
+        .inputs()
+        .iter()
+        .map(|p| analyze_internal(p))
+        .collect::<Result<Vec<_>>>()?;
+
+    // get schema representing all available input fields. This is used for data type
+    // resolution only, so order does not matter here
+    let mut schema = merge_schema(new_inputs.iter().collect());
+
+    if let LogicalPlan::TableScan(ts) = plan {
+        let source_schema =
+            DFSchema::try_from_qualified_schema(&ts.table_name, &ts.source.schema())?;
+        schema.merge(&source_schema);
     }
 
-    let mut expr_rewriter = OperatorToFunctionRewriter {};
+    let mut expr_rewriter = OperatorToFunctionRewriter {
+        schema: Arc::new(schema),
+    };
 
     let new_expr = plan
         .expressions()
@@ -66,17 +84,28 @@ fn analyze_internal(plan: LogicalPlan) -> Result<LogicalPlan> {
         .map(|expr| expr.rewrite(&mut expr_rewriter))
         .collect::<Result<Vec<_>>>()?;
 
-    // Not found cases that inputs more than one
-    assert_eq!(plan.inputs().len(), 1);
-    let input = plan.inputs()[0];
+    plan.with_new_exprs(new_expr, &new_inputs)
 
-    Ok(LogicalPlan::Projection(Projection::try_new(
-        new_expr,
-        input.to_owned().into(),
-    )?))
+    // match &plan {
+    //     LogicalPlan::Projection(_) => Ok(LogicalPlan::Projection(Projection::try_new(
+    //         new_expr,
+    //         Arc::new(new_inputs[0].clone()),
+    //     )?)),
+    //     _ => plan.with_new_exprs(new_expr, &new_inputs),
+    // }
+    // // Not found cases that inputs more than one
+    // assert_eq!(plan.inputs().len(), 1);
+    // let input = plan.inputs()[0];
+
+    // Ok(LogicalPlan::Projection(Projection::try_new(
+    //     new_expr,
+    //     input.to_owned().into(),
+    // )?))
 }
 
-pub(crate) struct OperatorToFunctionRewriter {}
+pub(crate) struct OperatorToFunctionRewriter {
+    pub(crate) schema: DFSchemaRef,
+}
 
 impl TreeNodeRewriter for OperatorToFunctionRewriter {
     type N = Expr;
@@ -88,11 +117,19 @@ impl TreeNodeRewriter for OperatorToFunctionRewriter {
                 op,
                 ref right,
             }) => {
-                if let Some(fun) = rewrite_array_concat_operator_to_func(
+                if let Some(fun) = rewrite_array_concat_operator_to_func_for_column(
                     left.as_ref(),
                     op,
                     right.as_ref(),
-                ) {
+                    self.schema.as_ref(),
+                )?
+                .or_else(|| {
+                    rewrite_array_concat_operator_to_func(
+                        left.as_ref(),
+                        op,
+                        right.as_ref(),
+                    )
+                }) {
                     // Convert &Box<Expr> -> Expr
                     let left = (**left).clone();
                     let right = (**right).clone();
@@ -101,6 +138,7 @@ impl TreeNodeRewriter for OperatorToFunctionRewriter {
                         args: vec![left, right],
                     }));
                 }
+
                 Ok(expr)
             }
             _ => Ok(expr),
@@ -110,15 +148,15 @@ impl TreeNodeRewriter for OperatorToFunctionRewriter {
 
 /// Summary of the logic below:
 ///
-/// array || array -> array concat
+/// 1) array || array -> array concat
 ///
-/// array || scalar -> array append
+/// 2) array || scalar -> array append
 ///
-/// scalar || array -> array prepend
+/// 3) scalar || array -> array prepend
 ///
-/// (arry concat, array append, array prepend) || array -> array concat
+/// 4) (arry concat, array append, array prepend) || array -> array concat
 ///
-/// (arry concat, array append, array prepend) || scalar -> array append
+/// 5) (arry concat, array append, array prepend) || scalar -> array append
 fn rewrite_array_concat_operator_to_func(
     left: &Expr,
     op: Operator,
@@ -228,5 +266,70 @@ fn rewrite_array_concat_operator_to_func(
         ) => Some(BuiltinScalarFunction::ArrayPrepend),
 
         _ => None,
+    }
+}
+
+/// Summary of the logic below:
+///
+/// 1) (arry concat, array append, array prepend) || column -> (array append, array concat)
+///
+/// 2) column1 || column2 -> (array prepend, array append, array concat)
+fn rewrite_array_concat_operator_to_func_for_column(
+    left: &Expr,
+    op: Operator,
+    right: &Expr,
+    schema: &DFSchema,
+) -> Result<Option<BuiltinScalarFunction>> {
+    if op != Operator::StringConcat {
+        return Ok(None);
+    }
+
+    match (left, right) {
+        // Column cases:
+        // 1) array_prepend/append/concat || column
+        (
+            Expr::ScalarFunction(ScalarFunction {
+                func_def:
+                    ScalarFunctionDefinition::BuiltIn(BuiltinScalarFunction::ArrayPrepend),
+                args: _left_args,
+            }),
+            Expr::Column(c),
+        )
+        | (
+            Expr::ScalarFunction(ScalarFunction {
+                func_def:
+                    ScalarFunctionDefinition::BuiltIn(BuiltinScalarFunction::ArrayAppend),
+                args: _left_args,
+            }),
+            Expr::Column(c),
+        )
+        | (
+            Expr::ScalarFunction(ScalarFunction {
+                func_def:
+                    ScalarFunctionDefinition::BuiltIn(BuiltinScalarFunction::ArrayConcat),
+                args: _left_args,
+            }),
+            Expr::Column(c),
+        ) => {
+            let d = schema.field_from_column(c)?.data_type();
+            let ndim = list_ndims(d);
+            match ndim {
+                0 => Ok(Some(BuiltinScalarFunction::ArrayAppend)),
+                _ => Ok(Some(BuiltinScalarFunction::ArrayConcat)),
+            }
+        }
+        // 2) select column1 || column2
+        (Expr::Column(c1), Expr::Column(c2)) => {
+            let d0 = schema.field_from_column(c1)?.data_type();
+            let d1 = schema.field_from_column(c2)?.data_type();
+            let ndim0 = list_ndims(d0);
+            let ndim1 = list_ndims(d1);
+            match (ndim0, ndim1) {
+                (0, _) => Ok(Some(BuiltinScalarFunction::ArrayPrepend)),
+                (_, 0) => Ok(Some(BuiltinScalarFunction::ArrayAppend)),
+                _ => Ok(Some(BuiltinScalarFunction::ArrayConcat)),
+            }
+        }
+        _ => Ok(None),
     }
 }
