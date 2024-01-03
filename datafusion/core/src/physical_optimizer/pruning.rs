ac4ca35ab471c1f61cca7d3fc6d446171a9bbc8a
my-vegetable-has-exploded <wy1109468038@gmail.com>
2024-01-03 17:42:51 +0800

    fix: check guarantees in allways_true

--- datafusion/core/src/physical_optimizer/pruning.rs
+++ datafusion/core/src/physical_optimizer/pruning.rs
@@ -295,9 +295,9 @@ impl PruningPredicate {
         &self.predicate_expr
     }
 
-    /// Returns true if this pruning predicate is "always true" (aka will not prune anything)
+    /// Returns true if this pruning predicate is "always true" and literal_guarantees is empty(aka will not prune anything)
     pub fn allways_true(&self) -> bool {
-        is_always_true(&self.predicate_expr)
+        is_always_true(&self.predicate_expr) && self.literal_guarantees.is_empty()
     }
 
     pub(crate) fn required_columns(&self) -> &RequiredColumns {
