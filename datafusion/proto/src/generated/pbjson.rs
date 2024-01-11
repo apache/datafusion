7e5a4caa4fe9fb58aaf660e2e7bbe2c871c096ef
Alex Huang <huangweijun1001@gmail.com>
2024-01-12 04:23:52 +0800

    feat: support `array_resize` (#8744)
    * support array_resize
    
    * support column
    
    * support LargeList
    
    * add function discription
    
    * add example
    
    * fix ci
    
    * remove useless files
    
    * rename variable and improve error
    
    * clean comment
    
    * rename variable
    
    * improve error output
    
    * use MutableArray
    
    * refactor code and reduce extra function calls
    
    * fix error

--- datafusion/proto/src/generated/pbjson.rs
+++ datafusion/proto/src/generated/pbjson.rs
@@ -22334,6 +22334,7 @@ impl serde::Serialize for ScalarFunction {
             Self::FindInSet => "FindInSet",
             Self::ArraySort => "ArraySort",
             Self::ArrayDistinct => "ArrayDistinct",
+            Self::ArrayResize => "ArrayResize",
         };
         serializer.serialize_str(variant)
     }
@@ -22475,6 +22476,7 @@ impl<'de> serde::Deserialize<'de> for ScalarFunction {
             "FindInSet",
             "ArraySort",
             "ArrayDistinct",
+            "ArrayResize",
         ];
 
         struct GeneratedVisitor;
@@ -22645,6 +22647,7 @@ impl<'de> serde::Deserialize<'de> for ScalarFunction {
                     "FindInSet" => Ok(ScalarFunction::FindInSet),
                     "ArraySort" => Ok(ScalarFunction::ArraySort),
                     "ArrayDistinct" => Ok(ScalarFunction::ArrayDistinct),
+                    "ArrayResize" => Ok(ScalarFunction::ArrayResize),
                     _ => Err(serde::de::Error::unknown_variant(value, FIELDS)),
                 }
             }
