5a6d80d93d5010d84bb30154dfb37ddbe85deaa4
Andrew Lamb <andrew@nerdnetworks.org>
2024-01-12 16:35:34 -0500

    Restore test without explicit region

--- datafusion-cli/src/exec.rs
+++ datafusion-cli/src/exec.rs
@@ -340,6 +340,11 @@ mod tests {
         let session_token = "fake_session_token";
         let location = "s3://bucket/path/file.parquet";
 
+        // Missing region, use object_store defaults
+        let sql = format!("CREATE EXTERNAL TABLE test STORED AS PARQUET
+            OPTIONS('access_key_id' '{access_key_id}', 'secret_access_key' '{secret_access_key}') LOCATION '{location}'");
+        create_external_table_test(location, &sql).await?;
+
         // Should be OK
         let sql = format!("CREATE EXTERNAL TABLE test STORED AS PARQUET
             OPTIONS('access_key_id' '{access_key_id}', 'secret_access_key' '{secret_access_key}', 'region' '{region}', 'session_token' '{session_token}') LOCATION '{location}'");
