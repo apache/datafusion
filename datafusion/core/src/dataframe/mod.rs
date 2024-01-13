9c2daa472538c5259b528fb210eac8cc9e27873b
comphead <comphead@ukr.net>
2024-01-13 15:04:15 -0800

    update doc

--- datafusion/core/src/dataframe/mod.rs
+++ datafusion/core/src/dataframe/mod.rs
@@ -1159,7 +1159,7 @@ impl DataFrame {
     /// The method supports case sensitive rename with wrapping column name into one of following symbols (  "  or  '  or  `  )
     /// 
     /// Alternatively setting Datafusion param `datafusion.sql_parser.enable_ident_normalization` to `false` will enable  
-    /// case sensitive rename without need of wrap column name into special symbols 
+    /// case sensitive rename without need to wrap column name into special symbols 
     /// 
     /// ```
     /// # use datafusion::prelude::*;
