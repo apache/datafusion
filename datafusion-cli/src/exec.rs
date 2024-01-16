fd8067c6495fa3048dfab26814ec7be4fd845446
Trent Hauck <trent@trenthauck.com>
2024-01-16 03:53:07 -0800

    Change cli to propagate error to exit code (#8856)
    * feat(cli): use error exit code when command or file
    
    * fix: remove prints
    
    * style: rust fmt
    
    * refactor: use CLI specific error for display
    
    * refactor: better use statements
    
    * Revert "refactor: better use statements"
    
    This reverts commit fac8c3a2e9c3072307679b149543ead834ef1035.
    
    * Revert "refactor: use CLI specific error for display"
    
    This reverts commit e58d331439438566acc4779bca9a873ed77d0818.
    
    * refactor: wrap main_inner, use ExitCode

--- datafusion-cli/src/exec.rs
+++ datafusion-cli/src/exec.rs
@@ -53,13 +53,12 @@ pub async fn exec_from_commands(
     ctx: &mut SessionContext,
     commands: Vec<String>,
     print_options: &PrintOptions,
-) {
+) -> Result<()> {
     for sql in commands {
-        match exec_and_print(ctx, print_options, sql).await {
-            Ok(_) => {}
-            Err(err) => println!("{err}"),
-        }
+        exec_and_print(ctx, print_options, sql).await?;
     }
+
+    Ok(())
 }
 
 /// run and execute SQL statements and commands from a file, against a context with the given print options
@@ -67,7 +66,7 @@ pub async fn exec_from_lines(
     ctx: &mut SessionContext,
     reader: &mut BufReader<File>,
     print_options: &PrintOptions,
-) {
+) -> Result<()> {
     let mut query = "".to_owned();
 
     for line in reader.lines() {
@@ -97,26 +96,28 @@ pub async fn exec_from_lines(
     // run the left over query if the last statement doesn't contain ‘;’
     // ignore if it only consists of '\n'
     if query.contains(|c| c != '\n') {
-        match exec_and_print(ctx, print_options, query).await {
-            Ok(_) => {}
-            Err(err) => println!("{err}"),
-        }
+        exec_and_print(ctx, print_options, query).await?;
     }
+
+    Ok(())
 }
 
 pub async fn exec_from_files(
     ctx: &mut SessionContext,
     files: Vec<String>,
     print_options: &PrintOptions,
-) {
+) -> Result<()> {
     let files = files
         .into_iter()
         .map(|file_path| File::open(file_path).unwrap())
         .collect::<Vec<_>>();
+
     for file in files {
         let mut reader = BufReader::new(file);
-        exec_from_lines(ctx, &mut reader, print_options).await;
+        exec_from_lines(ctx, &mut reader, print_options).await?;
     }
+
+    Ok(())
 }
 
 /// run and execute SQL statements and commands against a context with the given print options
@@ -215,6 +216,7 @@ async fn exec_and_print(
                  MsSQL, ClickHouse, BigQuery, Ansi."
         )
     })?;
+
     let statements = DFParser::parse_sql_with_dialect(&sql, dialect.as_ref())?;
     for statement in statements {
         let mut plan = ctx.state().statement_to_plan(statement).await?;
