cd95b7948477d8d76141c9608853152892b01dd7
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2024-01-18 14:02:18 -0800

    add doc comment with rationale for create_cte_worktable method

--- datafusion/sql/src/planner.rs
+++ datafusion/sql/src/planner.rs
@@ -61,7 +61,11 @@ pub trait ContextProvider {
         not_impl_err!("Table Functions are not supported")
     }
 
-    /// TODO: add doc
+    /// This provides a worktable (an intermediate table that is used to store the results of a CTE during execution)
+    /// We don't directly implement this in the logical plan's ['SqlToRel`]
+    /// because the sql code needs access to a table that contains execution-related types that can't be a direct dependency
+    /// of the sql crate (namely, the `CteWorktable`).
+    /// The ContextProvider provides a way to "hide" this dependency.
     fn create_cte_work_table(
         &self,
         _name: &str,
