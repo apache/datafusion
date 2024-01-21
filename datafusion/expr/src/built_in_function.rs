0116e2a9b4a3ed4491802e19195769b96b7a971a
Alex Huang <huangweijun1001@gmail.com>
2024-01-21 20:30:30 +0800

    feat: support `stride` in `array_slice`, change indexes to be`1` based (#8829)
    * support array slice
    
    * fix argument
    
    * fix typo
    
    * support from and to is negative
    
    * fix conflict
    
    * modify user doc
    
    * refactor code
    
    * fix clippy
    
    * add 1-index test
    
    ---------
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/expr/src/built_in_function.rs
+++ datafusion/expr/src/built_in_function.rs
@@ -977,7 +977,10 @@ impl BuiltinScalarFunction {
             BuiltinScalarFunction::ArrayReplaceAll => {
                 Signature::any(3, self.volatility())
             }
-            BuiltinScalarFunction::ArraySlice => Signature::any(3, self.volatility()),
+            BuiltinScalarFunction::ArraySlice => {
+                Signature::variadic_any(self.volatility())
+            }
+
             BuiltinScalarFunction::ArrayToString => {
                 Signature::variadic_any(self.volatility())
             }
