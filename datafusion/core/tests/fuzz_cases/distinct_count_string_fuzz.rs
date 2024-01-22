a80b39cd25dbdaaba2adca974215aae1b03d1303
Andrew Lamb <andrew@nerdnetworks.org>
2024-01-22 09:28:45 -0500

    refine fuzz test

--- datafusion/core/tests/fuzz_cases/distinct_count_string_fuzz.rs
+++ datafusion/core/tests/fuzz_cases/distinct_count_string_fuzz.rs
@@ -17,307 +17,195 @@
 
 //! Compare DistinctCount for string with naive HashSet and Short String Optimized HashSet
 
-use std::any::Any;
 use std::sync::Arc;
 
 use arrow::array::ArrayRef;
-use arrow::datatypes::DataType;
-use arrow::datatypes::Field;
 use arrow::record_batch::RecordBatch;
-use arrow_array::cast::{as_list_array, as_string_array};
-use arrow_array::{ListArray, StringArray};
-use datafusion::physical_expr::aggregate::utils::down_cast_any_ref;
-use datafusion::physical_plan::aggregates::{
-    AggregateExec, AggregateMode, PhysicalGroupBy,
-};
-use datafusion_common::utils::array_into_list_array;
-use datafusion_common::Result;
-use datafusion_common::ScalarValue;
-use datafusion_expr::Accumulator;
-use datafusion_physical_expr::expressions::format_state_name;
-use datafusion_physical_expr::PhysicalExpr;
+use arrow_array::{Array, GenericStringArray, OffsetSizeTrait, UInt32Array};
 
+use arrow_array::cast::AsArray;
+use datafusion::datasource::MemTable;
 use rand::rngs::StdRng;
-use rand::{Rng, SeedableRng};
+use rand::{thread_rng, Rng, SeedableRng};
 use std::collections::HashSet;
+use tokio::task::JoinSet;
 
-use datafusion::physical_plan::memory::MemoryExec;
-use datafusion::physical_plan::{collect, ExecutionPlan};
 use datafusion::prelude::{SessionConfig, SessionContext};
-use datafusion_physical_expr::expressions::{col, DistinctCount};
-use datafusion_physical_expr::AggregateExpr;
+use test_utils::stagger_batch;
 
-#[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+#[tokio::test(flavor = "multi_thread")]
 async fn distinct_count_string_test() {
     // max length of generated strings
-    let max_lens = [4, 8, 16, 32, 64, 128, 256, 512];
-    let n = 300;
-    // number of rows in each batch
-    let row_lens = [8, 16, 32, 64, 128, 256, 512, 1024];
-    for row_len in row_lens {
-        let mut handles = Vec::new();
-        for i in 0..n {
-            let test_idx = i % max_lens.len();
-            let max_len = max_lens[test_idx];
-            let job = tokio::spawn(run_distinct_count_test(make_staggered_batches(
-                max_len, row_len, i as u64,
-            )));
-            handles.push(job);
+    let mut join_set = JoinSet::new();
+    let mut rng = thread_rng();
+    for null_pct in [0.0, 0.01, 0.1, 0.5] {
+        for _ in 0..100 {
+            let max_len = rng.gen_range(1..50);
+            let num_strings = rng.gen_range(1..100);
+            let num_distinct_strings = if num_strings > 1 {
+                rng.gen_range(1..num_strings)
+            } else {
+                num_strings
+            };
+            let generator = BatchGenerator {
+                max_len,
+                num_strings,
+                num_distinct_strings,
+                null_pct,
+                rng: StdRng::from_seed(rng.gen()),
+            };
+            join_set.spawn(async move { run_distinct_count_test(generator).await });
         }
-        for job in handles {
-            job.await.unwrap();
-        }
+    }
+    while let Some(join_handle) = join_set.join_next().await {
+        // propagate errors
+        join_handle.unwrap();
     }
 }
 
-/// Perform batch and streaming aggregation with same input
-/// and verify outputs of `AggregateExec` with pipeline breaking stream `GroupedHashAggregateStream`
-/// and non-pipeline breaking stream `BoundedAggregateStream` produces same result.
-async fn run_distinct_count_test(input1: Vec<RecordBatch>) {
-    let schema = input1[0].schema();
+/// Run COUNT DISTINCT using SQL and compare the result to computing the
+/// distinct count using HashSet<String>
+async fn run_distinct_count_test(mut generator: BatchGenerator) {
+    let input = generator.make_input_batches();
+
+    let schema = input[0].schema();
     let session_config = SessionConfig::new().with_batch_size(50);
     let ctx = SessionContext::new_with_config(session_config);
 
-    let control_group_source =
-        Arc::new(MemoryExec::try_new(&[input1.clone()], schema.clone(), None).unwrap());
-
-    let experimental_source =
-        Arc::new(MemoryExec::try_new(&[input1.clone()], schema.clone(), None).unwrap());
-
-    let distinct_count_expr = vec![
-        Arc::new(DistinctCount::new(
-            DataType::Utf8,
-            col("a", &schema).unwrap(),
-            "distinct_count1",
-        )) as Arc<dyn AggregateExpr>,
-        Arc::new(DistinctCount::new(
-            DataType::Utf8,
-            col("b", &schema).unwrap(),
-            "distinct_count2",
-        )) as Arc<dyn AggregateExpr>,
+    // split input into two partitions
+    let partition_len = input.len() / 2;
+    let partitions = vec![
+        input[0..partition_len].to_vec(),
+        input[partition_len..].to_vec(),
     ];
 
-    let expr = vec![(col("c", &schema).unwrap(), "c".to_string())];
-    let group_by = PhysicalGroupBy::new_single(expr);
+    let provider = MemTable::try_new(schema, partitions).unwrap();
+    ctx.register_table("t", Arc::new(provider)).unwrap();
+    // input has two columns, a and b.  The result is the number of distinct
+    // values in each column.
+    //
+    // Note, we need  at least two count distinct aggregates to trigger the
+    // count distinct aggregate. Otherwise, the optimizer will rewrite the
+    // `COUNT(DISTINCT a)` to `COUNT(*) from (SELECT DISTINCT a FROM t)`
+    let results = ctx
+        .sql("SELECT COUNT(DISTINCT a), COUNT(DISTINCT b) FROM t")
+        .await
+        .unwrap()
+        .collect()
+        .await
+        .unwrap();
 
-    let mode = AggregateMode::FinalPartitioned;
-    let filter_expr = vec![None; distinct_count_expr.len()];
+    // get all the strings from the first column of the result (distinct a)
+    let expected_a = extract_distinct_strings::<i32>(&input, 0).len();
+    let result_a = extract_i64(&results, 0);
+    assert_eq!(expected_a, result_a);
 
-    let distinct_count_control_group = Arc::new(
-        AggregateExec::try_new(
-            mode,
-            group_by.clone(),
-            distinct_count_expr,
-            filter_expr.clone(),
-            experimental_source,
-            schema.clone(),
-        )
-        .unwrap(),
-    ) as Arc<dyn ExecutionPlan>;
-
-    let distinct_count_expr = vec![
-        Arc::new(DistinctCountForTest::new(
-            DataType::Utf8,
-            col("a", &schema).unwrap(),
-            "distinct_count1",
-        )) as Arc<dyn AggregateExpr>,
-        Arc::new(DistinctCountForTest::new(
-            DataType::Utf8,
-            col("b", &schema).unwrap(),
-            "distinct_count2",
-        )) as Arc<dyn AggregateExpr>,
-    ];
-
-    let distinct_count_experimental_group = Arc::new(
-        AggregateExec::try_new(
-            mode,
-            group_by,
-            distinct_count_expr,
-            filter_expr,
-            control_group_source,
-            schema,
-        )
-        .unwrap(),
-    ) as Arc<dyn ExecutionPlan>;
-
-    let task_ctx = ctx.task_ctx();
-    let collected_control_group =
-        collect(distinct_count_experimental_group.clone(), task_ctx.clone())
-            .await
-            .unwrap();
-
-    let collected_experimental_group =
-        collect(distinct_count_control_group.clone(), task_ctx.clone())
-            .await
-            .unwrap();
-
-    assert_eq!(collected_control_group, collected_experimental_group);
+    // get all the strings from the second column of the result (distinct b(
+    let expected_b = extract_distinct_strings::<i64>(&input, 1).len();
+    let result_b = extract_i64(&results, 1);
+    assert_eq!(expected_b, result_b);
 }
 
-fn make_staggered_batches(
-    max_len: usize,
-    row_len: usize,
-    random_seed: u64,
-) -> Vec<RecordBatch> {
-    // use a random number generator to pick a random sized output
-    let mut rng = StdRng::seed_from_u64(random_seed);
+/// Return all (non null) distinct strings from  column col_idx
+fn extract_distinct_strings<O: OffsetSizeTrait>(
+    results: &[RecordBatch],
+    col_idx: usize,
+) -> Vec<String> {
+    results
+        .iter()
+        .flat_map(|batch| {
+            let array = batch.column(col_idx).as_string::<O>();
+            // remove nulls via 'flatten'
+            array.iter().flatten().map(|s| s.to_string())
+        })
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect()
+}
 
-    fn gen_data(rng: &mut StdRng, row_len: usize, max_len: usize) -> ListArray {
-        let data: Vec<String> = (0..row_len)
+// extract the value from the Int64 column in col_idx in batch and return
+// it as a usize
+fn extract_i64(results: &[RecordBatch], col_idx: usize) -> usize {
+    assert_eq!(results.len(), 1);
+    let array = results[0]
+        .column(col_idx)
+        .as_any()
+        .downcast_ref::<arrow::array::Int64Array>()
+        .unwrap();
+    assert_eq!(array.len(), 1);
+    assert!(!array.is_null(0));
+    array.value(0).try_into().unwrap()
+}
+
+struct BatchGenerator {
+    //// The maximum length of the strings
+    max_len: usize,
+    /// the total number of strings in the output
+    num_strings: usize,
+    /// The number of distinct strings in the columns
+    num_distinct_strings: usize,
+    /// The percentage of nulls in the columns
+    null_pct: f64,
+    /// Random number generator
+    rng: StdRng,
+}
+
+impl BatchGenerator {
+    /// Make batches of random strings with a random length columns "a" and "b":
+    ///
+    /// * "a" is a StringArray
+    /// * "b" is a LargeStringArray
+    fn make_input_batches(&mut self) -> Vec<RecordBatch> {
+        // use a random number generator to pick a random sized output
+
+        let batch = RecordBatch::try_from_iter(vec![
+            ("a", self.gen_data::<i32>()),
+            ("b", self.gen_data::<i64>()),
+        ])
+        .unwrap();
+
+        stagger_batch(batch)
+    }
+
+    /// Creates a StringArray or LargeStringArray with random strings according
+    /// to the parameters of the BatchGenerator
+    fn gen_data<O: OffsetSizeTrait>(&mut self) -> ArrayRef {
+        // table of strings from which to draw
+        let distinct_strings: GenericStringArray<O> = (0..self.num_distinct_strings)
+            .map(|_| Some(random_string(&mut self.rng, self.max_len)))
+            .collect();
+
+        // pick num_strings randomly from the distinct string table
+        let indicies: UInt32Array = (0..self.num_strings)
             .map(|_| {
-                let len = rng.gen_range(0..max_len) + 1;
-                "a".repeat(len)
+                if self.rng.gen::<f64>() < self.null_pct {
+                    None
+                } else if self.num_distinct_strings > 1 {
+                    let range = 1..(self.num_distinct_strings as u32);
+                    Some(self.rng.gen_range(range))
+                } else {
+                    Some(0)
+                }
             })
             .collect();
-        array_into_list_array(Arc::new(StringArray::from(data)))
+
+        let options = None;
+        arrow::compute::take(&distinct_strings, &indicies, options).unwrap()
     }
-
-    let inputa = gen_data(&mut rng, row_len, max_len);
-    let inputb = gen_data(&mut rng, row_len, max_len);
-    let input_groupby = gen_data(&mut rng, row_len, max_len);
-    let batch = RecordBatch::try_from_iter(vec![
-        ("a", Arc::new(inputa) as ArrayRef),
-        ("b", Arc::new(inputb) as ArrayRef),
-        ("c", Arc::new(input_groupby) as ArrayRef), // column for group by
-    ])
-    .unwrap();
-
-    vec![batch]
 }
 
-/// Expression for a COUNT(DISTINCT) aggregation.
-#[derive(Debug)]
-pub struct DistinctCountForTest {
-    /// Column name
-    name: String,
-    /// The DataType used to hold the state for each input
-    state_data_type: DataType,
-    /// The input arguments
-    expr: Arc<dyn PhysicalExpr>,
-}
-
-impl DistinctCountForTest {
-    /// Create a new COUNT(DISTINCT) aggregate function.
-    pub fn new(
-        input_data_type: DataType,
-        expr: Arc<dyn PhysicalExpr>,
-        name: impl Into<String>,
-    ) -> Self {
-        Self {
-            name: name.into(),
-            state_data_type: input_data_type,
-            expr,
+/// Return a string of random characters of length 1..=max_len
+fn random_string(rng: &mut StdRng, max_len: usize) -> String {
+    // pick characters at random (not just ascii)
+    match max_len {
+        0 => "".to_string(),
+        1 => String::from(rng.gen::<char>()),
+        _ => {
+            let len = rng.gen_range(1..=max_len);
+            rng.sample_iter::<char, _>(rand::distributions::Standard)
+                .take(len)
+                .map(char::from)
+                .collect::<String>()
         }
-    }
-}
-impl AggregateExpr for DistinctCountForTest {
-    /// Return a reference to Any that can be used for downcasting
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-
-    fn field(&self) -> Result<Field> {
-        Ok(Field::new(&self.name, DataType::Int64, true))
-    }
-
-    fn state_fields(&self) -> Result<Vec<Field>> {
-        Ok(vec![Field::new_list(
-            format_state_name(&self.name, "count distinct"),
-            Field::new("item", self.state_data_type.clone(), true),
-            false,
-        )])
-    }
-
-    fn expressions(&self) -> Vec<Arc<dyn PhysicalExpr>> {
-        vec![self.expr.clone()]
-    }
-
-    fn create_accumulator(&self) -> Result<Box<dyn Accumulator>> {
-        match &self.state_data_type {
-            DataType::Utf8 => Ok(Box::new(StringDistinctCountAccumulatorForTest::new())),
-            _ => panic!(
-                "Unsupported type for COUNT(DISTINCT): {:?}",
-                self.state_data_type
-            ),
-        }
-    }
-
-    fn name(&self) -> &str {
-        &self.name
-    }
-}
-
-impl PartialEq<dyn Any> for DistinctCountForTest {
-    fn eq(&self, other: &dyn Any) -> bool {
-        down_cast_any_ref(other)
-            .downcast_ref::<Self>()
-            .map(|x| {
-                self.name == x.name
-                    && self.state_data_type == x.state_data_type
-                    && self.expr.eq(&x.expr)
-            })
-            .unwrap_or(false)
-    }
-}
-
-#[derive(Debug)]
-struct StringDistinctCountAccumulatorForTest(HashSet<String>);
-impl StringDistinctCountAccumulatorForTest {
-    fn new() -> Self {
-        Self(HashSet::new())
-    }
-}
-
-impl Accumulator for StringDistinctCountAccumulatorForTest {
-    fn state(&self) -> Result<Vec<ScalarValue>> {
-        let arr =
-            StringArray::from(self.0.iter().map(|s| s.as_str()).collect::<Vec<&str>>());
-        let list = Arc::new(array_into_list_array(Arc::new(arr)));
-        Ok(vec![ScalarValue::List(list)])
-    }
-
-    fn update_batch(&mut self, values: &[ArrayRef]) -> Result<()> {
-        if values.is_empty() {
-            return Ok(());
-        }
-
-        let array = as_string_array(&values[0]);
-        for v in array.iter().flatten() {
-            self.0.insert(v.to_string());
-        }
-
-        Ok(())
-    }
-
-    fn merge_batch(&mut self, states: &[ArrayRef]) -> Result<()> {
-        if states.is_empty() {
-            return Ok(());
-        }
-        assert_eq!(
-            states.len(),
-            1,
-            "count_distinct states must be single array"
-        );
-
-        let arr = as_list_array(&states[0]);
-        arr.iter().try_for_each(|maybe_list| {
-            if let Some(list) = maybe_list {
-                let array = as_string_array(&list);
-                for v in array.iter().flatten() {
-                    self.0.insert(v.to_string());
-                }
-            };
-            Ok(())
-        })
-    }
-
-    fn evaluate(&self) -> Result<ScalarValue> {
-        Ok(ScalarValue::Int64(Some(self.0.len() as i64)))
-    }
-
-    fn size(&self) -> usize {
-        // Size of accumulator
-        // + SSOStringHashSet size
-        std::mem::size_of_val(self) + self.0.capacity() * std::mem::size_of::<String>()
     }
 }
