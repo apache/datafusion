f3b0ebe283b29e80757f3310f4a445c14966602c
Weijun-H <huangweijun1001@gmail.com>
2024-02-06 10:58:34 +0800

    Fix NULL values in FixedSizeList creation

--- datafusion/common/src/scalar.rs
+++ datafusion/common/src/scalar.rs
@@ -1385,24 +1385,37 @@ impl ScalarValue {
                 .map(|s| s.to_array())
                 .collect::<Result<Vec<_>>>()?;
 
-            let capacity = Capacities::Array(arrays.iter().map(|arr| arr.len()).sum());
+            let capacity = Capacities::Array(
+                arrays
+                    .iter()
+                    .filter(|arr| !arr.is_null(0))
+                    .map(|arr| arr.len())
+                    .sum(),
+            );
             // ScalarValue::List contains a single element ListArray.
             let nulls = arrays
                 .iter()
                 .map(|arr| arr.is_null(0))
                 .collect::<Vec<bool>>();
-            let arrays_data = arrays.iter().map(|arr| arr.to_data()).collect::<Vec<_>>();
+            let arrays_data = arrays
+                .iter()
+                .filter(|arr| !arr.is_null(0))
+                .map(|arr| arr.to_data())
+                .collect::<Vec<_>>();
 
             let arrays_ref = arrays_data.iter().collect::<Vec<_>>();
             let mut mutable =
                 MutableArrayData::with_capacities(arrays_ref, true, capacity);
 
             // ScalarValue::List contains a single element ListArray.
+            let mut cnt = 0;
             for (index, is_null) in (0..arrays.len()).zip(nulls.into_iter()) {
                 if is_null {
-                    mutable.extend_nulls(1)
+                    mutable.extend_nulls(1);
+                    cnt += 1;
                 } else {
-                    mutable.extend(index, 0, 1);
+                    // mutable array contains non-null elements
+                    mutable.extend(index - cnt, 0, 1);
                 }
             }
             let data = mutable.freeze();
