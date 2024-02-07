10e6d1870280296b31c6faff29ddf3f8fe98bbc4
Tv <tv@eagain.net>
2024-02-07 20:12:40 +0000

    Typo in docstring (#9149)

--- datafusion/physical-plan/src/lib.rs
+++ datafusion/physical-plan/src/lib.rs
@@ -236,7 +236,7 @@ pub trait ExecutionPlan: Debug + DisplayAs + Send + Sync {
     fn children(&self) -> Vec<Arc<dyn ExecutionPlan>>;
 
     /// Returns a new `ExecutionPlan` where all existing children were replaced
-    /// by the `children`, oi order
+    /// by the `children`, in order
     fn with_new_children(
         self: Arc<Self>,
         children: Vec<Arc<dyn ExecutionPlan>>,
