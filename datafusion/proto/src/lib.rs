de56ba200a35dec6a199980379eeb2087c4f38b0
Andrew Lamb <andrew@nerdnetworks.org>
2024-02-17 10:46:29 -0500

    Clean up documentation for datafusion-substrait and datafusion-proto

--- datafusion/proto/src/lib.rs
+++ datafusion/proto/src/lib.rs
@@ -15,7 +15,97 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//! Serde code for logical plans and expressions.
+//! Serialization / Deserializing plans to bytes
+//!
+//! This crate provides support for serializing and deserializing the
+//! following structures to and from bytes:
+//!
+//! 1. [`LogicalPlan`]'s (including [`Expr`]),
+//!
+//! 2. [`ExecutionPlan`]s (including [`PhysiscalExpr`])
+//!
+//!
+//! Internally, this crate is implemented by converting the plans to [protocol
+//! buffers] using [prost].
+//!
+//! [protocol buffers]: https://developers.google.com/protocol-buffers
+//! [prost]: https://docs.rs/prost/latest/prost/
+//!
+//! ## See Also
+//!
+//! The binary format created by this crate supports the full range of DataFusion
+//! plans, but is DataFusion specific. See [datafusion-substrait] which can encode
+//! many DataFusion plans using the [substrait.io] standard.
+//!
+//! [datafusion-substrait]: https://docs.rs/datafusion-substrait/latest/datafusion_substrait
+//! [substrait.io]: https://substrait.io
+//!
+//! # Examples
+//!
+//! ## Serializing Expressions (`Expr`)
+//! ```
+//! use datafusion_common::Result;
+//! use datafusion_expr::{col, lit, Expr};
+//! use datafusion_proto::bytes::Serializeable;
+//!
+//!    // Create a new `Expr` a < 32
+//!    let expr = col("a").lt(lit(5i32));
+//!
+//!     // Convert it to an opaque form
+//!    let bytes = expr.to_bytes()?;
+//!
+//!    // Decode bytes from somewhere (over network, etc.)
+//!    let decoded_expr = Expr::from_bytes(&bytes)?;
+//!     assert_eq!(expr, decoded_expr);
+//!    Ok(())
+//! ```
+//!
+//! ## Serializing [`LogicalPlan`]s
+
+Based on [examples/logical_plan_serde.rs](examples/logical_plan_serde.rs)
+
+```rust
+use datafusion::prelude::*;
+use datafusion_common::Result;
+use datafusion_proto::bytes::{logical_plan_from_bytes, logical_plan_to_bytes};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let ctx = SessionContext::new();
+    ctx.register_csv("t1", "tests/testdata/test.csv", CsvReadOptions::default())
+        .await
+        ?;
+    let plan = ctx.table("t1").await?.into_optimized_plan()?;
+    let bytes = logical_plan_to_bytes(&plan)?;
+    let logical_round_trip = logical_plan_from_bytes(&bytes, &ctx)?;
+    assert_eq!(format!("{:?}", plan), format!("{:?}", logical_round_trip));
+    Ok(())
+}
+```
+
+## Serializing Physical Plans
+
+
+```rust
+use datafusion::prelude::*;
+use datafusion_common::Result;
+use datafusion_proto::bytes::{physical_plan_from_bytes,physical_plan_to_bytes};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let ctx = SessionContext::new();
+    ctx.register_csv("t1", "tests/testdata/test.csv", CsvReadOptions::default())
+        .await
+        ?;
+    let logical_plan = ctx.table("t1").await?.into_optimized_plan()?;
+    let physical_plan = ctx.create_physical_plan(&logical_plan).await?;
+    let bytes = physical_plan_to_bytes(physical_plan.clone())?;
+    let physical_round_trip = physical_plan_from_bytes(&bytes, &ctx)?;
+    assert_eq!(format!("{:?}", physical_plan), format!("{:?}", physical_round_trip));
+    Ok(())
+}
+
+```
 
 pub mod bytes;
 pub mod common;
--- datafusion/substrait/README.md
+++ datafusion/substrait/README.md
@@ -17,18 +17,42 @@
   under the License.
 -->
 
-# DataFusion + Substrait
+# Apache Arrow DataFusion Substrait
+
+This crate contains a [Substrait] producer and consumer for Apache Arrow
+[DataFusion] plans. See [API Docs] for details and examples.
+
+[API Docs]: https://docs.rs/datafusion-substrait/latest/datafusion_substrait
+
+[DataFusion] is an extensible query execution framework,
+written in Rust, that uses Apache Arrow as its in-memory format.
 
 [Substrait](https://substrait.io/) provides a cross-language serialization format for relational algebra, based on
 protocol buffers.
 
-This repository provides a Substrait producer and consumer for DataFusion:
+This crate provides support format for serializing (the producer) and deserializing the
+following structures to and from Substrait protobuf:
 
-- The producer converts a DataFusion logical plan into a Substrait protobuf.
-- The consumer converts a Substrait protobuf into a DataFusion logical plan.
+1. [`LogicalPlan`]'s (including [`Expr`]),
+2. [`ExecutionPlan`]s (including [`PhysiscalExpr`])
+
 
 Potential uses of this crate:
-
+- Build systems that run Substrait plans using DataFusion.
+- Connect DataFusion based systems to the broader ecosystem
 - Replace the current [DataFusion protobuf definition](https://github.com/apache/arrow-datafusion/blob/main/datafusion/proto/proto/datafusion.proto) used in Ballista for passing query plan fragments to executors
 - Make it easier to pass query plans over FFI boundaries, such as from Python to Rust
-- Allow Apache Calcite query plans to be executed in DataFusion
+- Run plans created by Apache Calcite to run in DataFusion
+
+## See Also
+
+Substrait does not (yet) support the full range of plans and expressions than
+that DataFusion offers. See [datafusion-proto] for a crate which can 
+
+
+[substrait]: https://substrait.io/
+[datafusion]: https://arrow.apache.org/datafusion
+[`logicalplan`]: https://docs.rs/datafusion/latest/datafusion/logical_expr/enum.LogicalPlan.html
+[`expr`]: https://docs.rs/datafusion/latest/datafusion/logical_expr/expr/enum.Expr.html
+[`executionplan`]: https://docs.rs/datafusion/latest/datafusion/physical_plan/trait.ExecutionPlan.html
+[`physiscalexpr`]: https://docs.rs/datafusion/latest/datafusion/physical_expr/trait.PhysicalExpr.html
