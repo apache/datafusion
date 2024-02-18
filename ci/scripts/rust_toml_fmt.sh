d37f78f71260174334c64733f6cc27d919dd690b
Ruihang Xia <waynestxia@gmail.com>
2024-02-18 22:28:04 +0800

    update doc
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- ci/scripts/rust_toml_fmt.sh
+++ ci/scripts/rust_toml_fmt.sh
@@ -17,10 +17,9 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# Run cargo-tomlfmt with flag `-d` in dry run to check formatting
+# Run `taplo format` with flag `--check` in dry run to check formatting
 # without overwritng the file. If any error occur, you may want to
-# rerun 'cargo tomlfmt -p path/to/Cargo.toml' without '-d' to fix
-# the formatting automatically.
+# rerun `taplo format` to fix the formatting automatically.
 set -ex
 taplo format
 done
