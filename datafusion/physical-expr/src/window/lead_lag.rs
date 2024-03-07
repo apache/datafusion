98114a09ced7dc9abb31b01d027e2db8e7977ed1
Yanxin Xiang <yanxinxiang0917@outlook.com>
2024-03-07 14:51:43 -0600

    adding prototype
    a

--- datafusion/physical-expr/src/window/lead_lag.rs
+++ datafusion/physical-expr/src/window/lead_lag.rs
@@ -17,15 +17,13 @@
 
 //! Defines physical expression for `lead` and `lag` that can evaluated
 //! at runtime during query execution
-
 use crate::window::BuiltInWindowFunctionExpr;
 use crate::PhysicalExpr;
 use arrow::array::ArrayRef;
+use arrow::compute::cast;
 use arrow::datatypes::{DataType, Field};
-use arrow_array::Array;
-use datafusion_common::{
-    arrow_datafusion_err, exec_datafusion_err, DataFusionError, Result, ScalarValue,
-};
+use arrow_array::{Array, Scalar};
+use datafusion_common::{arrow_datafusion_err, DataFusionError, Result, ScalarValue};
 use datafusion_expr::PartitionEvaluator;
 use std::any::Any;
 use std::cmp::min;
@@ -151,6 +149,86 @@ impl WindowShiftEvaluator {
     }
 }
 
+fn create_empty_array(
+    value: Option<&ScalarValue>,
+    data_type: &DataType,
+    size: usize,
+) -> Result<ArrayRef> {
+    use arrow::array::new_null_array;
+    let array = value
+        .as_ref()
+        .map(|scalar| scalar.to_array_of_size(size))
+        .transpose()?
+        .unwrap_or_else(|| new_null_array(data_type, size));
+    if array.data_type() != data_type {
+        cast(&array, data_type).map_err(|e| arrow_datafusion_err!(e))
+    } else {
+        Ok(array)
+    }
+}
+// implement ignore null for evaluate_all
+fn evaluate_all_with_ignore_null(
+    array: &ArrayRef,
+    offset: i64,
+    default_value: &ScalarValue,
+    is_lag: bool,
+) -> Result<ArrayRef, DataFusionError> {
+    let valid_indices: Vec<usize> = (0..array.len())
+        .filter(|&index| array.is_valid(index))
+        .collect();
+
+    let new_array_results: Result<Vec<_>, DataFusionError> = (0..array.len())
+        .map(|id| {
+            let direction = is_lag ^ (offset > 0);
+            let result_index: Option<usize>;
+            match valid_indices.binary_search(&id) {
+                Ok(pos) => {
+                    result_index = if direction {
+                        pos.checked_add(offset as usize)
+                    } else {
+                        pos.checked_sub(offset.abs() as usize)
+                    }
+                    .and_then(|new_pos| {
+                        if new_pos < valid_indices.len() {
+                            Some(valid_indices[new_pos])
+                        } else {
+                            None
+                        }
+                    });
+                }
+                Err(pos) => {
+                    result_index = if direction {
+                        if pos < valid_indices.len() {
+                            pos.checked_add(offset as usize)
+                        } else {
+                            None
+                        }
+                    } else {
+                        if pos > 0 {
+                            pos.checked_sub(offset.abs() as usize)
+                        } else {
+                            None
+                        }
+                    }
+                    .and_then(|new_pos| {
+                        if new_pos < valid_indices.len() {
+                            Some(valid_indices[new_pos])
+                        } else {
+                            None
+                        }
+                    });
+                }
+            }
+            match result_index {
+                Some(index) => ScalarValue::try_from_array(array, index),
+                None => Ok(default_value.clone()),
+            }
+        })
+        .collect();
+
+    let new_array = new_array_results?;
+    Ok(ScalarValue::iter_to_array(new_array.into_iter())?)
+}
 // TODO: change the original arrow::compute::kernels::window::shift impl to support an optional default value
 fn shift_with_default_value(
     array: &ArrayRef,
@@ -326,14 +404,18 @@ impl PartitionEvaluator for WindowShiftEvaluator {
         values: &[ArrayRef],
         _num_rows: usize,
     ) -> Result<ArrayRef> {
-        if self.ignore_nulls {
-            return Err(exec_datafusion_err!(
-                "IGNORE NULLS mode for LAG and LEAD is not supported for WindowAggExec"
-            ));
-        }
         // LEAD, LAG window functions take single column, values will have size 1
         let value = &values[0];
-        shift_with_default_value(value, self.shift_offset, &self.default_value)
+        if !self.ignore_nulls {
+            shift_with_default_value(value, self.shift_offset, &self.default_value)
+        } else {
+            evaluate_all_with_ignore_null(
+                value,
+                self.shift_offset,
+                &self.default_value,
+                self.is_lag(),
+            )
+        }
     }
 
     fn supports_bounded_execution(&self) -> bool {
