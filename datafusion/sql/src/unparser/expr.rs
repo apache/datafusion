2cda8b0a25040959042e3e5aef9280c3cddb7df9
Michiel De Backker <mail@backkem.me>
2024-03-10 13:32:36 +0100

    fix: add license headers

--- datafusion/sql/src/unparser/expr.rs
+++ datafusion/sql/src/unparser/expr.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use datafusion_common::{not_impl_err, Column, Result, ScalarValue};
 use datafusion_expr::{
     expr::{Alias, InList, ScalarFunction, WindowFunction},
@@ -9,9 +26,10 @@ use super::Unparser;
 
 /// Convert a DataFusion [`Expr`] to `sqlparser::ast::Expr`
 ///
-/// This function is the opposite of `SqlToRel::sql_to_expr`
+/// This function is the opposite of `SqlToRel::sql_to_expr` and can
+/// be used to, among other things, convert `Expr`s to strings.
 ///
-/// Example
+/// # Example
 /// ```
 /// use datafusion_expr::{col, lit};
 /// use datafusion_sql::unparser::expr_to_sql;
