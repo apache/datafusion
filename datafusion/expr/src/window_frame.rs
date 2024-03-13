7d91a0fac2091fbfb8016d03f186713b0d22bad3
comphead <comphead@users.noreply.github.com>
2024-03-13 10:35:22 -0700

    minor: update Debug trait impl for WindowsFrame (#9587)
    * minor: update dbg for WindowsFrame
    
    * fix tests

--- datafusion/expr/src/window_frame.rs
+++ datafusion/expr/src/window_frame.rs
@@ -110,8 +110,8 @@ impl fmt::Debug for WindowFrame {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "WindowFrame {{ units: {:?}, start_bound: {:?}, end_bound: {:?} }}",
-            self.units, self.start_bound, self.end_bound
+            "WindowFrame {{ units: {:?}, start_bound: {:?}, end_bound: {:?}, is_causal: {:?} }}",
+            self.units, self.start_bound, self.end_bound, self.causal
         )?;
         Ok(())
     }
