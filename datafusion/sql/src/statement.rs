7af17baeba4365746a63e59446ee381e5f19b8d2
Kunal Kundu <kunal99kundu@gmail.com>
2024-03-13 15:51:40 +0530

    support unprefixed config format options

--- datafusion/sql/src/statement.rs
+++ datafusion/sql/src/statement.rs
@@ -855,6 +855,26 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
         let file_type = try_infer_file_type(&mut options, &statement.target)?;
         let partition_by = take_partition_by(&mut options);
 
+        match &file_type {
+            // Renames un-prefixed keys to support legacy format specific options
+            FileType::CSV | FileType::JSON | FileType::PARQUET => {
+                let prefix = format!("{}", file_type);
+                let keys_to_rename: Vec<_> = options
+                    .keys()
+                    .filter(|key| !key.starts_with(&prefix))
+                    .cloned()
+                    .collect();
+
+                for key in keys_to_rename {
+                    if let Some(value) = options.remove(&key) {
+                        let new_key = format!("{}.{}", prefix, key);
+                        options.insert(new_key, value);
+                    }
+                }
+            }
+            _ => {}
+        }
+
         Ok(LogicalPlan::Copy(CopyTo {
             input: Arc::new(input),
             output_url: statement.target,
