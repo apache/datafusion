77f737ade20a1fcc92381a5b885746fb77125c5b
jonahgao <jonahgao@msn.com>
2024-03-15 17:43:44 +0800

    fix sql_integration

--- datafusion/sql/tests/sql_integration.rs
+++ datafusion/sql/tests/sql_integration.rs
@@ -1387,46 +1387,17 @@ fn recursive_ctes() {
               select n + 1 FROM numbers WHERE N < 10
         )
         select * from numbers;";
-    let err = logical_plan(sql).expect_err("query should have failed");
-    assert_eq!(
-        "This feature is not implemented: Recursive CTEs are not enabled",
-        err.strip_backtrace()
-    );
-}
-
-#[test]
-fn recursive_ctes_enabled() {
-    let sql = "
-        WITH RECURSIVE numbers AS (
-              select 1 as n
-            UNION ALL
-              select n + 1 FROM numbers WHERE N < 10
-        )
-        select * from numbers;";
-
-    // manually setting up test here so that we can enable recursive ctes
-    let mut context = MockContextProvider::default();
-    context.options_mut().execution.enable_recursive_ctes = true;
-
-    let planner = SqlToRel::new_with_options(&context, ParserOptions::default());
-    let result = DFParser::parse_sql_with_dialect(sql, &GenericDialect {});
-    let mut ast = result.unwrap();
-
-    let plan = planner
-        .statement_to_plan(ast.pop_front().unwrap())
-        .expect("recursive cte plan creation failed");
-
-    assert_eq!(
-        format!("{plan:?}"),
+    quick_test(
+        sql,
         "Projection: numbers.n\
-        \n  SubqueryAlias: numbers\
-        \n    RecursiveQuery: is_distinct=false\
-        \n      Projection: Int64(1) AS n\
-        \n        EmptyRelation\
-        \n      Projection: numbers.n + Int64(1)\
-        \n        Filter: numbers.n < Int64(10)\
-        \n          TableScan: numbers"
-    );
+    \n  SubqueryAlias: numbers\
+    \n    RecursiveQuery: is_distinct=false\
+    \n      Projection: Int64(1) AS n\
+    \n        EmptyRelation\
+    \n      Projection: numbers.n + Int64(1)\
+    \n        Filter: numbers.n < Int64(10)\
+    \n          TableScan: numbers",
+    )
 }
 
 #[test]
@@ -2777,10 +2748,6 @@ struct MockContextProvider {
 }
 
 impl MockContextProvider {
-    fn options_mut(&mut self) -> &mut ConfigOptions {
-        &mut self.options
-    }
-
     fn with_udf(mut self, udf: ScalarUDF) -> Self {
         self.udfs.insert(udf.name().to_string(), Arc::new(udf));
         self
