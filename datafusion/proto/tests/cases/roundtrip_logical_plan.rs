73556374851658075e34ddfbf39f509c8def80cb
Eren Avsarogullari <eren@apache.org>
2024-03-17 23:12:22 -0700

    Issue-9635 - Address review comment

--- datafusion/proto/tests/cases/roundtrip_logical_plan.rs
+++ datafusion/proto/tests/cases/roundtrip_logical_plan.rs
@@ -591,6 +591,20 @@ async fn roundtrip_expr_api() -> Result<()> {
             lit(2),
         ),
         array_positions(make_array(vec![lit(4), lit(3), lit(3), lit(1)]), lit(3)),
+        array_except(
+            make_array(vec![lit(1), lit(2), lit(3)]),
+            make_array(vec![lit(1), lit(2)]),
+        ),
+        array_remove(make_array(vec![lit(4), lit(3), lit(2), lit(1)]), lit(3)),
+        array_remove_n(
+            make_array(vec![lit(1), lit(3), lit(3), lit(3)]),
+            lit(3),
+            lit(2),
+        ),
+        array_remove_all(
+            make_array(vec![lit(3), lit(3), lit(2), lit(3), lit(1)]),
+            lit(3),
+        ),
     ];
 
     // ensure expressions created with the expr api can be round tripped
