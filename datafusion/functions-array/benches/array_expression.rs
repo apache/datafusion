a445c8553248dc29304107844a91a5c14acf6081
Andrew Lamb <andrew@nerdnetworks.org>
2024-03-24 08:41:43 -0400

    Fix compilation of datafusion-array

--- datafusion/functions-array/benches/array_expression.rs
+++ datafusion/functions-array/benches/array_expression.rs
@@ -18,12 +18,11 @@
 #[macro_use]
 extern crate criterion;
 extern crate arrow;
-extern crate datafusion;
 
-mod data_utils;
+//mod data_utils;
 use crate::criterion::Criterion;
-use datafusion::functions_array::expr_fn::{array_replace_all, make_array};
 use datafusion_expr::lit;
+use datafusion_functions_array::expr_fn::{array_replace_all, make_array};
 
 fn criterion_benchmark(c: &mut Criterion) {
     // Construct large arrays for benchmarking
