99b7a552d6ebf03997afd7d06f48da1e2adc4d94
Mustafa Akur <mustafa.akur@synnada.ai>
2024-03-25 11:34:00 +0300

    Use existing const evaluator

--- datafusion/optimizer/src/lib.rs
+++ datafusion/optimizer/src/lib.rs
@@ -47,6 +47,7 @@ pub mod utils;
 pub mod test;
 
 pub use optimizer::{OptimizerConfig, OptimizerContext, OptimizerRule};
+pub use simplify_expressions::ConstEvaluator;
 pub use utils::optimize_children;
 
 mod plan_signature;
