4bf8e4adefabaa5b7d448c48e8e0097c556ad84c
Mustafa Akur <mustafa.akur@synnada.ai>
2024-03-27 12:59:36 +0300

    Add non-column expression equality tracking to filter exec

--- datafusion/physical-plan/src/lib.rs
+++ datafusion/physical-plan/src/lib.rs
@@ -33,7 +33,6 @@ use datafusion_common::config::ConfigOptions;
 use datafusion_common::utils::DataPtr;
 use datafusion_common::Result;
 use datafusion_execution::TaskContext;
-use datafusion_physical_expr::expressions::Column;
 use datafusion_physical_expr::{
     EquivalenceProperties, LexOrdering, PhysicalSortExpr, PhysicalSortRequirement,
 };
