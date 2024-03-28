774ce116fc5cc36933554af593ead59db3b3ce7d
gstvg <28798827+gstvg@users.noreply.github.com>
2024-03-28 01:10:45 +0000

    add tests and corretly handle mixed arrray and scalar values

--- datafusion/functions/src/core/named_struct.rs
+++ datafusion/functions/src/core/named_struct.rs
@@ -17,7 +17,7 @@
 
 use arrow::array::StructArray;
 use arrow::datatypes::{DataType, Field, Fields};
-use datafusion_common::{exec_err, Result, ScalarValue};
+use datafusion_common::{exec_err, internal_err, Result, ScalarValue};
 use datafusion_expr::{ColumnarValue, Expr, ExprSchemable};
 use datafusion_expr::{ScalarUDFImpl, Signature, Volatility};
 use std::any::Any;
@@ -27,40 +27,50 @@ use std::sync::Arc;
 fn named_struct_expr(args: &[ColumnarValue]) -> Result<ColumnarValue> {
     // do not accept 0 arguments.
     if args.is_empty() {
-        return exec_err!("named_struct requires at least one pair of arguments");
+        return exec_err!("named_struct requires at least one pair of arguments, got 0 instead");
     }
 
     if args.len() % 2 != 0 {
-        return exec_err!("named_struct requires an even number of arguments");
+        return exec_err!("named_struct requires an even number of arguments, got {} instead", args.len());
     }
 
-    let fields = args
+    let (names, values): (Vec<_>, Vec<_>) = args
         .chunks_exact(2)
-        .map(|chunk| {
-            let name = &chunk[0];
-            let value = &chunk[1];
+        .enumerate()
+        .map(|(i, chunk)| {
 
-            if let ColumnarValue::Scalar(ScalarValue::Utf8(Some(name))) = name {
-                let array_ref = match value {
-                    ColumnarValue::Array(array) => array.clone(),
-                    ColumnarValue::Scalar(scalar) => scalar.to_array()?.clone(),
-                };
+            let name_column = &chunk[0];
 
-                Ok((
-                    Arc::new(Field::new(
-                        name.clone(),
-                        array_ref.data_type().clone(),
-                        true,
-                    )),
-                    array_ref,
-                ))
-            } else {
-                exec_err!("named_struct even arguments must be string literals")
-            }
+            let name = match name_column {
+                ColumnarValue::Scalar(ScalarValue::Utf8(Some(name_scalar))) => name_scalar,
+                _ => return exec_err!("named_struct even arguments must be string literals, got {name_column:?} instead at position {}", i * 2)
+            };
+
+            Ok((name, chunk[1].clone()))
         })
-        .collect::<Result<Vec<_>>>()?;
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .unzip();
+
+    let arrays = ColumnarValue::values_to_arrays(&values)?;
+
+    let fields = names.into_iter()
+        .zip(arrays)
+        .map(|(name, value)| {
+            (
+                Arc::new(Field::new(
+                    name,
+                    value.data_type().clone(),
+                    true,
+                )),
+                value,
+            )
+        })
+        .collect::<Vec<_>>();
+
     Ok(ColumnarValue::Array(Arc::new(StructArray::from(fields))))
 }
+
 #[derive(Debug)]
 pub(super) struct NamedStructFunc {
     signature: Signature,
@@ -78,6 +88,7 @@ impl ScalarUDFImpl for NamedStructFunc {
     fn as_any(&self) -> &dyn Any {
         self
     }
+
     fn name(&self) -> &str {
         "named_struct"
     }
@@ -87,9 +98,7 @@ impl ScalarUDFImpl for NamedStructFunc {
     }
 
     fn return_type(&self, _arg_types: &[DataType]) -> Result<DataType> {
-        Err(datafusion_common::DataFusionError::Internal(
-            "return_type called instead of return_type_from_exprs".into(),
-        ))
+        internal_err!("named_struct: return_type called instead of return_type_from_exprs")
     }
 
     fn return_type_from_exprs(
@@ -100,23 +109,24 @@ impl ScalarUDFImpl for NamedStructFunc {
     ) -> Result<DataType> {
         // do not accept 0 arguments.
         if args.is_empty() {
-            return exec_err!("named_struct requires at least one pair of arguments");
+            return exec_err!("named_struct requires at least one pair of arguments, got 0 instead");
         }
 
         if args.len() % 2 != 0 {
-            return exec_err!("named_struct requires an even number of arguments");
+            return exec_err!("named_struct requires an even number of arguments, got {} instead", args.len());
         }
 
         let return_fields = args
             .chunks_exact(2)
-            .map(|chunk| {
+            .enumerate()
+            .map(|(i, chunk)| {
                 let name = &chunk[0];
                 let value = &chunk[1];
 
                 if let Expr::Literal(ScalarValue::Utf8(Some(name))) = name {
-                    Ok(Field::new(name.clone(), value.get_type(schema)?, true))
+                    Ok(Field::new(name, value.get_type(schema)?, true))
                 } else {
-                    exec_err!("named_struct even arguments must be string literals")
+                    exec_err!("named_struct even arguments must be string literals, got {name} instead at position {}", i * 2)
                 }
             })
             .collect::<Result<Vec<Field>>>()?;
