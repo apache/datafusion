cb19041871ed4fcdba0e84bd6becc0aa25db45bb
jonahgao <jonahgao@msn.com>
2024-03-28 17:55:51 +0800

    move cte related logic to its own mod

--- datafusion/sql/src/relation/mod.rs
+++ datafusion/sql/src/relation/mod.rs
@@ -72,7 +72,11 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
                             cte,
                             self.context_provider.get_table_source(table_ref.clone()),
                         ) {
-                            (Some(cte_plan), _) => Ok(cte_plan.clone()),
+                            (Some(cte_plan), _) => {
+                                let cte_plan = cte_plan.clone();
+                                planner_context.increment_cte_reference(table_name);
+                                Ok(cte_plan)
+                            }
                             (_, Ok(provider)) => {
                                 LogicalPlanBuilder::scan(table_ref, provider, None)?
                                     .build()
