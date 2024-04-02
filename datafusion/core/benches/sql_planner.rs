9b69be4f3a134f050435a8dd72d3e50d8556fad1
Andrew Lamb <andrew@nerdnetworks.org>
2024-04-02 05:29:22 -0400

    Move TPCH schema definition

--- datafusion/core/benches/sql_planner.rs
+++ datafusion/core/benches/sql_planner.rs
@@ -22,6 +22,7 @@ extern crate datafusion;
 
 mod data_utils;
 use crate::criterion::Criterion;
+use crate::data_utils::tpch_schemas;
 use arrow::datatypes::{DataType, Field, Fields, Schema};
 use datafusion::datasource::MemTable;
 use datafusion::execution::context::SessionContext;
@@ -48,114 +49,16 @@ fn physical_plan(ctx: &SessionContext, sql: &str) {
 }
 
 /// Create schema with the specified number of columns
-pub fn create_schema(column_prefix: &str, num_columns: usize) -> Schema {
+fn create_schema(column_prefix: &str, num_columns: usize) -> Schema {
     let fields: Fields = (0..num_columns)
         .map(|i| Field::new(format!("{column_prefix}{i}"), DataType::Int32, true))
         .collect();
     Schema::new(fields)
 }
 
-pub fn create_table_provider(column_prefix: &str, num_columns: usize) -> Arc<MemTable> {
+fn create_table_provider(column_prefix: &str, num_columns: usize) -> Arc<MemTable> {
     let schema = Arc::new(create_schema(column_prefix, num_columns));
     MemTable::try_new(schema, vec![]).map(Arc::new).unwrap()
-}
-
-pub fn create_tpch_schemas() -> [(String, Schema); 8] {
-    let lineitem_schema = Schema::new(vec![
-        Field::new("l_orderkey", DataType::Int64, false),
-        Field::new("l_partkey", DataType::Int64, false),
-        Field::new("l_suppkey", DataType::Int64, false),
-        Field::new("l_linenumber", DataType::Int32, false),
-        Field::new("l_quantity", DataType::Decimal128(15, 2), false),
-        Field::new("l_extendedprice", DataType::Decimal128(15, 2), false),
-        Field::new("l_discount", DataType::Decimal128(15, 2), false),
-        Field::new("l_tax", DataType::Decimal128(15, 2), false),
-        Field::new("l_returnflag", DataType::Utf8, false),
-        Field::new("l_linestatus", DataType::Utf8, false),
-        Field::new("l_shipdate", DataType::Date32, false),
-        Field::new("l_commitdate", DataType::Date32, false),
-        Field::new("l_receiptdate", DataType::Date32, false),
-        Field::new("l_shipinstruct", DataType::Utf8, false),
-        Field::new("l_shipmode", DataType::Utf8, false),
-        Field::new("l_comment", DataType::Utf8, false),
-    ]);
-
-    let orders_schema = Schema::new(vec![
-        Field::new("o_orderkey", DataType::Int64, false),
-        Field::new("o_custkey", DataType::Int64, false),
-        Field::new("o_orderstatus", DataType::Utf8, false),
-        Field::new("o_totalprice", DataType::Decimal128(15, 2), false),
-        Field::new("o_orderdate", DataType::Date32, false),
-        Field::new("o_orderpriority", DataType::Utf8, false),
-        Field::new("o_clerk", DataType::Utf8, false),
-        Field::new("o_shippriority", DataType::Int32, false),
-        Field::new("o_comment", DataType::Utf8, false),
-    ]);
-
-    let part_schema = Schema::new(vec![
-        Field::new("p_partkey", DataType::Int64, false),
-        Field::new("p_name", DataType::Utf8, false),
-        Field::new("p_mfgr", DataType::Utf8, false),
-        Field::new("p_brand", DataType::Utf8, false),
-        Field::new("p_type", DataType::Utf8, false),
-        Field::new("p_size", DataType::Int32, false),
-        Field::new("p_container", DataType::Utf8, false),
-        Field::new("p_retailprice", DataType::Decimal128(15, 2), false),
-        Field::new("p_comment", DataType::Utf8, false),
-    ]);
-
-    let supplier_schema = Schema::new(vec![
-        Field::new("s_suppkey", DataType::Int64, false),
-        Field::new("s_name", DataType::Utf8, false),
-        Field::new("s_address", DataType::Utf8, false),
-        Field::new("s_nationkey", DataType::Int64, false),
-        Field::new("s_phone", DataType::Utf8, false),
-        Field::new("s_acctbal", DataType::Decimal128(15, 2), false),
-        Field::new("s_comment", DataType::Utf8, false),
-    ]);
-
-    let partsupp_schema = Schema::new(vec![
-        Field::new("ps_partkey", DataType::Int64, false),
-        Field::new("ps_suppkey", DataType::Int64, false),
-        Field::new("ps_availqty", DataType::Int32, false),
-        Field::new("ps_supplycost", DataType::Decimal128(15, 2), false),
-        Field::new("ps_comment", DataType::Utf8, false),
-    ]);
-
-    let customer_schema = Schema::new(vec![
-        Field::new("c_custkey", DataType::Int64, false),
-        Field::new("c_name", DataType::Utf8, false),
-        Field::new("c_address", DataType::Utf8, false),
-        Field::new("c_nationkey", DataType::Int64, false),
-        Field::new("c_phone", DataType::Utf8, false),
-        Field::new("c_acctbal", DataType::Decimal128(15, 2), false),
-        Field::new("c_mktsegment", DataType::Utf8, false),
-        Field::new("c_comment", DataType::Utf8, false),
-    ]);
-
-    let nation_schema = Schema::new(vec![
-        Field::new("n_nationkey", DataType::Int64, false),
-        Field::new("n_name", DataType::Utf8, false),
-        Field::new("n_regionkey", DataType::Int64, false),
-        Field::new("n_comment", DataType::Utf8, false),
-    ]);
-
-    let region_schema = Schema::new(vec![
-        Field::new("r_regionkey", DataType::Int64, false),
-        Field::new("r_name", DataType::Utf8, false),
-        Field::new("r_comment", DataType::Utf8, false),
-    ]);
-
-    [
-        ("lineitem".to_string(), lineitem_schema),
-        ("orders".to_string(), orders_schema),
-        ("part".to_string(), part_schema),
-        ("supplier".to_string(), supplier_schema),
-        ("partsupp".to_string(), partsupp_schema),
-        ("customer".to_string(), customer_schema),
-        ("nation".to_string(), nation_schema),
-        ("region".to_string(), region_schema),
-    ]
 }
 
 fn create_context() -> SessionContext {
@@ -169,8 +72,7 @@ fn create_context() -> SessionContext {
     ctx.register_table("t1000", create_table_provider("d", 1000))
         .unwrap();
 
-    let tpch_schemas = create_tpch_schemas();
-    tpch_schemas.iter().for_each(|(name, schema)| {
+    tpch_schemas().iter().for_each(|(name, schema)| {
         ctx.register_table(
             name,
             Arc::new(MemTable::try_new(Arc::new(schema.clone()), vec![]).unwrap()),
