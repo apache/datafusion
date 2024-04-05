8f6333b66c352a3d919bec5c0fece9553d7b9561
Andrew Lamb <andrew@nerdnetworks.org>
2024-04-05 16:37:16 -0400

    Minor: Return InternalError rather than panic for 'NamedStructField should be rewritten in OperatorToFunction'

--- datafusion/physical-expr/src/planner.rs
+++ datafusion/physical-expr/src/planner.rs
@@ -286,19 +286,23 @@ pub fn create_physical_expr(
         Expr::GetIndexedField(GetIndexedField { expr: _, field }) => {
             match field {
                 GetFieldAccess::NamedStructField { name: _ } => {
-                    unreachable!(
+                    return internal_err!(
                         "NamedStructField should be rewritten in OperatorToFunction"
                     )
                 }
                 GetFieldAccess::ListIndex { key: _ } => {
-                    unreachable!("ListIndex should be rewritten in OperatorToFunction")
+                    return internal_err!(
+                        "ListIndex should be rewritten in OperatorToFunction"
+                    )
                 }
                 GetFieldAccess::ListRange {
                     start: _,
                     stop: _,
                     stride: _,
                 } => {
-                    unreachable!("ListRange should be rewritten in OperatorToFunction")
+                    return internal_err!(
+                        "ListRange should be rewritten in OperatorToFunction"
+                    )
                 }
             };
         }
