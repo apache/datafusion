2f1c3ab679fd553ea9e18067ed894fc6cf1f567a
Jay Zhan <jayzhan211@gmail.com>
2024-04-07 10:25:09 +0800

    Move First Value UDAF and builtin first / last function to `aggregate-functions` (#9960)
    * backup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move PhysicalExpr
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move physical sort
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup dependencies
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * add readme
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * disable doc test
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move column
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fmt
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move aggregatexp
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move other two utils
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * license
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * switch to ignore
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move reverse order
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rename to common
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * backup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move acc to first value
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move builtin expr too
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * use macro
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fmt
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix doc
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * add todo
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rm comments
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rm unused
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rm unused code
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * change to private
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix lock
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * support roundtrip
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * remmove old format state
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move aggregate related things to aggr crate
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * move back to common
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * taplo
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rm comment
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * lock
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    ---------
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- datafusion/physical-plan/src/lib.rs
+++ datafusion/physical-plan/src/lib.rs
@@ -66,7 +66,6 @@ pub mod sorts;
 pub mod stream;
 pub mod streaming;
 pub mod tree_node;
-pub mod udaf;
 pub mod union;
 pub mod unnest;
 pub mod values;
@@ -91,6 +90,11 @@ pub use datafusion_physical_expr::{
 // Backwards compatibility
 pub use crate::stream::EmptyRecordBatchStream;
 pub use datafusion_execution::{RecordBatchStream, SendableRecordBatchStream};
+pub mod udaf {
+    pub use datafusion_physical_expr_common::aggregate::{
+        create_aggregate_expr, AggregateFunctionExpr,
+    };
+}
 
 /// Represent nodes in the DataFusion Physical Plan.
 ///
