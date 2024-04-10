c79e0b908987c1a9eba9bf812f5e7ebc44a4b653
Andrew Lamb <andrew@nerdnetworks.org>
2024-04-10 05:42:52 -0400

    touchups, add apply_subqueries, map_subqueries

--- datafusion/expr/src/logical_plan/tree_node.rs
+++ datafusion/expr/src/logical_plan/tree_node.rs
@@ -21,19 +21,21 @@
 //! * [`LogicalPlan::visit`]: recursively visit the node and all of its inputs
 //! * [`LogicalPlan::visit_with_subqueries`]: recursively visit the node and all of its inputs, including subqueries
 //! * [`LogicalPlan::apply_children`]: recursively visit all inputs of this node
-//! * [`LogicalPlan::apply_with_subqueries`]: recursively visit all inputs and embedded subqueries.
 //! * [`LogicalPlan::apply_expressions`]: (non recursively) visit all expressions of this node
+//! * [`LogicalPlan::apply_subqueries`]: (non recursively) visit all subqueries of this node
+//! * [`LogicalPlan::apply_with_subqueries`]: recursively visit all inputs and embedded subqueries.
 //!
 //! Rewriting (update) APIs:
 //! * [`LogicalPlan::exists`]: search for an expression in a plan
 //! * [`LogicalPlan::rewrite`]: recursively rewrite the node and all of its inputs
-//! * [`LogicalPlan::rewrite_with_subqueries`]: recursively rewrite the node and all of its inputs, including subqueries
 //! * [`LogicalPlan::map_children`]: recursively rewrite all inputs of this node
 //! * [`LogicalPlan::map_expressions`]: (non recursively) visit all expressions of this node
+//! * [`LogicalPlan::map_subqueries`]: (non recursively) rewrite all subqueries of this node
+//! * [`LogicalPlan::rewrite_with_subqueries`]: recursively rewrite the node and all of its inputs, including subqueries
 //!
 //! (Re)creation APIs (these require substantial cloning and thus are slow):
 //! * [`LogicalPlan::with_new_exprs`]: Create a new plan with different expressions
-//! * [`LogicalPlan::expressions`]: Create a new plan with different expressions
+//! * [`LogicalPlan::expressions`]: Return a copy of the plan's expressions
 use crate::{
     dml::CopyTo, Aggregate, Analyze, CreateMemoryTable, CreateView, CrossJoin,
     DdlStatement, Distinct, DistinctOn, DmlStatement, Explain, Extension, Filter, Join,
