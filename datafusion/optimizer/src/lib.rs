8ee38aad1b0c678283da5c0d029c6e1f49192216
Kaviraj <kavirajkanagaraj@gmail.com>
2024-04-14 15:37:40 +0200

    remove `push_down_projections.rs` from `lib.rs`
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- datafusion/optimizer/src/lib.rs
+++ datafusion/optimizer/src/lib.rs
@@ -47,7 +47,6 @@ pub mod optimizer;
 pub mod propagate_empty_relation;
 pub mod push_down_filter;
 pub mod push_down_limit;
-pub mod push_down_projection;
 pub mod replace_distinct_aggregate;
 pub mod rewrite_disjunctive_predicate;
 pub mod scalar_subquery_to_join;
