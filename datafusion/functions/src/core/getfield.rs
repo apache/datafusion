c972d7d27ed6e8544af3b49712ad92d685462398
Duong Cong Toai <duongcongtoai.fjc@gmail.com>
2024-04-23 23:02:52 +0200

    chore: temp code for demonstration

--- datafusion/functions/src/core/getfield.rs
+++ datafusion/functions/src/core/getfield.rs
@@ -15,14 +15,21 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use arrow::array::{Scalar, StringArray};
+use arrow::array::{
+    make_builder, Array, ArrayBuilder, AsArray, BooleanArray, BooleanBuilder, Datum,
+    NullArray, NullBuilder, Scalar, StringArray, StringBuilder, StructArray,
+};
+use arrow::compute::{is_null, FilterBuilder};
 use arrow::datatypes::DataType;
+use arrow::ipc::BoolBuilder;
 use datafusion_common::cast::{as_map_array, as_struct_array};
 use datafusion_common::{exec_err, ExprSchema, Result, ScalarValue};
 use datafusion_expr::field_util::GetFieldAccessSchema;
 use datafusion_expr::{ColumnarValue, Expr, ExprSchemable};
 use datafusion_expr::{ScalarUDFImpl, Signature, Volatility};
 use std::any::Any;
+use std::borrow::Borrow;
+use std::sync::Arc;
 
 #[derive(Debug)]
 pub struct GetFieldFunc {
@@ -107,29 +114,56 @@ impl ScalarUDFImpl for GetFieldFunc {
                 );
             }
         };
+
         match (array.data_type(), name) {
-                (DataType::Map(_, _), ScalarValue::Utf8(Some(k))) => {
-                    let map_array = as_map_array(array.as_ref())?;
-                    let key_scalar = Scalar::new(StringArray::from(vec![k.clone()]));
-                    let keys = arrow::compute::kernels::cmp::eq(&key_scalar, map_array.keys())?;
-                    let entries = arrow::compute::filter(map_array.entries(), &keys)?;
-                    let entries_struct_array = as_struct_array(entries.as_ref())?;
-                    Ok(ColumnarValue::Array(entries_struct_array.column(1).clone()))
-                }
-                (DataType::Struct(_), ScalarValue::Utf8(Some(k))) => {
-                    let as_struct_array = as_struct_array(&array)?;
-                    match as_struct_array.column_by_name(k) {
-                        None => exec_err!(
-                            "get indexed field {k} not found in struct"),
-                        Some(col) => Ok(ColumnarValue::Array(col.clone()))
+            (DataType::Map(_, _), ScalarValue::Utf8(Some(k))) => {
+                let map_array = as_map_array(array.as_ref())?;
+                let key_scalar: Scalar<arrow::array::GenericByteArray<arrow::datatypes::GenericStringType<i32>>> = Scalar::new(StringArray::from(vec![k.clone()]));
+                let keys = arrow::compute::kernels::cmp::eq(&key_scalar, map_array.keys())?;
+
+                // TODO: how to determine the type of this builder
+                let mut temp_builder= StringBuilder::new();
+
+                for entry in 0..map_array.len(){
+                    let end = map_array.value_offsets()[entry + 1] as usize;
+                    let start = map_array.value_offsets()[entry] as usize;
+
+                    // child_struct is a subset of the original struct 
+                    let child_struct = map_array.value(entry);
+
+                    // one key in this child struct matching the input key
+                    let entries = arrow::compute::filter(
+                        dict.borrow(),
+                    &keys.slice(start,end-start))?;
+                    // at least one key matched
+                    if entries.len() != 1 {
+                        temp_builder.append_null();
+                        continue
                     }
+                    // basically one row after filting
+                    let entries_struct_array = as_struct_array(entries.as_ref())?;
+                    let str = entries_struct_array
+                        .column(1).as_any().downcast_ref::<StringArray>().unwrap();
+                    temp_builder.append_value(str.value(0));
                 }
-                (DataType::Struct(_), name) => exec_err!(
-                    "get indexed field is only possible on struct with utf8 indexes. \
-                             Tried with {name:?} index"),
-                (dt, name) => exec_err!(
-                                "get indexed field is only possible on lists with int64 indexes or struct \
-                                         with utf8 indexes. Tried {dt:?} with {name:?} index"),
+
+                Ok(ColumnarValue::Array(Arc::new(temp_builder.finish())))
             }
+            (DataType::Struct(_), ScalarValue::Utf8(Some(k))) => {
+                let as_struct_array = as_struct_array(&array)?;
+                match as_struct_array.column_by_name(k) {
+                    None => exec_err!("get indexed field {k} not found in struct"),
+                    Some(col) => Ok(ColumnarValue::Array(col.clone())),
+                }
+            }
+            (DataType::Struct(_), name) => exec_err!(
+                "get indexed field is only possible on struct with utf8 indexes. \
+                             Tried with {name:?} index"
+            ),
+            (dt, name) => exec_err!(
+                "get indexed field is only possible on lists with int64 indexes or struct \
+                                         with utf8 indexes. Tried {dt:?} with {name:?} index"
+            ),
+        }
     }
 }
