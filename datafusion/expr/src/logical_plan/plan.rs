9dd9a2ff3949d31de877498d1c1d4b5ee4213340
Peter Toth <peter.toth@gmail.com>
2024-05-01 11:13:38 +0200

    Revert "fix(9870): common expression elimination optimization, should always re-find the correct expression during re-write. (#9871)"
    This reverts commit cd7a00b08309f7229073e4bba686d6271726ab1c.

--- datafusion/expr/src/logical_plan/plan.rs
+++ datafusion/expr/src/logical_plan/plan.rs
@@ -2245,7 +2245,7 @@ impl DistinctOn {
 
 /// Aggregates its input based on a set of grouping and aggregate
 /// expressions (e.g. SUM).
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 // mark non_exhaustive to encourage use of try_new/new()
 #[non_exhaustive]
 pub struct Aggregate {
