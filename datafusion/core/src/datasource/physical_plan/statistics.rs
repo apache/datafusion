a2c9b4e44e3363eaf518c0c7c67e528866a34f6a
suremarc <8771538+suremarc@users.noreply.github.com>
2024-05-01 01:40:24 -0500

    fix license

--- datafusion/core/src/datasource/physical_plan/statistics.rs
+++ datafusion/core/src/datasource/physical_plan/statistics.rs
@@ -1,10 +1,27 @@
-/*
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
-Use statistics to optimize physical planning.
-
-Currently, this module houses code to sort file groups if they are non-overlapping with
-respect to the required sort order. See [`MinMaxStatistics`]
-
+/*!
+ *
+ * Use statistics to optimize physical planning.
+ *
+ * Currently, this module houses code to sort file groups if they are non-overlapping with
+ * respect to the required sort order. See [`MinMaxStatistics`]
+ *
 */
 
 use std::sync::Arc;
