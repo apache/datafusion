3d0ca6fed4ec6c5a837f09309b9171e6686e3431
jayzhan211 <jayzhan211@gmail.com>
2024-05-12 14:01:10 +0800

    file
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- datafusion-examples/examples/regexp.rs
+++ datafusion-examples/examples/regexp.rs
@@ -33,7 +33,7 @@ async fn main() -> Result<()> {
     let ctx = SessionContext::new();
     ctx.register_csv(
         "examples",
-        "datafusion/physical-expr/tests/data/regex.csv",
+        "../../datafusion/physical-expr/tests/data/regex.csv",
         CsvReadOptions::new(),
     )
     .await?;
