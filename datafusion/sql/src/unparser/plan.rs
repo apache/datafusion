288a67163bf57c16cea59e6070a51dc2d4ef9941
Junhao Liu <junhaoliu2023@gmail.com>
2024-05-27 20:49:28 -0700

    cargo fmt

--- datafusion/sql/src/unparser/plan.rs
+++ datafusion/sql/src/unparser/plan.rs
@@ -16,8 +16,10 @@
 // under the License.
 
 use datafusion_common::{internal_err, not_impl_err, plan_err, DataFusionError, Result};
-use datafusion_expr::{expr::Alias, Distinct, Expr, JoinConstraint, JoinType, LogicalPlan};
-use sqlparser::ast::{self};
+use datafusion_expr::{
+    expr::Alias, Distinct, Expr, JoinConstraint, JoinType, LogicalPlan,
+};
+use sqlparser::ast::{self, SetExpr};
 
 use crate::unparser::utils::unproject_agg_exprs;
 
@@ -273,25 +275,22 @@ impl Unparser<'_> {
             }
             LogicalPlan::Distinct(distinct) => {
                 let now_distinct = match distinct {
-                    Distinct::All(_) => {
-                        ast::Distinct::Distinct
-                    },
+                    Distinct::All(_) => ast::Distinct::Distinct,
                     Distinct::On(on) => {
-                        let exprs = on.on_expr.iter().map(|e| self.expr_to_sql(e)).collect::<Result<Vec<_>>>()?;
+                        let exprs = on
+                            .on_expr
+                            .iter()
+                            .map(|e| self.expr_to_sql(e))
+                            .collect::<Result<Vec<_>>>()?;
                         ast::Distinct::On(exprs)
-                    },
+                    }
                 };
                 let input = match distinct {
                     Distinct::All(input) => input,
                     Distinct::On(on) => on.input.as_ref(),
                 };
                 select.distinct(Some(now_distinct));
-                self.select_to_sql_recursively(
-                    input,
-                    query,
-                    select,
-                    relation,
-                )
+                self.select_to_sql_recursively(input, query, select, relation)
             }
             LogicalPlan::Join(join) => {
                 match join.join_constraint {
