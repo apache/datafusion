423a7ca2b52209022d8b1c7978cd05343b42ec85
Andrew Lamb <andrew@nerdnetworks.org>
2024-06-19 13:58:32 -0400

    only inline unwrap_arc

--- datafusion/expr/src/logical_plan/tree_node.rs
+++ datafusion/expr/src/logical_plan/tree_node.rs
@@ -380,7 +380,6 @@ pub fn unwrap_arc(plan: Arc<LogicalPlan>) -> LogicalPlan {
 }
 
 /// Applies `f` to rewrite a `Arc<LogicalPlan>` without copying, if possible
-#[inline(always)]
 fn rewrite_arc<F: FnMut(LogicalPlan) -> Result<Transformed<LogicalPlan>>>(
     plan: Arc<LogicalPlan>,
     mut f: F,
@@ -389,7 +388,6 @@ fn rewrite_arc<F: FnMut(LogicalPlan) -> Result<Transformed<LogicalPlan>>>(
 }
 
 /// rewrite a `Vec` of `Arc<LogicalPlan>` without copying, if possible
-#[inline(always)]
 fn rewrite_arcs<F: FnMut(LogicalPlan) -> Result<Transformed<LogicalPlan>>>(
     input_plans: Vec<Arc<LogicalPlan>>,
     mut f: F,
