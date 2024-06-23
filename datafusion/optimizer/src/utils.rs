636829867e0cf5f893569db77990daedb84788b7
Andrew Lamb <andrew@nerdnetworks.org>
2024-06-23 13:13:36 -0400

    Migrate more code from `Expr::to_columns`  to `Expr::column_refs` (#11067)
    * Migrate code from `Expr::to_columns`  to `Expr::column_refs`

--- datafusion/optimizer/src/utils.rs
+++ datafusion/optimizer/src/utils.rs
@@ -17,7 +17,7 @@
 
 //! Utility functions leveraged by the query optimizer rules
 
-use std::collections::{BTreeSet, HashMap};
+use std::collections::{BTreeSet, HashMap, HashSet};
 
 use crate::{OptimizerConfig, OptimizerRule};
 
@@ -64,6 +64,17 @@ pub fn optimize_children(
     } else {
         Ok(None)
     }
+}
+
+/// Returns true if `expr` contains all columns in `schema_cols`
+pub(crate) fn has_all_column_refs(expr: &Expr, schema_cols: &HashSet<Column>) -> bool {
+    let column_refs = expr.column_refs();
+    // note can't use HashSet::intersect because of different types (owned vs References)
+    schema_cols
+        .iter()
+        .filter(|c| column_refs.contains(c))
+        .count()
+        == column_refs.len()
 }
 
 pub(crate) fn collect_subquery_cols(
