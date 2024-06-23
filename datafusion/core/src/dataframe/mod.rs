6f10dbc124de6b999f47a7f45e5dc1c486f8a4dd
Xin Li <33629085+xinlifoobar@users.noreply.github.com>
2024-06-23 18:11:36 +0800

    Support parsing SQL strings to Exprs (#10995)
    * Draft parse_sql
    
    * Allow stirng pass
    
    * Complete sql to expr support
    
    * Add examples
    
    * Add unit tests
    
    * Fix format
    
    * Remove async for trival operation and add parquet demo
    
    * Fix comments
    
    * fix comments
    
    * fix comments
    
    * Fix doc link

--- datafusion/core/src/dataframe/mod.rs
+++ datafusion/core/src/dataframe/mod.rs
@@ -177,6 +177,33 @@ impl DataFrame {
         }
     }
 
+    /// Creates logical expression from a SQL query text.
+    /// The expression is created and processed againt the current schema.
+    ///
+    /// # Example: Parsing SQL queries
+    /// ```
+    /// # use arrow::datatypes::{DataType, Field, Schema};
+    /// # use datafusion::prelude::*;
+    /// # use datafusion_common::{DFSchema, Result};
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<()> {
+    /// // datafusion will parse number as i64 first.
+    /// let sql = "a > 1 and b in (1, 10)";
+    /// let expected = col("a").gt(lit(1 as i64))
+    ///   .and(col("b").in_list(vec![lit(1 as i64), lit(10 as i64)], false));
+    /// let ctx = SessionContext::new();
+    /// let df = ctx.read_csv("tests/data/example.csv", CsvReadOptions::new()).await?;
+    /// let expr = df.parse_sql_expr(sql)?;
+    /// assert_eq!(expected, expr);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn parse_sql_expr(&self, sql: &str) -> Result<Expr> {
+        let df_schema = self.schema();
+
+        self.session_state.create_logical_expr(sql, df_schema)
+    }
+
     /// Consume the DataFrame and produce a physical plan
     pub async fn create_physical_plan(self) -> Result<Arc<dyn ExecutionPlan>> {
         self.session_state.create_physical_plan(&self.plan).await
