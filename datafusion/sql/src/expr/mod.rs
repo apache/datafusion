fe66daaa81738dc60afc297a79897faaaa127724
Jay Zhan <jayzhan211@gmail.com>
2024-07-04 07:29:41 +0800

    Support DuckDB style struct syntax (#11214)
    * struct literal
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * add nested
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fmt
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rm useless comment
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * switch to NYI error, derive debug/clone
    
    * improve documentation strings
    
    * Avoid stack overflow by putting code in a new function
    
    ---------
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/sql/src/expr/mod.rs
+++ datafusion/sql/src/expr/mod.rs
@@ -18,8 +18,11 @@
 use arrow_schema::DataType;
 use arrow_schema::TimeUnit;
 use datafusion_expr::planner::PlannerResult;
+use datafusion_expr::planner::RawDictionaryExpr;
 use datafusion_expr::planner::RawFieldAccessExpr;
-use sqlparser::ast::{CastKind, Expr as SQLExpr, Subscript, TrimWhereField, Value};
+use sqlparser::ast::{
+    CastKind, DictionaryField, Expr as SQLExpr, Subscript, TrimWhereField, Value,
+};
 
 use datafusion_common::{
     internal_datafusion_err, internal_err, not_impl_err, plan_err, DFSchema, Result,
@@ -619,8 +622,40 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
                     }
                 },
             ))),
+            SQLExpr::Dictionary(fields) => {
+                self.try_plan_dictionary_literal(fields, schema, planner_context)
+            }
             _ => not_impl_err!("Unsupported ast node in sqltorel: {sql:?}"),
         }
+    }
+
+    fn try_plan_dictionary_literal(
+        &self,
+        fields: Vec<DictionaryField>,
+        schema: &DFSchema,
+        planner_context: &mut PlannerContext,
+    ) -> Result<Expr> {
+        let mut keys = vec![];
+        let mut values = vec![];
+        for field in fields {
+            let key = lit(field.key.value);
+            let value =
+                self.sql_expr_to_logical_expr(*field.value, schema, planner_context)?;
+            keys.push(key);
+            values.push(value);
+        }
+
+        let mut raw_expr = RawDictionaryExpr { keys, values };
+
+        for planner in self.planners.iter() {
+            match planner.plan_dictionary_literal(raw_expr, schema)? {
+                PlannerResult::Planned(expr) => {
+                    return Ok(expr);
+                }
+                PlannerResult::Original(expr) => raw_expr = expr,
+            }
+        }
+        not_impl_err!("Unsupported dictionary literal: {raw_expr:?}")
     }
 
     /// Parses a struct(..) expression
