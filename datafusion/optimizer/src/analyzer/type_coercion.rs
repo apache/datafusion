d91a03f495b0856c1c2bfe5fc4db2ed1fe5352bf
Xin Li <33629085+xinlifoobar@users.noreply.github.com>
2024-07-18 02:44:15 -0700

    Move handlign of NULL literals in where clause to type coercion pass  (#11491)
    * Revert "Support `NULL` literals in where clause  (#11266)"
    
    This reverts commit fa0191772e87e04da2598aedb7fe11dd49f88f88.
    
    * Followup Support NULL literals in where clause
    
    * misc err change
    
    * adopt comparison_coercion
    
    * Fix comments
    
    * Fix comments

--- datafusion/optimizer/src/analyzer/type_coercion.rs
+++ datafusion/optimizer/src/analyzer/type_coercion.rs
@@ -84,7 +84,7 @@ impl AnalyzerRule for TypeCoercion {
 /// Assumes that children have already been optimized
 fn analyze_internal(
     external_schema: &DFSchema,
-    plan: LogicalPlan,
+    mut plan: LogicalPlan,
 ) -> Result<Transformed<LogicalPlan>> {
     // get schema representing all available input fields. This is used for data type
     // resolution only, so order does not matter here
@@ -102,6 +102,16 @@ fn analyze_internal(
     // like case:
     // select t2.c2 from t1 where t1.c1 in (select t2.c1 from t2 where t2.c2=t1.c3)
     schema.merge(external_schema);
+
+    if let LogicalPlan::Filter(filter) = &mut plan {
+        if let Ok(new_predicate) = filter
+            .predicate
+            .clone()
+            .cast_to(&DataType::Boolean, filter.input.schema())
+        {
+            filter.predicate = new_predicate;
+        }
+    }
 
     let mut expr_rewrite = TypeCoercionRewriter::new(&schema);
 
