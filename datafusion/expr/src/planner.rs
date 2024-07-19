3076d548d70aa14422cb8b696aca2c93a4ab97c0
Dharan Aditya <dharan.aditya@gmail.com>
2024-07-19 11:40:49 +0530

    formatting

--- datafusion/expr/src/planner.rs
+++ datafusion/expr/src/planner.rs
@@ -186,7 +186,9 @@ pub trait ExprPlanner: Send + Sync {
         _qualifier: Option<&TableReference>,
         _nested_names: &[String],
     ) -> Result<PlannerResult<Vec<Expr>>> {
-        not_impl_err!("Default planner compound identifier hasn't been implemented for ExprPlanner")
+        not_impl_err!(
+            "Default planner compound identifier hasn't been implemented for ExprPlanner"
+        )
     }
 }
 
