8d8732ce198ea43d2ce0f2240a3be610f754ac8d
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2024-07-19 06:01:49 -0400

    Add a config to force using string view in benchmark (#11514)
    * add a knob to force string view in benchmark
    
    * fix sql logic test
    
    * update doc
    
    * fix ci
    
    * fix ci only test
    
    * Update benchmarks/src/util/options.rs
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>
    
    * Update datafusion/common/src/config.rs
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>
    
    * update tests
    
    ---------
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- benchmarks/src/tpch/run.rs
+++ benchmarks/src/tpch/run.rs
@@ -120,6 +120,7 @@ impl RunOpt {
             .config()
             .with_collect_statistics(!self.disable_statistics);
         config.options_mut().optimizer.prefer_hash_join = self.prefer_hash_join;
+        config.options_mut().execution.schema_force_string_view = self.common.string_view;
         let ctx = SessionContext::new_with_config(config);
 
         // register tables
@@ -339,6 +340,7 @@ mod tests {
             partitions: Some(2),
             batch_size: 8192,
             debug: false,
+            string_view: false,
         };
         let opt = RunOpt {
             query: Some(query),
@@ -372,6 +374,7 @@ mod tests {
             partitions: Some(2),
             batch_size: 8192,
             debug: false,
+            string_view: false,
         };
         let opt = RunOpt {
             query: Some(query),
