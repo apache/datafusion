fbde31fcc48ed8ecd0d05723bf0473c0ac559e82
Tim Saucer <timsaucer@gmail.com>
2024-07-23 09:12:00 -0400

    Window helper functions should return Expr

--- datafusion/expr/src/window_function.rs
+++ datafusion/expr/src/window_function.rs
@@ -5,43 +5,43 @@ use crate::{expr::WindowFunction, BuiltInWindowFunction, Expr, Literal};
 /// Create an expression to represent the `row_number` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn row_number() -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::RowNumber, vec![])
+pub fn row_number() -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::RowNumber, vec![]))
 }
 
 /// Create an expression to represent the `rank` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn rank() -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::Rank, vec![])
+pub fn rank() -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::Rank, vec![]))
 }
 
 /// Create an expression to represent the `dense_rank` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn dense_rank() -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::DenseRank, vec![])
+pub fn dense_rank() -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::DenseRank, vec![]))
 }
 
 /// Create an expression to represent the `percent_rank` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn percent_rank() -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::PercentRank, vec![])
+pub fn percent_rank() -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::PercentRank, vec![]))
 }
 
 /// Create an expression to represent the `cume_dist` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn cume_dist() -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::CumeDist, vec![])
+pub fn cume_dist() -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::CumeDist, vec![]))
 }
 
 /// Create an expression to represent the `ntile` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn ntile(arg: Expr) -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::Ntile, vec![arg])
+pub fn ntile(arg: Expr) -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::Ntile, vec![arg]))
 }
 
 /// Create an expression to represent the `lag` window function
@@ -51,15 +51,15 @@ pub fn lag(
     arg: Expr,
     shift_offset: Option<i64>,
     default_value: Option<ScalarValue>,
-) -> WindowFunction {
+) -> Expr {
     let shift_offset_lit = shift_offset
         .map(|v| v.lit())
         .unwrap_or(ScalarValue::Null.lit());
     let default_lit = default_value.unwrap_or(ScalarValue::Null).lit();
-    WindowFunction::new(
+    Expr::WindowFunction(WindowFunction::new(
         BuiltInWindowFunction::Lag,
         vec![arg, shift_offset_lit, default_lit],
-    )
+    ))
 }
 
 /// Create an expression to represent the `lead` window function
@@ -69,34 +69,34 @@ pub fn lead(
     arg: Expr,
     shift_offset: Option<i64>,
     default_value: Option<ScalarValue>,
-) -> WindowFunction {
+) -> Expr {
     let shift_offset_lit = shift_offset
         .map(|v| v.lit())
         .unwrap_or(ScalarValue::Null.lit());
     let default_lit = default_value.unwrap_or(ScalarValue::Null).lit();
-    WindowFunction::new(
+    Expr::WindowFunction(WindowFunction::new(
         BuiltInWindowFunction::Lead,
         vec![arg, shift_offset_lit, default_lit],
-    )
+    ))
 }
 
 /// Create an expression to represent the `first_value` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn first_value(arg: Expr) -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::FirstValue, vec![arg])
+pub fn first_value(arg: Expr) -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::FirstValue, vec![arg]))
 }
 
 /// Create an expression to represent the `last_value` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn last_value(arg: Expr) -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::LastValue, vec![arg])
+pub fn last_value(arg: Expr) -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::LastValue, vec![arg]))
 }
 
 /// Create an expression to represent the `nth_value` window function
 ///
 /// Note: call [`WindowFunction::build]` to create an [`Expr`]
-pub fn nth_value(arg: Expr, n: i64) -> WindowFunction {
-    WindowFunction::new(BuiltInWindowFunction::NthValue, vec![arg, n.lit()])
+pub fn nth_value(arg: Expr, n: i64) -> Expr {
+    Expr::WindowFunction(WindowFunction::new(BuiltInWindowFunction::NthValue, vec![arg, n.lit()]))
 }
