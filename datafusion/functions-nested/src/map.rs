70e46e29aa68b7c562065985d1687fccc6b84d81
Dharan Aditya <dharan.aditya@gmail.com>
2024-07-29 14:03:16 +0530

    crude impl to support array

--- datafusion/functions-nested/src/map.rs
+++ datafusion/functions-nested/src/map.rs
@@ -15,17 +15,21 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use crate::make_array::make_array;
-use arrow::array::ArrayData;
-use arrow_array::{Array, ArrayRef, MapArray, StructArray};
-use arrow_buffer::{Buffer, ToByteSlice};
-use arrow_schema::{DataType, Field, SchemaBuilder};
-use datafusion_common::{exec_err, ScalarValue};
-use datafusion_expr::expr::ScalarFunction;
-use datafusion_expr::{ColumnarValue, Expr, ScalarUDFImpl, Signature, Volatility};
 use std::any::Any;
 use std::collections::VecDeque;
 use std::sync::Arc;
+
+use arrow::array::{ArrayData, Capacities, MutableArrayData};
+use arrow_array::{Array, ArrayRef, GenericListArray, MapArray, StructArray};
+use arrow_buffer::{Buffer, OffsetBuffer, ToByteSlice};
+use arrow_schema::{DataType, Field, SchemaBuilder};
+
+use datafusion_common::cast::as_list_array;
+use datafusion_common::{exec_err, ScalarValue};
+use datafusion_expr::expr::ScalarFunction;
+use datafusion_expr::{ColumnarValue, Expr, ScalarUDFImpl, Signature, Volatility};
+
+use crate::make_array::make_array;
 
 /// Returns a map created from a key list and a value list
 pub fn map(keys: Vec<Expr>, values: Vec<Expr>) -> Expr {
@@ -73,7 +77,12 @@ fn get_first_array_ref(
             ScalarValue::FixedSizeList(array) => Ok(array.value(0)),
             _ => exec_err!("Expected array, got {:?}", value),
         },
-        ColumnarValue::Array(array) => exec_err!("Expected scalar, got {:?}", array),
+        ColumnarValue::Array(array) => {
+            // return ListArray
+            // First element in array has all keys in first row,
+            // Same for values
+            Ok(array.to_owned())
+        }
     }
 }
 
@@ -88,6 +97,10 @@ fn make_map_batch_internal(
 
     if keys.len() != values.len() {
         return exec_err!("map requires key and value lists to have the same length");
+    }
+
+    if keys.data_type().is_nested() && values.data_type().is_nested() {
+        return handle_array(keys, values);
     }
 
     let key_field = Arc::new(Field::new("key", keys.data_type().clone(), false));
@@ -127,6 +140,84 @@ fn make_map_batch_internal(
     } else {
         ColumnarValue::Array(map_array)
     })
+}
+
+fn create_map(keys: ArrayRef, values: ArrayRef) -> ArrayData {
+    let key_field = Arc::new(Field::new("key", keys.data_type().clone(), false));
+    let value_field = Arc::new(Field::new("value", values.data_type().clone(), true));
+    let mut entry_struct_buffer: VecDeque<(Arc<Field>, ArrayRef)> = VecDeque::new();
+    let mut entry_offsets_buffer = VecDeque::new();
+    entry_offsets_buffer.push_back(0);
+
+    entry_struct_buffer.push_back((Arc::clone(&key_field), Arc::clone(&keys)));
+    entry_struct_buffer.push_back((Arc::clone(&value_field), Arc::clone(&values)));
+    entry_offsets_buffer.push_back(keys.len() as u32);
+
+    let entry_struct: Vec<(Arc<Field>, ArrayRef)> = entry_struct_buffer.into();
+    let entry_struct = StructArray::from(entry_struct);
+
+    let map_data_type = DataType::Map(
+        Arc::new(Field::new(
+            "entries",
+            entry_struct.data_type().clone(),
+            false,
+        )),
+        false,
+    );
+
+    let entry_offsets: Vec<u32> = entry_offsets_buffer.into();
+    let entry_offsets_buffer = Buffer::from(entry_offsets.to_byte_slice());
+
+    let map_data = ArrayData::builder(map_data_type)
+        .len(entry_offsets.len() - 1)
+        .add_buffer(entry_offsets_buffer)
+        .add_child_data(entry_struct.to_data())
+        .build()
+        .unwrap();
+    map_data
+}
+
+fn handle_array(
+    keys: ArrayRef,
+    values: ArrayRef,
+) -> datafusion_common::Result<ColumnarValue> {
+    let keys = as_list_array(keys.as_ref())?;
+    let values = as_list_array(values.as_ref())?;
+
+    let mut map_data_vec = vec![];
+    for (k, v) in keys.iter().zip(values.iter()) {
+        map_data_vec.push(create_map(k.unwrap(), v.unwrap()));
+    }
+
+    let total_len = map_data_vec.len();
+    let mut offsets: Vec<i32> = Vec::with_capacity(total_len);
+    offsets.push(0);
+
+    let capacity = Capacities::Array(total_len);
+    let map_data_vec_ref = map_data_vec.iter().collect::<Vec<_>>();
+    let mut mutable = MutableArrayData::with_capacities(map_data_vec_ref, true, capacity);
+
+    // let num_rows = args[0].len();
+
+    for (arr_idx, arg) in map_data_vec.iter().enumerate() {
+        if !arg.is_null(0) {
+            mutable.extend(arr_idx, 0, arg.len());
+        } else {
+            mutable.extend_nulls(1);
+        }
+        offsets.push(mutable.len() as i32);
+    }
+    let data = mutable.freeze();
+
+    let data_type = map_data_vec[0].data_type().to_owned();
+
+    let array = Arc::new(GenericListArray::<i32>::try_new(
+        Arc::new(Field::new("item", data_type, true)),
+        OffsetBuffer::new(offsets.into()),
+        arrow_array::make_array(data),
+        None,
+    )?);
+    Ok(ColumnarValue::Array(array))
 }
 
 #[derive(Debug)]
@@ -180,10 +271,15 @@ impl ScalarUDFImpl for MapFunc {
             true,
         ));
         let fields = builder.finish().fields;
-        Ok(DataType::Map(
+        let map_dt = DataType::Map(
             Arc::new(Field::new("entries", DataType::Struct(fields), false)),
             false,
-        ))
+        );
+        if !arg_types[0].is_nested() {
+            return Ok(map_dt);
+        } else {
+            Ok(DataType::List(Arc::new(Field::new("item", map_dt, true))))
+        }
     }
 
     fn invoke(&self, args: &[ColumnarValue]) -> datafusion_common::Result<ColumnarValue> {
