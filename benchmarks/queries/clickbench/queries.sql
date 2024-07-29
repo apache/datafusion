89b5298359c91e079343822d60b6b01f4a97579d
jayzhan211 <jayzhan211@gmail.com>
2024-07-29 23:25:35 +0800

    upd test
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- benchmarks/queries/clickbench/queries.sql
+++ benchmarks/queries/clickbench/queries.sql
@@ -1,2 +1,3 @@
-SELECT "UserID", "SearchPhrase", COUNT(*) FROM hits GROUP BY "UserID", "SearchPhrase" ORDER BY COUNT(*) DESC;
+SELECT "UserID", "SearchPhrase", COUNT(*) FROM hits GROUP BY "UserID", "SearchPhrase" ORDER BY COUNT(*) DESC LIMIT 10;
 SELECT "UserID", concat("SearchPhrase", repeat('hello', 100)) as s, COUNT(*) FROM hits GROUP BY "UserID", s ORDER BY COUNT(*) DESC;
+SELECT "UserID", concat("SearchPhrase", repeat('hello', 100)) as s, COUNT(*) FROM hits GROUP BY "UserID", s ORDER BY COUNT(*) DESC LIMIT 10;
