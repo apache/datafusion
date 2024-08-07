c8846a6bee27e4b44e0e2c43450063247dfbc0b9
jayzhan211 <jayzhan211@gmail.com>
2024-08-07 21:13:47 +0800

    revert change in calc_requirements
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- datafusion/physical-plan/src/windows/mod.rs
+++ datafusion/physical-plan/src/windows/mod.rs
@@ -379,8 +379,6 @@ impl BuiltInWindowFunctionExpr for WindowUDFExpr {
     }
 }
 
-// TODO: Find a way to make clippy happy
-#[allow(clippy::needless_borrow)]
 pub(crate) fn calc_requirements<
     T: Borrow<Arc<dyn PhysicalExpr>>,
     S: Borrow<PhysicalSortExpr>,
@@ -398,7 +396,7 @@ pub(crate) fn calc_requirements<
         let PhysicalSortExpr { expr, options } = element.borrow();
         if !sort_reqs.iter().any(|e| e.expr.eq(expr)) {
             sort_reqs.push(PhysicalSortRequirement::new(
-                Arc::clone(&expr),
+                Arc::clone(expr),
                 Some(*options),
             ));
         }
