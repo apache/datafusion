b9bf6c9ec4698aa7a383a936328ef7df22e8fbb3
Samuel Colvin <s@muelcolvin.com>
2024-08-08 17:51:37 +0100

    Fix `Duration` vs `Interval` comparisons and `Interval` as LHS (#11876)
    * fix duration vs. interval and interval as LHS
    
    * add more operators to "interval vs. duration comparison" slt tests

--- datafusion/expr/src/type_coercion/binary.rs
+++ datafusion/expr/src/type_coercion/binary.rs
@@ -1130,7 +1130,9 @@ fn temporal_coercion(lhs_type: &DataType, rhs_type: &DataType) -> Option<DataTyp
     use arrow::datatypes::TimeUnit::*;
 
     match (lhs_type, rhs_type) {
-        (Interval(_), Interval(_)) => Some(Interval(MonthDayNano)),
+        (Interval(_) | Duration(_), Interval(_) | Duration(_)) => {
+            Some(Interval(MonthDayNano))
+        }
         (Date64, Date32) | (Date32, Date64) => Some(Date64),
         (Timestamp(_, None), Date64) | (Date64, Timestamp(_, None)) => {
             Some(Timestamp(Nanosecond, None))
