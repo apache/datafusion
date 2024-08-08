946666f5072f6a171c1c5bb3fd5052a48f8c0242
albinsabu2023 <albinsullas2023@gmail.com>
2024-08-09 00:15:41 +0530

    corrected it to Option<Arc<Statistics>>

--- datafusion/core/src/datasource/listing/mod.rs
+++ datafusion/core/src/datasource/listing/mod.rs
@@ -78,7 +78,7 @@ pub struct PartitionedFile {
     ///
     /// DataFusion relies on these statistics for planning (in particular to sort file groups),
     /// so if they are incorrect, incorrect answers may result.
-    pub statistics: Option<Arc(Statistics)>,
+    pub statistics: Option<Arc<Statistics>>,
     /// An optional field for user defined per object metadata
     pub extensions: Option<Arc<dyn std::any::Any + Send + Sync>>,
 }
