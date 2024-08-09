b8798db02213a7ff72f23f1a4084a893f346c2bb
Jacob Sherin <jacob@protoship.io>
2024-08-09 18:00:36 +0530

    Adds partition evaluator

--- datafusion/functions-window/src/row_number.rs
+++ datafusion/functions-window/src/row_number.rs
@@ -77,12 +77,12 @@ impl WindowUDFImpl for RowNumber {
     }
 
     fn partition_evaluator(&self) -> Result<Box<dyn PartitionEvaluator>> {
-        todo!()
+        Ok(Box::<NumRowsEvaluator>::default())
     }
 }
 
 /// State for the 'ROW_NUMBER' built-in window function.
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct NumRowsEvaluator {
     n_rows: usize,
 }
