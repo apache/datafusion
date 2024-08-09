7c41323fa46963a6889adafd5d4a8e43c69291b2
Jay Zhan <jayzhan211@gmail.com>
2024-08-09 09:00:02 +0800

    Rename  `Expr::display_name` to  `Expr::schema_name`, make `UNNEST` naming conform to convention (#11797)
    * introduce schema_name
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cleanup
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix doc
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * reuse for simple case
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * unnest + udf
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fmt
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * add display name for udf
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix name in udf
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rename unnest
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rm column
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * inlis
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fmt
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * udaf
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * case
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * use write
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix test
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix window
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * window
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * like and similar to
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * rm display name
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * comment
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * cliip
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix doc
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * display
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix conflict
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix merge conflict
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * with display
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * function for exprs
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * clippy
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    * fix doc
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>
    
    ---------
    
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- datafusion/optimizer/src/scalar_subquery_to_join.rs
+++ datafusion/optimizer/src/scalar_subquery_to_join.rs
@@ -188,9 +188,9 @@ impl OptimizerRule for ScalarSubqueryToJoin {
 
                 let mut proj_exprs = vec![];
                 for expr in projection.expr.iter() {
-                    let old_expr_name = expr.display_name()?;
+                    let old_expr_name = expr.schema_name().to_string();
                     let new_expr = expr_to_rewrite_expr_map.get(expr).unwrap();
-                    let new_expr_name = new_expr.display_name()?;
+                    let new_expr_name = new_expr.schema_name().to_string();
                     if new_expr_name != old_expr_name {
                         proj_exprs.push(new_expr.clone().alias(old_expr_name))
                     } else {
