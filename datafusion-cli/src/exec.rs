e006a3aa9cdc89fe1b3f402b0b0cef3ce3fb67c2
Chojan Shang <psiace@apache.org>
2024-08-10 19:28:04 +0800

    Ingore shebang at top of file
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- datafusion-cli/src/exec.rs
+++ datafusion-cli/src/exec.rs
@@ -70,6 +70,9 @@ pub async fn exec_from_lines(
 
     for line in reader.lines() {
         match line {
+            Ok(line) if line.starts_with("#!") => {
+                continue;
+            }
             Ok(line) if line.starts_with("--") => {
                 continue;
             }
