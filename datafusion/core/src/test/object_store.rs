880a77d27cd96d939622c8cb07252900ffb3f57b
Ruihang Xia <waynestxia@gmail.com>
2024-08-15 17:00:46 +0800

    chore: fix license header and add checker
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- datafusion/core/src/test/object_store.rs
+++ datafusion/core/src/test/object_store.rs
@@ -14,7 +14,9 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
 //! Object store implementation used for testing
+
 use crate::execution::context::SessionState;
 use crate::execution::session_state::SessionStateBuilder;
 use crate::prelude::SessionContext;
--- test-utils/src/string_gen.rs
+++ test-utils/src/string_gen.rs
@@ -14,8 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-//
-// use arrow::array::{ArrayRef, GenericStringArray, OffsetSizeTrait, RecordBatch, UInt32Array};
+
 use crate::stagger_batch;
 use arrow::array::{ArrayRef, GenericStringArray, OffsetSizeTrait, UInt32Array};
 use arrow::record_batch::RecordBatch;
