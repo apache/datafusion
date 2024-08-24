946705120c0ae4c274c1419ed676e2c9721e8df9
Thinh Bui <thinh.ducbui@gmail.com>
2024-08-23 18:54:48 -0700

    clippy fix

--- datafusion/expr-common/src/type_coercion/binary.rs
+++ datafusion/expr-common/src/type_coercion/binary.rs
@@ -937,7 +937,7 @@ fn string_concat_coercion(lhs_type: &DataType, rhs_type: &DataType) -> Option<Da
             string_concat_internal_coercion(from_type, &LargeUtf8)
         }
         (Dictionary(_, lhs_value_type), Dictionary(_, rhs_value_type)) => {
-            string_coercion(&lhs_value_type, &rhs_value_type).or(None)
+            string_coercion(lhs_value_type, rhs_value_type).or(None)
         }
         _ => None,
     })
