41b2b59947bcdfb9bc7c55cb85d6f4581d18ddd6
Dharan Aditya <dharan.aditya@gmail.com>
2024-08-31 19:15:22 +0530

    remove get_field_with_expr

--- datafusion/functions/src/core/mod.rs
+++ datafusion/functions/src/core/mod.rs
@@ -82,13 +82,7 @@ pub mod expr_fn {
 
     #[doc = "Returns the value of the field with the given name from the struct"]
     pub fn get_field(arg1: Expr, arg2: impl Literal) -> Expr {
-        get_field_from_expr(arg1, arg2.lit())
-    }
-
-    /// Returns the value of the field with the given name from the struct & Map.
-    /// **Internal use only.** This function is added to support the map use case.
-    pub fn get_field_from_expr(arg1: Expr, arg2: Expr) -> Expr {
-        super::get_field().call(vec![arg1, arg2])
+        super::get_field().call(vec![arg1, arg2.lit()])
     }
 }
 
