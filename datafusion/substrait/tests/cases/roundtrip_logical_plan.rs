aed84c27461dd5c123ad86829200c0bb6a33db98
wiedld <wiedld@users.noreply.github.com>
2024-09-06 01:10:37 -1000

    Support Utf8View and BinaryView in substrait serialization. (#12199)
    * feat(12118): logical plan support for Utf8View
    
    * feat(12118): physical plan support for Utf8View
    
    * feat(12118): logical plan support for BinaryView
    
    * feat(12118): physical plan support for BinaryView
    
    * refactor(12118): remove BinaryView work-arounds, now that upstream arrow changes are in
    
    ---------
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/substrait/tests/cases/roundtrip_logical_plan.rs
+++ datafusion/substrait/tests/cases/roundtrip_logical_plan.rs
@@ -716,8 +716,10 @@ async fn all_type_literal() -> Result<()> {
             date32_col = arrow_cast('2020-01-01', 'Date32') AND
             binary_col = arrow_cast('binary', 'Binary') AND
             large_binary_col = arrow_cast('large_binary', 'LargeBinary') AND
+            view_binary_col = arrow_cast('binary_view', 'BinaryView') AND
             utf8_col = arrow_cast('utf8', 'Utf8') AND
-            large_utf8_col = arrow_cast('large_utf8', 'LargeUtf8');",
+            large_utf8_col = arrow_cast('large_utf8', 'LargeUtf8') AND
+            view_utf8_col = arrow_cast('utf8_view', 'Utf8View');",
     )
         .await
 }
@@ -1231,9 +1233,11 @@ async fn create_all_type_context() -> Result<SessionContext> {
         Field::new("date64_col", DataType::Date64, true),
         Field::new("binary_col", DataType::Binary, true),
         Field::new("large_binary_col", DataType::LargeBinary, true),
+        Field::new("view_binary_col", DataType::BinaryView, true),
         Field::new("fixed_size_binary_col", DataType::FixedSizeBinary(42), true),
         Field::new("utf8_col", DataType::Utf8, true),
         Field::new("large_utf8_col", DataType::LargeUtf8, true),
+        Field::new("view_utf8_col", DataType::Utf8View, true),
         Field::new_list("list_col", Field::new("item", DataType::Int64, true), true),
         Field::new_list(
             "large_list_col",
