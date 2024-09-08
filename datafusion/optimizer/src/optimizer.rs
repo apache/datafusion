c7d35e824a44811ab646beafc2c4d1a00ec81872
Devan <devandbenz@gmail.com>
2024-09-08 11:51:52 -0500

    fmt
    Signed-off-by: Devan <devandbenz@gmail.com>

--- datafusion/optimizer/src/optimizer.rs
+++ datafusion/optimizer/src/optimizer.rs
@@ -365,6 +365,7 @@ impl Optimizer {
         let start_time = Instant::now();
         let options = config.options();
         let mut new_plan = plan;
+        
         let mut previous_plans = HashSet::with_capacity(16);
         previous_plans.insert(LogicalPlanSignature::new(&new_plan));
 
