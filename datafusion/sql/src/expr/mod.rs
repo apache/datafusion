7a3602ae0d9b7552a79f8b128b555c9311a33319
Jefffrey <jeffrey.vo.australia@gmail.com>
2024-09-09 07:51:49 +1000

    Formatting

--- datafusion/sql/src/expr/mod.rs
+++ datafusion/sql/src/expr/mod.rs
@@ -177,9 +177,7 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
         // NOTE: This function is called recusively, so each match arm body should be as
         //       small as possible to avoid stack overflows in debug builds. Follow the
         //       common pattern of extracting into a separate function for non-trivial
-        //       arms.
-        //
-        // See https://github.com/apache/datafusion/pull/12384 for more context
+        //       arms. See https://github.com/apache/datafusion/pull/12384 for more context.
         match sql {
             SQLExpr::Value(value) => {
                 self.parse_value(value, planner_context.prepare_param_data_types())
