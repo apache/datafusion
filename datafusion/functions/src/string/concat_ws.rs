66a576df61501a97125c96441ad2e1deecdcd552
Devan <devandbenz@gmail.com>
2024-09-09 16:36:55 -0500

    sum inner buffer for stringviewarray data_size

--- datafusion/functions/src/string/concat_ws.rs
+++ datafusion/functions/src/string/concat_ws.rs
@@ -204,7 +204,7 @@ impl ScalarUDFImpl for ConcatWsFunc {
                         DataType::Utf8View => {
                             let string_array = as_string_view_array(array)?;
 
-                            data_size += string_array.len();
+                            data_size += string_array.data_buffers().iter().map(|buf| buf.len()).sum();
                             let column = if array.is_nullable() {
                                 ColumnarValueRef::NullableStringViewArray(string_array)
                             } else {
