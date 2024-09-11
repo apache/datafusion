62e7e6a4181bb207f6f968fcd7d525ac438a4d5d
Tim Saucer <timsaucer@gmail.com>
2024-09-11 12:38:16 -0400

    Cargo fmt

--- datafusion/core/src/dataframe/mod.rs
+++ datafusion/core/src/dataframe/mod.rs
@@ -1472,7 +1472,11 @@ impl DataFrame {
                     Some(new_column.clone())
                 } else {
                     let e = col(Column::from((qualifier, field)));
-                    window_fn_str.as_ref().filter(|s| *s == &e.to_string()).is_none().then_some(e)
+                    window_fn_str
+                        .as_ref()
+                        .filter(|s| *s == &e.to_string())
+                        .is_none()
+                        .then_some(e)
                 }
             })
             .collect();
