016decf5d73425ad60a40abbca31a4e437ba8450
Edmondo Porcu <edmondo.porcu@gmail.com>
2024-09-13 23:21:39 +0000

    Removing imports

--- datafusion/physical-optimizer/src/aggregate_statistics.rs
+++ datafusion/physical-optimizer/src/aggregate_statistics.rs
@@ -26,9 +26,7 @@ use datafusion_physical_plan::projection::ProjectionExec;
 use datafusion_physical_plan::{expressions, ExecutionPlan, Statistics};
 
 use crate::PhysicalOptimizerRule;
-use datafusion_common::stats::Precision;
 use datafusion_common::tree_node::{Transformed, TransformedResult, TreeNode};
-use datafusion_common::utils::expr::COUNT_STAR_EXPANSION;
 use datafusion_physical_plan::placeholder_row::PlaceholderRowExec;
 use datafusion_physical_plan::udaf::AggregateFunctionExpr;
 
