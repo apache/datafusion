a026b303be36cf2bfb0012c16a02aa1235e69b53
kamille <caoruiqiu.crq@antgroup.com>
2024-09-19 18:58:07 +0800

    remove some dbg!.

--- datafusion/physical-plan/src/repartition/mod.rs
+++ datafusion/physical-plan/src/repartition/mod.rs
@@ -300,7 +300,7 @@ impl BatchPartitioner {
                             // Tracking time required for repartitioned batches construction
                             let _timer = partitioner_timer.timer();
 
-                            if dbg!(batch.num_rows()) != dbg!(indices.len()) {
+                            if batch.num_rows() != indices.len() {
                                 // Produce batches based on indices
                                 let columns = batch
                                     .columns()
