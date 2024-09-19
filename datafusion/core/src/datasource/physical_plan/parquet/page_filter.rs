cfc4af3d99c7f6823dbd14bc01d334132f34d6cc
Valentin Lorentz <vlorentz@softwareheritage.org>
2024-09-19 06:59:30 +0000

    parquet: Make page_index/pushdown metrics consistent with row_group metrics
    1. Rename `{pushdown,page_index}_filtered` to `{pushdown,page_index}_pruned`
    2. Add `{pushdown,page_index}_matched`
    
    The latter makes it clearer in EXPLAIN ANALYZE when the Page Index is
    not checked because their row groups were already eliminated
    (with a Bloom Filter or row group statistics).

--- datafusion/core/src/datasource/physical_plan/parquet/page_filter.rs
+++ datafusion/core/src/datasource/physical_plan/parquet/page_filter.rs
@@ -178,6 +178,8 @@ impl PagePruningAccessPlanFilter {
 
         // track the total number of rows that should be skipped
         let mut total_skip = 0;
+        // track the total number of rows that should not be skipped
+        let mut total_pass = 0;
 
         // for each row group specified in the access plan
         let row_group_indexes = access_plan.row_group_indexes();
@@ -242,8 +244,10 @@ impl PagePruningAccessPlanFilter {
             if let Some(overall_selection) = overall_selection {
                 if overall_selection.selects_any() {
                     let rows_skipped = rows_skipped(&overall_selection);
-                    trace!("Overall selection from predicate skipped {rows_skipped}: {overall_selection:?}");
+                    let rows_passed = rows_passed(&overall_selection);
+                    trace!("Overall selection from predicate skipped {rows_skipped}, passed {rows_passed}: {overall_selection:?}");
                     total_skip += rows_skipped;
+                    total_pass += rows_passed;
                     access_plan.scan_selection(row_group_index, overall_selection)
                 } else {
                     // Selection skips all rows, so skip the entire row group
@@ -258,7 +262,8 @@ impl PagePruningAccessPlanFilter {
             }
         }
 
-        file_metrics.page_index_rows_filtered.add(total_skip);
+        file_metrics.page_index_rows_pruned.add(total_skip);
+        file_metrics.page_index_rows_matched.add(total_pass);
         access_plan
     }
 
@@ -274,6 +279,14 @@ fn rows_skipped(selection: &RowSelection) -> usize {
     selection
         .iter()
         .fold(0, |acc, x| if x.skip { acc + x.row_count } else { acc })
+}
+
+/// returns the number of rows not skipped in the selection
+/// TODO should this be upstreamed to RowSelection?
+fn rows_passed(selection: &RowSelection) -> usize {
+    selection
+        .iter()
+        .fold(0, |acc, x| if x.skip { acc } else { acc + x.row_count })
 }
 
 fn update_selection(
