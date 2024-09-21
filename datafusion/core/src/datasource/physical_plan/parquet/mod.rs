1b7e5cf0683475ca49f143fd110135ae9e5a4f2f
waruto <wmc314@outlook.com>
2024-09-21 21:08:39 +0800

    Update datafusion/core/src/datasource/physical_plan/parquet/mod.rs
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/core/src/datasource/physical_plan/parquet/mod.rs
+++ datafusion/core/src/datasource/physical_plan/parquet/mod.rs
@@ -743,6 +743,7 @@ impl ExecutionPlan for ParquetExec {
     fn statistics(&self) -> Result<Statistics> {
         // When filters are pushed down, we have no way of knowing the exact statistics.
         // Note that pruning predicate is also a kind of filter pushdown.
+        // (bloom filters use `pruning_predicate` too)
         let stats = if self.pruning_predicate.is_some()
             || self.page_pruning_predicate.is_some()
             || (self.predicate.is_some() && self.pushdown_filters())
