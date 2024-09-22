300a39b89308ae949a99058660cb69e112e65e3c
Val Lorentz <vlorentz@softwareheritage.org>
2024-09-22 13:14:44 +0200

    parquet: Make page_index/pushdown metrics consistent with row_group metrics (#12545)
    * parquet: Make page_index/pushdown metrics consistent with row_group metrics
    
    1. Rename `{pushdown,page_index}_filtered` to `{pushdown,page_index}_pruned`
    2. Add `{pushdown,page_index}_matched`
    
    The latter makes it clearer in EXPLAIN ANALYZE when the Page Index is
    not checked because their row groups were already eliminated
    (with a Bloom Filter or row group statistics).
    
    * Add missing metric definitions in the docs
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>
    
    * s/pass/select/
    
    ---------
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/core/src/datasource/physical_plan/parquet/metrics.rs
+++ datafusion/core/src/datasource/physical_plan/parquet/metrics.rs
@@ -40,11 +40,15 @@ pub struct ParquetFileMetrics {
     /// Total number of bytes scanned
     pub bytes_scanned: Count,
     /// Total rows filtered out by predicates pushed into parquet scan
-    pub pushdown_rows_filtered: Count,
+    pub pushdown_rows_pruned: Count,
+    /// Total rows passed predicates pushed into parquet scan
+    pub pushdown_rows_matched: Count,
     /// Total time spent evaluating pushdown filters
     pub pushdown_eval_time: Time,
     /// Total rows filtered out by parquet page index
-    pub page_index_rows_filtered: Count,
+    pub page_index_rows_pruned: Count,
+    /// Total rows passed through the parquet page index
+    pub page_index_rows_matched: Count,
     /// Total time spent evaluating parquet page index filters
     pub page_index_eval_time: Time,
 }
@@ -80,16 +84,22 @@ impl ParquetFileMetrics {
             .with_new_label("filename", filename.to_string())
             .counter("bytes_scanned", partition);
 
-        let pushdown_rows_filtered = MetricBuilder::new(metrics)
+        let pushdown_rows_pruned = MetricBuilder::new(metrics)
             .with_new_label("filename", filename.to_string())
-            .counter("pushdown_rows_filtered", partition);
+            .counter("pushdown_rows_pruned", partition);
+        let pushdown_rows_matched = MetricBuilder::new(metrics)
+            .with_new_label("filename", filename.to_string())
+            .counter("pushdown_rows_matched", partition);
 
         let pushdown_eval_time = MetricBuilder::new(metrics)
             .with_new_label("filename", filename.to_string())
             .subset_time("pushdown_eval_time", partition);
-        let page_index_rows_filtered = MetricBuilder::new(metrics)
+        let page_index_rows_pruned = MetricBuilder::new(metrics)
             .with_new_label("filename", filename.to_string())
-            .counter("page_index_rows_filtered", partition);
+            .counter("page_index_rows_pruned", partition);
+        let page_index_rows_matched = MetricBuilder::new(metrics)
+            .with_new_label("filename", filename.to_string())
+            .counter("page_index_rows_matched", partition);
 
         let page_index_eval_time = MetricBuilder::new(metrics)
             .with_new_label("filename", filename.to_string())
@@ -102,9 +112,11 @@ impl ParquetFileMetrics {
             row_groups_matched_statistics,
             row_groups_pruned_statistics,
             bytes_scanned,
-            pushdown_rows_filtered,
+            pushdown_rows_pruned,
+            pushdown_rows_matched,
             pushdown_eval_time,
-            page_index_rows_filtered,
+            page_index_rows_pruned,
+            page_index_rows_matched,
             page_index_eval_time,
         }
     }
