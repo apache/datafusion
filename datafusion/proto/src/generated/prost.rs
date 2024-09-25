b35e72074e75aec353cf357c5fe75c8c5f6b98a6
Duong Cong Toai <35887761+duongcongtoai@users.noreply.github.com>
2024-09-25 12:27:11 +0200

    Refactor to support recursive unnest in physical plan (#11577)
    * chore: poc
    
    * fix unnest struct
    
    * UT for memoization
    
    * remove unnessary projection
    
    * chore: temp test case
    
    * multi depth unnest supported
    
    * chore: add map of original column and transformed col
    
    * transformation map to physical layer
    
    * prototype for recursive array length
    
    * chore: some compile err
    
    * finalize input type in physical layer
    
    * chore: refactor unnest builder
    
    * add unnesting type inferred
    
    * fix compile err
    
    * fail test in builder
    
    * Compile err
    
    * chore: detect some bugs
    
    * some work
    
    * support recursive unnest in physical layer
    
    * UT for new build batch function
    
    * compile err
    
    * fix unnesting into empty arrays
    
    * some comment
    
    * fix unnest struct
    
    * some note
    
    * chore: fix all test failure
    
    * fix projection pushdown
    
    * custom rewriter for recursive unnest
    
    * simplify
    
    * rm unnecessary projection
    
    * chore: better comments
    
    * more comments
    
    * chore: better comments
    
    * remove breaking api
    
    * rename
    
    * more unit test
    
    * remove debug
    
    * clean up
    
    * fix proto
    
    * fix dataframe
    
    * fix clippy
    
    * cargo fmt
    
    * fix some test
    
    * fix all test
    
    * fix unnest in join
    
    * fix doc and tests
    
    * chore: better doc
    
    * better doc
    
    * tune comment
    
    * rm todo
    
    * refactor
    
    * chore: reserve test
    
    * add a basic test
    
    * chore: more document
    
    * doc on ColumnUnnestType List
    
    * chore: add partialord to new types

--- datafusion/proto/src/generated/prost.rs
+++ datafusion/proto/src/generated/prost.rs
@@ -396,9 +396,9 @@ pub struct UnnestNode {
     #[prost(message, optional, boxed, tag = "1")]
     pub input: ::core::option::Option<::prost::alloc::boxed::Box<LogicalPlanNode>>,
     #[prost(message, repeated, tag = "2")]
-    pub exec_columns: ::prost::alloc::vec::Vec<super::datafusion_common::Column>,
-    #[prost(uint64, repeated, tag = "3")]
-    pub list_type_columns: ::prost::alloc::vec::Vec<u64>,
+    pub exec_columns: ::prost::alloc::vec::Vec<ColumnUnnestExec>,
+    #[prost(message, repeated, tag = "3")]
+    pub list_type_columns: ::prost::alloc::vec::Vec<ColumnUnnestListItem>,
     #[prost(uint64, repeated, tag = "4")]
     pub struct_type_columns: ::prost::alloc::vec::Vec<u64>,
     #[prost(uint64, repeated, tag = "5")]
@@ -407,6 +407,44 @@ pub struct UnnestNode {
     pub schema: ::core::option::Option<super::datafusion_common::DfSchema>,
     #[prost(message, optional, tag = "7")]
     pub options: ::core::option::Option<UnnestOptions>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ColumnUnnestListItem {
+    #[prost(uint32, tag = "1")]
+    pub input_index: u32,
+    #[prost(message, optional, tag = "2")]
+    pub recursion: ::core::option::Option<ColumnUnnestListRecursion>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ColumnUnnestListRecursions {
+    #[prost(message, repeated, tag = "2")]
+    pub recursions: ::prost::alloc::vec::Vec<ColumnUnnestListRecursion>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ColumnUnnestListRecursion {
+    #[prost(message, optional, tag = "1")]
+    pub output_column: ::core::option::Option<super::datafusion_common::Column>,
+    #[prost(uint32, tag = "2")]
+    pub depth: u32,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ColumnUnnestExec {
+    #[prost(message, optional, tag = "1")]
+    pub column: ::core::option::Option<super::datafusion_common::Column>,
+    #[prost(oneof = "column_unnest_exec::UnnestType", tags = "2, 3, 4")]
+    pub unnest_type: ::core::option::Option<column_unnest_exec::UnnestType>,
+}
+/// Nested message and enum types in `ColumnUnnestExec`.
+pub mod column_unnest_exec {
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum UnnestType {
+        #[prost(message, tag = "2")]
+        List(super::ColumnUnnestListRecursions),
+        #[prost(message, tag = "3")]
+        Struct(super::super::datafusion_common::EmptyMessage),
+        #[prost(message, tag = "4")]
+        Inferred(super::super::datafusion_common::EmptyMessage),
+    }
 }
 #[derive(Clone, Copy, PartialEq, ::prost::Message)]
 pub struct UnnestOptions {
@@ -1100,12 +1138,19 @@ pub struct UnnestExecNode {
     pub input: ::core::option::Option<::prost::alloc::boxed::Box<PhysicalPlanNode>>,
     #[prost(message, optional, tag = "2")]
     pub schema: ::core::option::Option<super::datafusion_common::Schema>,
-    #[prost(uint64, repeated, tag = "3")]
-    pub list_type_columns: ::prost::alloc::vec::Vec<u64>,
+    #[prost(message, repeated, tag = "3")]
+    pub list_type_columns: ::prost::alloc::vec::Vec<ListUnnest>,
     #[prost(uint64, repeated, tag = "4")]
     pub struct_type_columns: ::prost::alloc::vec::Vec<u64>,
     #[prost(message, optional, tag = "5")]
     pub options: ::core::option::Option<UnnestOptions>,
+}
+#[derive(Clone, Copy, PartialEq, ::prost::Message)]
+pub struct ListUnnest {
+    #[prost(uint32, tag = "1")]
+    pub index_in_input_schema: u32,
+    #[prost(uint32, tag = "2")]
+    pub depth: u32,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct PhysicalExtensionNode {
