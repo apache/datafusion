0c4bde3c37300e14bdb9c6ecdb62b1389cbc099f
Bruce Ritchie <bruce.ritchie@veeva.com>
2024-09-29 09:23:39 -0400

    Cargo fmt update.

--- datafusion/expr/src/lib.rs
+++ datafusion/expr/src/lib.rs
@@ -94,7 +94,7 @@ pub use table_source::{TableProviderFilterPushDown, TableSource, TableType};
 pub use udaf::{aggregate_doc_sections, AggregateUDF, AggregateUDFImpl, ReversedUDAF};
 pub use udf::{scalar_doc_sections, ScalarUDF, ScalarUDFImpl};
 pub use udf_docs::{DocSection, Documentation, DOCUMENTATION_NONE, DOC_SECTION_NONE};
-pub use udwf::{ReversedUDWF, window_doc_sections, WindowUDF, WindowUDFImpl};
+pub use udwf::{window_doc_sections, ReversedUDWF, WindowUDF, WindowUDFImpl};
 pub use window_frame::{WindowFrame, WindowFrameBound, WindowFrameUnits};
 
 #[cfg(test)]
