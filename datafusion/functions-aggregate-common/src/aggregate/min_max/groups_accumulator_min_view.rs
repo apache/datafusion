77b980fedb8e7a0c20e16c715c3b0697907a28ed
Devan <devandbenz@gmail.com>
2024-09-30 09:19:56 -0500

    fix: rm not needed import

--- datafusion/functions-aggregate-common/src/aggregate/min_max/groups_accumulator_min_view.rs
+++ datafusion/functions-aggregate-common/src/aggregate/min_max/groups_accumulator_min_view.rs
@@ -1,4 +1,3 @@
-use std::os::macos::raw::stat;
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
