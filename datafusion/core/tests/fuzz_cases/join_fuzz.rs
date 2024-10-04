2f880f2982a13c597774a5808d13a8e84af45143
comphead <comphead@ukr.net>
2024-10-04 14:47:34 -0700

    cleanup

--- datafusion/core/tests/fuzz_cases/join_fuzz.rs
+++ datafusion/core/tests/fuzz_cases/join_fuzz.rs
@@ -491,11 +491,6 @@ impl JoinFuzzTestCase {
                 nlj_formatted.trim().lines().collect();
             nlj_formatted_sorted.sort_unstable();
 
-            println!("=============== HashJoinExec ==================");
-            hj_formatted_sorted.iter().for_each(|s| println!("{}", s));
-            println!("=============== SortMergeJoinExec ==================");
-            smj_formatted_sorted.iter().for_each(|s| println!("{}", s));
-
             if debug
                 && ((join_tests.contains(&JoinTestType::NljHj) && nlj_rows != hj_rows)
                     || (join_tests.contains(&JoinTestType::HjSmj) && smj_rows != hj_rows))
