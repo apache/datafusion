4897ed9972982189d168c7732252b48759726c46
Andrew Lamb <andrew@nerdnetworks.org>
2024-10-08 22:45:44 -0400

    Minor: add documentation note about `NullState` (#12791)
    * Minor: add documentation note about `NullState`
    
    * Remove unecessary copy/paste license
    
    * Update datafusion/expr-common/src/groups_accumulator.rs

--- datafusion/functions-aggregate/src/min_max.rs
+++ datafusion/functions-aggregate/src/min_max.rs
@@ -17,21 +17,6 @@
 //! [`Max`] and [`MaxAccumulator`] accumulator for the `max` function
 //! [`Min`] and [`MinAccumulator`] accumulator for the `min` function
 
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
 use arrow::array::{
     ArrayRef, BinaryArray, BinaryViewArray, BooleanArray, Date32Array, Date64Array,
     Decimal128Array, Decimal256Array, Float16Array, Float32Array, Float64Array,
