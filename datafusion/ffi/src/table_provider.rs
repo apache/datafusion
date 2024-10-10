0211869e2cb20884adf1c5741e2cee5eb3a5ae75
Tim Saucer <timsaucer@gmail.com>
2024-10-10 09:53:20 -0400

    Add table type

--- datafusion/ffi/src/table_provider.rs
+++ datafusion/ffi/src/table_provider.rs
@@ -22,15 +22,17 @@ use datafusion::{
 };
 use futures::executor::block_on;
 
+use crate::table_source::FFI_TableType;
+
 use super::{
     execution_plan::{ExportedExecutionPlan, FFI_ExecutionPlan},
     session_config::{FFI_SessionConfig, SessionConfigPrivateData},
 };
 use datafusion::error::Result;
 
+/// A stable interface for creating a DataFusion TableProvider.
 #[repr(C)]
 #[derive(Debug)]
-#[allow(missing_docs)]
 #[allow(non_camel_case_types)]
 pub struct FFI_TableProvider {
     pub version: i64,
@@ -47,6 +49,8 @@ pub struct FFI_TableProvider {
             err_code: *mut c_int,
         ) -> *mut FFI_ExecutionPlan,
     >,
+    pub table_type: Option<unsafe extern "C" fn(provider: *const FFI_TableProvider) -> FFI_TableType>,
+
     pub private_data: *mut c_void,
 }
 
@@ -57,11 +61,17 @@ struct ProviderPrivateData {
     provider: Box<dyn TableProvider + Send>,
 }
 
+/// Wrapper struct to provide access functions from the FFI interface to the underlying
+/// TableProvider. This struct is allowed to access `private_data` because it lives on
+/// the provider's side of the FFI interace.
 struct ExportedTableProvider(*const FFI_TableProvider);
 
-// The callback used to get array schema
-unsafe extern "C" fn provider_schema(provider: *const FFI_TableProvider) -> FFI_ArrowSchema {
-    ExportedTableProvider(provider).provider_schema()
+unsafe extern "C" fn schema_fn_wrapper(provider: *const FFI_TableProvider) -> FFI_ArrowSchema {
+    ExportedTableProvider(provider).schema()
+}
+
+unsafe extern "C" fn table_type_fn_wrapper(provider: *const FFI_TableProvider) -> FFI_TableType {
+    ExportedTableProvider(provider).table_type()
 }
 
 unsafe extern "C" fn scan_fn_wrapper(
@@ -74,7 +84,6 @@ unsafe extern "C" fn scan_fn_wrapper(
     limit: c_int,
     err_code: *mut c_int,
 ) -> *mut FFI_ExecutionPlan {
-    println!("entered scan_fn_wrapper");
     let config = unsafe { (*session_config).private_data as *const SessionConfigPrivateData };
     let session = SessionStateBuilder::new()
         .with_config((*config).config.clone())
@@ -103,8 +112,6 @@ unsafe extern "C" fn scan_fn_wrapper(
     let plan =
         ExportedTableProvider(provider).provider_scan(&ctx, maybe_projections, filters_vec, limit);
 
-    println!("leaving scan_fn_wrapper, has plan? {}", plan.is_ok());
-
     match plan {
         Ok(plan) => {
             *err_code = 0;
@@ -122,13 +129,20 @@ impl ExportedTableProvider {
         unsafe { &*((*self.0).private_data as *const ProviderPrivateData) }
     }
 
-    pub fn provider_schema(&self) -> FFI_ArrowSchema {
+    pub fn schema(&self) -> FFI_ArrowSchema {
         let private_data = self.get_private_data();
         let provider = &private_data.provider;
 
         // This does silently fail because TableProvider does not return a result
-        // so we expect it to always pass. Maybe some logging should be added.
+        // so we expect it to always pass.
         FFI_ArrowSchema::try_from(provider.schema().as_ref()).unwrap_or(FFI_ArrowSchema::empty())
+    }
+
+    pub fn table_type(&self) -> FFI_TableType {
+        let private_data = self.get_private_data();
+        let provider = &private_data.provider;
+
+        provider.table_type().into()
     }
 
     pub fn provider_scan(
@@ -164,8 +178,9 @@ impl FFI_TableProvider {
 
         Self {
             version: 2,
-            schema: Some(provider_schema),
+            schema: Some(schema_fn_wrapper),
             scan: Some(scan_fn_wrapper),
+            table_type: Some(table_type_fn_wrapper),
             private_data: Box::into_raw(private_data) as *mut c_void,
         }
     }
@@ -185,51 +200,54 @@ impl FFI_TableProvider {
             version: 0,
             schema: None,
             scan: None,
+            table_type: None,
             private_data: std::ptr::null_mut(),
         }
     }
 }
 
+/// This wrapper struct exists on the reciever side of the FFI interface, so it has
+/// no guarantees about being able to access the data in `private_data`. Any functions
+/// defined on this struct must only use the stable functions provided in 
+/// FFI_TableProvider to interact with the foreign table provider.
+#[derive(Debug)]
+struct ForeignTableProvider(*const FFI_TableProvider);
+
+unsafe impl Send for ForeignTableProvider {}
+unsafe impl Sync for ForeignTableProvider {}
+
 #[async_trait]
-impl TableProvider for FFI_TableProvider {
-    /// Returns the table provider as [`Any`](std::any::Any) so that it can be
-    /// downcast to a specific implementation.
+impl TableProvider for ForeignTableProvider {
     fn as_any(&self) -> &dyn Any {
         self
     }
 
-    /// Get a reference to the schema for this table
     fn schema(&self) -> SchemaRef {
-        let schema = match self.schema {
-            Some(func) => unsafe { Schema::try_from(&func(self)).ok() },
-            None => None,
+        let schema = unsafe {
+            let schema_fn = (*self.0).schema;
+            schema_fn.map(|func| func(self.0)).and_then(|s| Schema::try_from(&s).ok()).unwrap_or(Schema::empty())
         };
-        Arc::new(schema.unwrap_or(Schema::empty()))
+
+        Arc::new(schema)
     }
 
-    /// Get the type of this table for metadata/catalog purposes.
     fn table_type(&self) -> TableType {
-        todo!()
+        unsafe {
+            let table_type_fn = (*self.0).table_type;
+            table_type_fn.map(|func| func(self.0)).unwrap_or(FFI_TableType::Base).into()
+        }
     }
 
-    /// Create an ExecutionPlan that will scan the table.
-    /// The table provider will be usually responsible of grouping
-    /// the source data into partitions that can be efficiently
-    /// parallelized or distributed.
     async fn scan(
         &self,
         session: &dyn Session,
         projection: Option<&Vec<usize>>,
         filters: &[Expr],
-        // limit can be used to reduce the amount scanned
-        // from the datasource as a performance optimization.
-        // If set, it contains the amount of rows needed by the `LogicalPlan`,
-        // The datasource should return *at least* this number of rows if available.
         limit: Option<usize>,
     ) -> Result<Arc<dyn ExecutionPlan>> {
-        let scan_fn = self.scan.ok_or(DataFusionError::NotImplemented(
+        let scan_fn = unsafe { (*self.0).scan.ok_or(DataFusionError::NotImplemented(
             "Scan not defined on FFI_TableProvider".to_string(),
-        ))?;
+        ))?};
 
         let session_config = FFI_SessionConfig::new(session);
 
@@ -251,11 +269,10 @@ impl TableProvider for FFI_TableProvider {
             None => -1,
         };
 
-        println!("Within scan about to call unsafe scan_fn");
         let mut err_code = 0;
         let plan = unsafe {
             let plan_ptr = scan_fn(
-                self,
+                self.0,
                 &session_config,
                 n_projections,
                 projections_ptr,
@@ -271,16 +288,8 @@ impl TableProvider for FFI_TableProvider {
                 ));
             }
 
-            println!(
-                "Finished scan_fn inside FFI_TableProvider::scan {}",
-                plan_ptr.is_null()
-            );
-
-            let p = ExportedExecutionPlan::new(plan_ptr)?;
-            println!("ExportedExecutionPlan::new returned inside scan()");
-            p
+            ExportedExecutionPlan::new(plan_ptr)?
         };
-        println!("Scan returned with some plan.");
 
         Ok(Arc::new(plan))
     }
