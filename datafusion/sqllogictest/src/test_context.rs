a9b6bd56ed10ac23585d1010c923e5c457525274
wiedld <dlw405@gmail.com>
2024-10-15 14:11:45 -0700

    test(12733): reproducer of when metadata from the left side is not transferred to the right side

--- datafusion/sqllogictest/src/test_context.rs
+++ datafusion/sqllogictest/src/test_context.rs
@@ -319,11 +319,21 @@ pub async fn register_metadata_tables(ctx: &SessionContext) {
             String::from("metadata_key"),
             String::from("the l_name field"),
         )]));
+    let nonnull_name =
+        Field::new("nonnull_name", DataType::Utf8, false).with_metadata(HashMap::from([
+            (
+                String::from("metadata_key"),
+                String::from("the nonnull_name field"),
+            ),
+        ]));
 
-    let schema = Schema::new(vec![id, name, l_name]).with_metadata(HashMap::from([(
-        String::from("metadata_key"),
-        String::from("the entire schema"),
-    )]));
+    let schema =
+        Schema::new(vec![id, name, l_name, nonnull_name]).with_metadata(HashMap::from([
+            (
+                String::from("metadata_key"),
+                String::from("the entire schema"),
+            ),
+        ]));
 
     let batch = RecordBatch::try_new(
         Arc::new(schema),
@@ -331,6 +341,11 @@ pub async fn register_metadata_tables(ctx: &SessionContext) {
             Arc::new(Int32Array::from(vec![Some(1), None, Some(3)])) as _,
             Arc::new(StringArray::from(vec![None, Some("bar"), Some("baz")])) as _,
             Arc::new(StringArray::from(vec![None, Some("l_bar"), Some("l_baz")])) as _,
+            Arc::new(StringArray::from(vec![
+                Some("no_foo"),
+                Some("no_bar"),
+                Some("no_baz"),
+            ])) as _,
         ],
     )
     .unwrap();
