eef498c298821ef9ea8e88562774f75d7f8afc15
wiedld <dlw405@gmail.com>
2024-10-15 14:11:45 -0700

    test(12733): reproducer of when metadata from the left side is not transferred to the right side

--- datafusion/sqllogictest/src/test_context.rs
+++ datafusion/sqllogictest/src/test_context.rs
@@ -319,17 +319,28 @@ pub async fn register_metadata_tables(ctx: &SessionContext) {
             String::from("metadata_key"),
             String::from("the l_name field"),
         )]));
+
     let ts = Field::new("ts", DataType::Timestamp(TimeUnit::Nanosecond, None), false)
         .with_metadata(HashMap::from([(
             String::from("metadata_key"),
             String::from("ts non-nullable field"),
         )]));
 
+    let nonnull_name =
+        Field::new("nonnull_name", DataType::Utf8, false).with_metadata(HashMap::from([
+            (
+                String::from("metadata_key"),
+                String::from("the nonnull_name field"),
+            ),
+        ]));
+
     let schema =
-        Schema::new(vec![id, name, l_name, ts]).with_metadata(HashMap::from([(
-            String::from("metadata_key"),
-            String::from("the entire schema"),
-        )]));
+        Schema::new(vec![id, name, l_name, ts, nonnull_name]).with_metadata(HashMap::from([
+            (
+                String::from("metadata_key"),
+                String::from("the entire schema"),
+            ),
+        ]));
 
     let batch = RecordBatch::try_new(
         Arc::new(schema),
@@ -341,6 +352,11 @@ pub async fn register_metadata_tables(ctx: &SessionContext) {
                 1599572549190855123,
                 1599572549190855123,
                 1599572549190855123,
+            ])) as _,
+            Arc::new(StringArray::from(vec![
+                Some("no_foo"),
+                Some("no_bar"),
+                Some("no_baz"),
             ])) as _,
         ],
     )
