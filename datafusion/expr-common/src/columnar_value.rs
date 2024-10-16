ee3cfdc460abb9e48207f74af44a4160c8074e17
Georgi Krastev <georgi.krastev@coralogix.com>
2024-10-16 11:36:12 +0200

    Handle one-element array return value in ScalarFunctionExpr
    This was done in #12922 only for math functions.
    We now generalize this fallback to all scalar UDFs.

--- datafusion/expr-common/src/columnar_value.rs
+++ datafusion/expr-common/src/columnar_value.rs
@@ -217,17 +217,6 @@ impl ColumnarValue {
             }
         }
     }
-
-    /// Converts an [`ArrayRef`] to a [`ColumnarValue`] based on the supplied arguments.
-    /// This is useful for scalar UDF implementations to fulfil their contract:
-    /// if all arguments are scalar values, the result should also be a scalar value.
-    pub fn from_args_and_result(args: &[Self], result: ArrayRef) -> Result<Self> {
-        if result.len() == 1 && args.iter().all(|arg| matches!(arg, Self::Scalar(_))) {
-            Ok(Self::Scalar(ScalarValue::try_from_array(&result, 0)?))
-        } else {
-            Ok(Self::Array(result))
-        }
-    }
 }
 
 #[cfg(test)]
