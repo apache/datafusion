9323702e913ff88d14e58ab3be94256fecd1f3a7
Jonah Gao <jonahgao@msn.com>
2024-10-22 00:36:58 +0800

    Update datafusion/expr/src/expr_rewriter/mod.rs
    Co-authored-by: Jax Liu <liugs963@gmail.com>

--- datafusion/expr/src/expr_rewriter/mod.rs
+++ datafusion/expr/src/expr_rewriter/mod.rs
@@ -306,7 +306,7 @@ impl NamePreserver {
     /// Create a new NamePreserver for rewriting the `expr` that is part of the specified plan
     pub fn new(plan: &LogicalPlan) -> Self {
         Self {
-            // The schema of Filter and Join nodes comes from their inputs rather than their output expressions,
+            // The schema of Filter, Join, and Limit nodes comes from their inputs rather than their output expressions,
             // so there is no need to use aliases to preserve expression names.
             use_alias: !matches!(
                 plan,
