1a5b5809a66f390f5a8ab15ed34055bdb5133135
Agaev Guseyn <60943542+agscpp@users.noreply.github.com>
2024-10-22 18:05:54 +0300

    Fix functions with Volatility::Volatile and parameters (#13001)
    Co-authored-by: Agaev Huseyn <h.agaev@vkteam.ru>

--- datafusion/physical-expr/src/scalar_function.rs
+++ datafusion/physical-expr/src/scalar_function.rs
@@ -141,10 +141,7 @@ impl PhysicalExpr for ScalarFunctionExpr {
             .collect::<Result<Vec<_>>>()?;
 
         // evaluate the function
-        let output = match self.args.is_empty() {
-            true => self.fun.invoke_no_args(batch.num_rows()),
-            false => self.fun.invoke(&inputs),
-        }?;
+        let output = self.fun.invoke_batch(&inputs, batch.num_rows())?;
 
         if let ColumnarValue::Array(array) = &output {
             if array.len() != batch.num_rows() {
