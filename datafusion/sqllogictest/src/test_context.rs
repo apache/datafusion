a8e7c682531fb57d7ccca7d7b50f1941a3d4653d
itsjunetime <junewelker@gmail.com>
2024-10-22 16:04:25 -0600

    fmt

--- datafusion/sqllogictest/src/test_context.rs
+++ datafusion/sqllogictest/src/test_context.rs
@@ -334,13 +334,12 @@ pub async fn register_metadata_tables(ctx: &SessionContext) {
             ),
         ]));
 
-    let schema =
-        Schema::new(vec![id, name, l_name, ts, nonnull_name]).with_metadata(HashMap::from([
-            (
-                String::from("metadata_key"),
-                String::from("the entire schema"),
-            ),
-        ]));
+    let schema = Schema::new(vec![id, name, l_name, ts, nonnull_name]).with_metadata(
+        HashMap::from([(
+            String::from("metadata_key"),
+            String::from("the entire schema"),
+        )]),
+    );
 
     let batch = RecordBatch::try_new(
         Arc::new(schema),
