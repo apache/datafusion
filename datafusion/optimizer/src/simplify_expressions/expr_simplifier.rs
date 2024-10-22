e647c365208d185b43a45aa38ef8726d06506a11
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2024-10-22 10:57:14 -0500

    fmt

--- datafusion/optimizer/src/simplify_expressions/expr_simplifier.rs
+++ datafusion/optimizer/src/simplify_expressions/expr_simplifier.rs
@@ -1447,31 +1447,36 @@ impl<'a, S: SimplifyInfo> TreeNodeRewriter for Simplifier<'a, S> {
             // Rules for Like
             Expr::Like(Like {
                 expr: ref like_expr,
-                ref pattern,  // Changed to ref pattern to avoid moving
+                ref pattern, // Changed to ref pattern to avoid moving
                 negated,
                 escape_char: _,
                 case_insensitive: _,
             }) => {
-                if let Expr::Literal(ScalarValue::Utf8(Some(pattern))) = pattern.as_ref() {
+                if let Expr::Literal(ScalarValue::Utf8(Some(pattern))) = pattern.as_ref()
+                {
                     // Special case: pattern is just "%"
                     if pattern == "%" && !is_null(&like_expr) {
                         return Ok(Transformed::yes(lit(!negated)));
                     }
-                
+
                     let pct_wildcard_index = pattern.find('%');
                     let underscore_index = pattern.find('_');
-                
+
                     // If no wildcards, treat as equality check
                     if underscore_index.is_none() && pct_wildcard_index.is_none() {
-                        return Ok(
-                            Transformed::yes(Expr::BinaryExpr(BinaryExpr {
-                                left: like_expr.clone(),
-                                op: if negated { Operator::NotEq } else { Operator::Eq },
-                                right: Box::new(Expr::Literal(ScalarValue::Utf8(Some(pattern.to_string())))),
-                            }))
-                        );
+                        return Ok(Transformed::yes(Expr::BinaryExpr(BinaryExpr {
+                            left: like_expr.clone(),
+                            op: if negated {
+                                Operator::NotEq
+                            } else {
+                                Operator::Eq
+                            },
+                            right: Box::new(Expr::Literal(ScalarValue::Utf8(Some(
+                                pattern.to_string(),
+                            )))),
+                        })));
                     }
-                
+
                     // Handle single % at end case (prefix search)
                     if let Some(index) = pct_wildcard_index {
                         if index == pattern.len() - 1 {
@@ -1480,7 +1485,11 @@ impl<'a, S: SimplifyInfo> TreeNodeRewriter for Simplifier<'a, S> {
                                 func: datafusion_functions::string::starts_with(),
                                 args: vec![*like_expr.clone(), lit(prefix)],
                             });
-                            return Ok(Transformed::yes(if negated { new_expr.not() } else { new_expr }));
+                            return Ok(Transformed::yes(if negated {
+                                new_expr.not()
+                            } else {
+                                new_expr
+                            }));
                         }
                     }
                 }
@@ -2870,7 +2879,10 @@ mod tests {
         assert_no_change(regex_match(col("c1"), lit("$foo^")));
 
         // regular expressions that match a partial literal
-        assert_change(regex_match(col("c1"), lit("^foo")), starts_with(col("c1"), "foo"));
+        assert_change(
+            regex_match(col("c1"), lit("^foo")),
+            starts_with(col("c1"), "foo"),
+        );
         assert_change(regex_match(col("c1"), lit("foo$")), like(col("c1"), "%foo"));
         assert_change(
             regex_match(col("c1"), lit("^foo|bar$")),
