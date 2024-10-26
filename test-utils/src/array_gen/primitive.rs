1b3f475b354ddb7470cd7ed2ce21f888d2ec448b
Leslie Su <3530611790@qq.com>
2024-10-26 18:29:37 +0800

    feat: Add `Date32`/`Date64`  in aggregate fuzz testing (#13041)
    * refactor PrimitiveArrayGenerator.
    
    * support Date32/Date64 type in data generator.
    
    * fix format.
    
    * remove unnecessary type para in PrimitiveArrayGenerator.
    
    * introduce FromNative trait and replace the unsafe.

--- test-utils/src/array_gen/primitive.rs
+++ test-utils/src/array_gen/primitive.rs
@@ -15,13 +15,44 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use arrow::array::{ArrayRef, PrimitiveArray, UInt32Array};
-use arrow::datatypes::{
-    Float32Type, Float64Type, Int16Type, Int32Type, Int64Type, Int8Type, UInt16Type,
-    UInt32Type, UInt64Type, UInt8Type,
-};
+use arrow::array::{ArrayRef, ArrowPrimitiveType, PrimitiveArray, UInt32Array};
+use arrow::datatypes::DataType;
+use rand::distributions::Standard;
+use rand::prelude::Distribution;
 use rand::rngs::StdRng;
 use rand::Rng;
+
+/// Trait for converting type safely from a native type T impl this trait.
+pub trait FromNative: std::fmt::Debug + Send + Sync + Copy + Default {
+    /// Convert native type from i64.
+    fn from_i64(_: i64) -> Option<Self> {
+        None
+    }
+}
+
+macro_rules! native_type {
+    ($t: ty $(, $from:ident)*) => {
+        impl FromNative for $t {
+            $(
+                #[inline]
+                fn $from(v: $t) -> Option<Self> {
+                    Some(v)
+                }
+            )*
+        }
+    };
+}
+
+native_type!(i8);
+native_type!(i16);
+native_type!(i32);
+native_type!(i64, from_i64);
+native_type!(u8);
+native_type!(u16);
+native_type!(u32);
+native_type!(u64);
+native_type!(f32);
+native_type!(f64);
 
 /// Randomly generate primitive array
 pub struct PrimitiveArrayGenerator {
@@ -35,46 +66,61 @@ pub struct PrimitiveArrayGenerator {
     pub rng: StdRng,
 }
 
-macro_rules! impl_gen_data {
-    ($NATIVE_TYPE:ty, $ARROW_TYPE:ident) => {
-        paste::paste! {
-            pub fn [< gen_data_ $NATIVE_TYPE >](&mut self) -> ArrayRef {
-                    // table of strings from which to draw
-                    let distinct_primitives: PrimitiveArray<$ARROW_TYPE> = (0..self.num_distinct_primitives)
-                    .map(|_| Some(self.rng.gen::<$NATIVE_TYPE>()))
-                    .collect();
-
-                // pick num_strings randomly from the distinct string table
-                let indicies: UInt32Array = (0..self.num_primitives)
-                    .map(|_| {
-                        if self.rng.gen::<f64>() < self.null_pct {
-                            None
-                        } else if self.num_distinct_primitives > 1 {
-                            let range = 1..(self.num_distinct_primitives as u32);
-                            Some(self.rng.gen_range(range))
-                        } else {
-                            Some(0)
-                        }
-                    })
-                    .collect();
-
-                let options = None;
-                arrow::compute::take(&distinct_primitives, &indicies, options).unwrap()
-            }
-        }
-    };
-}
-
 // TODO: support generating more primitive arrays
 impl PrimitiveArrayGenerator {
-    impl_gen_data!(i8, Int8Type);
-    impl_gen_data!(i16, Int16Type);
-    impl_gen_data!(i32, Int32Type);
-    impl_gen_data!(i64, Int64Type);
-    impl_gen_data!(u8, UInt8Type);
-    impl_gen_data!(u16, UInt16Type);
-    impl_gen_data!(u32, UInt32Type);
-    impl_gen_data!(u64, UInt64Type);
-    impl_gen_data!(f32, Float32Type);
-    impl_gen_data!(f64, Float64Type);
+    pub fn gen_data<A>(&mut self) -> ArrayRef
+    where
+        A: ArrowPrimitiveType,
+        A::Native: FromNative,
+        Standard: Distribution<<A as ArrowPrimitiveType>::Native>,
+    {
+        // table of primitives from which to draw
+        let distinct_primitives: PrimitiveArray<A> = (0..self.num_distinct_primitives)
+            .map(|_| {
+                Some(match A::DATA_TYPE {
+                    DataType::Int8
+                    | DataType::Int16
+                    | DataType::Int32
+                    | DataType::Int64
+                    | DataType::UInt8
+                    | DataType::UInt16
+                    | DataType::UInt32
+                    | DataType::UInt64
+                    | DataType::Float32
+                    | DataType::Float64
+                    | DataType::Date32 => self.rng.gen::<A::Native>(),
+
+                    DataType::Date64 => {
+                        // TODO: constrain this range to valid dates if necessary
+                        let date_value = self.rng.gen_range(i64::MIN..=i64::MAX);
+                        let millis_per_day = 86_400_000;
+                        let adjusted_value = date_value - (date_value % millis_per_day);
+                        A::Native::from_i64(adjusted_value).unwrap()
+                    }
+
+                    _ => {
+                        let arrow_type = A::DATA_TYPE;
+                        panic!("Unsupported arrow data type: {arrow_type}")
+                    }
+                })
+            })
+            .collect();
+
+        // pick num_primitves randomly from the distinct string table
+        let indicies: UInt32Array = (0..self.num_primitives)
+            .map(|_| {
+                if self.rng.gen::<f64>() < self.null_pct {
+                    None
+                } else if self.num_distinct_primitives > 1 {
+                    let range = 1..(self.num_distinct_primitives as u32);
+                    Some(self.rng.gen_range(range))
+                } else {
+                    Some(0)
+                }
+            })
+            .collect();
+
+        let options = None;
+        arrow::compute::take(&distinct_primitives, &indicies, options).unwrap()
+    }
 }
