86dcb113a627ae4a3c39d9c037d79dbbf305f18d
kamille <caoruiqiu.crq@antgroup.com>
2024-10-28 14:05:15 +0800

    fallback to total scalarized `GroupValuesColumn` in streaming aggregation.

--- datafusion/physical-plan/src/aggregates/group_values/column.rs
+++ datafusion/physical-plan/src/aggregates/group_values/column.rs
@@ -115,7 +115,7 @@ impl GroupIndexView {
 
 /// A [`GroupValues`] that stores multiple columns of group values.
 ///
-pub struct GroupValuesColumn {
+pub struct VectorizedGroupValuesColumn {
     /// The output schema
     schema: SchemaRef,
 
@@ -176,7 +176,7 @@ pub struct GroupValuesColumn {
     random_state: RandomState,
 }
 
-impl GroupValuesColumn {
+impl VectorizedGroupValuesColumn {
     /// Create a new instance of GroupValuesColumn if supported for the specified schema
     pub fn try_new(schema: SchemaRef) -> Result<Self> {
         let map = RawTable::with_capacity(0);
@@ -195,43 +195,6 @@ impl GroupValuesColumn {
             vectorized_equal_to_results: Default::default(),
             vectorized_append_row_indices: Default::default(),
         })
-    }
-
-    /// Returns true if [`GroupValuesColumn`] supported for the specified schema
-    pub fn supported_schema(schema: &Schema) -> bool {
-        schema
-            .fields()
-            .iter()
-            .map(|f| f.data_type())
-            .all(Self::supported_type)
-    }
-
-    /// Returns true if the specified data type is supported by [`GroupValuesColumn`]
-    ///
-    /// In order to be supported, there must be a specialized implementation of
-    /// [`GroupColumn`] for the data type, instantiated in [`Self::intern`]
-    fn supported_type(data_type: &DataType) -> bool {
-        matches!(
-            *data_type,
-            DataType::Int8
-                | DataType::Int16
-                | DataType::Int32
-                | DataType::Int64
-                | DataType::UInt8
-                | DataType::UInt16
-                | DataType::UInt32
-                | DataType::UInt64
-                | DataType::Float32
-                | DataType::Float64
-                | DataType::Utf8
-                | DataType::LargeUtf8
-                | DataType::Binary
-                | DataType::LargeBinary
-                | DataType::Date32
-                | DataType::Date64
-                | DataType::Utf8View
-                | DataType::BinaryView
-        )
     }
 
     /// Collect vectorized context by checking hash values of `cols` in `map`
@@ -546,7 +509,7 @@ macro_rules! instantiate_primitive {
     };
 }
 
-impl GroupValues for GroupValuesColumn {
+impl GroupValues for VectorizedGroupValuesColumn {
     fn intern(&mut self, cols: &[ArrayRef], groups: &mut Vec<usize>) -> Result<()> {
         let n_rows = cols[0].len();
 
@@ -766,113 +729,294 @@ impl GroupValues for GroupValuesColumn {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use std::sync::Arc;
+/// A [`GroupValues`] that stores multiple columns of group values.
+///
+///
+pub struct GroupValuesColumn {
+    /// The output schema
+    schema: SchemaRef,
 
-    use ahash::RandomState;
-    use arrow_array::{ArrayRef, Int64Array, StringArray};
-    use arrow_schema::{DataType, Field, Schema};
-    use datafusion_common::hash_utils::create_hashes;
+    /// Logically maps group values to a group_index in
+    /// [`Self::group_values`] and in each accumulator
+    ///
+    /// Uses the raw API of hashbrown to avoid actually storing the
+    /// keys (group values) in the table
+    ///
+    /// keys: u64 hashes of the GroupValue
+    /// values: (hash, group_index)
+    map: RawTable<(u64, usize)>,
 
-    use crate::aggregates::group_values::{column::GroupValuesColumn, GroupValues};
+    /// The size of `map` in bytes
+    map_size: usize,
 
-    #[test]
-    fn test() {
-        // ***************************************************************
-        // The test group cols, the schema is `a(Int64) + b(String)`.
-        // It should cover following input rows situations:
-        //   - a: null + b: null
-        //   - a: not null + b: null
-        //   - a: null + b: not null
-        //   - a: not null + b: not null
-        //
-        // And it should cover following repeating situations:
-        //   - Rows unique
-        //   - Rows repeating in two `cols`
-        //   - Rows repeating in single `cols`
-        // ***************************************************************
+    /// The actual group by values, stored column-wise. Compare from
+    /// the left to right, each column is stored as [`GroupColumn`].
+    ///
+    /// Performance tests showed that this design is faster than using the
+    /// more general purpose [`GroupValuesRows`]. See the ticket for details:
+    /// <https://github.com/apache/datafusion/pull/12269>
+    ///
+    /// [`GroupValuesRows`]: crate::aggregates::group_values::row::GroupValuesRows
+    group_values: Vec<Box<dyn GroupColumn>>,
 
-        let schema = Arc::new(Schema::new(vec![
-            Field::new("a", DataType::Int64, true),
-            Field::new("b", DataType::Utf8, true),
-        ]));
-        // // Case 1
-        // Cols 1
-        let a: ArrayRef = Arc::new(Int64Array::from(vec![
-            None,
-            Some(42),
-            None,
-            Some(24),
-            Some(4224),
-        ]));
-        let b: ArrayRef = Arc::new(StringArray::from(vec![
-            None,
-            None,
-            Some("42"),
-            Some("24"),
-            Some("4224"),
-        ]));
-        let cols1 = vec![a, b];
+    /// reused buffer to store hashes
+    hashes_buffer: Vec<u64>,
 
-        // Cols 2
-        let a: ArrayRef = Arc::new(Int64Array::from(vec![
-            None,
-            Some(42),
-            None,
-            Some(24),
-            Some(2442),
-        ]));
-        let b: ArrayRef = Arc::new(StringArray::from(vec![
-            None,
-            None,
-            Some("42"),
-            Some("24"),
-            Some("2442"),
-        ]));
-        let cols2 = vec![a, b];
+    /// Random state for creating hashes
+    random_state: RandomState,
+}
 
-        // Cols 3
-        let a: ArrayRef = Arc::new(Int64Array::from(vec![
-            None,
-            Some(42),
-            None,
-            Some(24),
-            None,
-            Some(42),
-            None,
-            Some(24),
-            Some(4224),
-        ]));
-        let b: ArrayRef = Arc::new(StringArray::from(vec![
-            None,
-            None,
-            Some("42"),
-            Some("24"),
-            None,
-            None,
-            Some("42"),
-            Some("24"),
-            Some("4224"),
-        ]));
-        let cols3 = vec![a, b];
+impl GroupValuesColumn {
+    /// Create a new instance of GroupValuesColumn if supported for the specified schema
+    pub fn try_new(schema: SchemaRef) -> Result<Self> {
+        let map = RawTable::with_capacity(0);
+        Ok(Self {
+            schema,
+            map,
+            map_size: 0,
+            group_values: vec![],
+            hashes_buffer: Default::default(),
+            random_state: Default::default(),
+        })
+    }
+}
 
-        let mut group_values = GroupValuesColumn::try_new(schema).unwrap();
-        let mut groups = Vec::new();
-        group_values.intern(&cols1, &mut groups).unwrap();
-        group_values.intern(&cols2, &mut groups).unwrap();
-        group_values.intern(&cols3, &mut groups).unwrap();
+impl GroupValues for GroupValuesColumn {
+    fn intern(&mut self, cols: &[ArrayRef], groups: &mut Vec<usize>) -> Result<()> {
+        let n_rows = cols[0].len();
+
+        if self.group_values.is_empty() {
+            let mut v = Vec::with_capacity(cols.len());
+
+            for f in self.schema.fields().iter() {
+                let nullable = f.is_nullable();
+                match f.data_type() {
+                    &DataType::Int8 => instantiate_primitive!(v, nullable, Int8Type),
+                    &DataType::Int16 => instantiate_primitive!(v, nullable, Int16Type),
+                    &DataType::Int32 => instantiate_primitive!(v, nullable, Int32Type),
+                    &DataType::Int64 => instantiate_primitive!(v, nullable, Int64Type),
+                    &DataType::UInt8 => instantiate_primitive!(v, nullable, UInt8Type),
+                    &DataType::UInt16 => instantiate_primitive!(v, nullable, UInt16Type),
+                    &DataType::UInt32 => instantiate_primitive!(v, nullable, UInt32Type),
+                    &DataType::UInt64 => instantiate_primitive!(v, nullable, UInt64Type),
+                    &DataType::Float32 => {
+                        instantiate_primitive!(v, nullable, Float32Type)
+                    }
+                    &DataType::Float64 => {
+                        instantiate_primitive!(v, nullable, Float64Type)
+                    }
+                    &DataType::Date32 => instantiate_primitive!(v, nullable, Date32Type),
+                    &DataType::Date64 => instantiate_primitive!(v, nullable, Date64Type),
+                    &DataType::Utf8 => {
+                        let b = ByteGroupValueBuilder::<i32>::new(OutputType::Utf8);
+                        v.push(Box::new(b) as _)
+                    }
+                    &DataType::LargeUtf8 => {
+                        let b = ByteGroupValueBuilder::<i64>::new(OutputType::Utf8);
+                        v.push(Box::new(b) as _)
+                    }
+                    &DataType::Binary => {
+                        let b = ByteGroupValueBuilder::<i32>::new(OutputType::Binary);
+                        v.push(Box::new(b) as _)
+                    }
+                    &DataType::LargeBinary => {
+                        let b = ByteGroupValueBuilder::<i64>::new(OutputType::Binary);
+                        v.push(Box::new(b) as _)
+                    }
+                    dt => {
+                        return not_impl_err!("{dt} not supported in GroupValuesColumn")
+                    }
+                }
+            }
+            self.group_values = v;
+        }
+
+        // tracks to which group each of the input rows belongs
+        groups.clear();
+
+        // 1.1 Calculate the group keys for the group values
+        let batch_hashes = &mut self.hashes_buffer;
+        batch_hashes.clear();
+        batch_hashes.resize(n_rows, 0);
+        create_hashes(cols, &self.random_state, batch_hashes)?;
+
+        for (row, &target_hash) in batch_hashes.iter().enumerate() {
+            let entry = self.map.get_mut(target_hash, |(exist_hash, group_idx)| {
+                // Somewhat surprisingly, this closure can be called even if the
+                // hash doesn't match, so check the hash first with an integer
+                // comparison first avoid the more expensive comparison with
+                // group value. https://github.com/apache/datafusion/pull/11718
+                if target_hash != *exist_hash {
+                    return false;
+                }
+
+                fn check_row_equal(
+                    array_row: &dyn GroupColumn,
+                    lhs_row: usize,
+                    array: &ArrayRef,
+                    rhs_row: usize,
+                ) -> bool {
+                    array_row.equal_to(lhs_row, array, rhs_row)
+                }
+
+                for (i, group_val) in self.group_values.iter().enumerate() {
+                    if !check_row_equal(group_val.as_ref(), *group_idx, &cols[i], row) {
+                        return false;
+                    }
+                }
+
+                true
+            });
+
+            let group_idx = match entry {
+                // Existing group_index for this group value
+                Some((_hash, group_idx)) => *group_idx,
+                //  1.2 Need to create new entry for the group
+                None => {
+                    // Add new entry to aggr_state and save newly created index
+                    // let group_idx = group_values.num_rows();
+                    // group_values.push(group_rows.row(row));
+
+                    let mut checklen = 0;
+                    let group_idx = self.group_values[0].len();
+                    for (i, group_value) in self.group_values.iter_mut().enumerate() {
+                        group_value.append_val(&cols[i], row);
+                        let len = group_value.len();
+                        if i == 0 {
+                            checklen = len;
+                        } else {
+                            debug_assert_eq!(checklen, len);
+                        }
+                    }
+
+                    // for hasher function, use precomputed hash value
+                    self.map.insert_accounted(
+                        (target_hash, group_idx),
+                        |(hash, _group_index)| *hash,
+                        &mut self.map_size,
+                    );
+                    group_idx
+                }
+            };
+            groups.push(group_idx);
+        }
+
+        Ok(())
     }
 
-    #[test]
-    fn test2() {
-        let col1 = Arc::new(Int64Array::from(vec![Some(1), Some(0)])) as _;
-        let col2 = Arc::new(Int64Array::from(vec![Some(0), Some(1)])) as _;
-        let col3 = Arc::new(Int64Array::from(vec![Some(0), Some(0)])) as _;
-        let cols = vec![col1, col2, col3];
-        let random_state = RandomState::with_seeds(0, 0, 0, 0);
-        let mut hash_buffer = vec![0; 2];
-        create_hashes(&cols, &random_state, &mut hash_buffer);
-        dbg!(&hash_buffer);
+    fn size(&self) -> usize {
+        let group_values_size: usize = self.group_values.iter().map(|v| v.size()).sum();
+        group_values_size + self.map_size + self.hashes_buffer.allocated_size()
     }
+
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    fn len(&self) -> usize {
+        if self.group_values.is_empty() {
+            return 0;
+        }
+
+        self.group_values[0].len()
+    }
+
+    fn emit(&mut self, emit_to: EmitTo) -> Result<Vec<ArrayRef>> {
+        let mut output = match emit_to {
+            EmitTo::All => {
+                let group_values = std::mem::take(&mut self.group_values);
+                debug_assert!(self.group_values.is_empty());
+
+                group_values
+                    .into_iter()
+                    .map(|v| v.build())
+                    .collect::<Vec<_>>()
+            }
+            EmitTo::First(n) => {
+                let output = self
+                    .group_values
+                    .iter_mut()
+                    .map(|v| v.take_n(n))
+                    .collect::<Vec<_>>();
+
+                // SAFETY: self.map outlives iterator and is not modified concurrently
+                unsafe {
+                    for bucket in self.map.iter() {
+                        // Decrement group index by n
+                        match bucket.as_ref().1.checked_sub(n) {
+                            // Group index was >= n, shift value down
+                            Some(sub) => bucket.as_mut().1 = sub,
+                            // Group index was < n, so remove from table
+                            None => self.map.erase(bucket),
+                        }
+                    }
+                }
+
+                output
+            }
+        };
+
+        // TODO: Materialize dictionaries in group keys (#7647)
+        for (field, array) in self.schema.fields.iter().zip(&mut output) {
+            let expected = field.data_type();
+            if let DataType::Dictionary(_, v) = expected {
+                let actual = array.data_type();
+                if v.as_ref() != actual {
+                    return Err(DataFusionError::Internal(format!(
+                        "Converted group rows expected dictionary of {v} got {actual}"
+                    )));
+                }
+                *array = cast(array.as_ref(), expected)?;
+            }
+        }
+
+        Ok(output)
+    }
+
+    fn clear_shrink(&mut self, batch: &RecordBatch) {
+        let count = batch.num_rows();
+        self.group_values.clear();
+        self.map.clear();
+        self.map.shrink_to(count, |_| 0); // hasher does not matter since the map is cleared
+        self.map_size = self.map.capacity() * std::mem::size_of::<(u64, usize)>();
+        self.hashes_buffer.clear();
+        self.hashes_buffer.shrink_to(count);
+    }
+}
+
+/// Returns true if [`GroupValuesColumn`] supported for the specified schema
+pub fn supported_schema(schema: &Schema) -> bool {
+    schema
+        .fields()
+        .iter()
+        .map(|f| f.data_type())
+        .all(supported_type)
+}
+
+/// Returns true if the specified data type is supported by [`GroupValuesColumn`]
+///
+/// In order to be supported, there must be a specialized implementation of
+/// [`GroupColumn`] for the data type, instantiated in [`Self::intern`]
+fn supported_type(data_type: &DataType) -> bool {
+    matches!(
+        *data_type,
+        DataType::Int8
+            | DataType::Int16
+            | DataType::Int32
+            | DataType::Int64
+            | DataType::UInt8
+            | DataType::UInt16
+            | DataType::UInt32
+            | DataType::UInt64
+            | DataType::Float32
+            | DataType::Float64
+            | DataType::Utf8
+            | DataType::LargeUtf8
+            | DataType::Binary
+            | DataType::LargeBinary
+            | DataType::Date32
+            | DataType::Date64
+            | DataType::Utf8View
+            | DataType::BinaryView
+    )
 }
--- datafusion/physical-plan/src/aggregates/row_hash.rs
+++ datafusion/physical-plan/src/aggregates/row_hash.rs
@@ -514,7 +514,7 @@ impl GroupedHashAggregateStream {
             ordering.as_slice(),
         )?;
 
-        let group_values = new_group_values(group_schema)?;
+        let group_values = new_group_values(group_schema, &group_ordering)?;
         timer.done();
 
         let exec_state = ExecutionState::ReadingInput;
