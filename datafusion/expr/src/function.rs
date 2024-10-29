8cf60d9aa803a976ba1f50aea4e0a5afbd4fb4eb
Michael-J-Ward <ward.michael.j@gmail.com>
2024-10-28 20:13:58 -0500

    chore: re-export functions_window_common::ExpressionArgs
    This struct is needed to implement the WindowUDFImpl trait.

--- datafusion/expr/src/function.rs
+++ datafusion/expr/src/function.rs
@@ -29,6 +29,7 @@ pub use datafusion_functions_aggregate_common::accumulator::{
 
 pub use datafusion_functions_window_common::field::WindowUDFFieldArgs;
 pub use datafusion_functions_window_common::partition::PartitionEvaluatorArgs;
+pub use datafusion_functions_window_common::expr::ExpressionArgs;
 
 #[derive(Debug, Clone, Copy)]
 pub enum Hint {
