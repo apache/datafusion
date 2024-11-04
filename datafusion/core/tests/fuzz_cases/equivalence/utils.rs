a0f753dc8acda25aab93ed4a6d6dc1f894e5f849
Ruihang Xia <waynestxia@gmail.com>
2024-11-04 15:25:52 +0800

    fix new header errors
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- datafusion/core/tests/fuzz_cases/equivalence/utils.rs
+++ datafusion/core/tests/fuzz_cases/equivalence/utils.rs
@@ -14,7 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-//
+
 use datafusion::physical_plan::expressions::col;
 use datafusion::physical_plan::expressions::Column;
 use datafusion_physical_expr::{ConstExpr, EquivalenceProperties, PhysicalSortExpr};
