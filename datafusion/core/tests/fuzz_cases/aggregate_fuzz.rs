a1a7194a11fd2c70af37208a31bed9d4bee1fe3c
Jonathan Chen <chenleejonathan@gmail.com>
2024-11-10 00:19:34 -0500

    add bool col

--- datafusion/core/tests/fuzz_cases/aggregate_fuzz.rs
+++ datafusion/core/tests/fuzz_cases/aggregate_fuzz.rs
@@ -210,6 +210,7 @@ fn baseline_config() -> DatasetGeneratorConfig {
         // low cardinality columns
         ColumnDescr::new("u8_low", DataType::UInt8).with_max_num_distinct(10),
         ColumnDescr::new("utf8_low", DataType::Utf8).with_max_num_distinct(10),
+        ColumnDescr::new("bool", DataType::Boolean),
     ];
 
     let min_num_rows = 512;
--- test-utils/src/array_gen/mod.rs
+++ test-utils/src/array_gen/mod.rs
@@ -15,11 +15,13 @@
 // specific language governing permissions and limitations
 // under the License.
 
+mod boolean;
 mod decimal;
 mod primitive;
 mod random_data;
 mod string;
 
+pub use boolean::BooleanArrayGenerator;
 pub use decimal::DecimalArrayGenerator;
 pub use primitive::PrimitiveArrayGenerator;
 pub use string::StringArrayGenerator;
