2ae09b3413fe6df4af37d8d931db4bf47ba195d1
Jay Zhan <jayzhan211@gmail.com>
2024-11-12 08:39:57 +0800

    fix comment
    Signed-off-by: Jay Zhan <jayzhan211@gmail.com>

--- datafusion/expr-common/src/type_coercion/binary.rs
+++ datafusion/expr-common/src/type_coercion/binary.rs
@@ -330,8 +330,7 @@ impl From<&DataType> for TypeCategory {
                     return TypeCategory::Array;
                 }
 
-                // String literal is possible to cast to many other types like numeric or datetime,
-                // therefore, it is categorized as a unknown type
+                // It is categorized as unknown type because the type will be resolved later on
                 if matches!(
                     data_type,
                     DataType::Utf8
