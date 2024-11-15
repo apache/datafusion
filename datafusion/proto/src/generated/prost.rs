29d6b90b9bdc7b0fb48b569204509df35118f77c
Sherin Jacob <jacob@protoship.io>
2024-11-16 00:49:15 +0530

    fix: serialize user-defined window functions to proto (#13421)
    * Adds roundtrip physical plan test
    
    * Adds enum for udwf to `WindowFunction`
    
    * initial fix for serializing udwf
    
    * Revives deleted test
    
    * Adds codec methods for physical plan
    
    * Rewrite error message
    
    * Minor: rename binding + formatting fixes
    
    * Extends `PhysicalExtensionCodec` for udwf
    
    * Minor: formatting
    
    * Restricts visibility to tests

--- datafusion/proto/src/generated/prost.rs
+++ datafusion/proto/src/generated/prost.rs
@@ -1266,7 +1266,7 @@ pub struct PhysicalWindowExprNode {
     pub name: ::prost::alloc::string::String,
     #[prost(bytes = "vec", optional, tag = "9")]
     pub fun_definition: ::core::option::Option<::prost::alloc::vec::Vec<u8>>,
-    #[prost(oneof = "physical_window_expr_node::WindowFunction", tags = "3")]
+    #[prost(oneof = "physical_window_expr_node::WindowFunction", tags = "3, 10")]
     pub window_function: ::core::option::Option<
         physical_window_expr_node::WindowFunction,
     >,
@@ -1278,6 +1278,8 @@ pub mod physical_window_expr_node {
         /// BuiltInWindowFunction built_in_function = 2;
         #[prost(string, tag = "3")]
         UserDefinedAggrFunction(::prost::alloc::string::String),
+        #[prost(string, tag = "10")]
+        UserDefinedWindowFunction(::prost::alloc::string::String),
     }
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
