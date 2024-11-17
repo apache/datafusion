a889299c60953ac0be0f404b2cf16c4ccac5b98d
Jay Zhan <jayzhan211@gmail.com>
2024-11-17 10:40:40 +0800

    count group
    Signed-off-by: Jay Zhan <jayzhan211@gmail.com>

--- datafusion/functions-aggregate/src/count.rs
+++ datafusion/functions-aggregate/src/count.rs
@@ -481,15 +481,11 @@ impl GroupsAccumulator for CountGroupsAccumulator {
         // Adds the counts with the partial counts
         self.counts.resize(total_num_groups, 0);
         match opt_filter {
-            Some(filter) => filter
-                .iter()
-                .zip(group_indices.iter())
-                .zip(partial_counts.iter())
-                .for_each(|((filter_value, &group_index), partial_count)| {
-                    if let Some(true) = filter_value {
-                        self.counts[group_index] += partial_count;
-                    }
-                }),
+            Some(filter) => {
+                filter.values().set_indices().for_each(|index| {
+                    self.counts[group_indices[index]] += partial_counts[index]
+                });
+            }
             None => group_indices.iter().zip(partial_counts.iter()).for_each(
                 |(&group_index, partial_count)| {
                     self.counts[group_index] += partial_count;
