510495df4caa3d2790c5d5c6f48f3c55812b80dd
kamille <caoruiqiu.crq@antgroup.com>
2024-11-23 23:31:48 +0800

    modify cargo toml.

--- datafusion/physical-plan/benches/vectorized_equal_to.rs
+++ datafusion/physical-plan/benches/vectorized_equal_to.rs
@@ -17,9 +17,10 @@
 
 use std::sync::Arc;
 
-use arrow::datatypes::Int64Type;
+use arrow::compute;
+use arrow::datatypes::{Int64Type, UInt64Type};
 use arrow::record_batch::RecordBatch;
-use arrow_array::{ArrayRef, Int32Array, Int64Array, StringArray};
+use arrow_array::{ArrayRef, Int32Array, Int64Array, StringArray, UInt64Array};
 use datafusion_execution::TaskContext;
 use datafusion_physical_expr::expressions::col;
 use datafusion_physical_expr::PhysicalSortExpr;
@@ -38,87 +39,99 @@ use test_utils::array_gen::PrimitiveArrayGenerator;
 
 struct PrimitiveEqualToBench {
     group_column: Box<dyn GroupColumn>,
-    lhr_rows: Vec<usize>,
+    lhs_rows: Vec<usize>,
     input_array: ArrayRef,
     rhs_rows: Vec<usize>,
+    results: Vec<bool>,
+    vectorized: bool,
 }
 
 impl PrimitiveEqualToBench {
-    pub fn new(null_pct: f64, nullable: bool) {
-        // Generate array
-        let mut data_gen = PrimitiveArrayGenerator {
-            num_primitives: 8192,
-            num_distinct_primitives: 64,
-            null_pct,
-            rng: StdRng::from_seed(thread_rng().gen()),
-        };
-        // let primitive_array = data_gen.gen_data();
+    pub fn new(
+        num_rows: usize,
+        num_distinct: usize,
+        num_compared_rows: usize,
+        nullable: bool,
+        vectorized: bool,
+    ) -> Self {
+        let mut rng = thread_rng();
 
-        // 
-        let group_colum: Box<dyn GroupColumn> = if nullable {
+        // Generate group array
+        let null_pct = if nullable { 0.5 } else { 0.0 };
+        let mut data_gen = PrimitiveArrayGenerator {
+            num_primitives: num_rows,
+            num_distinct_primitives: num_distinct,
+            null_pct,
+            rng: StdRng::from_seed(rng.gen()),
+        };
+        let group_array = data_gen.gen_data::<Int64Type>();
+        let group_len = group_array.len();
+
+        // Generate compared array
+        let mut compared_indices = Vec::with_capacity(num_compared_rows);
+        for _ in 0..num_compared_rows {
+            let compared_idx = rng.gen_range(0..group_len);
+            compared_indices.push(compared_idx as u64);
+        }
+        let compared_indices_array = UInt64Array::from(compared_indices.clone());
+        let compared_array =
+            compute::take(&group_array, &compared_indices_array, None).unwrap();
+
+        // Create group column, insert the group array
+        let mut group_column: Box<dyn GroupColumn> = if nullable {
             Box::new(PrimitiveGroupValueBuilder::<Int64Type, true>::new())
         } else {
             Box::new(PrimitiveGroupValueBuilder::<Int64Type, false>::new())
         };
+        let group_rows = (0..group_len).into_iter().collect::<Vec<_>>();
+        group_column.vectorized_append(&group_array, &group_rows);
+
+        // Build bench
+        let lhs_rows = compared_indices
+            .iter()
+            .map(|idx| *idx as usize)
+            .collect::<Vec<_>>();
+        let rhs_rows = (0..num_compared_rows).into_iter().collect::<Vec<_>>();
+
+        Self {
+            group_column,
+            lhs_rows,
+            input_array: compared_array,
+            rhs_rows,
+            results: vec![true; num_compared_rows],
+            vectorized,
+        }
+    }
+
+    fn run(&mut self) {
+        if self.vectorized {
+            self.group_column.vectorized_equal_to(
+                &self.lhs_rows,
+                &self.input_array,
+                &self.rhs_rows,
+                &mut self.results,
+            );
+        } else {
+            let iter = self.lhs_rows.iter().zip(self.rhs_rows.iter());
+            for (&lhs_row, &rhs_row) in iter {
+                self.group_column
+                    .equal_to(lhs_row, &self.input_array, rhs_row);
+            }
+        }
     }
 }
 
-// fn run_bench(
-//     c: &mut Criterion,
-//     has_same_value: bool,
-//     enable_round_robin_repartition: bool,
-//     batch_count: usize,
-//     partition_count: usize,
-//     description: &str,
-// ) {
-//     let task_ctx = TaskContext::default();
-//     let task_ctx = Arc::new(task_ctx);
+fn run_bench(c: &mut Criterion, description: &str, nullable: bool, vectorized: bool) {
+    let mut bench = PrimitiveEqualToBench::new(81920, 8192, 8192, nullable, vectorized);
+    c.bench_function(description, |b| b.iter(|| black_box(bench.run())));
+}
 
-//     let spm = Arc::new(generate_spm_for_round_robin_tie_breaker(
-//         has_same_value,
-//         enable_round_robin_repartition,
-//         batch_count,
-//         partition_count,
-//     )) as Arc<dyn ExecutionPlan>;
+fn criterion_benchmark(c: &mut Criterion) {
+    run_bench(c, "### vectorized + nullable", true, true);
+    run_bench(c, "### vectorized + non-nullable", false, true);
+    run_bench(c, "### scalarized + nullable", true, false);
+    run_bench(c, "### scalarized + non-nullable", false, false);
+}
 
-//     c.bench_function(description, |b| {
-//         b.to_async(FuturesExecutor)
-//             .iter(|| black_box(collect(Arc::clone(&spm), Arc::clone(&task_ctx))))
-//     });
-// }
-
-
-
-// fn criterion_benchmark(c: &mut Criterion) {
-//     let params = [
-//         (true, false, "low_card_without_tiebreaker"), // low cardinality, no tie breaker
-//         (true, true, "low_card_with_tiebreaker"),     // low cardinality, with tie breaker
-//         (false, false, "high_card_without_tiebreaker"), // high cardinality, no tie breaker
-//         (false, true, "high_card_with_tiebreaker"), // high cardinality, with tie breaker
-//     ];
-
-//     let batch_counts = [1, 25, 625];
-//     let partition_counts = [2, 8, 32];
-
-//     for &(has_same_value, enable_round_robin_repartition, cardinality_label) in &params {
-//         for &batch_count in &batch_counts {
-//             for &partition_count in &partition_counts {
-//                 let description = format!(
-//                     "{}_batch_count_{}_partition_count_{}",
-//                     cardinality_label, batch_count, partition_count
-//                 );
-//                 run_bench(
-//                     c,
-//                     has_same_value,
-//                     enable_round_robin_repartition,
-//                     batch_count,
-//                     partition_count,
-//                     &description,
-//                 );
-//             }
-//         }
-//     }
-// }
-
-// criterion_group!(benches, criterion_benchmark);
-// criterion_main!(benches);
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
