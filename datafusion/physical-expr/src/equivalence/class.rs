ba84d42250da57c827eabeb7a77d8d2e550ce647
jonahgao <jonahgao@msn.com>
2024-12-04 10:23:24 +0800

    Update import

--- datafusion/physical-expr/src/equivalence/class.rs
+++ datafusion/physical-expr/src/equivalence/class.rs
@@ -24,8 +24,7 @@ use std::fmt::Display;
 use std::sync::Arc;
 
 use datafusion_common::tree_node::{Transformed, TransformedResult, TreeNode};
-use datafusion_common::IndexSet;
-use datafusion_common::JoinType;
+use datafusion_common::{IndexSet, JoinType};
 use datafusion_physical_expr_common::physical_expr::format_physical_expr_list;
 
 /// A structure representing a expression known to be constant in a physical execution plan.
