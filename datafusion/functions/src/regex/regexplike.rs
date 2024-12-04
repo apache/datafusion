d841116fb7d3f8a568f10e7f584b7d5877de4b71
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-12-04 19:42:40 +0100

    Allow ColumnarValue to array conversion with less copying (#13644)
    `ColumnarValue::into_array` takes ownership of the columnar value and so
    requires copying data if the call site doesn't own the value.  This does
    not matter in many cases, since the `into_array` internally will copy
    data even more. It does matter however for the cases when to-array is
    called with desired array length of 1, which may happen when UDF
    implementation attempts to normalize arguments into arrays without
    expanding them. This pattern can be seen in regexp functions, but can be
    useful in downstream projects too.

--- datafusion/functions/src/regex/regexplike.rs
+++ datafusion/functions/src/regex/regexplike.rs
@@ -147,7 +147,7 @@ impl ScalarUDFImpl for RegexpLikeFunc {
         let inferred_length = len.unwrap_or(1);
         let args = args
             .iter()
-            .map(|arg| arg.clone().into_array(inferred_length))
+            .map(|arg| arg.to_array(inferred_length))
             .collect::<Result<Vec<_>>>()?;
 
         let result = regexp_like(&args);
