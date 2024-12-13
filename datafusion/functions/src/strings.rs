d70de6bb8779e4c47889bdcfc5e07947acb77c96
Jack Dockerty <jdockerty@influxdata.com>
2024-12-13 14:10:54 +0000

    fix: add `null_buffer` check for `LargeStringArray`
    Add a safety check to ensure that the alignment of buffers cannot be
    overflowed. This introduces a panic if they are not aligned through a
    runtime assertion.

--- datafusion/functions/src/strings.rs
+++ datafusion/functions/src/strings.rs
@@ -335,7 +335,27 @@ impl LargeStringArrayBuilder {
         unsafe { self.offsets_buffer.push_unchecked(next_offset) };
     }
 
+    /// Finalise the builder into a concrete [`LargeStringArray`].
+    ///
+    /// # Panics
+    ///
+    /// This method can panic when:
+    ///
+    /// - the provided `null_buffer` is not the same length as the `offsets_buffer`.
+    /// - the provided `null_buffer` is not the same length as the `value_buffer`.
     pub fn finish(self, null_buffer: Option<NullBuffer>) -> LargeStringArray {
+        if let Some(ref null_buffer) = null_buffer {
+            assert_eq!(
+                null_buffer.len(),
+                self.offsets_buffer.len() / size_of::<i64>() - 1,
+                "Null buffer and offsets buffer must be the same length"
+            );
+            assert_eq!(
+                null_buffer.len(),
+                self.value_buffer.len(),
+                "Null buffer and value buffer must be the same length"
+            );
+        }
         let array_builder = ArrayDataBuilder::new(DataType::LargeUtf8)
             .len(self.offsets_buffer.len() / size_of::<i64>() - 1)
             .add_buffer(self.offsets_buffer.into())
