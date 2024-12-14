dc2d89fe37be7a776d13d92aba52c88039450220
Weijun-H <huangweijun1001@gmail.com>
2024-12-14 17:07:45 +0800

    clean up

--- datafusion/physical-plan/src/sorts/merge.rs
+++ datafusion/physical-plan/src/sorts/merge.rs
@@ -148,8 +148,6 @@ pub(crate) struct SortPreservingMergeStream<C: CursorValues> {
     /// vector to ensure the next iteration starts with a different partition, preventing the same partition
     /// from being continuously polled.
     uninitiated_partitions: VecDeque<usize>,
-
-    enable_pull_based_execution: bool,
 }
 
 impl<C: CursorValues> SortPreservingMergeStream<C> {
@@ -161,7 +159,6 @@ impl<C: CursorValues> SortPreservingMergeStream<C> {
         fetch: Option<usize>,
         reservation: MemoryReservation,
         enable_round_robin_tie_breaker: bool,
-        enable_pull_based_execution: bool,
     ) -> Self {
         let stream_count = streams.partitions();
 
@@ -183,7 +180,6 @@ impl<C: CursorValues> SortPreservingMergeStream<C> {
             produced: 0,
             uninitiated_partitions: (0..stream_count).collect(),
             enable_round_robin_tie_breaker,
-            enable_pull_based_execution,
         }
     }
 
