a63c53fb27fbe57adf4df70081435b01d559c29e
Goksel Kabadayi <45314116+gokselk@users.noreply.github.com>
2024-12-17 16:50:06 +0300

    Simply use false value on default implementation

--- datafusion/expr/src/udf.rs
+++ datafusion/expr/src/udf.rs
@@ -680,14 +680,8 @@ pub trait ScalarUDFImpl: Debug + Send + Sync {
     }
 
     /// Whether the function preserves lexicographical ordering based on the input ordering
-    fn output_preserves_lex_ordering(&self, inputs: &[ExprProperties]) -> Result<bool> {
-        // Check for non-empty input and verify all elements have same sort properties as first
-        Ok(inputs.first().is_some_and(|first| {
-            inputs
-                .iter()
-                .skip(1)
-                .all(|input| input.sort_properties == first.sort_properties)
-        }))
+    fn output_preserves_lex_ordering(&self, _inputs: &[ExprProperties]) -> Result<bool> {
+        Ok(false)
     }
 
     /// Coerce arguments of a function call to types that the function can evaluate.
