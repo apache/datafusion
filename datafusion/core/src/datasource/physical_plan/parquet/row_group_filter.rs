452a8f4fa8fbfd88a5a71310bc28b9f78685c66f
Eduard Karacharov <eduard.karacharov@gmail.com>
2024-12-17 23:14:09 +0200

    fix: enable pruning by bloom filters for dictionary columns (#13768)

--- datafusion/core/src/datasource/physical_plan/parquet/row_group_filter.rs
+++ datafusion/core/src/datasource/physical_plan/parquet/row_group_filter.rs
@@ -228,6 +228,94 @@ struct BloomFilterStatistics {
     column_sbbf: HashMap<String, (Sbbf, Type)>,
 }
 
+impl BloomFilterStatistics {
+    /// Helper function for checking if [`Sbbf`] filter contains [`ScalarValue`].
+    ///
+    /// In case the type of scalar is not supported, returns `true`, assuming that the
+    /// value may be present.
+    fn check_scalar(sbbf: &Sbbf, value: &ScalarValue, parquet_type: &Type) -> bool {
+        match value {
+            ScalarValue::Utf8(Some(v))
+            | ScalarValue::Utf8View(Some(v))
+            | ScalarValue::LargeUtf8(Some(v)) => sbbf.check(&v.as_str()),
+            ScalarValue::Binary(Some(v))
+            | ScalarValue::BinaryView(Some(v))
+            | ScalarValue::LargeBinary(Some(v)) => sbbf.check(v),
+            ScalarValue::FixedSizeBinary(_size, Some(v)) => sbbf.check(v),
+            ScalarValue::Boolean(Some(v)) => sbbf.check(v),
+            ScalarValue::Float64(Some(v)) => sbbf.check(v),
+            ScalarValue::Float32(Some(v)) => sbbf.check(v),
+            ScalarValue::Int64(Some(v)) => sbbf.check(v),
+            ScalarValue::Int32(Some(v)) => sbbf.check(v),
+            ScalarValue::UInt64(Some(v)) => sbbf.check(v),
+            ScalarValue::UInt32(Some(v)) => sbbf.check(v),
+            ScalarValue::Decimal128(Some(v), p, s) => match parquet_type {
+                Type::INT32 => {
+                    //https://github.com/apache/parquet-format/blob/eb4b31c1d64a01088d02a2f9aefc6c17c54cc6fc/Encodings.md?plain=1#L35-L42
+                    // All physical type  are little-endian
+                    if *p > 9 {
+                        //DECIMAL can be used to annotate the following types:
+                        //
+                        // int32: for 1 <= precision <= 9
+                        // int64: for 1 <= precision <= 18
+                        return true;
+                    }
+                    let b = (*v as i32).to_le_bytes();
+                    // Use Decimal constructor after https://github.com/apache/arrow-rs/issues/5325
+                    let decimal = Decimal::Int32 {
+                        value: b,
+                        precision: *p as i32,
+                        scale: *s as i32,
+                    };
+                    sbbf.check(&decimal)
+                }
+                Type::INT64 => {
+                    if *p > 18 {
+                        return true;
+                    }
+                    let b = (*v as i64).to_le_bytes();
+                    let decimal = Decimal::Int64 {
+                        value: b,
+                        precision: *p as i32,
+                        scale: *s as i32,
+                    };
+                    sbbf.check(&decimal)
+                }
+                Type::FIXED_LEN_BYTE_ARRAY => {
+                    // keep with from_bytes_to_i128
+                    let b = v.to_be_bytes().to_vec();
+                    // Use Decimal constructor after https://github.com/apache/arrow-rs/issues/5325
+                    let decimal = Decimal::Bytes {
+                        value: b.into(),
+                        precision: *p as i32,
+                        scale: *s as i32,
+                    };
+                    sbbf.check(&decimal)
+                }
+                _ => true,
+            },
+            // One more parrern matching since not all data types are supported
+            // inside of a Dictionary
+            ScalarValue::Dictionary(_, inner) => match inner.as_ref() {
+                ScalarValue::Int32(_)
+                | ScalarValue::Int64(_)
+                | ScalarValue::UInt32(_)
+                | ScalarValue::UInt64(_)
+                | ScalarValue::Float32(_)
+                | ScalarValue::Float64(_)
+                | ScalarValue::Utf8(_)
+                | ScalarValue::LargeUtf8(_)
+                | ScalarValue::Binary(_)
+                | ScalarValue::LargeBinary(_) => {
+                    BloomFilterStatistics::check_scalar(sbbf, inner, parquet_type)
+                }
+                _ => true,
+            },
+            _ => true,
+        }
+    }
+}
+
 impl PruningStatistics for BloomFilterStatistics {
     fn min_values(&self, _column: &Column) -> Option<ArrayRef> {
         None
@@ -268,70 +356,7 @@ impl PruningStatistics for BloomFilterStatistics {
 
         let known_not_present = values
             .iter()
-            .map(|value| {
-                match value {
-                    ScalarValue::Utf8(Some(v)) | ScalarValue::Utf8View(Some(v)) => {
-                        sbbf.check(&v.as_str())
-                    }
-                    ScalarValue::Binary(Some(v)) | ScalarValue::BinaryView(Some(v)) => {
-                        sbbf.check(v)
-                    }
-                    ScalarValue::FixedSizeBinary(_size, Some(v)) => sbbf.check(v),
-                    ScalarValue::Boolean(Some(v)) => sbbf.check(v),
-                    ScalarValue::Float64(Some(v)) => sbbf.check(v),
-                    ScalarValue::Float32(Some(v)) => sbbf.check(v),
-                    ScalarValue::Int64(Some(v)) => sbbf.check(v),
-                    ScalarValue::Int32(Some(v)) => sbbf.check(v),
-                    ScalarValue::UInt64(Some(v)) => sbbf.check(v),
-                    ScalarValue::UInt32(Some(v)) => sbbf.check(v),
-                    ScalarValue::Decimal128(Some(v), p, s) => match parquet_type {
-                        Type::INT32 => {
-                            //https://github.com/apache/parquet-format/blob/eb4b31c1d64a01088d02a2f9aefc6c17c54cc6fc/Encodings.md?plain=1#L35-L42
-                            // All physical type  are little-endian
-                            if *p > 9 {
-                                //DECIMAL can be used to annotate the following types:
-                                //
-                                // int32: for 1 <= precision <= 9
-                                // int64: for 1 <= precision <= 18
-                                return true;
-                            }
-                            let b = (*v as i32).to_le_bytes();
-                            // Use Decimal constructor after https://github.com/apache/arrow-rs/issues/5325
-                            let decimal = Decimal::Int32 {
-                                value: b,
-                                precision: *p as i32,
-                                scale: *s as i32,
-                            };
-                            sbbf.check(&decimal)
-                        }
-                        Type::INT64 => {
-                            if *p > 18 {
-                                return true;
-                            }
-                            let b = (*v as i64).to_le_bytes();
-                            let decimal = Decimal::Int64 {
-                                value: b,
-                                precision: *p as i32,
-                                scale: *s as i32,
-                            };
-                            sbbf.check(&decimal)
-                        }
-                        Type::FIXED_LEN_BYTE_ARRAY => {
-                            // keep with from_bytes_to_i128
-                            let b = v.to_be_bytes().to_vec();
-                            // Use Decimal constructor after https://github.com/apache/arrow-rs/issues/5325
-                            let decimal = Decimal::Bytes {
-                                value: b.into(),
-                                precision: *p as i32,
-                                scale: *s as i32,
-                            };
-                            sbbf.check(&decimal)
-                        }
-                        _ => true,
-                    },
-                    _ => true,
-                }
-            })
+            .map(|value| BloomFilterStatistics::check_scalar(sbbf, value, parquet_type))
             // The row group doesn't contain any of the values if
             // all the checks are false
             .all(|v| !v);
