0dccc817466c74b4cf69d587a6740e81f9120bbd
mertak-synnada <mertak67+synaada@gmail.com>
2024-12-17 10:44:32 +0300

    add test for unnest

--- datafusion/core/tests/dataframe/mod.rs
+++ datafusion/core/tests/dataframe/mod.rs
@@ -1249,20 +1249,32 @@ async fn unnest_aggregate_columns() -> Result<()> {
 #[tokio::test]
 async fn unnest_no_empty_batches() -> Result<()> {
     let mut shape_id_builder = UInt32Builder::new();
+    let mut tag_id_builder = UInt32Builder::new();
 
-    for shape_id in 1..=5 {
-        shape_id_builder.append_value(shape_id as u32);
+    for shape_id in 1..=10 {
+        for tag_id in 1..=10 {
+            shape_id_builder.append_value(shape_id as u32);
+            tag_id_builder.append_value((shape_id * 10 + tag_id) as u32);
+        }
     }
 
     let batch = RecordBatch::try_from_iter(vec![
         ("shape_id", Arc::new(shape_id_builder.finish()) as ArrayRef),
+        ("tag_id", Arc::new(tag_id_builder.finish()) as ArrayRef),
     ])?;
 
     let ctx = SessionContext::new();
     ctx.register_batch("shapes", batch)?;
     let df = ctx.table("shapes").await?;
 
-    let results = df.clone().collect().await?;
+    let results = df
+        .clone()
+        .aggregate(
+            vec![col("shape_id")],
+            vec![array_agg(col("tag_id")).alias("tag_id")],
+        )?
+        .collect()
+        .await?;
 
     // Assert that there are no empty batches in result
     for rb in results {
