fa581d01d44c04650927c020ba9eaf3429edff8a
Max Meldrum <11488530+Max-Meldrum@users.noreply.github.com>
2025-01-08 14:57:57 +0100

    rework SortExec::with_node_id to not drop preserve_partitioning

--- datafusion/physical-plan/src/sorts/sort.rs
+++ datafusion/physical-plan/src/sorts/sort.rs
@@ -981,10 +981,14 @@ impl ExecutionPlan for SortExec {
         self: Arc<Self>,
         _node_id: usize,
     ) -> Result<Option<Arc<dyn ExecutionPlan>>> {
-        let mut new_plan =
-            SortExec::new(self.expr.clone(), self.input.clone()).with_fetch(self.fetch());
-        let new_props = new_plan.cache.clone().with_node_id(_node_id);
-        new_plan.cache = new_props;
+        let new_plan = SortExec {
+            input: self.input.clone(),
+            expr: self.expr.clone(),
+            fetch: self.fetch,
+            metrics_set: self.metrics_set.clone(),
+            preserve_partitioning: self.preserve_partitioning,
+            cache: self.cache.clone().with_node_id(_node_id),
+        };
         Ok(Some(Arc::new(new_plan)))
     }
 }
