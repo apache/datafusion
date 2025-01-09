add558b3c2a98f159095d1a20574de16356dfe75
Jagdish Parihar <jatin6972@gmail.com>
2025-01-09 17:37:56 +0530

    added "DEFAULT_CLI_FORMAT_OPTIONS"  for cli and sqllotic test

--- datafusion/common/src/format.rs
+++ datafusion/common/src/format.rs
@@ -27,3 +27,6 @@ pub const DEFAULT_CAST_OPTIONS: CastOptions<'static> = CastOptions {
     safe: false,
     format_options: DEFAULT_FORMAT_OPTIONS,
 };
+
+pub const DEFAULT_CLI_FORMAT_OPTIONS: FormatOptions<'static> =
+    FormatOptions::new().with_duration_format(DurationFormat::Pretty).with_null("NULL");
--- datafusion/sqllogictest/src/engines/datafusion_engine/normalize.rs
+++ datafusion/sqllogictest/src/engines/datafusion_engine/normalize.rs
@@ -20,7 +20,7 @@ use arrow::array::{Array, AsArray};
 use arrow::datatypes::Fields;
 use arrow::util::display::ArrayFormatter;
 use arrow::{array, array::ArrayRef, datatypes::DataType, record_batch::RecordBatch};
-use datafusion_common::format::DEFAULT_FORMAT_OPTIONS;
+use datafusion_common::format::DEFAULT_CLI_FORMAT_OPTIONS;
 use datafusion_common::DataFusionError;
 use std::path::PathBuf;
 use std::sync::LazyLock;
@@ -240,7 +240,7 @@ pub fn cell_to_string(col: &ArrayRef, row: usize) -> Result<String> {
                 Ok(cell_to_string(dict.values(), key)?)
             }
             _ => {
-                let f = ArrayFormatter::try_new(col.as_ref(), &DEFAULT_FORMAT_OPTIONS);
+                let f = ArrayFormatter::try_new(col.as_ref(), &DEFAULT_CLI_FORMAT_OPTIONS);
                 Ok(f.unwrap().value(row).to_string())
             }
         }
