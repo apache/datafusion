6e00a8cf47905a939187046ff2fd5dea9ad4bc28
Marko Milenković <milenkovicm@users.noreply.github.com>
2025-01-12 13:51:54 +0000

    cover all cases in round trip test

--- datafusion/proto/tests/cases/roundtrip_logical_plan.rs
+++ datafusion/proto/tests/cases/roundtrip_logical_plan.rs
@@ -378,7 +378,6 @@ async fn roundtrip_logical_plan_sort() -> Result<()> {
 #[tokio::test]
 async fn roundtrip_logical_plan_dml() -> Result<()> {
     let ctx = SessionContext::new();
-
     let schema = Schema::new(vec![
         Field::new("a", DataType::Int64, true),
         Field::new("b", DataType::Decimal128(15, 2), true),
@@ -390,13 +389,26 @@ async fn roundtrip_logical_plan_dml() -> Result<()> {
         CsvReadOptions::default().schema(&schema),
     )
     .await?;
-
-    let query = "INSERT INTO T1 VALUES (1, null)";
-    let plan = ctx.sql(query).await?.into_optimized_plan()?;
-
-    let bytes = logical_plan_to_bytes(&plan)?;
-    let logical_round_trip = logical_plan_from_bytes(&bytes, &ctx)?;
-    assert_eq!(format!("{plan}"), format!("{logical_round_trip}"));
+    let queries = [
+        "INSERT INTO T1 VALUES (1, null)",
+        "INSERT OVERWRITE T1 VALUES (1, null)",
+        "REPLACE INTO T1 VALUES (1, null)",
+        "INSERT OR REPLACE INTO T1 VALUES (1, null)",
+        "DELETE FROM T1",
+        "UPDATE T1 SET a = 1",
+        "CREATE TABLE T2 AS SELECT * FROM T1",
+    ];
+    for query in queries {
+        let plan = ctx.sql(query).await?.into_optimized_plan()?;
+        let bytes = logical_plan_to_bytes(&plan)?;
+        let logical_round_trip = logical_plan_from_bytes(&bytes, &ctx)?;
+        assert_eq!(
+            format!("{plan}"),
+            format!("{logical_round_trip}"),
+            "failed query roundtrip: {}",
+            query
+        );
+    }
 
     Ok(())
 }
