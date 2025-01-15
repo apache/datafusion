579e046b3548443b4714845d4732d25f40765f65
Nicholas Gates <nick@nickgates.com>
2025-01-15 14:18:59 +0000

    fix up

--- datafusion/physical-plan/src/memory.rs
+++ datafusion/physical-plan/src/memory.rs
@@ -875,6 +875,7 @@ mod tests {
                     distinct_count: Precision::Absent,
                     max_value: Precision::Absent,
                     min_value: Precision::Absent,
+                    sum_value: Precision::Absent,
                 },],
             }
         );
