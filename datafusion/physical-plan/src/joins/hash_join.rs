6e1998e74e90f74ebdc8336648d4bfea2c8d7677
ctsk <9384305+ctsk@users.noreply.github.com>
2025-01-22 11:06:07 +0100

    Don't plan coalesce for CollectLeft joins

--- datafusion/physical-plan/src/joins/hash_join.rs
+++ datafusion/physical-plan/src/joins/hash_join.rs
@@ -684,7 +684,7 @@ impl ExecutionPlan for HashJoinExec {
     fn required_input_distribution(&self) -> Vec<Distribution> {
         match self.mode {
             PartitionMode::CollectLeft => vec![
-                Distribution::SinglePartition,
+                Distribution::UnspecifiedDistribution,
                 Distribution::UnspecifiedDistribution,
             ],
             PartitionMode::Partitioned => {
