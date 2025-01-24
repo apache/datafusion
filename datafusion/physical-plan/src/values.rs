36a23b7b5a05fb5302dc4e1fe27e18208cbb7da9
Shehab <11789402+shehabgamin@users.noreply.github.com>
2025-01-23 23:48:39 -0800

    update deprecation message for values exec

--- datafusion/physical-plan/src/values.rs
+++ datafusion/physical-plan/src/values.rs
@@ -34,7 +34,10 @@ use datafusion_execution::TaskContext;
 use datafusion_physical_expr::EquivalenceProperties;
 
 /// Execution plan for values list based relation (produces constant rows)
-#[deprecated(since = "45.0.0", note = "Use `MemoryExec::try_new_as_values` instead")]
+#[deprecated(
+    since = "45.0.0",
+    note = "Use `MemoryExec::try_new_as_values` or `MemoryExec::try_new_from_batches` instead"
+)]
 #[derive(Debug, Clone)]
 pub struct ValuesExec {
     /// The schema
