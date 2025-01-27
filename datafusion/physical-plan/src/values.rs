3bba784e2ef208543e9ba204489868f6d34f1851
Andrew Lamb <andrew@nerdnetworks.org>
2025-01-27 10:35:40 -0500

    Improve deprecation message for `MemoryExec`

--- datafusion/physical-plan/src/values.rs
+++ datafusion/physical-plan/src/values.rs
@@ -27,6 +27,7 @@ use crate::{
     PhysicalExpr,
 };
 
+use crate::memory::MemoryExec;
 use arrow::datatypes::{Schema, SchemaRef};
 use arrow::record_batch::{RecordBatch, RecordBatchOptions};
 use datafusion_common::{internal_err, plan_err, Result, ScalarValue};
@@ -34,7 +35,13 @@ use datafusion_execution::TaskContext;
 use datafusion_physical_expr::EquivalenceProperties;
 
 /// Execution plan for values list based relation (produces constant rows)
-#[deprecated(since = "45.0.0", note = "Use `MemoryExec::try_new_as_values` instead")]
+///
+/// Note this structure is the same as [`MemoryExec`] and is deprecated.
+/// Please see the following for alternatives
+/// * [`MemoryExec::try_new`]
+/// * [`MemoryExec::try_new_from_batches`]
+///
+#[deprecated(since = "45.0.0", note = "Use `MemoryExec` instead")]
 #[derive(Debug, Clone)]
 pub struct ValuesExec {
     /// The schema
@@ -48,6 +55,7 @@ pub struct ValuesExec {
 #[allow(deprecated)]
 impl ValuesExec {
     /// Create a new values exec from data as expr
+    #[deprecated(since = "45.0.0", note = "Use `MemoryExec::try_new` instead")]
     pub fn try_new(
         schema: SchemaRef,
         data: Vec<Vec<Arc<dyn PhysicalExpr>>>,
@@ -101,6 +109,10 @@ impl ValuesExec {
     ///
     /// Errors if any of the batches don't match the provided schema, or if no
     /// batches are provided.
+    #[deprecated(
+        since = "45.0.0",
+        note = "Use `MemoryExec::try_new_from_batches` instead"
+    )]
     pub fn try_new_from_batches(
         schema: SchemaRef,
         batches: Vec<RecordBatch>,
