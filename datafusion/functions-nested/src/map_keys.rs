97e5e47e4a285ce15d8c27436b03bd1229cc0387
Cyprien Huet <chuet@palantir.com>
2025-02-02 07:37:51 +0400

    fix test

--- datafusion/functions-nested/src/map_keys.rs
+++ datafusion/functions-nested/src/map_keys.rs
@@ -126,7 +126,7 @@ fn map_keys_inner(args: &[ArrayRef]) -> Result<ArrayRef> {
     };
 
     Ok(Arc::new(ListArray::new(
-        Arc::new(Field::new_list_field(map_array.key_type().clone(), true)),
+        Arc::new(Field::new_list_field(map_array.key_type().clone(), false)),
         map_array.offsets().clone(),
         Arc::clone(map_array.keys()),
         map_array.nulls().cloned(),
