4bb1a468c1e08b46cd7ab7bc12bc2ca0fad8d4a9
zhuqi-lucas <821684824@qq.com>
2025-02-03 14:20:24 +0800

    Add fix

--- datafusion/physical-optimizer/src/limit_pushdown.rs
+++ datafusion/physical-optimizer/src/limit_pushdown.rs
@@ -149,6 +149,7 @@ pub fn pushdown_limit_helper(
         if limit_exec.input().as_any().is::<CoalescePartitionsExec>() {
             // If the child is a `CoalescePartitionsExec`, we should not remove the limit
             // the push_down through the `CoalescePartitionsExec` to each partition will not guarantee the limit.
+            // todo we may have a better solution if we can support with_fetch for limit inside CoalescePartitionsExec.
             global_state.satisfied = true;
             return Ok((Transformed::no(pushdown_plan), global_state));
         }
