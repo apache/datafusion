cc61f6aaf7d2958ab8e775f3cc93cb0c985c93b7
logan-keede <chawlakranti8@gmail.com>
2025-02-12 02:01:36 +0530

    fix: forgotten Header

--- datafusion/catalog-listing/src/file_scan_config.rs
+++ datafusion/catalog-listing/src/file_scan_config.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use std::{borrow::Cow, collections::HashMap, marker::PhantomData, sync::Arc};
 
 use arrow::{
@@ -11,6 +28,7 @@ use arrow::{
 use datafusion_common::{exec_err, Result};
 use datafusion_common::{DataFusionError, ScalarValue};
 use log::warn;
+
 /// A helper that projects partition columns into the file record batches.
 ///
 /// One interesting trick is the usage of a cache for the key buffers of the partition column
