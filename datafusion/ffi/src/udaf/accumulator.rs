53fafc9096ffdcecef1433f1ffac8b70c3e90089
Tim Saucer <timsaucer@gmail.com>
2025-02-19 08:59:37 -0500

    MVP for aggregate udf via FFI

--- datafusion/ffi/src/udaf/accumulator.rs
+++ datafusion/ffi/src/udaf/accumulator.rs
@@ -1,7 +1,21 @@
-use std::{
-    ffi::c_void,
-    sync::{Arc, Mutex},
-};
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::ffi::c_void;
 
 use abi_stable::{
     std_types::{RResult, RString, RVec},
@@ -45,10 +59,6 @@ pub struct FFI_Accumulator {
     ) -> RResult<(), RString>,
 
     pub supports_retract_batch: unsafe extern "C" fn(accumulator: &Self) -> bool,
-
-    /// Used to create a clone on the provider of the accumulator. This should
-    /// only need to be called by the receiver of the accumulator.
-    // pub clone: unsafe extern "C" fn(accumulator: &Self) -> Self,
 
     /// Release the memory of the private data when it is no longer being used.
     pub release: unsafe extern "C" fn(accumulator: &mut Self),
