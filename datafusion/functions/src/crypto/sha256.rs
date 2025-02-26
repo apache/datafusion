e6ced6c5ba172b4c16291a63665821951e737b04
Cheng-Yuan-Lai <a186235@g,ail.com>
2025-02-26 23:38:25 +0800

    fix signatrue for sha256

--- datafusion/functions/src/crypto/sha256.rs
+++ datafusion/functions/src/crypto/sha256.rs
@@ -19,7 +19,7 @@
 use super::basic::{sha256, utf8_or_binary_to_binary_type};
 use arrow::datatypes::DataType;
 use datafusion_common::{
-    types::{logical_binary, logical_string},
+    types::{logical_binary, logical_string, NativeType},
     Result,
 };
 use datafusion_expr::{
@@ -57,15 +57,15 @@ impl Default for SHA256Func {
 impl SHA256Func {
     pub fn new() -> Self {
         Self {
-            signature: Signature::one_of(
-                vec![
-                    TypeSignature::Coercible(vec![Coercion::new_exact(
+            signature: Signature::new(
+                TypeSignature::Coercible(vec![Coercion::new_implicit(
+                    TypeSignatureClass::Native(logical_binary()),
+                    vec![
                         TypeSignatureClass::Native(logical_string()),
-                    )]),
-                    TypeSignature::Coercible(vec![Coercion::new_exact(
                         TypeSignatureClass::Native(logical_binary()),
-                    )]),
-                ],
+                    ],
+                    NativeType::Binary,
+                )]),
                 Volatility::Immutable,
             ),
         }
