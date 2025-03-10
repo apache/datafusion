16a8e30d04761aa032a28d631955a606db46c311
irenjj <renj.jiang@gmail.com>
2025-03-11 03:11:16 +0800

    Improve parsing `extra_info` in tree explain (#15125)
    * Improve parsing `extra_info` in tree explain
    
    * Improve documentation and CrossJoinDisplay
    
    ---------
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/physical-plan/src/sorts/partial_sort.rs
+++ datafusion/physical-plan/src/sorts/partial_sort.rs
@@ -228,11 +228,11 @@ impl DisplayAs for PartialSortExec {
             }
             DisplayFormatType::TreeRender => match self.fetch {
                 Some(fetch) => {
-                    writeln!(f, "limit={fetch}")?;
-                    writeln!(f, "sort keys={}", self.expr)
+                    writeln!(f, "{}", self.expr)?;
+                    writeln!(f, "limit={fetch}")
                 }
                 None => {
-                    writeln!(f, "sort keys={}", self.expr)
+                    writeln!(f, "{}", self.expr)
                 }
             },
         }
