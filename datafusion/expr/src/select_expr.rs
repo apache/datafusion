983a7598118d09b0db51b0ee524fb1d8b0db42b9
Jay Zhan <jayzhan211@gmail.com>
2025-03-15 09:57:02 +0800

    add doc

--- datafusion/expr/src/select_expr.rs
+++ datafusion/expr/src/select_expr.rs
@@ -22,10 +22,48 @@ use datafusion_common::{Column, TableReference};
 
 use crate::{expr::WildcardOptions, Expr};
 
+/// Represents a SELECT expression in a SQL query.
+///
+/// `SelectExpr` supports three types of expressions commonly found in the SELECT clause:
+///
+/// * Wildcard (`*`) - Selects all columns
+/// * Qualified wildcard (`table.*`) - Selects all columns from a specific table
+/// * Regular expression - Any other expression like columns, functions, literals etc.
+///
+/// This enum is typically used when you need to handle wildcards. After expanding `*` in the query,
+/// you can use `Expr` for all other expressions.
+/// 
+/// # Examples
+///
+/// ```
+/// use datafusion_expr::col;
+/// use datafusion_expr::expr::WildcardOptions;
+/// use datafusion_expr::select_expr::SelectExpr;
+///
+/// // SELECT *
+/// let wildcard = SelectExpr::Wildcard(WildcardOptions::default());
+///
+/// // SELECT mytable.*
+/// let qualified = SelectExpr::QualifiedWildcard(
+///     "mytable".into(),
+///     WildcardOptions::default()
+/// );
+///
+/// // SELECT col1 
+/// let expr = SelectExpr::Expression(col("col1").into());
+/// ```
 #[derive(Clone, Debug)]
 pub enum SelectExpr {
+    /// Represents a wildcard (`*`) that selects all columns from all tables.
+    /// The `WildcardOptions` control additional behavior like exclusions.
     Wildcard(WildcardOptions),
+
+    /// Represents a qualified wildcard (`table.*`) that selects all columns from a specific table.
+    /// The `TableReference` specifies the table and `WildcardOptions` control additional behavior.
     QualifiedWildcard(TableReference, WildcardOptions),
+
+    /// Represents any other valid SELECT expression like column references,
+    /// function calls, literals, etc.
     Expression(Expr),
 }
 
