a27a92886a3cf2562130cc91df430bb52f9f4a40
Jay Zhan <jayzhan211@gmail.com>
2025-03-16 18:56:42 +0800

    Fix wildcard dataframe case (#15230)
    * fix wildcard dataframe
    
    * upd comment
    
    * fix tests
    
    * doc
    
    * fix test
    
    * add doc
    
    * fmt

--- datafusion/expr/src/lib.rs
+++ datafusion/expr/src/lib.rs
@@ -48,6 +48,7 @@ pub mod expr_fn;
 pub mod expr_rewriter;
 pub mod expr_schema;
 pub mod function;
+pub mod select_expr;
 pub mod groups_accumulator {
     pub use datafusion_expr_common::groups_accumulator::*;
 }
