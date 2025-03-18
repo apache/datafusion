dc16a0aaddabcc6f903ad884f1d1c094c315297b
logan-keede <chawlakranti8@gmail.com>
2025-03-18 07:27:11 +0530

    fix:doctest

--- datafusion/physical-expr/src/physical_expr.rs
+++ datafusion/physical-expr/src/physical_expr.rs
@@ -84,7 +84,7 @@ use datafusion_expr::{Expr, SortExpr};
 /// ```
 /// // Create orderings from columns "id" and "name"
 /// # use arrow::datatypes::{Schema, Field, DataType};
-/// # use datafusion_catalog::create_ordering;
+/// # use datafusion_physical_expr::create_ordering;
 /// # use datafusion_common::Column;
 /// # use datafusion_expr::{Expr, SortExpr};
 /// #
