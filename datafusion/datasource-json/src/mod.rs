50a2ab9431e1466be618ca2b96d8f7713b87f3cb
Andrew Lamb <andrew@nerdnetworks.org>
2025-03-17 20:31:55 -0400

    Improve comments

--- datafusion/datasource-json/src/mod.rs
+++ datafusion/datasource-json/src/mod.rs
@@ -14,6 +14,9 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
+// Make sure fast / cheap clones on Arc are explicit:
+// https://github.com/apache/datafusion/issues/11143
 #![cfg_attr(not(test), deny(clippy::clone_on_ref_ptr))]
 
 pub mod file_format;
