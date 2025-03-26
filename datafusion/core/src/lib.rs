d677172632ea5cec2654217adaf882b38bcb7092
Jay Zhan <jay.zhan@synnada.ai>
2025-03-26 15:26:17 +0800

    Move `DataSink` to `datasource` and add session crate (#15371)
    * session
    
    * clippy
    
    * fmt
    
    * session
    
    * minor
    
    * Update README.md
    
    * doc
    
    * fmt
    
    * doc
    
    ---------
    
    Co-authored-by: berkaysynnada <berkay.sahin@synnada.ai>

--- datafusion/core/src/lib.rs
+++ datafusion/core/src/lib.rs
@@ -649,6 +649,8 @@
 //!
 //! * [datafusion_common]: Common traits and types
 //! * [datafusion_catalog]: Catalog APIs such as [`SchemaProvider`] and [`CatalogProvider`]
+//! * [datafusion_datasource]: File and Data IO such as [`FileSource`] and [`DataSink`]
+//! * [datafusion_session]: [`Session`] and related structures
 //! * [datafusion_execution]: State and structures needed for execution
 //! * [datafusion_expr]: [`LogicalPlan`], [`Expr`] and related logical planning structure
 //! * [datafusion_functions]: Scalar function packages
@@ -664,6 +666,9 @@
 //!
 //! [`SchemaProvider`]: datafusion_catalog::SchemaProvider
 //! [`CatalogProvider`]: datafusion_catalog::CatalogProvider
+//! [`Session`]: datafusion_session::Session
+//! [`FileSource`]: datafusion_datasource::file::FileSource
+//! [`DataSink`]: datafusion_datasource::sink::DataSink
 //!
 //! ## Citing DataFusion in Academic Papers
 //!
