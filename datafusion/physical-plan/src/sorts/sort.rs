f75c7dc2c61a85baede4698c5fc456803d46a925
xudong963 <wxd963996380@gmail.com>
2025-03-31 19:12:25 +0800

    save

--- datafusion/physical-plan/src/sorts/sort.rs
+++ datafusion/physical-plan/src/sorts/sort.rs
@@ -1300,6 +1300,10 @@ impl ExecutionPlan for SortExec {
         Statistics::with_fetch(self.input.statistics()?, self.schema(), self.fetch, 0, 1)
     }
 
+    fn statistics_by_partition(&self) -> Result<Vec<Statistics>> {
+        todo!()
+    }
+
     fn with_fetch(&self, limit: Option<usize>) -> Option<Arc<dyn ExecutionPlan>> {
         Some(Arc::new(SortExec::with_fetch(self, limit)))
     }
