bbb859436d409fe90373d18a47086693640e5241
xudong963 <wxd963996380@gmail.com>
2025-04-02 19:28:43 +0800

    Fix: after repartitioning, statistics should be inexact

--- datafusion/core/src/datasource/listing/table.rs
+++ datafusion/core/src/datasource/listing/table.rs
@@ -1129,7 +1129,6 @@ impl ListingTable {
         let file_groups = file_group.split_files(self.options.target_partitions);
         compute_all_files_statistics(
             file_groups,
-            self.schema(),
             self.options.collect_stat,
             inexact_stats,
         )
