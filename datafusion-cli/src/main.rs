dc92235e87b68620f3f187d79e2c18de8cafdc17
John Anderson <panic@semiosix.com>
2025-04-05 14:43:03 -0400

    remove now-outdated comment

--- datafusion-cli/src/main.rs
+++ datafusion-cli/src/main.rs
@@ -184,7 +184,6 @@ async fn main_inner() -> Result<()> {
         quiet: args.quiet,
         maxrows: args.maxrows,
         color: args.color,
-        // TODO only set this if stdout is a tty, otherwise None
         pager: datafusion_cli::pager::main_default_pager(),
     };
 
