2101f4ee09aabebb83aa8430138187d9d7e40fb7
Siew Kam Onn <kosiew@gmail.com>
2025-04-09 12:01:44 +0800

    fix: remove duplicate data type checks for left and right operands in BinaryExpr evaluation

--- datafusion/physical-expr/src/expressions/binary.rs
+++ datafusion/physical-expr/src/expressions/binary.rs
@@ -379,9 +379,6 @@ impl PhysicalExpr for BinaryExpr {
             return Ok(result);
         }
 
-        let left_data_type = lhs.data_type();
-        let right_data_type = rhs.data_type();
-
         // Check for short-circuit cases that need RHS value
         if let Some(result) = get_short_circuit_result(&lhs, &self.op, Some(rhs.clone()))
         {
