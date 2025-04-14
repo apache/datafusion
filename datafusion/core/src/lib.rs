237023c05d37d6cca00cf775a57fb742920cf552
marvelshan <reborn7875@gmail.com>
2025-04-14 22:36:40 +0800

    doc: update doctest reference to renamed format_options.md

--- datafusion/core/src/lib.rs
+++ datafusion/core/src/lib.rs
@@ -1021,8 +1021,8 @@ doc_comment::doctest!(
 
 #[cfg(doctest)]
 doc_comment::doctest!(
-    "../../../docs/source/user-guide/sql/write_options.md",
-    user_guide_sql_write_options
+    "../../../docs/source/user-guide/sql/format_options.md",
+    user_guide_sql_format_options
 );
 
 #[cfg(doctest)]
