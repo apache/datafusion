83448d1277e503b20c92e872bdfe6a978f862a9a
blaginin <dmitrii@blaginin.me>
2025-04-22 20:46:29 +0100

    Cleanup

--- datafusion/functions/src/datetime/to_char.rs
+++ datafusion/functions/src/datetime/to_char.rs
@@ -171,7 +171,6 @@ impl ScalarUDFImpl for ToCharFunc {
 }
 
 fn _build_format_options<'a>(
-    // todo!
     data_type: &DataType,
     format: Option<&'a str>,
 ) -> Result<FormatOptions<'a>, Result<ColumnarValue>> {
