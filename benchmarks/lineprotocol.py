0790500fed38aa3c5b7743280a5e1e64a2d06e42
Sara Ghodsi <55705790+saraghds@users.noreply.github.com>
2025-04-24 19:20:55 -0400

    fix: complete the license

--- benchmarks/lineprotocol.py
+++ benchmarks/lineprotocol.py
@@ -14,6 +14,8 @@ Unless required by applicable law or agreed to in writing,
 software distributed under the License is distributed on an
 "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
 
 Converts a given json to LineProtocol format that can be 
 visualised by grafana/other systems that support LineProtocol. 
