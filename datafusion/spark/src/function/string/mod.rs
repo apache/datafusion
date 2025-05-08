a35122fd831d17b9c12d1570b9e40a86ae3be394
Andy Grove <agrove@apache.org>
2025-05-08 08:05:37 -0600

    Add Spark-compatible char expression

--- datafusion/spark/src/function/string/mod.rs
+++ datafusion/spark/src/function/string/mod.rs
@@ -16,12 +16,14 @@
 // under the License.
 
 pub mod ascii;
+pub mod chr;
 
 use datafusion_expr::ScalarUDF;
 use datafusion_functions::make_udf_function;
 use std::sync::Arc;
 
 make_udf_function!(ascii::SparkAscii, ascii);
+make_udf_function!(chr::SparkChar, chr);
 
 pub mod expr_fn {
     use datafusion_functions::export_functions;
@@ -29,6 +31,11 @@ pub mod expr_fn {
     export_functions!((
         ascii,
         "Returns the ASCII code point of the first character of string.",
+        arg1
+    ));
+    export_functions!((
+        chr,
+        "Returns the ASCII character having the binary equivalent to col. If col is larger than 256 the result is equivalent to char(col % 256).",
         arg1
     ));
 }
