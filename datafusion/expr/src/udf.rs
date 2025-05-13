2f194284dd040926eb7c8b0ddb1b9d518b8d0f4b
Andrew Lamb <andrew@nerdnetworks.org>
2025-05-13 15:18:44 -0400

    fmt

--- datafusion/expr/src/udf.rs
+++ datafusion/expr/src/udf.rs
@@ -451,10 +451,10 @@ pub trait ScalarUDFImpl: Debug + Send + Sync {
     ///
     /// # Notes
     ///
-/// For the majority of UDFs, implementing [`Self::return_type`] is sufficient, 
-/// as the result type is typically a deterministic function of the input types 
-/// (e.g., `sqrt(f32)` consistently yields `f32`). Implementing this method directly 
-/// is generally unnecessary unless the return type depends on runtime values.
+    /// For the majority of UDFs, implementing [`Self::return_type`] is sufficient,
+    /// as the result type is typically a deterministic function of the input types
+    /// (e.g., `sqrt(f32)` consistently yields `f32`). Implementing this method directly
+    /// is generally unnecessary unless the return type depends on runtime values.
     ///
     /// This function can be used for more advanced cases such as:
     ///
