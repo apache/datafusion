de0590c87b35bde3021082ed53c66427355ec95f
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-05-17 22:19:29 -0400

    Revert bits

--- datafusion/core/tests/sql/explain_analyze.rs
+++ datafusion/core/tests/sql/explain_analyze.rs
@@ -561,8 +561,7 @@ async fn csv_explain_verbose_plans() {
 async fn explain_analyze_runs_optimizers(#[values("*", "1")] count_expr: &str) {
     // repro for https://github.com/apache/datafusion/issues/917
     // where EXPLAIN ANALYZE was not correctly running optimizer
-    let cfg = SessionConfig::new().with_collect_statistics(true);
-    let ctx = SessionContext::new_with_config(cfg);
+    let ctx = SessionContext::new();
     register_alltypes_parquet(&ctx).await;
 
     // This happens as an optimization pass where count(*)/count(1) can be
