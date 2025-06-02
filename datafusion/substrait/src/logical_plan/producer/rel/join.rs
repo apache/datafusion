69b9dfb7fa8fb98e4cb230c97ffcacfd66c76e2d
Jonathan <chenleejonathan@gmail.com>
2025-06-01 22:49:24 -0400

    producer fix

--- datafusion/substrait/src/logical_plan/producer/rel/join.rs
+++ datafusion/substrait/src/logical_plan/producer/rel/join.rs
@@ -113,6 +113,7 @@ fn to_substrait_jointype(join_type: JoinType) -> join_rel::JoinType {
         JoinType::LeftAnti => join_rel::JoinType::LeftAnti,
         JoinType::LeftSemi => join_rel::JoinType::LeftSemi,
         JoinType::LeftMark => join_rel::JoinType::LeftMark,
+        JoinType::RightMark => join_rel::JoinType::RightMark,
         JoinType::RightAnti | JoinType::RightSemi => {
             unimplemented!()
         }
