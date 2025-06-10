1389e9e8df1a4ae5ad73eea29eb1f8867b89e230
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-06-10 07:48:47 -0500

    Add late pruning of Parquet files based on file level statistics (#16014)

--- datafusion/core/tests/physical_optimizer/filter_pushdown/util.rs
+++ datafusion/core/tests/physical_optimizer/filter_pushdown/util.rs
@@ -57,7 +57,11 @@ pub struct TestOpener {
 }
 
 impl FileOpener for TestOpener {
-    fn open(&self, _file_meta: FileMeta) -> Result<FileOpenFuture> {
+    fn open(
+        &self,
+        _file_meta: FileMeta,
+        _file: PartitionedFile,
+    ) -> Result<FileOpenFuture> {
         let mut batches = self.batches.clone();
         if let Some(batch_size) = self.batch_size {
             let batch = concat_batches(&batches[0].schema(), &batches)?;
