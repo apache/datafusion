da7a103ab7450a21a4780ce63ec91ffbad26635b
Daniël Heres <danielheres@gmail.com>
2025-06-10 23:59:18 +0200

    Add API for tracking distinct arrays

--- datafusion/execution/src/memory_pool/pool.rs
+++ datafusion/execution/src/memory_pool/pool.rs
@@ -157,7 +157,7 @@ impl MemoryPool for GreedyMemoryPoolWithTracking {
                     .lock()
                     .entry(addr)
                     .and_modify(|ref_count| *ref_count += array.get_array_memory_size())
-                    .or_insert(1)
+                    .or_insert(1);
 
                 // If this is the first time we see this array, we need to grow the pool
                 if ref_count == 1 {
