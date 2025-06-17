bf9f396c7ac150bd7197a1f55f17baa9d17a1056
Siew Kam Onn <kosiew@gmail.com>
2025-06-17 19:42:15 +0800

    fuzz: include dict with null values

--- datafusion/core/tests/fuzz_cases/record_batch_generator.rs
+++ datafusion/core/tests/fuzz_cases/record_batch_generator.rs
@@ -730,9 +730,9 @@ impl RecordBatchGenerator {
                     num_distinct,
                     batch_gen_rng,
                     array_gen_rng.clone(),
-                    // Once https://github.com/apache/datafusion/issues/16228 is fixed
+                    // Since https://github.com/apache/datafusion/issues/16228 is fixed
                     // we can also generate nulls in values
-                    0.0, // null values are generated on the key level
+                    null_pct,
                 );
 
                 match key_type.as_ref() {
