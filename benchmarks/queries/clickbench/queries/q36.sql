977ce0d1cc5b8cf88ac8869ef93c9491f7da0143
Andrew Lamb <andrew@nerdnetworks.org>
2025-06-28 10:58:01 -0400

    Add to the rest of the files

--- benchmarks/queries/clickbench/queries/q36.sql
+++ benchmarks/queries/clickbench/queries/q36.sql
@@ -1,1 +1,4 @@
+-- Must set for ClickBench hits_partitioned dataset. See https://github.com/apache/datafusion/issues/16591
+-- set datafusion.execution.parquet.binary_as_string = true
+
 SELECT "URL", COUNT(*) AS PageViews FROM hits WHERE "CounterID" = 62 AND "EventDate" >= '2013-07-01' AND "EventDate" <= '2013-07-31' AND "DontCountHits" = 0 AND "IsRefresh" = 0 AND "URL" <> '' GROUP BY "URL" ORDER BY PageViews DESC LIMIT 10;
--- benchmarks/queries/clickbench/queries/q41.sql
+++ benchmarks/queries/clickbench/queries/q41.sql
@@ -1,3 +1,3 @@
--- Must set when reading ClickBecnch hits_partitioned dataset using StringView (otherwise it is treated as binary)
+-- Must set for ClickBench hits_partitioned dataset. See https://github.com/apache/datafusion/issues/16591
 -- set datafusion.execution.parquet.binary_as_string = true
 SELECT "WindowClientWidth", "WindowClientHeight", COUNT(*) AS PageViews FROM hits WHERE "CounterID" = 62 AND "EventDate" >= '2013-07-01' AND "EventDate" <= '2013-07-31' AND "IsRefresh" = 0 AND "DontCountHits" = 0 AND "URLHash" = 2868770270353813622 GROUP BY "WindowClientWidth", "WindowClientHeight" ORDER BY PageViews DESC LIMIT 10 OFFSET 10000;
