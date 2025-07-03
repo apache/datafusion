0a61de9ee6239593d4e3151b2c17b678f41e1b5b
Leon Lin <lliangyu@amazon.com>
2025-07-03 12:49:09 -0700

    Update test file condition

--- datafusion/sqllogictest/bin/sqllogictests.rs
+++ datafusion/sqllogictest/bin/sqllogictests.rs
@@ -243,7 +243,7 @@ async fn run_test_file_substrait_round_trip(
     };
     setup_scratch_dir(&relative_path)?;
 
-    let count: u64 = get_record_count(&path, "DataFusion".to_string());
+    let count: u64 = get_record_count(&path, "DatafusionSubstraitRoundTrip".to_string());
     let pb = mp.add(ProgressBar::new(count));
 
     pb.set_style(mp_style);
@@ -256,7 +256,7 @@ async fn run_test_file_substrait_round_trip(
             pb.clone(),
         ))
     });
-    runner.add_label("DataFusion");
+    runner.add_label("DatafusionSubstraitRoundTrip");
     runner.with_column_validator(strict_column_validator);
     runner.with_normalizer(value_normalizer);
     runner.with_validator(validator);
