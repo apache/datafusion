92d8632b01b9897a5fd1f9fdb753156b847e8ba9
Andrew Lamb <andrew@nerdnetworks.org>
2025-07-08 10:30:24 -0400

    Add comments to ClickBench queries about setting binary_as_string (#16605)
    * Add comments to ClickBench queries about setting binary_as_string
    
    * Add to the rest of the files
    
    * Add to extended too
    
    * Apply suggestions from code review
    
    Co-authored-by: Oleks V <comphead@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Oleks V <comphead@users.noreply.github.com>

--- benchmarks/queries/clickbench/extended/q6.sql
+++ benchmarks/queries/clickbench/extended/q6.sql
@@ -1,1 +1,4 @@
+-- Must set for ClickBench hits_partitioned dataset. See https://github.com/apache/datafusion/issues/16591
+-- set datafusion.execution.parquet.binary_as_string = true
+
 SELECT COUNT(*) AS ShareCount FROM hits WHERE "IsMobile" = 1 AND "MobilePhoneModel" LIKE 'iPhone%' AND "SocialAction" = 'share' AND "SocialSourceNetworkID" IN (5, 12) AND "ClientTimeZone" BETWEEN -5 AND 5 AND regexp_match("Referer", '\/campaign\/(spring|summer)_promo') IS NOT NULL AND CASE WHEN split_part(split_part("URL", 'resolution=', 2), '&', 1) ~ '^\d+$' THEN split_part(split_part("URL", 'resolution=', 2), '&', 1)::INT ELSE 0 END > 1920 AND levenshtein(CAST("UTMSource" AS STRING), CAST("UTMCampaign" AS STRING)) < 3;
