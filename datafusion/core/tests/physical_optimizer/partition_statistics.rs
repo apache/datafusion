9c6ebfd0dbe21ffcf037817ad8be271ca09f9c50
Ian Lai <Ian.Lai@senao.com>
2025-07-08 09:50:01 +0000

    feat: replace partition_statistics test to use insta

--- datafusion/core/tests/physical_optimizer/partition_statistics.rs
+++ datafusion/core/tests/physical_optimizer/partition_statistics.rs
@@ -17,6 +17,7 @@
 
 #[cfg(test)]
 mod test {
+    use insta::assert_snapshot;
     use std::sync::Arc;
 
     use arrow::array::{Int32Array, RecordBatch};
@@ -50,7 +51,7 @@ mod test {
     use datafusion_physical_plan::sorts::sort::SortExec;
     use datafusion_physical_plan::union::UnionExec;
     use datafusion_physical_plan::{
-        execute_stream_partitioned, get_plan_string, ExecutionPlan,
+        displayable, execute_stream_partitioned, get_plan_string, ExecutionPlan,
         ExecutionPlanProperties,
     };
 
@@ -536,22 +537,26 @@ mod test {
             .build()
             .map(Arc::new)?];
 
-        let aggregate_exec_partial = Arc::new(AggregateExec::try_new(
-            AggregateMode::Partial,
-            group_by.clone(),
-            aggr_expr.clone(),
-            vec![None],
-            Arc::clone(&scan),
-            scan_schema.clone(),
-        )?) as _;
-
-        let mut plan_string = get_plan_string(&aggregate_exec_partial);
-        let _ = plan_string.swap_remove(1);
-        let expected_plan = vec![
-            "AggregateExec: mode=Partial, gby=[id@0 as id, 1 + id@0 as expr], aggr=[COUNT(c)]",
-            //"  DataSourceExec: file_groups={2 groups: [[.../datafusion/core/tests/data/test_statistics_per_partition/date=2025-03-01/j5fUeSDQo22oPyPU.parquet, .../datafusion/core/tests/data/test_statistics_per_partition/date=2025-03-02/j5fUeSDQo22oPyPU.parquet], [.../datafusion/core/tests/data/test_statistics_per_partition/date=2025-03-03/j5fUeSDQo22oPyPU.parquet, .../datafusion/core/tests/data/test_statistics_per_partition/date=2025-03-04/j5fUeSDQo22oPyPU.parquet]]}, projection=[id, date], file_type=parquet
-        ];
-        assert_eq!(plan_string, expected_plan);
+        let aggregate_exec_partial: Arc<dyn ExecutionPlan> =
+            Arc::new(AggregateExec::try_new(
+                AggregateMode::Partial,
+                group_by.clone(),
+                aggr_expr.clone(),
+                vec![None],
+                Arc::clone(&scan),
+                scan_schema.clone(),
+            )?) as _;
+        let formatted = displayable(aggregate_exec_partial.as_ref())
+            .indent(true)
+            .to_string();
+        let actual = formatted.trim();
+        assert_snapshot!(
+            actual,
+            @r"
+        AggregateExec: mode=Partial, gby=[id@0 as id, 1 + id@0 as expr], aggr=[COUNT(c)]
+          DataSourceExec: file_groups={2 groups: [[home/ian_lai/projects/datafusion/datafusion/core/tests/data/test_statistics_per_partition/date=2025-03-01/j5fUeSDQo22oPyPU.parquet, home/ian_lai/projects/datafusion/datafusion/core/tests/data/test_statistics_per_partition/date=2025-03-02/j5fUeSDQo22oPyPU.parquet], [home/ian_lai/projects/datafusion/datafusion/core/tests/data/test_statistics_per_partition/date=2025-03-03/j5fUeSDQo22oPyPU.parquet, home/ian_lai/projects/datafusion/datafusion/core/tests/data/test_statistics_per_partition/date=2025-03-04/j5fUeSDQo22oPyPU.parquet]]}, projection=[id, date], file_type=parquet
+        "
+        );
 
         let p0_statistics = aggregate_exec_partial.partition_statistics(Some(0))?;
 
