ae646d1b77b0edbbba3dd0f1aa933add7a525776
Siew Kam Onn <kosiew@gmail.com>
2025-07-15 16:07:59 +0800

    fix: remove unnecessary blank line in config.rs

--- datafusion/common/src/config.rs
+++ datafusion/common/src/config.rs
@@ -347,7 +347,6 @@ config_namespace! {
         /// metadata memory consumption
         pub batch_size: usize, default = 8192
 
-
         /// When set to true, record batches will be examined between each operator and
         /// small batches will be coalesced into larger batches. This is helpful when there
         /// are highly selective filters or joins that could produce tiny output batches. The
