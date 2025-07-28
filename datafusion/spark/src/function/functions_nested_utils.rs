a17cba01496ab614c69b2f66ba38a414d3db5952
Alan Tang <jmtangcs@gmail.com>
2025-07-28 14:26:39 +0800

    feat(spark): implement spark array function array
    Signed-off-by: Alan Tang <jmtangcs@gmail.com>

---
+++ datafusion/spark/src/function/functions_nested_utils.rs
@@ -0,0 +1,36 @@
+use arrow::array::{Array, ArrayRef};
+use datafusion_common::{Result, ScalarValue};
+use datafusion_expr::ColumnarValue;
+
+/// array function wrapper that differentiates between scalar (length 1) and array.
+pub(crate) fn make_scalar_function<F>(
+    inner: F,
+) -> impl Fn(&[ColumnarValue]) -> Result<ColumnarValue>
+where
+    F: Fn(&[ArrayRef]) -> Result<ArrayRef>,
+{
+    move |args: &[ColumnarValue]| {
+        // first, identify if any of the arguments is an Array. If yes, store its `len`,
+        // as any scalar will need to be converted to an array of len `len`.
+        let len = args
+            .iter()
+            .fold(Option::<usize>::None, |acc, arg| match arg {
+                ColumnarValue::Scalar(_) => acc,
+                ColumnarValue::Array(a) => Some(a.len()),
+            });
+
+        let is_scalar = len.is_none();
+
+        let args = ColumnarValue::values_to_arrays(args)?;
+
+        let result = (inner)(&args);
+
+        if is_scalar {
+            // If all inputs are scalar, keeps output as scalar
+            let result = result.and_then(|arr| ScalarValue::try_from_array(&arr, 0));
+            result.map(ColumnarValue::Scalar)
+        } else {
+            result.map(ColumnarValue::Array)
+        }
+    }
+}
