7b0a2025ac0a3399fa98ca199cf024b3c0b7903a
Siew Kam Onn <kosiew@gmail.com>
2025-07-30 19:35:16 +0800

    Update SQL query in `explain_memory` example to group and summarize results

--- datafusion-examples/examples/explain_memory.rs
+++ datafusion-examples/examples/explain_memory.rs
@@ -16,7 +16,7 @@ async fn main() -> Result<()> {
     // Configure a memory pool limited to 16 MiB and track consumers
     const MB: usize = 1024 * 1024;
     let tracked_pool = Arc::new(TrackConsumersPool::new(
-        GreedyMemoryPool::new(16 * MB),
+        GreedyMemoryPool::new(15 * MB),
         NonZeroUsize::new(5).unwrap(),
     ));
     let pool: Arc<dyn MemoryPool> = tracked_pool.clone();
@@ -32,7 +32,7 @@ async fn main() -> Result<()> {
     println!("{}", reservation.explain_memory()?);
 
     let df = ctx
-        .sql("select * from generate_series(1,500000) as t(v) order by v")
+        .sql("select v % 1000 as group_key, count(*) as cnt, sum(v) as sum_v from generate_series(1,500000) as t(v) group by v % 1000 order by group_key")
         .await?;
 
     if let Err(e) = df.collect().await {
