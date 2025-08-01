a6acf765624671c0845041a1c11e99f369d1e146
Siew Kam Onn <kosiew@gmail.com>
2025-08-01 11:07:49 +0800

    fix: remove unused EnhancedMemoryReport import from prelude

--- datafusion/core/src/prelude.rs
+++ datafusion/core/src/prelude.rs
@@ -27,9 +27,7 @@
 
 pub use crate::dataframe;
 pub use crate::dataframe::DataFrame;
-pub use crate::execution::context::{
-    EnhancedMemoryReport, SQLOptions, SessionConfig, SessionContext,
-};
+pub use crate::execution::context::{SQLOptions, SessionConfig, SessionContext};
 pub use crate::execution::options::{
     AvroReadOptions, CsvReadOptions, NdJsonReadOptions, ParquetReadOptions,
 };
