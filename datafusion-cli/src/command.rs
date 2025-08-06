173486cc614facda06eb65dd097dce119ecadc31
Siew Kam Onn <kosiew@gmail.com>
2025-08-06 12:14:21 +0800

    fix: Allow 'on' as an alias for 'enable' in MemoryProfiling command

--- datafusion-cli/src/command.rs
+++ datafusion-cli/src/command.rs
@@ -113,7 +113,7 @@ impl Command {
             }
             Self::MemoryProfiling(subcmd) => {
                 match subcmd.as_deref() {
-                    Some("enable") => {
+                    Some("enable" | "on") => {
                         let _ = ctx.enable_memory_profiling();
                         print_options.memory_profiling = true;
                         println!("Memory profiling enabled for next query");
