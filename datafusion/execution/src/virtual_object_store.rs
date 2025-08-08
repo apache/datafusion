5076fbf5dbd3f0525880e4fae5b65d914bc939e8
Siew Kam Onn <kosiew@gmail.com>
2025-08-08 15:22:24 +0800

    Reorganize imports and update license comments in `virtual_object_store.rs`

--- datafusion/execution/src/virtual_object_store.rs
+++ datafusion/execution/src/virtual_object_store.rs
@@ -1,16 +1,29 @@
-use std::collections::HashMap;
-use std::fmt;
-use std::sync::Arc;
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//! Virtual object store implementation for DataFusion.
 
 use async_trait::async_trait;
-use futures::stream::BoxStream;
-use futures::{stream, StreamExt, TryStreamExt};
-use object_store::path::Path;
+use futures::{stream, stream::BoxStream, StreamExt, TryStreamExt};
 use object_store::{
-    Error, GetOptions, GetResult, ListResult, MultipartUpload, ObjectMeta, ObjectStore,
-    PutMultipartOptions, PutOptions, PutPayload, PutResult, Result,
+    path::Path, Error, GetOptions, GetResult, ListResult, MultipartUpload, ObjectMeta,
+    ObjectStore, PutMultipartOptions, PutOptions, PutPayload, PutResult, Result,
 };
-
+use std::{collections::HashMap, fmt, sync::Arc};
 /// A virtual [`ObjectStore`] that routes requests to underlying stores based on
 /// the first path segment.
 #[derive(Clone)]
