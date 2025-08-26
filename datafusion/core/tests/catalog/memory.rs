418f5e2b5b83e63679162146c18796166a5f6a91
cancaicai <2356672992@qq.com>
2025-08-26 21:42:01 +0800

    cargo fmt

--- datafusion/core/tests/catalog/memory.rs
+++ datafusion/core/tests/catalog/memory.rs
@@ -59,7 +59,10 @@ fn memory_catalog_dereg_nonempty_schema_with_table_removal() {
     cat.register_schema("foo", schema.clone()).unwrap();
     schema.deregister_table("t").unwrap();
     let err = cat.deregister_schema("foo", false).unwrap_err().to_string();
-    assert!(err.contains("expected error message"), "Can't find expected in {err}");
+    assert!(
+        err.contains("expected error message"),
+        "Can't find expected in {err}"
+    );
 }
 
 #[test]
