9f153ec907d08a6a233668c36d3dc04a07746f39
Yongting You <2010youy01@gmail.com>
2025-08-28 17:39:17 +0800

    Update datafusion/optimizer/src/extract_equijoin_predicate.rs
    Co-authored-by: Jonathan Chen <chenleejonathan@gmail.com>

--- datafusion/optimizer/src/extract_equijoin_predicate.rs
+++ datafusion/optimizer/src/extract_equijoin_predicate.rs
@@ -212,7 +212,7 @@ fn split_op_and_other_join_predicates(
     operator: Operator,
 ) -> Result<(Vec<EquijoinPredicate>, Option<Expr>)> {
     if !matches!(operator, Operator::Eq | Operator::IsNotDistinctFrom) {
-        return internal_err!("This function should only be used to split equal or 'IS NOT DISTINCT FROM' opreator");
+        return internal_err!("This function should only be used to split equal or 'IS NOT DISTINCT FROM' operator");
     }
 
     let exprs = split_conjunction_owned(filter);
