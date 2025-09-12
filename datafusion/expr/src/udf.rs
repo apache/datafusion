86d01be2b681bdba9a262c7c18acf31620a467fd
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-09-12 15:18:13 +0200

    Improve style

--- datafusion/expr/src/udf.rs
+++ datafusion/expr/src/udf.rs
@@ -235,23 +235,21 @@ impl ScalarUDF {
     pub fn invoke_with_args(&self, args: ScalarFunctionArgs) -> Result<ColumnarValue> {
         #[cfg(debug_assertions)]
         let return_field = Arc::clone(&args.return_field);
-        match self.inner.invoke_with_args(args) {
-            Ok(value) => {
-                // Maybe this could be enabled always?
-                // This doesn't use debug_assert!, but it's meant to run anywhere except on production, so it's same in spirit, so conditioning on debug_assertions.
-                #[cfg(debug_assertions)]
-                if &value.data_type() != return_field.data_type() {
-                    return datafusion_common::exec_err!("Function '{}' returned value of type '{:?}' while the following type was promised at planning time and expected: '{:?}'",
+        let result = self.inner.invoke_with_args(args)?;
+        // Maybe this could be enabled always?
+        // This doesn't use debug_assert!, but it's meant to run anywhere except on production. Ot's same in spirit, thus conditioning on debug_assertions.
+        #[cfg(debug_assertions)]
+        {
+            if &result.data_type() != return_field.data_type() {
+                return datafusion_common::exec_err!("Function '{}' returned value of type '{:?}' while the following type was promised at planning time and expected: '{:?}'",
                         self.name(),
-                        value.data_type(),
+                        result.data_type(),
                         return_field.data_type()
                     );
-                }
-                // TODO verify return data is non-null when it was promised to be?
-                Ok(value)
             }
-            Err(e) => Err(e),
+            // TODO verify return data is non-null when it was promised to be?
         }
+        Ok(result)
     }
 
     /// Get the circuits of inner implementation
