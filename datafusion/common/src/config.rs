b659c6158ac00ef5febdd0d8417a7c171bb9ea9a
Yongting You <2010youy01@gmail.com>
2025-09-14 16:39:53 +0800

    bot review

--- datafusion/common/src/config.rs
+++ datafusion/common/src/config.rs
@@ -839,7 +839,7 @@ config_namespace! {
         /// Example usage: SET datafusion.optimizer.join_method_priority = 'hj, nlj'
         ///
         /// Note: If this option is not set (default empty string), the deprecated legacy option `prefer_hash_join` will be used.
-        pub join_method_priority: String, transform = str::to_lowercase, default = "".to_string()
+        pub join_method_priority: String, default = "".to_string()
 
         /// Enables planning HashJoin operators. If set to false, the optimizer will avoid
         /// producing HashJoin plans and consider other join strategies instead.
