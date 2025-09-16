986cfcde0ee2bb2cd2e8cc684a497896b278a2d9
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-09-16 19:57:27 +0200

    Enable `inefficient_to_string` Clippy lint (#17583)
    Enable `inefficient_to_string` Clippy lint and update the code. The code
    changes were done by Clippy `--fix`.
    
    The lint addresses a Rust's type system or stdlib limitation, which
    results in `(&&String).to_string()` being a lot slower than
    `(&String).to_string()` and `(*(&&String)).to_string()`. The difference
    is 40% in my measurements on 10-character long strings. The difference
    comes from the fact that the faster code paths are optimized for
    `String` and the `&&` double reference somehow prevents the compiler
    from taking such specialized code path.

--- datafusion/expr/src/logical_plan/builder.rs
+++ datafusion/expr/src/logical_plan/builder.rs
@@ -2482,12 +2482,12 @@ mod tests {
             return plan_err!("Plan should have returned an DataFusionError::Internal");
         };
 
-        let desc = desc
+        let desc = (*desc
             .split(DataFusionError::BACK_TRACE_SEP)
             .collect::<Vec<&str>>()
             .first()
-            .unwrap_or(&"")
-            .to_string();
+            .unwrap_or(&""))
+        .to_string();
 
         assert_snapshot!(desc, @"trying to unnest on invalid data type UInt32");
 
