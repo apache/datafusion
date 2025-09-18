120296282fa6f594120aa6429a581feba5992a3c
Jonathan <chenleejonathan@gmail.com>
2025-09-17 20:47:46 -0400

    add flag

--- datafusion/core/src/physical_planner.rs
+++ datafusion/core/src/physical_planner.rs
@@ -1227,6 +1227,7 @@ impl DefaultPhysicalPlanner {
                                 | JoinType::LeftMark
                                 | JoinType::RightMark
                         )
+                        && session_state.config_options().optimizer.allow_piecewise_merge_join
                     {
                         let Expr::BinaryExpr(be) = &range_filters[0] else {
                             return plan_err!(
