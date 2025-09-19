450fea45c595796ff4da61d8b5e120433dc65092
Georgi Krastev <georgi.krastev@coralogix.com>
2025-09-19 18:22:52 +0200

    Reduce cloning in LogicalPlanBuilder
     - Migrate function arguments from `LogicalPlan` to `impl Into<Arc<LogicalPlan>>`
     - Update usages (mostly in tests)

--- datafusion/optimizer/src/optimize_projections/mod.rs
+++ datafusion/optimizer/src/optimize_projections/mod.rs
@@ -899,7 +899,7 @@ mod tests {
     use crate::optimize_projections::OptimizeProjections;
     use crate::optimizer::Optimizer;
     use crate::test::{
-        assert_fields_eq, scan_empty, test_table_scan, test_table_scan_fields,
+        assert_fields_eq, test_table_scan, test_table_scan_fields,
         test_table_scan_with_name,
     };
     use crate::{OptimizerContext, OptimizerRule};
@@ -1843,7 +1843,8 @@ mod tests {
         let table_scan = test_table_scan()?;
 
         let schema = Schema::new(vec![Field::new("c1", DataType::UInt32, false)]);
-        let table2_scan = scan_empty(Some("test2"), &schema, None)?.build()?;
+        let table2_scan =
+            datafusion_expr::table_scan(Some("test2"), &schema, None)?.build()?;
 
         let plan = LogicalPlanBuilder::from(table_scan)
             .join(table2_scan, JoinType::Left, (vec!["a"], vec!["c1"]), None)?
@@ -1895,7 +1896,8 @@ mod tests {
         let table_scan = test_table_scan()?;
 
         let schema = Schema::new(vec![Field::new("c1", DataType::UInt32, false)]);
-        let table2_scan = scan_empty(Some("test2"), &schema, None)?.build()?;
+        let table2_scan =
+            datafusion_expr::table_scan(Some("test2"), &schema, None)?.build()?;
 
         let plan = LogicalPlanBuilder::from(table_scan)
             .join(table2_scan, JoinType::Left, (vec!["a"], vec!["c1"]), None)?
@@ -1950,7 +1952,8 @@ mod tests {
         let table_scan = test_table_scan()?;
 
         let schema = Schema::new(vec![Field::new("a", DataType::UInt32, false)]);
-        let table2_scan = scan_empty(Some("test2"), &schema, None)?.build()?;
+        let table2_scan =
+            datafusion_expr::table_scan(Some("test2"), &schema, None)?.build()?;
 
         let plan = LogicalPlanBuilder::from(table_scan)
             .join_using(table2_scan, JoinType::Left, vec!["a".into()])?
