9e39d6c223e5ccafb34a1b2e0a5514f10363f1d3
comphead <comphead@ukr.net>
2025-10-06 08:27:01 -0700

    clippy

--- datafusion/catalog/src/stream.rs
+++ datafusion/catalog/src/stream.rs
@@ -28,9 +28,7 @@ use std::sync::Arc;
 use crate::{Session, TableProvider, TableProviderFactory};
 use arrow::array::{RecordBatch, RecordBatchReader, RecordBatchWriter};
 use arrow::datatypes::SchemaRef;
-use datafusion_common::{
-    config_err, exec_datafusion_err, plan_err, Constraints, DataFusionError, Result,
-};
+use datafusion_common::{config_err, plan_err, Constraints, DataFusionError, Result};
 use datafusion_common_runtime::SpawnedTask;
 use datafusion_datasource::sink::{DataSink, DataSinkExec};
 use datafusion_execution::{SendableRecordBatchStream, TaskContext};
