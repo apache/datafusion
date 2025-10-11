df815d38cf88e91baa6ace36fb77c80defff37f0
Kazantsev Maksim <mn.kazantsev@gmail.com>
2025-10-11 19:47:53 +0400

    Impl spark bit not function

--- datafusion/spark/src/function/bitwise/bit_not.rs
+++ datafusion/spark/src/function/bitwise/bit_not.rs
@@ -15,20 +15,18 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use arrow::array::{ArrayRef, AsArray, Int16Array, Int32Array, Int64Array, Int8Array};
-use arrow::datatypes::{DataType, Int16Type, Int32Type, Int64Type, Int8Type};
-use datafusion_common::{exec_err, plan_err, Result};
-use datafusion_expr::{
-    ColumnarValue, ScalarFunctionArgs, ScalarUDFImpl, Signature, Volatility,
-};
-use datafusion_functions::utils::make_scalar_function;
-use std::any::Any;
-use std::sync::Arc;
+use arrow::compute::kernels::bitwise;
+use arrow::datatypes::{Int16Type, Int32Type, Int64Type, Int8Type};
+use arrow::{array::*, datatypes::DataType};
+use datafusion::common::{plan_err, Result};
+use datafusion::functions::utils::make_scalar_function;
+use datafusion::logical_expr::{ColumnarValue, TypeSignature, Volatility};
+use datafusion::logical_expr::{ScalarFunctionArgs, ScalarUDFImpl, Signature};
+use std::{any::Any, sync::Arc};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct SparkBitNot {
     signature: Signature,
-    aliases: Vec<String>,
 }
 
 impl Default for SparkBitNot {
@@ -40,8 +38,15 @@ impl Default for SparkBitNot {
 impl SparkBitNot {
     pub fn new() -> Self {
         Self {
-            signature: Signature::user_defined(Volatility::Immutable),
-            aliases: vec![],
+            signature: Signature::one_of(
+                vec![
+                    TypeSignature::Exact(vec![DataType::Int8]),
+                    TypeSignature::Exact(vec![DataType::Int16]),
+                    TypeSignature::Exact(vec![DataType::Int32]),
+                    TypeSignature::Exact(vec![DataType::Int64]),
+                ],
+                Volatility::Immutable,
+            ),
         }
     }
 }
@@ -55,28 +60,12 @@ impl ScalarUDFImpl for SparkBitNot {
         "bit_not"
     }
 
-    fn aliases(&self) -> &[String] {
-        &self.aliases
-    }
-
     fn signature(&self) -> &Signature {
         &self.signature
     }
 
     fn return_type(&self, arg_types: &[DataType]) -> Result<DataType> {
-        Ok(match arg_types[0] {
-            DataType::Int8 => DataType::Int8,
-            DataType::Int16 => DataType::Int16,
-            DataType::Int32 => DataType::Int32,
-            DataType::Int64 => DataType::Int64,
-            DataType::Null => DataType::Null,
-            _ => {
-                return exec_err!(
-                    "{} function can only accept integral arrays",
-                    self.name()
-                )
-            }
-        })
+        Ok(arg_types[0].clone())
     }
 
     fn invoke_with_args(&self, args: ScalarFunctionArgs) -> Result<ColumnarValue> {
@@ -87,143 +76,30 @@ impl ScalarUDFImpl for SparkBitNot {
     }
 }
 
-macro_rules! bit_not_op {
-    ($OPERAND:expr, $PT:ident, $AT:ident) => {{
-        let result: $AT = $OPERAND
-            .as_primitive::<$PT>()
-            .iter()
-            .map(|x| x.map(|y| !y))
-            .collect();
-        Ok(Arc::new(result))
-    }};
-}
-
-pub fn spark_bit_not(value_array: &[ArrayRef]) -> Result<ArrayRef> {
-    let value_array = value_array[0].as_ref();
-    match value_array.data_type() {
-        DataType::Int8 => bit_not_op!(value_array, Int8Type, Int8Array),
-        DataType::Int16 => bit_not_op!(value_array, Int16Type, Int16Array),
-        DataType::Int32 => bit_not_op!(value_array, Int32Type, Int32Array),
-        DataType::Int64 => bit_not_op!(value_array, Int64Type, Int64Array),
-        _ => exec_err!("bit_not can't be evaluated because the expression's type is {:?}, not signed int", value_array.data_type()),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_spark_bit_not_int32() -> Result<()> {
-        let int32_array = Int32Array::from(vec![
-            Some(1),
-            Some(2),
-            None,
-            Some(12345),
-            Some(89),
-            Some(-3456),
-        ]);
-        let expected = &Int32Array::from(vec![
-            Some(-2),
-            Some(-3),
-            None,
-            Some(-12346),
-            Some(-90),
-            Some(3455),
-        ]);
-
-        let result = spark_bit_not(&[Arc::new(int32_array)])?;
-        let result = result.as_primitive::<Int32Type>();
-
-        assert_eq!(result, expected);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_spark_bit_not_int8() -> Result<()> {
-        let int8_array = Int8Array::from(vec![
-            Some(0),
-            Some(127),
-            Some(-128),
-            None,
-            Some(42),
-            Some(-73),
-            Some(1),
-        ]);
-        let expected = &Int8Array::from(vec![
-            Some(-1),
-            Some(-128),
-            Some(127),
-            None,
-            Some(-43),
-            Some(72),
-            Some(-2),
-        ]);
-
-        let result = spark_bit_not(&[Arc::new(int8_array)])?;
-        let result = result.as_primitive::<Int8Type>();
-
-        assert_eq!(result, expected);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_spark_bit_not_int16() -> Result<()> {
-        let int16_array = Int16Array::from(vec![
-            Some(0),
-            Some(32767),
-            Some(-32768),
-            None,
-            Some(12345),
-            Some(-24680),
-            Some(0x5555),
-        ]);
-        let expected = &Int16Array::from(vec![
-            Some(-1),
-            Some(-32768),
-            Some(32767),
-            None,
-            Some(-12346),
-            Some(24679),
-            Some(-0x5556),
-        ]);
-
-        let result = spark_bit_not(&[Arc::new(int16_array)])?;
-        let result = result.as_primitive::<Int16Type>();
-
-        assert_eq!(result, expected);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_spark_bit_not_int64() -> Result<()> {
-        let int64_array = Int64Array::from(vec![
-            Some(0),
-            Some(i64::MAX),
-            Some(i64::MIN),
-            None,
-            Some(1234567890123456),
-            Some(-9876543210987654),
-            Some(0x5555555555555555),
-        ]);
-        let expected = &Int64Array::from(vec![
-            Some(-1),
-            Some(i64::MIN),
-            Some(i64::MAX),
-            None,
-            Some(-1234567890123457),
-            Some(9876543210987653),
-            Some(-0x5555555555555556),
-        ]);
-
-        let result = spark_bit_not(&[Arc::new(int64_array)])?;
-        let result = result.as_primitive::<Int64Type>();
-
-        assert_eq!(result, expected);
-
-        Ok(())
+pub fn spark_bit_not(args: &[ArrayRef]) -> Result<ArrayRef> {
+    let array = args[0].as_ref();
+    match array.data_type() {
+        DataType::Int8 => {
+            let result: Int8Array = bitwise::bitwise_not(&array.as_primitive::<Int8Type>())?;
+            Ok(Arc::new(result))
+        }
+        DataType::Int16 => {
+            let result: Int16Array = bitwise::bitwise_not(&array.as_primitive::<Int16Type>())?;
+            Ok(Arc::new(result))
+        }
+        DataType::Int32 => {
+            let result: Int32Array = bitwise::bitwise_not(&array.as_primitive::<Int32Type>())?;
+            Ok(Arc::new(result))
+        }
+        DataType::Int64 => {
+            let result: Int64Array = bitwise::bitwise_not(&array.as_primitive::<Int64Type>())?;
+            Ok(Arc::new(result))
+        }
+        _ => {
+            plan_err!(
+                "bit_not function does not support data type: {}",
+                array.data_type()
+            )
+        }
     }
 }
