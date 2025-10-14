5662ca55080856944223025e741ad16dcb9c3ee2
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-10-14 13:51:07 -0500

    fix docs

--- datafusion/optimizer/src/push_down_sort.rs
+++ datafusion/optimizer/src/push_down_sort.rs
@@ -56,6 +56,8 @@ use datafusion_expr::{Expr, LogicalPlanContext, ScanOrdering, SortExpr};
 /// Sort: test.a ASC NULLS LAST
 ///   TableScan: test preferred_ordering=[test.a ASC NULLS LAST]
 /// ```
+/// 
+/// [`TableProvider`]: https://docs.rs/datafusion/latest/datafusion/datasource/trait.TableProvider.html
 #[derive(Default, Debug)]
 pub struct PushDownSort {}
 
