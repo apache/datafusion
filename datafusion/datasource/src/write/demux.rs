596509d81c8fa4d3a61029ae19ccf170309024d1
Bert Vermeiren <bert.vermeiren@datadobi.com>
2025-10-15 13:54:42 +0200

    Fix COPY TO does not produce an output file for the empty set

--- datafusion/datasource/src/write/demux.rs
+++ datafusion/datasource/src/write/demux.rs
@@ -171,6 +171,22 @@ async fn row_count_demuxer(
         max_rows_per_file
     };
 
+    // Single-file output requires creating at least one file stream in advance.
+    // If no record batches are present in the input stream (zero-row scenario),
+    if single_file_output {
+        open_file_streams.push(create_new_file_stream(
+            &base_output_path,
+            &write_id,
+            part_idx,
+            &file_extension,
+            single_file_output,
+            max_buffered_batches,
+            &mut tx,
+        )?);
+        row_counts.push(0);
+        part_idx += 1;
+    }
+
     while let Some(rb) = input.next().await.transpose()? {
         // ensure we have at least minimum_parallel_files open
         if open_file_streams.len() < minimum_parallel_files {
