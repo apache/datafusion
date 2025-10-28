ebe213d6c32e52dfcb249e416c938634cc7f11bc
Sergey Zhukov <szhukov@aligntech.com>
2025-10-28 14:04:07 +0300

    fix merging conflict in datafusion-examples/examples/parquet/encrypted.rs

--- datafusion-examples/examples/parquet/encrypted.rs
+++ datafusion-examples/examples/parquet/encrypted.rs
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use datafusion::common::DataFusionError;
+use datafusion::common::{DataFusionError, Result};
 use datafusion::config::{ConfigFileEncryptionProperties, TableParquetOptions};
 use datafusion::dataframe::{DataFrame, DataFrameWriteOptions};
 use datafusion::logical_expr::{col, lit};
