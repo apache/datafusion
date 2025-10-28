b3b3afe380153b383e3f4f29cb0d5e3118a5fb95
Michael Kleen <mkleen@gmail.com>
2025-10-28 15:11:47 +0100

    Move generate_series projection logic into LazyMemoryStream

--- datafusion/proto/src/physical_plan/mod.rs
+++ datafusion/proto/src/physical_plan/mod.rs
@@ -1940,10 +1940,13 @@ impl protobuf::PhysicalPlanNode {
         };
 
         let table = GenerateSeriesTable::new(Arc::clone(&schema), args);
-        let generator =
-            table.as_generator(generate_series.target_batch_size as usize, None)?;
+        let generator = table.as_generator(generate_series.target_batch_size as usize)?;
 
-        Ok(Arc::new(LazyMemoryExec::try_new(schema, vec![generator])?))
+        Ok(Arc::new(LazyMemoryExec::try_new(
+            schema,
+            None,
+            vec![generator],
+        )?))
     }
 
     fn try_into_cooperative_physical_plan(
