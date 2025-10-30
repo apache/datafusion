9435513e5d65f96bc6ce66bd551e80b56f1fd903
Khanh Duong <dqkqdlot@gmail.com>
2025-10-31 03:45:59 +0900

    fix: `DataFrame::select_columns` and `DataFrame::drop_columns` for qualified duplicated field names (#18236)
    ## Which issue does this PR close?
    
    - Closes #18212.
    
    ## Rationale for this change
    
    `DataFrame::drop_columns` only considers one field for each `name`,
    it fails to drop columns from dataframe containing duplicated names
    from different relations. Such as `mark` columns created by multiples 
    `Join::LeftMark`.
    
    `DataFrame::select_columns` has the same issue, it fails to select
    columns
    with the same name from different relations.
    
    ## What changes are included in this PR?
    
    Allow `DataFrame::drop_columns` and `DataFrame::select_columns`
    work with duplicated names from different relations.
    
    ## Are these changes tested?
    
    Yes.
    
    ## Are there any user-facing changes?
    
    No.
    
    ---------
    
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/core/tests/dataframe/mod.rs
+++ datafusion/core/tests/dataframe/mod.rs
@@ -405,6 +405,55 @@ async fn select_with_periods() -> Result<()> {
 }
 
 #[tokio::test]
+async fn select_columns_duplicated_names_from_different_qualifiers() -> Result<()> {
+    let t1 = test_table_with_name("t1")
+        .await?
+        .select_columns(&["c1"])?
+        .limit(0, Some(3))?;
+    let t2 = test_table_with_name("t2")
+        .await?
+        .select_columns(&["c1"])?
+        .limit(3, Some(3))?;
+    let t3 = test_table_with_name("t3")
+        .await?
+        .select_columns(&["c1"])?
+        .limit(6, Some(3))?;
+
+    let join_res = t1
+        .join(t2, JoinType::Left, &["t1.c1"], &["t2.c1"], None)?
+        .join(t3, JoinType::Left, &["t1.c1"], &["t3.c1"], None)?;
+    assert_snapshot!(
+        batches_to_sort_string(&join_res.clone().collect().await.unwrap()),
+        @r"
+    +----+----+----+
+    | c1 | c1 | c1 |
+    +----+----+----+
+    | b  | b  |    |
+    | b  | b  |    |
+    | c  |    |    |
+    | d  |    | d  |
+    +----+----+----+
+    "
+    );
+
+    let select_res = join_res.select_columns(&["c1"])?;
+    assert_snapshot!(
+        batches_to_sort_string(&select_res.clone().collect().await.unwrap()),
+        @r"
+    +----+----+----+
+    | c1 | c1 | c1 |
+    +----+----+----+
+    | b  | b  |    |
+    | b  | b  |    |
+    | c  |    |    |
+    | d  |    | d  |
+    +----+----+----+
+    "
+    );
+    Ok(())
+}
+
+#[tokio::test]
 async fn drop_columns() -> Result<()> {
     // build plan using Table API
     let t = test_table().await?;
@@ -537,6 +586,54 @@ async fn drop_with_periods() -> Result<()> {
     | 2    |
     +------+
     "###
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn drop_columns_duplicated_names_from_different_qualifiers() -> Result<()> {
+    let t1 = test_table_with_name("t1")
+        .await?
+        .select_columns(&["c1"])?
+        .limit(0, Some(3))?;
+    let t2 = test_table_with_name("t2")
+        .await?
+        .select_columns(&["c1"])?
+        .limit(3, Some(3))?;
+    let t3 = test_table_with_name("t3")
+        .await?
+        .select_columns(&["c1"])?
+        .limit(6, Some(3))?;
+
+    let join_res = t1
+        .join(t2, JoinType::LeftMark, &["c1"], &["c1"], None)?
+        .join(t3, JoinType::LeftMark, &["c1"], &["c1"], None)?;
+    assert_snapshot!(
+        batches_to_sort_string(&join_res.clone().collect().await.unwrap()),
+        @r"
+    +----+-------+-------+
+    | c1 | mark  | mark  |
+    +----+-------+-------+
+    | b  | true  | false |
+    | c  | false | false |
+    | d  | false | true  |
+    +----+-------+-------+
+    "
+    );
+
+    let drop_res = join_res.drop_columns(&["mark"])?;
+    assert_snapshot!(
+        batches_to_sort_string(&drop_res.clone().collect().await.unwrap()),
+        @r"
+    +----+
+    | c1 |
+    +----+
+    | b  |
+    | c  |
+    | d  |
+    +----+
+    "
     );
 
     Ok(())
