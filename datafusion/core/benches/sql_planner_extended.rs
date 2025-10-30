1029a6c99195e95bc0822f42e6cb770d6b10e24a
Bruce Ritchie <bruce.ritchie@gmail.com>
2025-10-30 17:27:49 -0400

    We love clippy. doccomment -> comment.

--- datafusion/core/benches/sql_planner_extended.rs
+++ datafusion/core/benches/sql_planner_extended.rs
@@ -31,14 +31,14 @@ use std::ops::Rem;
 use std::sync::Arc;
 use tokio::runtime::Runtime;
 
-/// This benchmark suite is designed to test the performance of
-/// logical planning with a large plan containing unions, many columns
-/// with a variety of operations in it.
-///
-/// Since it is (currently) very slow to execute it has been separated
-/// out from the sql_planner benchmark suite to this file.
-///
-/// See https://github.com/apache/datafusion/issues/17261 for details.
+// This benchmark suite is designed to test the performance of
+// logical planning with a large plan containing unions, many columns
+// with a variety of operations in it.
+//
+// Since it is (currently) very slow to execute it has been separated
+// out from the sql_planner benchmark suite to this file.
+//
+// See https://github.com/apache/datafusion/issues/17261 for details.
 
 /// Registers a table like this:
 /// c0,c1,c2...,c99
