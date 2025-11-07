89d8710eb6f9af7fd1a96483c131c131c6f61292
Andrew Lamb <andrew@nerdnetworks.org>
2025-11-07 17:05:53 -0500

    Fixup spans again

--- datafusion/sql/tests/cases/diagnostic.rs
+++ datafusion/sql/tests/cases/diagnostic.rs
@@ -71,9 +71,9 @@ fn do_query(sql: &'static str) -> Diagnostic {
 /// ```rust
 /// let spans = get_spans(
 ///     "SELECT /*whole+left*/speed/*left*/ + /*right*/10/*right+whole*/ FROM cars",
-/// // whole is               ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-/// // left is                ^^^^^
-/// // right is                                        ^^
+///     // whole is           ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+///     // left is            ^^^^^
+///     // right is                                    ^^
 /// );
 /// dbg!(&spans["whole"]);
 /// dbg!(&spans["left"]);
