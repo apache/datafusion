09d83826322492b3e6d5a314108357c228b395c4
Daniël Heres <danielheres@gmail.com>
2025-11-09 22:06:58 +0100

    fix

--- datafusion/physical-plan/src/repartition/mod.rs
+++ datafusion/physical-plan/src/repartition/mod.rs
@@ -1232,6 +1232,7 @@ impl RepartitionExec {
                     coalesce_batches_partition.push_batch(batch)?;
 
                     if coalesce_batches_partition.has_completed_batch() {
+                        coalesce_batches_partition.finish_buffered_batch()?;
                         batch = coalesce_batches_partition
                             .next_completed_batch()
                             .expect("has_completed_batch returned true");
