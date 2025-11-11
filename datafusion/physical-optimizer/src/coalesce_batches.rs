2921a29dfb9f529c59be971262537274fb15a1e6
Daniël Heres <danielheres@gmail.com>
2025-11-11 19:40:02 +0100

    Remove FilterExec case and change sqllogic tests

--- datafusion/physical-optimizer/src/coalesce_batches.rs
+++ datafusion/physical-optimizer/src/coalesce_batches.rs
@@ -26,7 +26,7 @@ use datafusion_common::error::Result;
 use datafusion_common::{config::ConfigOptions, internal_err};
 use datafusion_physical_expr::Partitioning;
 use datafusion_physical_plan::{
-    async_func::AsyncFuncExec, coalesce_batches::CoalesceBatchesExec, filter::FilterExec,
+    async_func::AsyncFuncExec, coalesce_batches::CoalesceBatchesExec,
     joins::HashJoinExec, repartition::RepartitionExec, ExecutionPlan,
 };
 
@@ -60,8 +60,7 @@ impl PhysicalOptimizerRule for CoalesceBatches {
             // wrap those ones with a CoalesceBatchesExec operator. An alternate approach here
             // would be to build the coalescing logic directly into the operators
             // See https://github.com/apache/datafusion/issues/139
-            let wrap_in_coalesce = plan_any.downcast_ref::<FilterExec>().is_some()
-                || plan_any.downcast_ref::<HashJoinExec>().is_some()
+            let wrap_in_coalesce = plan_any.downcast_ref::<HashJoinExec>().is_some()
                 // Don't need to add CoalesceBatchesExec after a round robin RepartitionExec
                 || plan_any
                     .downcast_ref::<RepartitionExec>()
