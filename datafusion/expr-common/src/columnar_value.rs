51c13183a93235e62e03f3f056fbc64166e886ef
Kumar Ujjawal <ujjawalpathak6@gmail.com>
2025-11-14 21:08:48 +0530

    fixed clippy issue

--- datafusion/expr-common/src/columnar_value.rs
+++ datafusion/expr-common/src/columnar_value.rs
@@ -22,7 +22,7 @@ use arrow::compute::{kernels, CastOptions};
 use arrow::datatypes::DataType;
 use arrow::util::pretty::pretty_format_columns;
 use datafusion_common::format::DEFAULT_CAST_OPTIONS;
-use datafusion_common::{internal_err, DataFusionError, Result, ScalarValue};
+use datafusion_common::{internal_err, Result, ScalarValue};
 use std::fmt;
 use std::sync::Arc;
 
