79f2b14dc56cac8d77eb3e23c5a052418d3f6e11
Dhanush <dhanushhs51@gmail.com>
2025-11-16 10:37:47 +0530

    chore: enforce clippy lint needless_pass_by_value to datafusion-functions-aggregate-common (#18741)
    ## Which issue does this PR close?
    
    - Part of #18503
    
    ## What changes are included in this PR?
    enforce clippy lint ` needless_pass_by_value` to
    `datafusion-functions-aggregate-common`
    ## Are these changes tested?
    yes
    ## Are there any user-facing changes?
    no

--- datafusion/functions-aggregate-common/src/tdigest.rs
+++ datafusion/functions-aggregate-common/src/tdigest.rs
@@ -163,6 +163,7 @@ impl TDigest {
         }
     }
 
+    #[expect(clippy::needless_pass_by_value)]
     pub fn new_with_centroid(max_size: usize, centroid: Centroid) -> Self {
         TDigest {
             centroids: vec![centroid.clone()],
