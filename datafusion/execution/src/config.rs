ac89a5f6b4984cac39a3bb4e767a295f9445dc9b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-11-27 14:00:50 +0530

    add docstring

--- datafusion/execution/src/config.rs
+++ datafusion/execution/src/config.rs
@@ -117,6 +117,10 @@ impl Default for SessionConfig {
 /// 
 /// Extensions are indexed by their type `T`. If multiple values of the same type are provided, only the last one
 /// will be kept.
+/// 
+/// Extensions are opaque objects that are unknown to DataFusion itself but can be downcast by optimizer rules,
+/// execution plans, or other components that have access to the session config.
+/// They provide a flexible way to attach extra data or behavior to the session config.
 #[derive(Clone, Debug)]
 pub struct Extensions {
     inner: AnyMap,
