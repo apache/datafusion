9f725d9c7064813cda0de0f87d115354b68d76e6
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-11-27 13:59:05 +0530

    move projection handling into FileSource (#18627)
    - Part of https://github.com/apache/datafusion/issues/14993
    
    This moves ownership of projections from `FileScanConfig` into
    `FileSource`.
    Notably we do *not* do anything special with this in Parquet just yet: I
    leave it for a followup to actually use the projection expressions
    instead of column indices to e.g. generate the Parquet `ProjectionMask`
    directly from expressions (in particular to select leaves instead of
    roots for struct and variant access).

--- datafusion-examples/examples/data_io/parquet_advanced_index.rs
+++ datafusion-examples/examples/data_io/parquet_advanced_index.rs
@@ -502,7 +502,7 @@ impl TableProvider for IndexTableProvider {
         );
         let file_scan_config = FileScanConfigBuilder::new(object_store_url, file_source)
             .with_limit(limit)
-            .with_projection_indices(projection.cloned())
+            .with_projection_indices(projection.cloned())?
             .with_file(partitioned_file)
             .build();
 
