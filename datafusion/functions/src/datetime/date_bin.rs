d40433dd25a40b9cdcf4a3aafcfc1c47c93a9378
Jatin Kumar Singh <jatinkumarsingh694@gmail.com>
2025-12-02 00:34:45 +0530

    Update date_bin test to match new error message

--- datafusion/functions/src/datetime/date_bin.rs
+++ datafusion/functions/src/datetime/date_bin.rs
@@ -726,7 +726,7 @@ mod tests {
         let res = invoke_date_bin_with_args(args, 1, return_field);
         assert_eq!(
             res.err().unwrap().strip_backtrace(),
-            "Execution error: DATE_BIN expects origin argument to be a TIMESTAMP with nanosecond precision but got Timestamp(µs)"
+            "Execution error: DATE_BIN expects origin argument to be a TIMESTAMP with nanosecond precision or TIME64 but got Timestamp(µs)"
         );
 
         args = vec![
