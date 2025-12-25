a2f038a46d53f29baf4435548a34779db807478a
Kumar Ujjawal <ujjawalpathak6@gmail.com>
2025-12-25 23:57:10 +0530

    chore:enforce clippy::allow_attributes for FFI

--- datafusion/ffi/src/insert_op.rs
+++ datafusion/ffi/src/insert_op.rs
@@ -21,7 +21,6 @@ use datafusion_expr::logical_plan::dml::InsertOp;
 /// FFI safe version of [`InsertOp`].
 #[repr(C)]
 #[derive(StableAbi)]
-#[allow(non_camel_case_types)]
 pub enum FFI_InsertOp {
     Append,
     Overwrite,
--- datafusion/ffi/src/lib.rs
+++ datafusion/ffi/src/lib.rs
@@ -23,7 +23,8 @@
 // Make sure fast / cheap clones on Arc are explicit:
 // https://github.com/apache/datafusion/issues/11143
 #![deny(clippy::clone_on_ref_ptr)]
-#![cfg_attr(test, allow(clippy::needless_pass_by_value))]
+#![deny(clippy::allow_attributes)]
+#![cfg_attr(test, expect(clippy::needless_pass_by_value))]
 
 pub mod arrow_wrappers;
 pub mod catalog_provider;
