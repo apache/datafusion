31f77d17e8e4d3589e28ff9d5f55ee828a0f94aa
Qi Zhu <821684824@qq.com>
2025-12-26 13:47:51 +0800

    fix

--- datafusion/physical-plan/src/filter.rs
+++ datafusion/physical-plan/src/filter.rs
@@ -24,7 +24,7 @@ use itertools::Itertools;
 
 use super::{
     ColumnStatistics, DisplayAs, ExecutionPlanProperties, PlanProperties,
-    RecordBatchStream, SendableRecordBatchStream, SortOrderPushdownResult, Statistics,
+    RecordBatchStream, SendableRecordBatchStream, Statistics,
 };
 use crate::coalesce::LimitedBatchCoalescer;
 use crate::coalesce::PushBatchStatus::LimitReached;
@@ -65,7 +65,6 @@ use datafusion_physical_expr::{
 };
 
 use datafusion_physical_expr_common::physical_expr::fmt_sql;
-use datafusion_physical_expr_common::sort_expr::PhysicalSortExpr;
 use futures::stream::{Stream, StreamExt};
 use log::trace;
 
@@ -603,32 +602,6 @@ impl ExecutionPlan for FilterExec {
             updated_node,
         })
     }
-
-    fn try_pushdown_sort(
-        &self,
-        order: &[PhysicalSortExpr],
-    ) -> Result<SortOrderPushdownResult<Arc<dyn ExecutionPlan>>> {
-        if let Some(_child_ordering) = self.input.output_ordering() {
-            match self.input.try_pushdown_sort(order)? {
-                SortOrderPushdownResult::Exact { inner } => {
-                    let new_exec =
-                        Arc::new(self.clone()).with_new_children(vec![inner])?;
-                    Ok(SortOrderPushdownResult::Exact { inner: new_exec })
-                }
-                SortOrderPushdownResult::Inexact { inner } => {
-                    let new_exec =
-                        Arc::new(self.clone()).with_new_children(vec![inner])?;
-                    Ok(SortOrderPushdownResult::Inexact { inner: new_exec })
-                }
-                SortOrderPushdownResult::Unsupported => {
-                    Ok(SortOrderPushdownResult::Unsupported)
-                }
-            }
-        } else {
-            Ok(SortOrderPushdownResult::Unsupported)
-        }
-    }
-
     fn with_fetch(&self, fetch: Option<usize>) -> Option<Arc<dyn ExecutionPlan>> {
         Some(Arc::new(Self {
             predicate: Arc::clone(&self.predicate),
