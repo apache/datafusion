9aacabbb588a22b69f3b0446a75215f395264604
Nachiket Roy <nachiket.roy.2@gmail.com>
2025-12-27 13:27:20 +0000

    fix : allow to expect

--- datafusion/ffi/tests/utils/mod.rs
+++ datafusion/ffi/tests/utils/mod.rs
@@ -27,7 +27,7 @@ use datafusion_proto::logical_plan::DefaultLogicalExtensionCodec;
 // for use in FFI integration tests.
 // This helper centralizes setup logic and is kept intentionally
 // for upcoming FFI test expansions.
-#[allow(dead_code)]
+#[expect(dead_code)]
 pub fn ctx_and_codec() -> (Arc<SessionContext>, FFI_LogicalExtensionCodec) {
     let ctx = Arc::new(SessionContext::default());
     let task_ctx_provider = Arc::clone(&ctx) as Arc<dyn TaskContextProvider>;
