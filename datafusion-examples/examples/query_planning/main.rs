94adbba37b9bc78f8d45a02a174bf54263597cb0
Siew Kam Onn <kosiew@gmail.com>
2025-12-29 19:28:44 +0800

    Revert "add array functions example with logical and physical plan demonstrations"
    This reverts commit 94f1a99cee4e44e5176450156a684a2316af78e1.

--- datafusion-examples/examples/query_planning/main.rs
+++ datafusion-examples/examples/query_planning/main.rs
@@ -21,13 +21,12 @@
 //!
 //! ## Usage
 //! ```bash
-//! cargo run --example query_planning -- [all|analyzer_rule|array_functions|expr_api|optimizer_rule|parse_sql_expr|plan_to_sql|planner_api|pruning|thread_pools]
+//! cargo run --example query_planning -- [all|analyzer_rule|expr_api|optimizer_rule|parse_sql_expr|plan_to_sql|planner_api|pruning|thread_pools]
 //! ```
 //!
 //! Each subcommand runs a corresponding example:
 //! - `all` — run all examples included in this module
 //! - `analyzer_rule` — use a custom AnalyzerRule to change a query's semantics (row level access control)
-//! - `array_functions` — show logical and physical plans for queries with array_has, array_has_all, array_has_any
 //! - `expr_api` — create, execute, simplify, analyze and coerce `Expr`s
 //! - `optimizer_rule` — use a custom OptimizerRule to replace certain predicates
 //! - `parse_sql_expr` — parse SQL text into DataFusion `Expr`
@@ -37,7 +36,6 @@
 //! - `thread_pools` — demonstrate TrackConsumersPool for memory tracking and debugging with enhanced error messages and shows how to implement memory-aware ExecutionPlan with memory reservation and spilling
 
 mod analyzer_rule;
-mod array_functions;
 mod expr_api;
 mod optimizer_rule;
 mod parse_sql_expr;
@@ -55,7 +53,6 @@ use strum_macros::{Display, EnumIter, EnumString, VariantNames};
 enum ExampleKind {
     All,
     AnalyzerRule,
-    ArrayFunctions,
     ExprApi,
     OptimizerRule,
     ParseSqlExpr,
@@ -81,9 +78,6 @@ impl ExampleKind {
                 }
             }
             ExampleKind::AnalyzerRule => analyzer_rule::analyzer_rule().await?,
-            ExampleKind::ArrayFunctions => {
-                array_functions::array_functions_physical_plans().await?
-            }
             ExampleKind::ExprApi => expr_api::expr_api().await?,
             ExampleKind::OptimizerRule => optimizer_rule::optimizer_rule().await?,
             ExampleKind::ParseSqlExpr => parse_sql_expr::parse_sql_expr().await?,
