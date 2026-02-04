632671a40a3ea4c22ee6a48aa703e7e04b6dbebc
nuno-faria <nunofpfaria@gmail.com>
2026-02-04 08:51:51 +0000

    fix: Ensure columns are casted to the correct names with Unions

--- datafusion/expr/src/expr_rewriter/mod.rs
+++ datafusion/expr/src/expr_rewriter/mod.rs
@@ -261,9 +261,14 @@ fn coerce_exprs_for_schema(
                     #[expect(deprecated)]
                     Expr::Wildcard { .. } => Ok(expr),
                     _ => {
-                        // maintain the original name when casting
-                        let name = dst_schema.field(idx).name();
-                        Ok(expr.cast_to(new_type, src_schema)?.alias(name))
+                        match expr {
+                            // maintain the original name when casting a column
+                            Expr::Column(ref column) => {
+                                let name = column.name().to_owned();
+                                Ok(expr.cast_to(new_type, src_schema)?.alias(name))
+                            }
+                            _ => Ok(expr.cast_to(new_type, src_schema)?),
+                        }
                     }
                 }
             } else {
