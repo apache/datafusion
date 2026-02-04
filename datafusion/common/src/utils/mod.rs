d3ac7a351a3ad0e97f64d700d532bacab7b419dc
Albert Skalt <133099191+askalt@users.noreply.github.com>
2026-02-05 00:30:22 +0300

    Wrap immutable plan parts into Arc (make creating `ExecutionPlan`s less costly) (#19893)
    ## Which issue does this PR close?
    
    <!--
    We generally require a GitHub issue to be filed for all bug fixes and
    enhancements and this helps us generate change logs for our releases.
    You can link an issue to this PR using the GitHub syntax. For example
    `Closes #123` indicates that this PR will close issue #123.
    -->
    
    - Closes https://github.com/apache/datafusion/issues/19852
    
    ## Rationale for this change
    
    Improve performance of query planning and plan state re-set by making
    node clone cheap.
    
    ## What changes are included in this PR?
    
    - Store projection as `Option<Arc<[usize]>>` instead of
    `Option<Vec<usize>>` in `FilterExec`, `HashJoinExec`,
    `NestedLoopJoinExec`.
    - Store exprs as `Arc<[ProjectionExpr]>` instead of Vec in
    `ProjectionExprs`.
    - Store arced aggregation, filter, group by expressions within
    `AggregateExec`.

--- datafusion/common/src/utils/mod.rs
+++ datafusion/common/src/utils/mod.rs
@@ -70,10 +70,10 @@ use std::thread::available_parallelism;
 /// ```
 pub fn project_schema(
     schema: &SchemaRef,
-    projection: Option<&Vec<usize>>,
+    projection: Option<&impl AsRef<[usize]>>,
 ) -> Result<SchemaRef> {
     let schema = match projection {
-        Some(columns) => Arc::new(schema.project(columns)?),
+        Some(columns) => Arc::new(schema.project(columns.as_ref())?),
         None => Arc::clone(schema),
     };
     Ok(schema)
