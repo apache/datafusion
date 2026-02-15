0461acf58851797be6b853db661dd5fc8d9c74c5
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2026-02-15 08:07:36 -0500

    fix test

--- datafusion/core/tests/physical_optimizer/filter_pushdown.rs
+++ datafusion/core/tests/physical_optimizer/filter_pushdown.rs
@@ -263,7 +263,7 @@ async fn test_dynamic_filter_pushdown_through_hash_join_with_topk() {
     - SortExec: TopK(fetch=2), expr=[e@4 ASC], preserve_partitioning=[false]
     -   HashJoinExec: mode=Partitioned, join_type=Inner, on=[(a@0, d@0)]
     -     DataSourceExec: file_groups={1 group: [[test.parquet]]}, projection=[a, b, c], file_type=test, pushdown_supported=true
-    -     DataSourceExec: file_groups={1 group: [[test.parquet]]}, projection=[d, e, f], file_type=test, pushdown_supported=true, predicate=Optional(DynamicFilter [ empty ]) AND Optional(DynamicFilter [ empty ])
+    -     DataSourceExec: file_groups={1 group: [[test.parquet]]}, projection=[d, e, f], file_type=test, pushdown_supported=true, predicate=Optional(DynamicFilter [ empty ]) AND DynamicFilter [ empty ]
     "
     );
 
