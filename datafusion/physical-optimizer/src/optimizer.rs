coralogix/arrow-datafusion

Push limits past windows (#337) (#17347) v50

datafusion/core/src/physical_optimizer/optimizer.rs
@@ -17,9 +17,6 @@
 
 //! Physical optimizer traits
 
-use datafusion_physical_optimizer::PhysicalOptimizerRule;
-use std::sync::Arc;
-
 use super::projection_pushdown::ProjectionPushdown;
 use super::update_aggr_exprs::OptimizeAggregateOrder;
 use crate::physical_optimizer::aggregate_statistics::AggregateStatistics;
@@ -33,6 +30,9 @@ use crate::physical_optimizer::limited_distinct_aggregation::LimitedDistinctAggr
 use crate::physical_optimizer::output_requirements::OutputRequirements;
 use crate::physical_optimizer::sanity_checker::SanityCheckPlan;
 use crate::physical_optimizer::topk_aggregation::TopKAggregation;
+use datafusion_physical_optimizer::limit_pushdown_past_window::LimitPushPastWindows;
+use datafusion_physical_optimizer::PhysicalOptimizerRule;
+use std::sync::Arc;
 
 /// A rule-based physical optimizer.
 #[derive(Clone, Debug)]
@@ -92,6 +92,10 @@ impl PhysicalOptimizer {
             // into an `order by max(x) limit y`. In this case it will copy the limit value down
             // to the aggregation, allowing it to use only y number of accumulators.
             Arc::new(TopKAggregation::new()),
+            // Tries to push limits down through window functions, growing as appropriate
+            // This can possibly be combined with [LimitPushdown]
+            // It needs to come after [EnforceSorting]
+            Arc::new(LimitPushPastWindows::new()),
             // The ProjectionPushdown rule tries to push projections towards
             // the sources in the execution plan. As a result of this process,
             // a projection can disappear if it reaches the source providers, and

apache/datafusion

---
[Cherry-pick summary: v46→v47]
Source commit: 3cc79bbfad (Push limits past windows (#337) (#17347) v50)
Strategy: cherry-picked, minor adaptions (2 conflict regions in optimizer.rs + 3 in explain.slt)
Upstream PR: fork-only (extends LimitPushdown with window-specific variant)
Conflicts resolved:
- optimizer.rs: kept BOTH LimitPushPastWindows (new) AND LimitPushdown (HEAD), new rule runs first
- explain.slt (x3): inserted LimitPushPastWindows output line before existing LimitPushdown line;
  dropped cherry-pick's extra ProjectionPushdown line (already present in HEAD after LimitPushdown)
Test coverage: adequate
Tests: cargo nextest run -p datafusion-physical-optimizer passed (61 tests)

datafusion/physical-optimizer/src/optimizer.rs
@@ -34,6 +34,7 @@ use crate::sanity_checker::SanityCheckPlan;
 use crate::topk_aggregation::TopKAggregation;
 use crate::update_aggr_exprs::OptimizeAggregateOrder;
 
+use crate::limit_pushdown_past_window::LimitPushPastWindows;
 use datafusion_common::config::ConfigOptions;
 use datafusion_common::Result;
 use datafusion_physical_plan::ExecutionPlan;
@@ -121,6 +122,10 @@ impl PhysicalOptimizer {
             // into an `order by max(x) limit y`. In this case it will copy the limit value down
             // to the aggregation, allowing it to use only y number of accumulators.
             Arc::new(TopKAggregation::new()),
+            // Tries to push limits down through window functions, growing as appropriate
+            // This can possibly be combined with [LimitPushdown]
+            // It needs to come after [EnforceSorting]
+            Arc::new(LimitPushPastWindows::new()),
             // The LimitPushdown rule tries to push limits down as far as possible,
             // replacing operators with fetching variants, or adding limits
             // past operators that support limit pushdown.