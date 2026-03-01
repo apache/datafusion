c98ae53da1003f667ae120abbaa28c1877724a40
Ratul Dawar <ratuldawar11@gmail.com>
2026-03-01 15:38:47 +0530

    Fix FileStream scanning_total to include sync next-file open time

--- datafusion/datasource/src/file_stream.rs
+++ datafusion/datasource/src/file_stream.rs
@@ -126,7 +126,12 @@ impl FileStream {
                     Ok(reader) => {
                         // include time needed to start opening in `start_next_file`
                         self.file_stream_metrics.time_opening.stop();
-                        let next = self.start_next_file().transpose();
+                        let next = {
+                            let scanning_total_metric =
+                                self.file_stream_metrics.time_scanning_total.metrics.clone();
+                            let _timer = scanning_total_metric.timer();
+                            self.start_next_file().transpose()
+                        };
                         self.file_stream_metrics.time_scanning_until_data.start();
                         self.file_stream_metrics.time_scanning_total.start();
 
