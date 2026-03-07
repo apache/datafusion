1d7049e4f6f33844bdde9ef2e83881ce9bc9a182
Kartik Gupta <b123064@iiit-bh.ac.in>
2026-03-07 22:02:19 +0530

    fix/substrait-lambda-rextype-0.62.3+

--- datafusion/substrait/src/logical_plan/consumer/expr/mod.rs
+++ datafusion/substrait/src/logical_plan/consumer/expr/mod.rs
@@ -93,6 +93,13 @@ pub async fn from_substrait_rex(
             RexType::DynamicParameter(expr) => {
                 consumer.consume_dynamic_parameter(expr, input_schema).await
             }
+            RexType::Lambda(_) => {
+                not_impl_err!("Lambda expressions are not yet supported")
+            }
+            RexType::LambdaInvocation(_) => {
+                not_impl_err!("LambdaInvocation expressions are not yet supported")
+            }
+        },
         },
         None => substrait_err!("Expression must set rex_type: {expression:?}"),
     }
