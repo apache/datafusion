32c7644f821407da65c645a563bc08990641d509
olsemeno <sendtosemenov@gmail.com>
2026-03-17 13:38:54 +0400

    feat: implement regexp_extract scalar function (Spark compatible)

--- datafusion/functions/src/regex/mod.rs
+++ datafusion/functions/src/regex/mod.rs
@@ -23,6 +23,7 @@ use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 use std::sync::Arc;
 pub mod regexpcount;
+pub mod regexpextract;
 pub mod regexpinstr;
 pub mod regexplike;
 pub mod regexpmatch;
@@ -30,6 +31,7 @@ pub mod regexpreplace;
 
 // create UDFs
 make_udf_function!(regexpcount::RegexpCountFunc, regexp_count);
+make_udf_function!(regexpextract::RegexpExtractFunc, regexp_extract);
 make_udf_function!(regexpinstr::RegexpInstrFunc, regexp_instr);
 make_udf_function!(regexpmatch::RegexpMatchFunc, regexp_match);
 make_udf_function!(regexplike::RegexpLikeFunc, regexp_like);
@@ -121,6 +123,7 @@ pub mod expr_fn {
 pub fn functions() -> Vec<Arc<datafusion_expr::ScalarUDF>> {
     vec![
         regexp_count(),
+        regexp_extract(),
         regexp_match(),
         regexp_instr(),
         regexp_like(),
