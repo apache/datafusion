c04052cb0e932905c614e63deeddedf7ce306129
theirix <theirix@gmail.com>
2026-03-20 10:34:35 +0000

    Fix flaky test

--- datafusion/optimizer/src/analyzer/type_coercion.rs
+++ datafusion/optimizer/src/analyzer/type_coercion.rs
@@ -1889,7 +1889,7 @@ mod test {
             .unwrap()
             .strip_backtrace();
         assert!(err.contains("Function 'avg' failed to match any signature"));
-        assert!(err.contains("Error during planning: Function 'avg' requires Float64, but received String (DataType: Utf8)"));
+        assert!(err.contains("Function 'avg' requires Float64, but received String (DataType: Utf8)"));
         Ok(())
     }
 
