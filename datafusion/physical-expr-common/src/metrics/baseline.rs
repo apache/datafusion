5dd668ae128a3776715ed67035a484ae9971acf9
Siew Kam Onn <kosiew@gmail.com>
2026-03-30 15:50:35 +0800

    Fix metric type casing in BaselineMetrics

--- datafusion/physical-expr-common/src/metrics/baseline.rs
+++ datafusion/physical-expr-common/src/metrics/baseline.rs
@@ -90,7 +90,7 @@ impl BaselineMetrics {
                 .with_type(super::MetricType::SUMMARY)
                 .output_bytes(partition),
             output_batches: MetricBuilder::new(metrics)
-                .with_type(super::MetricType::DEV)
+                .with_type(super::MetricType::Dev)
                 .output_batches(partition),
         }
     }
