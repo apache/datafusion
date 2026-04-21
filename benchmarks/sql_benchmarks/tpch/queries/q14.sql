238d0a610eaab9fbea610946affa629b5c01bdae
Bruce Ritchie <bruce.ritchie@veeva.com>
2026-04-21 10:54:58 -0400

    Update benchmarks/sql_benchmarks/tpch/queries/q14.sql
    Co-authored-by: Martin Grigorov <martin-g@users.noreply.github.com>

--- benchmarks/sql_benchmarks/tpch/queries/q14.sql
+++ benchmarks/sql_benchmarks/tpch/queries/q14.sql
@@ -7,4 +7,4 @@ from lineitem,
      part
 where l_partkey = p_partkey
   and l_shipdate >= date '1995-09-01'
-  and l_shipdate < date '1995-09-01' + interval '1' month;;
+  and l_shipdate < date '1995-09-01' + interval '1' month;
