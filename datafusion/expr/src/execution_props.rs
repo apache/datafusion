17e50e0bda9adb02f380b001b34bc068d7c60e2f
gstvg <28798827+gstvg@users.noreply.github.com>
2026-04-30 18:24:00 -0300

    fix: add lambda_qualifiers to ExecutionProps debug test

--- datafusion/expr/src/execution_props.rs
+++ datafusion/expr/src/execution_props.rs
@@ -274,7 +274,7 @@ mod test {
     fn debug() {
         let props = ExecutionProps::new();
         assert_eq!(
-            "ExecutionProps { query_execution_start_time: None, alias_generator: AliasGenerator { next_id: 1 }, config_options: None, var_providers: None, subquery_indexes: {}, subquery_results: [] }",
+            "ExecutionProps { query_execution_start_time: None, alias_generator: AliasGenerator { next_id: 1 }, config_options: None, var_providers: None, subquery_indexes: {}, subquery_results: [], lambda_variable_qualifier: {} }",
             format!("{props:?}")
         );
     }
