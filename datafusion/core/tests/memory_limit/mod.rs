00c5d5f79485f062d2e45dec24d06b3d205f4353
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2026-05-06 15:55:18 +0300

    Make the ctor function `unsafe`

--- datafusion/core/tests/memory_limit/mod.rs
+++ datafusion/core/tests/memory_limit/mod.rs
@@ -63,7 +63,7 @@ use futures::StreamExt;
 use tokio::fs::File;
 
 #[cfg(test)]
-#[ctor::ctor]
+#[ctor::ctor(unsafe)]
 fn init() {
     // Enable RUST_LOG logging configuration for test
     let _ = env_logger::try_init();
