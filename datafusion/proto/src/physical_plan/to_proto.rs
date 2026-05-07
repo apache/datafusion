6dec7efcde7d0f507699f12b5f230a89f654947e
Marc Brinkmann <marc@pydantic.dev>
2026-05-07 15:54:51 +0200

    Serialize partitioned_by_file_group in FileScanExecConf protobuf

--- datafusion/proto/src/physical_plan/to_proto.rs
+++ datafusion/proto/src/physical_plan/to_proto.rs
@@ -757,6 +757,7 @@ pub fn serialize_file_scan_config(
         constraints: Some(conf.constraints.clone().into()),
         batch_size: conf.batch_size.map(|s| s as u64),
         projection_exprs,
+        partitioned_by_file_group: Some(conf.partitioned_by_file_group),
     })
 }
 
