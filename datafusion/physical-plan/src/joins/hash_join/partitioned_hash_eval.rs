d24abe87c1fb19a5a081eded717b94576031a067
Anurag Tryambak Raut <anuragtryambakraut@Anurags-MacBook-Air.local>
2026-05-22 19:01:02 +0530

    fix: remove invalid cfg(feature = "proto") in HashTableLookupExpr

--- datafusion/physical-plan/src/joins/hash_join/partitioned_hash_eval.rs
+++ datafusion/physical-plan/src/joins/hash_join/partitioned_hash_eval.rs
@@ -289,7 +289,7 @@ impl PartialEq for HashTableLookupExpr {
 
 impl Eq for HashTableLookupExpr {}
 
-#[cfg(feature = "proto")]
+
 impl HashTableLookupExpr {
     /// Serialize this expression to protobuf.
     ///
@@ -304,11 +304,10 @@ impl HashTableLookupExpr {
     pub fn try_to_proto(
         &self,
         _ctx: &datafusion_physical_expr_common::physical_expr::proto_encode::PhysicalExprEncodeCtx<'_>,
-    ) -> datafusion_common::Result<
-        Option<datafusion_proto_models::protobuf::PhysicalExprNode>,
-    > {
-        use datafusion_proto_common::ScalarValue;
+    ) -> datafusion_common::Result<Option<datafusion_proto_models::protobuf::PhysicalExprNode>>
+    {
         use datafusion_proto_common::scalar_value::Value;
+        use datafusion_proto_common::ScalarValue;
         use datafusion_proto_models::protobuf;
         use datafusion_proto_models::protobuf::physical_expr_node::ExprType;
 
