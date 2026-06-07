5be99f352c856f2fff9a81f1efc16fd1488e38a2
Bruce Ritchie <bruce.ritchie@gmail.com>
2026-06-07 14:30:31 -0400

    Use data_dir.

--- benchmarks/sql_benchmarks/clickbench_sorted/init/load.sql
+++ benchmarks/sql_benchmarks/clickbench_sorted/init/load.sql
@@ -3,6 +3,6 @@
 
 set datafusion.optimizer.prefer_existing_sort=true
 
-CREATE EXTERNAL TABLE hits_raw STORED AS PARQUET LOCATION 'data/hits_sorted.parquet' WITH ORDER ("${SORTED_BY:-EventTime}" ${SORTED_ORDER:-ASC});
+CREATE EXTERNAL TABLE hits_raw STORED AS PARQUET LOCATION '${DATA_DIR:-data}/hits_sorted.parquet' WITH ORDER ("${SORTED_BY:-EventTime}" ${SORTED_ORDER:-ASC});
 
 CREATE VIEW hits AS SELECT * EXCEPT ("EventDate"), CAST(CAST("EventDate" AS INTEGER) AS DATE) AS "EventDate" FROM hits_raw;
