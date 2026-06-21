1f9d8453c3adc27a469914a7e93c5d2f7605e0ed
Kevin-Li-2025 <2242139@qq.com>
2026-06-21 12:12:34 +0100

    Add native any_value groups accumulator
    Signed-off-by: Kevin-Li-2025 <2242139@qq.com>

--- datafusion/functions-aggregate/src/any_value.rs
+++ datafusion/functions-aggregate/src/any_value.rs
@@ -19,14 +19,19 @@
 
 use std::fmt::Debug;
 use std::hash::Hash;
+use std::mem::size_of_val;
 use std::sync::Arc;
 
+use arrow::array::{Array, ArrayRef, BooleanArray, BooleanBufferBuilder};
+use arrow::buffer::BooleanBuffer;
 use arrow::datatypes::{DataType, Field, FieldRef};
-use datafusion_common::{Result, not_impl_err};
+use datafusion_common::cast::as_boolean_array;
+use datafusion_common::{Result, ScalarValue, not_impl_err};
 use datafusion_expr::function::{AccumulatorArgs, StateFieldsArgs};
 use datafusion_expr::utils::{AggregateOrderSensitivity, format_state_name};
 use datafusion_expr::{
-    Accumulator, AggregateUDFImpl, Documentation, Signature, Volatility,
+    Accumulator, AggregateUDFImpl, Documentation, EmitTo, GroupsAccumulator, Signature,
+    Volatility,
 };
 use datafusion_macros::user_doc;
 
@@ -115,11 +120,237 @@ impl AggregateUDFImpl for AnyValue {
         ])
     }
 
+    fn groups_accumulator_supported(&self, _args: AccumulatorArgs) -> bool {
+        true
+    }
+
+    fn create_groups_accumulator(
+        &self,
+        args: AccumulatorArgs,
+    ) -> Result<Box<dyn GroupsAccumulator>> {
+        Ok(Box::new(AnyValueGroupsAccumulator::try_new(
+            args.return_field.data_type(),
+        )?))
+    }
+
     fn order_sensitivity(&self) -> AggregateOrderSensitivity {
         AggregateOrderSensitivity::Insensitive
     }
 
     fn documentation(&self) -> Option<&Documentation> {
         self.doc()
+    }
+}
+
+#[derive(Debug)]
+struct AnyValueGroupsAccumulator {
+    values: Vec<ScalarValue>,
+    is_set: BooleanBufferBuilder,
+    null_value: ScalarValue,
+}
+
+impl AnyValueGroupsAccumulator {
+    fn try_new(data_type: &DataType) -> Result<Self> {
+        Ok(Self {
+            values: vec![],
+            is_set: BooleanBufferBuilder::new(0),
+            null_value: ScalarValue::try_from(data_type)?,
+        })
+    }
+
+    fn ensure_groups(&mut self, total_num_groups: usize) {
+        if self.values.len() < total_num_groups {
+            self.values
+                .resize(total_num_groups, self.null_value.clone());
+            self.is_set.resize(total_num_groups);
+        }
+    }
+
+    fn take_state(&mut self, emit_to: EmitTo) -> (Vec<ScalarValue>, BooleanBuffer) {
+        let values = emit_to.take_needed(&mut self.values);
+        let is_set = self.is_set.finish();
+        match emit_to {
+            EmitTo::All => (values, is_set),
+            EmitTo::First(n) => {
+                let emitted = is_set.slice(0, n);
+                self.is_set
+                    .append_buffer(&is_set.slice(n, is_set.len() - n));
+                (values, emitted)
+            }
+        }
+    }
+
+    fn update_row(
+        &mut self,
+        values: &ArrayRef,
+        group_index: usize,
+        row: usize,
+    ) -> Result<()> {
+        if !self.is_set.get_bit(group_index) && values.is_valid(row) {
+            self.values[group_index] = ScalarValue::try_from_array(values, row)?;
+            self.is_set.set_bit(group_index, true);
+        }
+        Ok(())
+    }
+}
+
+impl GroupsAccumulator for AnyValueGroupsAccumulator {
+    fn update_batch(
+        &mut self,
+        values: &[ArrayRef],
+        group_indices: &[usize],
+        opt_filter: Option<&BooleanArray>,
+        total_num_groups: usize,
+    ) -> Result<()> {
+        assert_eq!(values.len(), 1, "any_value expects one argument");
+        let values = &values[0];
+        self.ensure_groups(total_num_groups);
+
+        for (row, &group_index) in group_indices.iter().enumerate() {
+            if opt_filter.is_none_or(|filter| filter.is_valid(row) && filter.value(row)) {
+                self.update_row(values, group_index, row)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn evaluate(&mut self, emit_to: EmitTo) -> Result<ArrayRef> {
+        let (values, _) = self.take_state(emit_to);
+        ScalarValue::iter_to_array(values)
+    }
+
+    fn state(&mut self, emit_to: EmitTo) -> Result<Vec<ArrayRef>> {
+        let (values, is_set) = self.take_state(emit_to);
+        Ok(vec![
+            ScalarValue::iter_to_array(values)?,
+            Arc::new(BooleanArray::new(is_set, None)),
+        ])
+    }
+
+    fn merge_batch(
+        &mut self,
+        values: &[ArrayRef],
+        group_indices: &[usize],
+        total_num_groups: usize,
+    ) -> Result<()> {
+        assert_eq!(values.len(), 2, "any_value expects value and is_set state");
+        let is_set = as_boolean_array(&values[1])?;
+        self.ensure_groups(total_num_groups);
+
+        for (row, &group_index) in group_indices.iter().enumerate() {
+            if is_set.is_valid(row)
+                && is_set.value(row)
+                && !self.is_set.get_bit(group_index)
+            {
+                self.values[group_index] = ScalarValue::try_from_array(&values[0], row)?;
+                self.is_set.set_bit(group_index, true);
+            }
+        }
+        Ok(())
+    }
+
+    fn supports_convert_to_state(&self) -> bool {
+        true
+    }
+
+    fn convert_to_state(
+        &self,
+        values: &[ArrayRef],
+        opt_filter: Option<&BooleanArray>,
+    ) -> Result<Vec<ArrayRef>> {
+        assert_eq!(values.len(), 1, "any_value expects one argument");
+        let values = &values[0];
+        let is_set = BooleanArray::from_iter((0..values.len()).map(|row| {
+            values.is_valid(row)
+                && opt_filter
+                    .is_none_or(|filter| filter.is_valid(row) && filter.value(row))
+        }));
+        Ok(vec![Arc::clone(values), Arc::new(is_set)])
+    }
+
+    fn size(&self) -> usize {
+        size_of_val(self)
+            + ScalarValue::size_of_vec(&self.values)
+            + self.is_set.capacity() / 8
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use arrow::array::{Int64Array, StringArray};
+
+    #[test]
+    fn groups_accumulator_uses_first_non_null_value() -> Result<()> {
+        let mut acc = AnyValueGroupsAccumulator::try_new(&DataType::Int64)?;
+        let values = Arc::new(Int64Array::from(vec![
+            None,
+            Some(10),
+            Some(11),
+            Some(20),
+            None,
+            Some(30),
+        ])) as ArrayRef;
+        let filter = BooleanArray::from(vec![true, true, true, false, true, true]);
+
+        acc.update_batch(&[values], &[0, 0, 0, 1, 1, 2], Some(&filter), 4)?;
+        let result = acc.evaluate(EmitTo::All)?;
+        let expected =
+            Arc::new(Int64Array::from(vec![Some(10), None, Some(30), None])) as ArrayRef;
+        assert_eq!(&result, &expected);
+        Ok(())
+    }
+
+    #[test]
+    fn groups_accumulator_merges_partial_state() -> Result<()> {
+        let mut acc = AnyValueGroupsAccumulator::try_new(&DataType::Utf8)?;
+        let values = Arc::new(StringArray::from(vec![Some("a"), Some("b"), Some("c")]))
+            as ArrayRef;
+        let is_set = Arc::new(BooleanArray::from(vec![false, true, true])) as ArrayRef;
+
+        acc.merge_batch(&[values, is_set], &[0, 0, 1], 3)?;
+        let state = acc.state(EmitTo::All)?;
+        let expected_values =
+            Arc::new(StringArray::from(vec![Some("b"), Some("c"), None])) as ArrayRef;
+        let expected_is_set =
+            Arc::new(BooleanArray::from(vec![true, true, false])) as ArrayRef;
+        assert_eq!(&state[0], &expected_values);
+        assert_eq!(&state[1], &expected_is_set);
+        Ok(())
+    }
+
+    #[test]
+    fn groups_accumulator_convert_to_state_applies_filter_and_nulls() -> Result<()> {
+        let acc = AnyValueGroupsAccumulator::try_new(&DataType::Int64)?;
+        let values = Arc::new(Int64Array::from(vec![Some(1), None, Some(3)])) as ArrayRef;
+        let filter = BooleanArray::from(vec![true, true, false]);
+
+        let state = acc.convert_to_state(&[Arc::clone(&values)], Some(&filter))?;
+        let expected_is_set =
+            Arc::new(BooleanArray::from(vec![true, false, false])) as ArrayRef;
+        assert_eq!(&state[0], &values);
+        assert_eq!(&state[1], &expected_is_set);
+        Ok(())
+    }
+
+    #[test]
+    fn groups_accumulator_emit_first_retains_remaining_groups() -> Result<()> {
+        let mut acc = AnyValueGroupsAccumulator::try_new(&DataType::Int64)?;
+        let values =
+            Arc::new(Int64Array::from(vec![Some(10), Some(20), Some(30)])) as ArrayRef;
+        acc.update_batch(&[values], &[0, 1, 2], None, 3)?;
+
+        let first = acc.evaluate(EmitTo::First(2))?;
+        let expected_first =
+            Arc::new(Int64Array::from(vec![Some(10), Some(20)])) as ArrayRef;
+        assert_eq!(&first, &expected_first);
+
+        let values = Arc::new(Int64Array::from(vec![Some(31), Some(40)])) as ArrayRef;
+        acc.update_batch(&[values], &[0, 1], None, 2)?;
+        let remaining = acc.evaluate(EmitTo::All)?;
+        let expected_remaining =
+            Arc::new(Int64Array::from(vec![Some(30), Some(40)])) as ArrayRef;
+        assert_eq!(&remaining, &expected_remaining);
+        Ok(())
     }
 }
