fb748cfd853b0df4f9ee3c9c3bff4040a6fd2ca2
Adam Gutglick <adamgsal@gmail.com>
2026-06-23 12:50:41 +0100

    fixes
    Signed-off-by: Adam Gutglick <adamgsal@gmail.com>

--- datafusion/physical-plan/src/joins/hash_join/exec.rs
+++ datafusion/physical-plan/src/joins/hash_join/exec.rs
@@ -2870,6 +2870,7 @@ async fn collect_left_input(
             &mut hashes_buffer,
             0,
             true,
+            NullEquality::NullEqualsNothing,
         )?;
 
         // Build rows whose value key is NULL get a dedicated scope map: any
