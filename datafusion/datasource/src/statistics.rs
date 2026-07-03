b065df4893dad4795073ec2513afe6bcff568fdb
Michael Kleen <mkleen@gmail.com>
2026-07-03 12:38:51 +0200

    Clean-up

--- datafusion/datasource/src/statistics.rs
+++ datafusion/datasource/src/statistics.rs
@@ -97,12 +97,12 @@ impl MinMaxStatistics {
                             .zip(s.column_statistics[i].max_value.get_value().cloned())
                             .ok_or_else(|| plan_datafusion_err!("statistics not found"))
                     } else {
-                        let partition_value_index = i - s.column_statistics.len();
-                        if pv.len() > partition_value_index {
-                            let partition_value = &pv[partition_value_index];
+                        if let Some(partition_value) =
+                            pv.get(i - s.column_statistics.len())
+                        {
                             Ok((partition_value.clone(), partition_value.clone()))
                         } else {
-                            Err(plan_datafusion_err!("statistics not found"))
+                            return Err(plan_datafusion_err!("statistics not found"));
                         }
                     }
                 })
