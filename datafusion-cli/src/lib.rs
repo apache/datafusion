2488f0bedd4f3f377b9fbfcfdc98645a685aa6af
matt rice <ratmice@gmail.com>
2026-07-10 12:52:33 -0700

    Move `main_inner` into a new `entrypoint` module.

--- datafusion-cli/src/lib.rs
+++ datafusion-cli/src/lib.rs
@@ -26,6 +26,7 @@ pub const DATAFUSION_CLI_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub mod catalog;
 pub mod cli_context;
 pub mod command;
+pub mod entry_point;
 pub mod exec;
 pub mod functions;
 pub mod helper;
