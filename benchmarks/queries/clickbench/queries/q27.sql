97253560c7cb4526a3e39142ead559eaae696487
Siew Kam Onn <kosiew@gmail.com>
2026-07-11 12:57:57 +0800

    refactor: update query and benchmark metrics to use octet_length() instead of length() for q27 and q28

--- benchmarks/queries/clickbench/queries/q27.sql
+++ benchmarks/queries/clickbench/queries/q27.sql
@@ -1,4 +1,4 @@
 -- Must set for ClickBench hits_partitioned dataset. See https://github.com/apache/datafusion/issues/16591
 -- set datafusion.execution.parquet.binary_as_string = true
 
-SELECT "CounterID", AVG(length("URL")) AS l, COUNT(*) AS c FROM hits WHERE "URL" <> '' GROUP BY "CounterID" HAVING COUNT(*) > 100000 ORDER BY l DESC LIMIT 25;
+SELECT "CounterID", AVG(octet_length("URL")) AS l, COUNT(*) AS c FROM hits WHERE "URL" <> '' GROUP BY "CounterID" HAVING COUNT(*) > 100000 ORDER BY l DESC LIMIT 25;
