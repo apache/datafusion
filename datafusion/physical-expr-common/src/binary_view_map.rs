f800afb542388c0690290ef7cdccf96c8e947e0b
kamille <kamille@apache.org>
2026-07-12 22:43:00 +0800

    test2.

--- datafusion/physical-expr-common/src/binary_view_map.rs
+++ datafusion/physical-expr-common/src/binary_view_map.rs
@@ -26,7 +26,7 @@ use arrow::datatypes::{BinaryViewType, ByteViewType, DataType, StringViewType};
 use datafusion_common::hash_utils::RandomState;
 use datafusion_common::hash_utils::create_hashes;
 use datafusion_common::utils::proxy::{HashTableAllocExt, VecAllocExt};
-use std::fmt::Debug;
+use std::{fmt::Debug, io::SeekFrom::Current};
 use std::mem::size_of;
 use std::sync::Arc;
 
@@ -396,7 +396,7 @@ where
     pub fn into_state(mut self) -> ArrayRef {
         // Flush any remaining in-progress buffer
         if !self.in_progress.is_empty() {
-            let flushed = std::mem::take(&mut self.in_progress);
+            let flushed = std::mem::replace(&mut self.in_progress, Vec::with_capacity(BYTE_VIEW_MAX_BLOCK_SIZE));
             self.completed.push(Buffer::from_vec(flushed));
         }
 
