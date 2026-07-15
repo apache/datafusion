4ec66180e235a47417e5005328dc5e52ae86cd49
Ariel Miculas <ariel.miculas@gmail.com>
2026-07-15 18:48:13 +0300

    refactor: simplify the expression

--- datafusion/physical-plan/src/sorts/merge.rs
+++ datafusion/physical-plan/src/sorts/merge.rs
@@ -420,11 +420,10 @@ impl<C: CursorValues> SortPreservingMergeStream<C> {
             let _ = cursor.advance();
             let finished = cursor.is_finished();
             if finished {
+                // Take the current cursor, leaving `None` in its place
+                let taken = self.cursors[stream_idx].take();
                 if self.enable_round_robin_tie_breaker {
-                    // Take the current cursor, leaving `None` in its place
-                    self.prev_cursors[stream_idx] = self.cursors[stream_idx].take();
-                } else {
-                    self.cursors[stream_idx].take();
+                    self.prev_cursors[stream_idx] = taken;
                 }
             }
             return finished;
