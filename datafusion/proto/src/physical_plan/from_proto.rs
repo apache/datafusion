cd9ce3488e49735f21a062c5bf26534a476a8b6f
buraksenn <buraksenb@gmail.com>
2026-07-30 18:14:32 +0300

    fix github suggestion deleting test setup

--- datafusion/proto/src/physical_plan/from_proto.rs
+++ datafusion/proto/src/physical_plan/from_proto.rs
@@ -863,6 +863,10 @@ mod tests {
     #[test]
     fn partitioned_file_statistics_roundtrip_with_partition_values() {
         use datafusion_common::Statistics;
+        let file_schema = Schema::new(vec![Field::new("a", DataType::Int32, true)]);
+        let pf = PartitionedFile::new("foo/bar.parquet", 1234)
+            .with_partition_values(vec![ScalarValue::from("2024-01-01")])
+            .with_statistics(Arc::new(Statistics::new_unknown(&file_schema)));
 
         // `statistics` covers the full table schema: file columns followed by one
         // entry per partition column.
