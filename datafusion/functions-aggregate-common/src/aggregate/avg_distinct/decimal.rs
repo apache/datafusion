be274ef009b54ae8dcaffc301eaabc59d8291794
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2026-08-13 14:19:43 +0200

    Revert "feat: enable clippy::large_digit_groups"
    This reverts commit fddce2011ecab52053ade2ea6faf2d36aaaeec74.

--- datafusion/functions-aggregate-common/src/aggregate/avg_distinct/decimal.rs
+++ datafusion/functions-aggregate-common/src/aggregate/avg_distinct/decimal.rs
@@ -173,10 +173,6 @@ mod tests {
     }
 
     #[test]
-    #[expect(
-        clippy::large_digit_groups,
-        reason = "The grouping spells out the decimal scale, e.g. 180_00000000 is 180 with scale 8"
-    )]
     fn test_decimal64_distinct_avg_accumulator() -> Result<()> {
         let precision = 10_u8;
         let scale = 4_i8;
@@ -206,10 +202,6 @@ mod tests {
     }
 
     #[test]
-    #[expect(
-        clippy::large_digit_groups,
-        reason = "The grouping spells out the decimal scale, e.g. 180_00000000 is 180 with scale 8"
-    )]
     fn test_decimal128_distinct_avg_accumulator() -> Result<()> {
         let precision = 10_u8;
         let scale = 4_i8;
@@ -239,10 +231,6 @@ mod tests {
     }
 
     #[test]
-    #[expect(
-        clippy::large_digit_groups,
-        reason = "The grouping spells out the decimal scale, e.g. 180_00000000 is 180 with scale 8"
-    )]
     fn test_decimal256_distinct_avg_accumulator() -> Result<()> {
         let precision = 50_u8;
         let scale = 2_i8;
