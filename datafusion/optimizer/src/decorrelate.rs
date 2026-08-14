2e76d714742377415988a0906657289f56c1efee
Ruchirtripathi <tripathiruchir05@gmail.com>
2026-08-14 17:13:20 +0530

    Fix ScalarSubqueryToJoin __always_true schema error

--- datafusion/optimizer/src/decorrelate.rs
+++ datafusion/optimizer/src/decorrelate.rs
@@ -350,15 +350,29 @@ impl TreeNodeRewriter for PullUpCorrelatedExpr {
                     new_correlated_cols
                         .insert(Column::new(Some(alias.alias.clone()), col.name.clone()));
                 }
+
+                let new_plan = if alias.input.schema().fields().len() != alias.schema.fields().len() {
+                    LogicalPlanBuilder::from((*alias.input).clone())
+                        .alias(alias.alias.clone())?
+                        .build()?
+                } else {
+                    plan.clone()
+                };
+
                 self.correlated_subquery_cols_map
-                    .insert(plan.clone(), new_correlated_cols);
+                    .insert(new_plan.clone(), new_correlated_cols);
                 if let Some(input_map) =
                     self.collected_count_expr_map.get(alias.input.deref())
                 {
                     self.collected_count_expr_map
-                        .insert(plan.clone(), input_map.clone());
+                        .insert(new_plan.clone(), input_map.clone());
                 }
-                Ok(Transformed::no(plan))
+                
+                if new_plan != plan {
+                    Ok(Transformed::yes(new_plan))
+                } else {
+                    Ok(Transformed::no(plan))
+                }
             }
             LogicalPlan::Limit(limit) => {
                 let input_expr_map = self
