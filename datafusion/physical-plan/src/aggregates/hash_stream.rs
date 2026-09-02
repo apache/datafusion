a8f719711f13e6e16de59e38d1bc302e265253b5
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2026-09-02 14:08:33 +0300

    rename

--- datafusion/physical-plan/src/aggregates/hash_stream.rs
+++ datafusion/physical-plan/src/aggregates/hash_stream.rs
@@ -855,7 +855,7 @@ impl FinalHashAggregateStream {
                         .await?
                 }
                 // Either all the input fit in memory or hit soft group limit with no spilling
-                None => self.produce_output(hash_table, emitter).await?,
+                None => self.produce_output_from_memory(hash_table, emitter).await?,
             }
 
             Ok(())
@@ -1028,7 +1028,7 @@ impl FinalHashAggregateStream {
 
     /// Emit final aggregate value batches:
     /// Input was exhausted without spilling, or the soft group limit was reached.
-    async fn produce_output(
+    async fn produce_output_from_memory(
         &mut self,
         mut hash_table: AggregateHashTable<FinalMarker>,
         mut emitter: TryEmitter<RecordBatch, DataFusionError>,
