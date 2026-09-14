2c70f0a1d0535de484b24a0d3d4cab5a4fde0d85
goutamadwant <workwithgoutam@gmail.com>
2026-09-13 21:32:55 -0700

    fix: regenerate range partitioning JSON bindings

--- datafusion/proto-models/src/generated/pbjson.rs
+++ datafusion/proto-models/src/generated/pbjson.rs
@@ -21933,7 +21933,7 @@ impl<'de> serde::Deserialize<'de> for PhysicalRangePartitioning {
                             if partition_count__.is_some() {
                                 return Err(serde::de::Error::duplicate_field("partitionCount"));
                             }
-                            partition_count__ =
+                            partition_count__ = 
                                 Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
                             ;
                         }
