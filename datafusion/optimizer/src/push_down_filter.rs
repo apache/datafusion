954da9ae06d9597a7dbc67106ae528eede7817fe
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2026-09-16 18:44:18 +0200

    Don't allow push down of predicates containing KeepInPlace expressions

--- datafusion/optimizer/src/push_down_filter.rs
+++ datafusion/optimizer/src/push_down_filter.rs
@@ -1312,6 +1312,7 @@ fn rewrite_projection(
         .partition(|(_, value)| {
             value.is_volatile()
                 || value.placement() == ExpressionPlacement::MoveTowardsLeafNodes
+                || value.placement() == ExpressionPlacement::KeepInPlace
         });
 
     let mut push_predicates = vec![];
@@ -1454,7 +1455,7 @@ mod tests {
     use crate::assert_optimized_plan_eq_snapshot;
     use crate::optimizer::Optimizer;
     use crate::simplify_expressions::SimplifyExpressions;
-    use crate::test::udfs::leaf_udf_expr;
+    use crate::test::udfs::{PlacementTestUDF, get_field_like, leaf_udf_expr};
     use crate::test::*;
     use datafusion_expr::test::function_stub::sum;
     use insta::assert_snapshot;
@@ -4391,6 +4392,37 @@ mod tests {
           Projection: leaf_udf(test.a) AS val, test.b, test.c
             TableScan: test, full_filters=[test.b > Int64(5)]
         "
+        )
+    }
+
+    #[test]
+    fn filter_not_pushed_through_nested_computed_projection() -> Result<()> {
+        let udf = ScalarUDF::new_from_impl(
+            PlacementTestUDF::new().with_placement(ExpressionPlacement::KeepInPlace),
+        );
+        let inner = LogicalPlanBuilder::from(test_table_scan()?)
+            .project(vec![udf.call(vec![col("a")]).alias("c1"), col("b")])?
+            .build()?;
+        let outer = LogicalPlanBuilder::from(inner)
+            .project(vec![
+                get_field_like(col("c1"), "x").alias("c2"),
+                col("c1"),
+                col("b"),
+            ])?
+            .build()?;
+        let plan = LogicalPlanBuilder::from(outer)
+            .filter(col("c1").is_not_null().and(col("c2").is_null()))?
+            .build()?;
+
+        assert_optimized_plan_equal!(
+            plan,
+            @r#"
+        Filter: c2 IS NULL
+          Projection: get_field_like(c1, Utf8("x")) AS c2, c1, test.b
+            Filter: c1 IS NOT NULL
+              Projection: keep_in_place_udf(test.a) AS c1, test.b
+                TableScan: test
+        "#
         )
     }
 
