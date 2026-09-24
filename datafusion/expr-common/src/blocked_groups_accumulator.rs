1b2a8f15433ce491ce9a3f8db0df69365e420e6b
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2026-09-24 18:29:33 +0300

    remove comments

--- datafusion/expr-common/src/blocked_groups_accumulator.rs
+++ datafusion/expr-common/src/blocked_groups_accumulator.rs
@@ -324,58 +324,6 @@ impl BlockedEmitTo {
     }
 }
 
-/// `GroupsAccumulator` implements a single aggregate (e.g. AVG) and
-/// stores the state for *all* groups internally.
-///
-/// Logically, a [`GroupsAccumulator`] stores a mapping from each group index to
-/// the state of the aggregate for that group. For example an implementation for
-/// `min` might look like
-///
-/// ```text
-///    ┌─────┐
-///    │  0  │───────────▶   100
-///    ├─────┤
-///    │  1  │───────────▶   200
-///    └─────┘
-///      ...                 ...
-///    ┌─────┐
-///    │ N-2 │───────────▶    50
-///    ├─────┤
-///    │ N-1 │───────────▶   200
-///    └─────┘
-///
-///
-///  Logical group      Current Min
-///     number          value for that
-///                     group
-/// ```
-///
-/// # Notes on Implementing `GroupsAccumulator`
-///
-/// All aggregates must first implement the simpler [`Accumulator`] trait, which
-/// handles state for a single group. Implementing `GroupsAccumulator` is
-/// optional and is harder to implement than `Accumulator`, but can be much
-/// faster for queries with many group values.  See the [Aggregating Millions of
-/// Groups Fast blog] for more background.
-/// For more background, please also see the [Aggregating Millions of Groups Fast in Apache Arrow DataFusion 28.0.0 blog]
-///
-/// [Aggregating Millions of Groups Fast in Apache Arrow DataFusion 28.0.0 blog]: https://datafusion.apache.org/blog/2023/08/05/datafusion_fast_grouping
-///
-/// [`NullState`] can help keep the state for groups that have not seen any
-/// values and produce the correct output for those groups.
-///
-/// [`NullState`]: https://docs.rs/datafusion/latest/datafusion/physical_expr/struct.NullState.html
-///
-/// # Details
-/// Each group is assigned a `group_index` by the hash table and each
-/// accumulator manages the specific state, one per `group_index`.
-///
-/// `group_index`es are contiguous (there aren't gaps), and thus it is
-/// expected that each `GroupsAccumulator` will use something like `Vec<..>`
-/// to store the group states.
-///
-/// [`Accumulator`]: crate::accumulator::Accumulator
-/// [Aggregating Millions of Groups Fast blog]: https://arrow.apache.org/blog/2023/08/05/datafusion_fast_grouping/
 pub trait BlockedGroupsAccumulator: Send + std::any::Any {
     fn batch_size(&self) -> usize;
 
